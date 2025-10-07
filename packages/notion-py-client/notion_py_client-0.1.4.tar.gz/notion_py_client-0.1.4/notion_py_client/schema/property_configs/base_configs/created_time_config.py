d9846016cbe99e08a86776002c10c3dba5c9be6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:25 +0000

    Add package notion-py-client/0.1.4/notion_py_client-0.1.4.tar.gz

---
+++ packages/notion-py-client/notion_py_client-0.1.4.tar.gz/notion_py_client-0.1.4/notion_py_client/schema/property_configs/base_configs/created_time_config.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+from pydantic import Field
+
+from ._base_config import BasePropertyConfig
+from ....models.primitives import EmptyObject
+from ....properties.base_properties._base_property import NotionPropertyType
+
+
+class CreatedTimePropertyConfig(BasePropertyConfig):
+    """Notionのcreated_timeプロパティ設定"""
+
+    type: Literal[NotionPropertyType.CREATED_TIME] = Field(
+        NotionPropertyType.CREATED_TIME, description="プロパティタイプ"
+    )
+    created_time: EmptyObject = Field(
+        default_factory=EmptyObject, description="created_time設定"
+    )
