38cf937a11127c370e20d81dc88f661529f00cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:04:43 +0000

    Add package accelbyte-py-sdk-service-platform/0.23.0/accelbyte_py_sdk_service_platform-0.23.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-platform/accelbyte_py_sdk_service_platform-0.23.0.tar.gz/accelbyte_py_sdk_service_platform-0.23.0/accelbyte_py_sdk/api/platform/operations/section/create_section.py
@@ -0,0 +1,439 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Platform Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ErrorEntity
+from ...models import FullSectionInfo
+from ...models import SectionCreate
+from ...models import ValidationErrorEntity
+
+
+class CreateSection(Operation):
+    """Create a section (createSection)
+
+    This API is used to create a section.
+
+    Other detail info:
+
+      * Returns : created a section
+
+
+
+    ## Restrictions for section extension and localization extension
+
+
+    1. Cannot use "." as the key name
+    -
+
+
+        { "data.2": "value" }
+
+
+    2. Cannot use "$" as the prefix in key names
+    -
+
+
+        { "$data": "value" }
+
+    Properties:
+        url: /platform/admin/namespaces/{namespace}/sections
+
+        method: POST
+
+        tags: ["Section"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        body: (body) REQUIRED SectionCreate in body
+
+        namespace: (namespace) REQUIRED str in path
+
+        store_id: (storeId) REQUIRED str in query
+
+    Responses:
+        201: Created - FullSectionInfo (successful operation)
+
+        400: Bad Request - ErrorEntity (20027: Invalid time range | 30021: Default language [{language}] required | 30023: Catalog plugin grpc server address required)
+
+        404: Not Found - ErrorEntity (30141: Store [{storeId}] does not exist in namespace [{namespace}] | 30641: View [{viewId}] does not exist in namespace [{namespace}])
+
+        409: Conflict - ErrorEntity (30173: Published store can't modify content)
+
+        422: Unprocessable Entity - ValidationErrorEntity (20002: validation error)
+    """
+
+    # region fields
+
+    _url: str = "/platform/admin/namespaces/{namespace}/sections"
+    _path: str = "/platform/admin/namespaces/{namespace}/sections"
+    _base_path: str = ""
+    _method: str = "POST"
+    _consumes: List[str] = ["application/json"]
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "platform"
+
+    body: SectionCreate  # REQUIRED in [body]
+    namespace: str  # REQUIRED in [path]
+    store_id: str  # REQUIRED in [query]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "body": self.get_body_params(),
+            "path": self.get_path_params(),
+            "query": self.get_query_params(),
+        }
+
+    def get_body_params(self) -> Any:
+        if not hasattr(self, "body") or self.body is None:
+            return None
+        return self.body.to_dict()
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    def get_query_params(self) -> dict:
+        result = {}
+        if hasattr(self, "store_id"):
+            result["storeId"] = self.store_id
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_body(self, value: SectionCreate) -> CreateSection:
+        self.body = value
+        return self
+
+    def with_namespace(self, value: str) -> CreateSection:
+        self.namespace = value
+        return self
+
+    def with_store_id(self, value: str) -> CreateSection:
+        self.store_id = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "body") and self.body:
+            result["body"] = self.body.to_dict(include_empty=include_empty)
+        elif include_empty:
+            result["body"] = SectionCreate()
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        if hasattr(self, "store_id") and self.store_id:
+            result["storeId"] = str(self.store_id)
+        elif include_empty:
+            result["storeId"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_201: Optional[FullSectionInfo] = None
+        error_400: Optional[ErrorEntity] = None
+        error_404: Optional[ErrorEntity] = None
+        error_409: Optional[ErrorEntity] = None
+        error_422: Optional[ValidationErrorEntity] = None
+
+        def ok(self) -> CreateSection.Response:
+            if self.error_400 is not None:
+                err = self.error_400.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_404 is not None:
+                err = self.error_404.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_409 is not None:
+                err = self.error_409.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_422 is not None:
+                err = self.error_422.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_201 is not None:
+                yield self.data_201
+                yield None
+            elif self.error_400 is not None:
+                yield None
+                yield self.error_400
+            elif self.error_404 is not None:
+                yield None
+                yield self.error_404
+            elif self.error_409 is not None:
+                yield None
+                yield self.error_409
+            elif self.error_422 is not None:
+                yield None
+                yield self.error_422
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        201: Created - FullSectionInfo (successful operation)
+
+        400: Bad Request - ErrorEntity (20027: Invalid time range | 30021: Default language [{language}] required | 30023: Catalog plugin grpc server address required)
+
+        404: Not Found - ErrorEntity (30141: Store [{storeId}] does not exist in namespace [{namespace}] | 30641: View [{viewId}] does not exist in namespace [{namespace}])
+
+        409: Conflict - ErrorEntity (30173: Published store can't modify content)
+
+        422: Unprocessable Entity - ValidationErrorEntity (20002: validation error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = CreateSection.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 201:
+                result.data_201 = FullSectionInfo.create_from_dict(content)
+            elif code == 400:
+                result.error_400 = ErrorEntity.create_from_dict(content)
+                result.error = result.error_400.translate_to_api_error()
+            elif code == 404:
+                result.error_404 = ErrorEntity.create_from_dict(content)
+                result.error = result.error_404.translate_to_api_error()
+            elif code == 409:
+                result.error_409 = ErrorEntity.create_from_dict(content)
+                result.error = result.error_409.translate_to_api_error()
+            elif code == 422:
+                result.error_422 = ValidationErrorEntity.create_from_dict(content)
+                result.error = result.error_422.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[
+        Union[None, FullSectionInfo],
+        Union[None, ErrorEntity, HttpResponse, ValidationErrorEntity],
+    ]:
+        """Parse the given response.
+
+        201: Created - FullSectionInfo (successful operation)
+
+        400: Bad Request - ErrorEntity (20027: Invalid time range | 30021: Default language [{language}] required | 30023: Catalog plugin grpc server address required)
+
+        404: Not Found - ErrorEntity (30141: Store [{storeId}] does not exist in namespace [{namespace}] | 30641: View [{viewId}] does not exist in namespace [{namespace}])
+
+        409: Conflict - ErrorEntity (30173: Published store can't modify content)
+
+        422: Unprocessable Entity - ValidationErrorEntity (20002: validation error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 201:
+            return FullSectionInfo.create_from_dict(content), None
+        if code == 400:
+            return None, ErrorEntity.create_from_dict(content)
+        if code == 404:
+            return None, ErrorEntity.create_from_dict(content)
+        if code == 409:
+            return None, ErrorEntity.create_from_dict(content)
+        if code == 422:
+            return None, ValidationErrorEntity.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, body: SectionCreate, namespace: str, store_id: str, **kwargs
+    ) -> CreateSection:
+        instance = cls()
+        instance.body = body
+        instance.namespace = namespace
+        instance.store_id = store_id
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> CreateSection:
+        instance = cls()
+        if "body" in dict_ and dict_["body"] is not None:
+            instance.body = SectionCreate.create_from_dict(
+                dict_["body"], include_empty=include_empty
+            )
+        elif include_empty:
+            instance.body = SectionCreate()
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        if "storeId" in dict_ and dict_["storeId"] is not None:
+            instance.store_id = str(dict_["storeId"])
+        elif include_empty:
+            instance.store_id = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "body": "body",
+            "namespace": "namespace",
+            "storeId": "store_id",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "body": True,
+            "namespace": True,
+            "storeId": True,
+        }
+
+    # endregion static methods
