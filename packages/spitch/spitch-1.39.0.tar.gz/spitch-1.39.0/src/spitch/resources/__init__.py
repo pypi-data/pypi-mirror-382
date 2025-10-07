d47a9da38962b7c140221e6aca4c97441c7a7d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:24 +0000

    Add package spitch/1.39.0/spitch-1.39.0.tar.gz

---
+++ packages/spitch/spitch-1.39.0.tar.gz/spitch-1.39.0/src/spitch/resources/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .text import (
+    TextResource,
+    AsyncTextResource,
+    TextResourceWithRawResponse,
+    AsyncTextResourceWithRawResponse,
+    TextResourceWithStreamingResponse,
+    AsyncTextResourceWithStreamingResponse,
+)
+from .files import (
+    FilesResource,
+    AsyncFilesResource,
+    FilesResourceWithRawResponse,
+    AsyncFilesResourceWithRawResponse,
+    FilesResourceWithStreamingResponse,
+    AsyncFilesResourceWithStreamingResponse,
+)
+from .speech import (
+    SpeechResource,
+    AsyncSpeechResource,
+    SpeechResourceWithRawResponse,
+    AsyncSpeechResourceWithRawResponse,
+    SpeechResourceWithStreamingResponse,
+    AsyncSpeechResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "SpeechResource",
+    "AsyncSpeechResource",
+    "SpeechResourceWithRawResponse",
+    "AsyncSpeechResourceWithRawResponse",
+    "SpeechResourceWithStreamingResponse",
+    "AsyncSpeechResourceWithStreamingResponse",
+    "TextResource",
+    "AsyncTextResource",
+    "TextResourceWithRawResponse",
+    "AsyncTextResourceWithRawResponse",
+    "TextResourceWithStreamingResponse",
+    "AsyncTextResourceWithStreamingResponse",
+    "FilesResource",
+    "AsyncFilesResource",
+    "FilesResourceWithRawResponse",
+    "AsyncFilesResourceWithRawResponse",
+    "FilesResourceWithStreamingResponse",
+    "AsyncFilesResourceWithStreamingResponse",
+]
