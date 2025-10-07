701c6178cdd400390baa71a580b238bdc97b865e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:05:39 +0000

    Add package agentUniverse/0.0.19.dev0/agentuniverse-0.0.19.dev0-py3-none-any.whl

---
+++ packages/agentUniverse/agentuniverse-0.0.19.dev0-py3-none-any.whl/agentuniverse/agent/action/knowledge/reader/file/__init__.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Time    : 2024/4/2 17:13
+# @Author  : wangchongshi
+# @Email   : wangchongshi.wcs@antgroup.com
+# @FileName: __init__.py
+
+from .xlsx_reader import XlsxReader
