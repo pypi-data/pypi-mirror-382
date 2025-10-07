616388155e5078637b672f643ae89c042748bb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:14 +0000

    Add package dbnd-tensorflow/1.0.30.1/dbnd_tensorflow-1.0.30.1-py2.py3-none-any.whl

---
+++ packages/dbnd-tensorflow/dbnd_tensorflow-1.0.30.1-py2.py3-none-any.whl/dbnd_tensorflow/marshalling/tensorflow_values.py
@@ -0,0 +1,18 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+from tensorflow.keras import models
+from tensorflow.keras.callbacks import History
+
+from targets.values.builtins_values import DataValueType
+
+
+class TensorflowModelValueType(DataValueType):
+    type = models.Model
+    type_str = "Model"
+    config_name = "tensorflow_model"
+
+
+class TensorflowHistoryValueType(DataValueType):
+    type = History
+    type_str = "History"
+    config_name = "tensorflow_history"
