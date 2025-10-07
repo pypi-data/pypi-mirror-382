9c1ae915bf3ff5c731a800bcf34da75f0ac92828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:09 +0000

    Add package django-whatsapp-api-wrapper/0.5.2/django_whatsapp_api_wrapper-0.5.2-py3-none-any.whl

---
+++ packages/django-whatsapp-api-wrapper/django_whatsapp_api_wrapper-0.5.2-py3-none-any.whl/django_whatsapp_api_wrapper/embedded_signup/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+
+class WhatsAppCloudApiBusinessSerializer(serializers.Serializer):
+    phone_number_id = serializers.CharField(required=False, allow_blank=True)
+    waba_id = serializers.CharField(required=False, allow_blank=True)
+    business_id = serializers.CharField(required=False, allow_blank=True)
+    current_step = serializers.CharField(required=False, allow_blank=True)
+    error_message = serializers.CharField(required=False, allow_blank=True)
+    error_id = serializers.CharField(required=False, allow_blank=True)
+    session_id = serializers.CharField(required=False, allow_blank=True)
+    timestamp = serializers.CharField(required=False, allow_blank=True)
+    phone_number = serializers.CharField(required=False, allow_blank=True)
+
+class WhatsAppEmbeddedSignUpSerializer(serializers.Serializer):
+    code = serializers.CharField(required=False, allow_blank=True)
+    business_token = serializers.CharField(required=False, allow_blank=True)
+    desired_pin = serializers.CharField(required=False, allow_blank=True, max_length=6)
+
+class EmbeddedSignupEventSerializer(serializers.Serializer):
+    data = WhatsAppCloudApiBusinessSerializer()
+    type = serializers.ChoiceField(choices=["WA_EMBEDDED_SIGNUP"])  # fixed per docs
+    event = serializers.CharField()  # FINISH, CANCEL, etc
