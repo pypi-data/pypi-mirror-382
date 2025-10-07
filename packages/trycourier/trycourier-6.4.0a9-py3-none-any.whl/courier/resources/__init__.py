0f655db4a60507f5aacdcd3eabc936c16a466adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 05:04:11 +0000

    Add package trycourier/6.4.0a9/trycourier-6.4.0a9-py3-none-any.whl

---
+++ packages/trycourier/trycourier-6.4.0a9-py3-none-any.whl/courier/resources/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .send import (
+    SendResource,
+    AsyncSendResource,
+    SendResourceWithRawResponse,
+    AsyncSendResourceWithRawResponse,
+    SendResourceWithStreamingResponse,
+    AsyncSendResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "SendResource",
+    "AsyncSendResource",
+    "SendResourceWithRawResponse",
+    "AsyncSendResourceWithRawResponse",
+    "SendResourceWithStreamingResponse",
+    "AsyncSendResourceWithStreamingResponse",
+]
