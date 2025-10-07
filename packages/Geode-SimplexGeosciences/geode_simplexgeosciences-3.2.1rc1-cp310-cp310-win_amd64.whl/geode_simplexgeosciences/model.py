3109c3628963ddcfa64e8ef4c81c28eb0e96fe2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:16 +0000

    Add package Geode-SimplexGeosciences/3.2.1rc1/geode_simplexgeosciences-3.2.1rc1-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-SimplexGeosciences/geode_simplexgeosciences-3.2.1rc1-cp310-cp310-win_amd64.whl/geode_simplexgeosciences/model.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_geosciences
+import geode_simplex
+
+from .bin.geode_simplexgeosciences_py_model import *
+SimplexGeosciencesModelLibrary.initialize()
