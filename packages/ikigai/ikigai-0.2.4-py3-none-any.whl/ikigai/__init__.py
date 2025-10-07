dbb228092f65c397c085e0dc859419262e314528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:45 +0000

    Add package ikigai/0.2.4/ikigai-0.2.4-py3-none-any.whl

---
+++ packages/ikigai/ikigai-0.2.4-py3-none-any.whl/ikigai/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present Harsh Parekh <harsh@ikigailabs.io>
+#
+# SPDX-License-Identifier: MIT
+
+from ikigai.ikigai import Ikigai
+
+__all__ = ["Ikigai"]
---
+++ packages/ikigai/ikigai-0.2.4-py3-none-any.whl/ikigai/client/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present Harsh Parekh <harsh@ikigailabs.io>
+#
+# SPDX-License-Identifier: MIT
+
+from ikigai.client.client import Client
+
+__all__ = ["Client"]
