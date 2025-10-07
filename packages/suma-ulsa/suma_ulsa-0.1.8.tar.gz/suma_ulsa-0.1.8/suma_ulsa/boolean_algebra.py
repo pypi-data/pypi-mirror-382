66ab5e0e671f731d71f8630fa3ec551d97d1d40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:04:29 +0000

    Add package suma-ulsa/0.1.8/suma_ulsa-0.1.8.tar.gz

---
+++ packages/suma-ulsa/suma_ulsa-0.1.8.tar.gz/suma_ulsa-0.1.8/suma_ulsa/boolean_algebra.py
@@ -0,0 +1,16 @@
+"""
+Number conversion utilities.
+"""
+try:
+    # Import from Rust extension
+    from ..suma_ulsa.boolean_algebra import *
+except ImportError:
+    # Fallback for type checkers
+    pass
+
+__all__ = [
+    "BooleanExpr",
+    "TruthTable", 
+    "parse_expression_debug",
+    "truth_table_from_expr",
+]
