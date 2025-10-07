6261f28f60f1c49b7f2ce6e1c194109db184694f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:05:11 +0000

    Add package alkuhlani-student/0.1.1/alkuhlani_student-0.1.1-py3-none-any.whl

---
+++ packages/alkuhlani-student/alkuhlani_student-0.1.1-py3-none-any.whl/alkuhlani_student/__init__.py
@@ -0,0 +1,12 @@
+"""
+Alkuhlani Student - Word Frequency Analyzer
+محلل تكرار الكلمات
+A simple Python package to analyze word frequency in text.
+"""
+
+__version__ = "0.1.1"
+__author__ = "Alkuhlani Student (CYS 2)"
+
+from .analyzer import word_frequency, analyze_text
+
+__all__ = ["word_frequency", "analyze_text"]
