a1a153469b455dff084f005fd9390a60946319d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:05:10 +0000

    Add package qflux/0.0.3/qflux-0.0.3.tar.gz

---
+++ packages/qflux/qflux-0.0.3.tar.gz/qflux-0.0.3/tests/test_import.py
@@ -0,0 +1,9 @@
+import qflux
+
+from qflux.closed_systems import DynamicsCS
+print("Checking Closed Systems...")
+print(dir(DynamicsCS))
+
+from qflux.variational_methods import qmad
+print("Checking Variational Methods...")
+print(dir(qmad))
