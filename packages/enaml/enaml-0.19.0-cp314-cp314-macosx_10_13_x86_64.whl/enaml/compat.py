eee94f8d0c6f343c9b9909b59d1ec27d92cbcda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:21 +0000

    Add package enaml/0.19.0/enaml-0.19.0-cp314-cp314-macosx_10_13_x86_64.whl

---
+++ packages/enaml/enaml-0.19.0-cp314-cp314-macosx_10_13_x86_64.whl/enaml/compat.py
@@ -0,0 +1,54 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013-2025, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#------------------------------------------------------------------------------
+import sys
+import re
+import codecs
+import tokenize
+
+PY310 = sys.version_info >= (3, 10)
+
+PY311 = sys.version_info >= (3, 11)
+
+PY312 = sys.version_info >= (3, 12)
+
+PY313 = sys.version_info >= (3, 13)
+
+PY314 = sys.version_info >= (3, 14)
+
+# Functions used to update the co_filename slot of a code object
+# Available in Python 3.5+ (tested up to 3.8)
+from _imp import _fix_co_filename
+
+
+def update_code_co_filename(code, src_path):
+    """Update the co_filename attribute of the code.
+
+    Parameters
+    ----------
+    code : types.CodeType
+        Code object from which the co_filename should be updated.
+
+    src_path : string
+        Path to the source file for the code object
+
+    Returns
+    -------
+    updated_code : types.CodeType
+        Code object whose co_filename field is set to src_path.
+
+    """
+    _fix_co_filename(code, src_path)
+    return code
+
+
+# Source file reading and encoding detection
+def read_source(filename):
+    with tokenize.open(filename) as f:
+        return f.read()
+
+detect_encoding = tokenize.detect_encoding
