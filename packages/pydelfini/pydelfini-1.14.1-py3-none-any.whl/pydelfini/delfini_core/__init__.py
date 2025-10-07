9da5a393faa61f303ee8e4a8bb4c40d939b2f472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 05:04:10 +0000

    Add package pydelfini/1.14.1/pydelfini-1.14.1-py3-none-any.whl

---
+++ packages/pydelfini/pydelfini-1.14.1-py3-none-any.whl/pydelfini/delfini_core/__init__.py
@@ -0,0 +1,12 @@
+""" A client library for accessing Delfini """
+from .client import AuthenticatedClient
+from .client import Client
+from .login import Login
+from .paginator import Paginator
+
+__all__ = (
+    "AuthenticatedClient",
+    "Client",
+    "Login",
+    "Paginator",
+)
