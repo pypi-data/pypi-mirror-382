7f70c7ccc10d3dac5fbbbcbe2c425eece12fbc72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:04:50 +0000

    Add package accelbyte-py-sdk-feat-auth/0.3.0/accelbyte_py_sdk_feat_auth-0.3.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-feat-auth/accelbyte_py_sdk_feat_auth-0.3.0-py3-none-any.whl/accelbyte_py_sdk/services/auth/__init__.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+
+from ._login import login
+from ._login import login_client
+from ._login import login_platform
+from ._login import login_user
+from ._login import logout
+from ._login import refresh_login
+
+from ._login import login_async
+from ._login import login_client_async
+from ._login import login_platform_async
+from ._login import login_user_async
+from ._login import logout_async
+from ._login import refresh_login_async
+
+from ._login import LoginTimerBase
+from ._login import LoginClientTimer
+from ._login import LoginPlatformTimer
+from ._login import LoginUserTimer
+from ._login import RefreshLoginTimer
+
+from ._login import DEFAULT_AUTO_REFRESH
+from ._login import DEFAULT_REFRESH_RATE
+
+from ._login import set_on_demand_token_refresher
+from ._login import OnDemandTokenRefresher
+
+from ._jwt import parse_access_token
+
+from ._utils import convert_bearer_auth_token_to_oauth_token_dict
