6e133ecdc1acc585e8921f51f7770ce486c060be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:15 +0000

    Add package dbnd-luigi/1.0.30.1/dbnd_luigi-1.0.30.1.tar.gz

---
+++ packages/dbnd-luigi/dbnd_luigi-1.0.30.1.tar.gz/dbnd_luigi-1.0.30.1/setup.py
@@ -0,0 +1,21 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+from os import path
+
+import setuptools
+
+from setuptools.config import read_configuration
+
+
+BASE_PATH = path.dirname(__file__)
+CFG_PATH = path.join(BASE_PATH, "setup.cfg")
+
+config = read_configuration(CFG_PATH)
+version = config["metadata"]["version"]
+
+setuptools.setup(
+    name="dbnd-luigi",
+    package_dir={"": "src"},
+    install_requires=["dbnd==" + version, "luigi<=3.0.2"],
+    extras_require={"test": ["docutils"]},  # Indirect luigi dependency
+)
---
+++ packages/dbnd-luigi/dbnd_luigi-1.0.30.1.tar.gz/dbnd_luigi-1.0.30.1/src/dbnd_luigi/__main__.py
@@ -0,0 +1,7 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+from dbnd_luigi.luigi_tracking import dbnd_luigi_run
+
+
+if __name__ == "__main__":
+    dbnd_luigi_run()
