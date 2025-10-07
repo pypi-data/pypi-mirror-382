194b208898fa3d84e696c45bdc7d34e47233175f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:47 +0000

    Add package pyveoliaidf/0.4.5/pyveoliaidf-0.4.5-py311-none-any.whl

---
+++ packages/pyveoliaidf/pyveoliaidf-0.4.5-py311-none-any.whl/pyveoliaidf/__init__.py
@@ -0,0 +1,4 @@
+from pyveoliaidf.enum import PropertyNameEnum  # noqa: F401
+from pyveoliaidf.client import Client  # noqa: F401
+from pyveoliaidf.client import LoginError  # noqa: F401
+from pyveoliaidf.version import __version__  # noqa: F401
