c7c7836fd8406b6509eaf5ffda4e59fa94855cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:04:50 +0000

    Add package soe-vinorm/0.3.1/soe_vinorm-0.3.1-py3-none-any.whl

---
+++ packages/soe-vinorm/soe_vinorm-0.3.1-py3-none-any.whl/soe_vinorm/__init__.py
@@ -0,0 +1,34 @@
+"""
+Soe Vinorm: Vietnamese Text Normalization Toolkit
+
+A Python library for converting Vietnamese text to its spoken form.
+"""
+
+__version__ = "0.3.1"
+__author__ = "Vinh Dang"
+__email__ = "quangvinh0842@gmail.com"
+
+from .normalizer import (
+    SoeNormalizer,
+    batch_normalize_texts,
+    normalize_text,
+)
+from .nsw_detector import CRFNSWDetector
+from .nsw_expander import RuleBasedNSWExpander
+from .text_processor import TextPostprocessor, TextPreprocessor
+from .utils import (
+    load_abbreviation_dict,
+    load_vietnamese_syllables,
+)
+
+__all__ = [
+    "SoeNormalizer",
+    "normalize_text",
+    "batch_normalize_texts",
+    "CRFNSWDetector",
+    "RuleBasedNSWExpander",
+    "TextPreprocessor",
+    "TextPostprocessor",
+    "load_vietnamese_syllables",
+    "load_abbreviation_dict",
+]
