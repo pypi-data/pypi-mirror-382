9b19be93d8982afa08325618ca46136e4b1ce1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:17 +0000

    Add package kuzu-memory/1.2.0/kuzu_memory-1.2.0.tar.gz

---
+++ packages/kuzu-memory/kuzu_memory-1.2.0.tar.gz/kuzu_memory-1.2.0/src/kuzu_memory/__version__.py
@@ -0,0 +1,7 @@
+"""Version information for KuzuMemory."""
+
+__version__ = "1.2.0"
+__version_info__ = tuple(int(i) for i in __version__.split("."))
+
+# Database schema version for migration support
+DB_SCHEMA_VERSION = "1.0"
