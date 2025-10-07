1813f5bafe8e4cbd8c4381e10b5ce6c8e58e1ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:25 +0000

    Add package gllm-inference-binary/0.5.35/gllm_inference_binary-0.5.35-cp312-cp312-win_amd64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.35-cp312-cp312-win_amd64.whl/gllm_inference/lm_invoker/schema/datasaur.pyi
@@ -0,0 +1,12 @@
+class Key:
+    """Defines valid keys in Datasaur."""
+    CONTEXTS: str
+    MAX_RETRIES: str
+    NAME: str
+    TIMEOUT: str
+    TYPE: str
+    URL: str
+
+class InputType:
+    """Defines valid input types in Datasaur."""
+    URL: str
