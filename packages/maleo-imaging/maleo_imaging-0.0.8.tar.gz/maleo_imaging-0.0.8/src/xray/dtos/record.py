c152fda497fd2223990e9dd950e805770a49a6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:04:51 +0000

    Add package maleo-imaging/0.0.8/maleo_imaging-0.0.8.tar.gz

---
+++ packages/maleo-imaging/maleo_imaging-0.0.8.tar.gz/maleo_imaging-0.0.8/src/xray/dtos/record.py
@@ -0,0 +1,186 @@
+from pydantic import BaseModel, Field
+from typing import Annotated, Generic, Literal, TypeVar, overload
+from uuid import UUID
+from maleo.enums.identity import OptionalGender
+from maleo.enums.status import (
+    DataStatus as DataStatusEnum,
+    ListOfDataStatuses,
+    FULL_DATA_STATUSES,
+)
+from maleo.schemas.mixins.identity import (
+    DataIdentifier,
+    IdentifierTypeValue,
+    Ids,
+    UUIDs,
+    Name,
+    OrganizationId,
+    OrganizationIds,
+    UserId,
+    UserIds,
+)
+from maleo.schemas.mixins.status import DataStatus
+from maleo.schemas.mixins.timestamp import DataTimestamp
+from maleo.schemas.parameter import (
+    ReadSingleParameter as BaseReadSingleParameter,
+    ReadPaginatedMultipleParameter,
+    StatusUpdateParameter as BaseStatusUpdateParameter,
+    DeleteSingleParameter as BaseDeleteSingleParameter,
+)
+from maleo.types.datetime import OptionalDate
+from maleo.types.integer import OptionalListOfIntegers, OptionalInteger
+from maleo.types.string import OptionalString
+from maleo.types.uuid import OptionalListOfUUIDs
+from ..enums.record import IdentifierType
+from ..types.record import IdentifierValueType
+
+
+class CreateParameter(BaseModel):
+    organization_id: Annotated[
+        OptionalInteger, Field(None, description="Organization ID", ge=1)
+    ] = None
+    user_id: Annotated[int, Field(..., description="User ID", ge=1)]
+    name: Annotated[OptionalString, Field(None, description="Name", max_length=200)] = (
+        None
+    )
+    gender: Annotated[OptionalGender, Field(None, description="Gender")] = None
+    date_of_birth: Annotated[OptionalDate, Field(None, description="Date of Birth")] = (
+        None
+    )
+    description: Annotated[OptionalString, Field(None, description="Description")] = (
+        None
+    )
+    impression: Annotated[OptionalString, Field(None, description="Impression")] = None
+    diagnosis: Annotated[str, Field(..., description="Diagnosis")]
+    content_type: Annotated[str, Field(..., description="Content type")]
+    image: Annotated[bytes, Field(..., description="Image data")]
+    filename: Annotated[str, Field(..., description="File name")]
+
+
+class ReadMultipleParameter(
+    ReadPaginatedMultipleParameter,
+    UserIds[OptionalListOfIntegers],
+    OrganizationIds[OptionalListOfIntegers],
+    UUIDs[OptionalListOfUUIDs],
+    Ids[OptionalListOfIntegers],
+):
+    pass
+
+
+class ReadSingleParameter(BaseReadSingleParameter[IdentifierType, IdentifierValueType]):
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier: Literal[IdentifierType.ID],
+        value: int,
+        statuses: ListOfDataStatuses = list(FULL_DATA_STATUSES),
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier: Literal[IdentifierType.UUID],
+        value: UUID,
+        statuses: ListOfDataStatuses = list(FULL_DATA_STATUSES),
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter": ...
+    @classmethod
+    def new(
+        cls,
+        identifier: IdentifierType,
+        value: IdentifierValueType,
+        statuses: ListOfDataStatuses = list(FULL_DATA_STATUSES),
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter":
+        return cls(
+            identifier=identifier,
+            value=value,
+            statuses=statuses,
+            use_cache=use_cache,
+        )
+
+
+class BaseUpdateData(
+    Name[OptionalString],
+):
+    name: Annotated[OptionalString, Field(None, description="Name", max_length=200)] = (
+        None
+    )
+    gender: Annotated[OptionalGender, Field(None, description="Gender")] = None
+    date_of_birth: Annotated[OptionalDate, Field(None, description="Date of Birth")] = (
+        None
+    )
+    description: Annotated[OptionalString, Field(None, description="Description")] = (
+        None
+    )
+    impression: Annotated[OptionalString, Field(None, description="Impression")] = None
+
+
+class FullUpdateData(BaseUpdateData):
+    diagnosis: Annotated[str, Field(..., description="Diagnosis")]
+
+
+class PartialUpdateData(BaseUpdateData):
+    diagnosis: Annotated[OptionalString, Field(None, description="Diagnosis")] = None
+
+
+UpdateDataT = TypeVar("UpdateDataT", FullUpdateData, PartialUpdateData)
+
+
+class UpdateDataMixin(BaseModel, Generic[UpdateDataT]):
+    data: UpdateDataT = Field(..., description="Update data")
+
+
+class UpdateParameter(
+    UpdateDataMixin[UpdateDataT],
+    IdentifierTypeValue[
+        IdentifierType,
+        IdentifierValueType,
+    ],
+    Generic[UpdateDataT],
+):
+    pass
+
+
+class StatusUpdateParameter(
+    BaseStatusUpdateParameter,
+):
+    pass
+
+
+class DeleteSingleParameter(
+    BaseDeleteSingleParameter[IdentifierType, IdentifierValueType]
+):
+    pass
+
+
+class BaseRecordData(
+    Name[OptionalString],
+    UserId[int],
+    OrganizationId[OptionalInteger],
+    DataStatus[DataStatusEnum],
+    DataTimestamp,
+    DataIdentifier,
+):
+    organization_id: Annotated[
+        OptionalInteger, Field(None, description="Organization ID", ge=1)
+    ] = None
+    user_id: Annotated[int, Field(..., description="User ID", ge=1)]
+    name: Annotated[OptionalString, Field(None, description="Name", max_length=200)] = (
+        None
+    )
+    gender: Annotated[OptionalGender, Field(None, description="Gender")] = None
+    date_of_birth: Annotated[OptionalDate, Field(None, description="Date of Birth")] = (
+        None
+    )
+    description: Annotated[OptionalString, Field(None, description="Description")] = (
+        None
+    )
+    impression: Annotated[OptionalString, Field(None, description="Impression")] = None
+    diagnosis: Annotated[str, Field(..., description="Diagnosis")]
+    filename: Annotated[str, Field(..., description="File's name")]
+
+
+class RecordData(BaseRecordData):
+    url: Annotated[str, Field(..., description="File's URL")]
