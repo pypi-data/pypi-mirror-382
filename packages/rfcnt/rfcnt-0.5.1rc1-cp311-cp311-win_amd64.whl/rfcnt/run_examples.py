e87e4097a474a2b16d3932877d403b916f2e58db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:19 +0000

    Add package rfcnt/0.5.1rc1/rfcnt-0.5.1rc1-cp311-cp311-win_amd64.whl

---
+++ packages/rfcnt/rfcnt-0.5.1rc1-cp311-cp311-win_amd64.whl/rfcnt/run_examples.py
@@ -0,0 +1,10 @@
+from .tests import examples
+
+
+def main():
+    print("Running example 1...")
+    examples.example_1()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/rfcnt/rfcnt-0.5.1rc1-cp311-cp311-win_amd64.whl/rfcnt/tests/__init__.py
@@ -0,0 +1,2 @@
+from . import examples  # noqa F401
+from . import test_rfcnt  # noqa F401
