0d6f4de10aa0a6727671cb06d81728526214fc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:05:33 +0000

    Add package odoo-addon-datev-export/15.0.2.0.0/odoo_addon_datev_export-15.0.2.0.0-py3-none-any.whl

---
+++ packages/odoo-addon-datev-export/odoo_addon_datev_export-15.0.2.0.0-py3-none-any.whl/odoo/addons/datev_export/models/res_company.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2023 initOS GmbH
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    datev_consultant_number = fields.Char(
+        "Consultant Number",
+        size=8,
+        help="Number from 1000 to 99999999",
+    )
+
+    datev_client_number = fields.Char(
+        "Client Number",
+        size=5,
+        help="Number from 0 to 99999",
+    )
+
+    datev_account_code_length = fields.Integer(
+        "DATEV account code length",
+        default=5,
+    )
+
+    datev_partner_numbering = fields.Selection(
+        string="DATEV Partner numbering",
+        selection="_selection_datev_partner_numbering",
+        default="none",
+    )
+
+    datev_customer_sequence_id = fields.Many2one(
+        "ir.sequence", "DATEV sequence for customers"
+    )
+
+    datev_supplier_sequence_id = fields.Many2one(
+        "ir.sequence", "DATEV sequence for suppliers"
+    )
+
+    def _selection_datev_partner_numbering(self):
+        reports_installed = (
+            "l10n_de_datev_reports" in self.env["ir.module.module"]._installed()
+        )
+        return (
+            [("none", "None")]
+            + ([("ee", "Enterprises Edition")] if reports_installed else [])
+            + [("sequence", "Sequence")]
+        )
