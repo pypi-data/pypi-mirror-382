d066f9adcbcbc49aa5c5793e640cdf6012f22c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:09 +0000

    Add package pytest-b-logger/1.1.2/pytest_b_logger-1.1.2-py3-none-any.whl

---
+++ packages/pytest-b-logger/pytest_b_logger-1.1.2-py3-none-any.whl/b_logger/entities/prints.py
@@ -0,0 +1,21 @@
+import uuid
+from enum import Enum
+
+from b_logger.utils.basedatamodel import BaseDataModel
+
+
+# class PrintStatus(str, Enum):
+#     PASSED = 'passed'
+#     FAILED = 'failed'
+#     WARNING = 'warning'
+#     NONE = 'none'
+
+
+class Print(BaseDataModel):
+    def __init__(self, data):
+        self.id = f'print_{uuid.uuid4()}'
+        self.title = data
+        self.parent_id = None
+
+    def set_parent_id(self, parent_id):
+        self.parent_id = parent_id
