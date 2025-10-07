bd54ab92d3d1aed08d68b135a4aef0efcf949654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:05:01 +0000

    Add package my-first-ml/0.1.0/my_first_ml-0.1.0-py3-none-any.whl

---
+++ packages/my-first-ml/my_first_ml-0.1.0-py3-none-any.whl/mtme/__init__.py
@@ -0,0 +1,10 @@
+"""
+my-first-ml (mtme)
+A test package for ML explanation utilities
+"""
+
+__version__ = "0.1.0"
+
+from .core import hello
+
+__all__ = ["hello"]
---
+++ packages/my-first-ml/my_first_ml-0.1.0-py3-none-any.whl/mtme/core.py
@@ -0,0 +1,14 @@
+"""
+Core functionality for mtme package
+"""
+
+def hello():
+    """
+    Simple hello function that prints a greeting.
+    
+    Example:
+        >>> import mtme
+        >>> mtme.hello()
+        Hello!
+    """
+    print("Hello!")
