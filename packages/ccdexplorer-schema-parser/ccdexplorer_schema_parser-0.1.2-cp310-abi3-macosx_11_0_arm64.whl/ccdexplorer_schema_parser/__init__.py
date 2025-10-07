5f60e22fcaf2c7c83948f73bb45ce7e9fb98e04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:43 +0000

    Add package ccdexplorer-schema-parser/0.1.2/ccdexplorer_schema_parser-0.1.2-cp310-abi3-macosx_11_0_arm64.whl

---
+++ packages/ccdexplorer-schema-parser/ccdexplorer_schema_parser-0.1.2-cp310-abi3-macosx_11_0_arm64.whl/ccdexplorer_schema_parser/__init__.py
@@ -0,0 +1,5 @@
+from .ccdexplorer_schema_parser import *
+
+__doc__ = ccdexplorer_schema_parser.__doc__
+if hasattr(ccdexplorer_schema_parser, "__all__"):
+    __all__ = ccdexplorer_schema_parser.__all__
