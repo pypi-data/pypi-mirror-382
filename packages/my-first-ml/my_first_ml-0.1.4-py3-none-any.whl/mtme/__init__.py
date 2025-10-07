c9a0aea4afece059a0f7198c75971fbbaeb4b128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:04:58 +0000

    Add package my-first-ml/0.1.4/my_first_ml-0.1.4-py3-none-any.whl

---
+++ packages/my-first-ml/my_first_ml-0.1.4-py3-none-any.whl/mtme/__init__.py
@@ -0,0 +1,10 @@
+"""
+my-first-ml (mtme)
+A test package for ML explanation utilities
+"""
+
+__version__ = "0.1.4"
+
+from .core import describe, wine_classification, iris_classification, matplotlib_sb, numpy_basic, scipy_eda
+
+__all__ = ["describe", "wine_classification", "iris_classification", "matplotlib_sb", "numpy_basic", "scipy_eda", "imbalance"]
