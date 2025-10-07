d93e68b62ca6388ab24065da14a9726d9d7d739c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:04:14 +0000

    Add package snowflake-ml-python/1.16.0/snowflake_ml_python-1.16.0-py3-none-any.whl

---
+++ packages/snowflake-ml-python/snowflake_ml_python-1.16.0-py3-none-any.whl/snowflake/ml/jobs/__init__.py
@@ -0,0 +1,23 @@
+from snowflake.ml.jobs._utils.types import JOB_STATUS
+from snowflake.ml.jobs.decorators import remote
+from snowflake.ml.jobs.job import MLJob
+from snowflake.ml.jobs.manager import (
+    delete_job,
+    get_job,
+    list_jobs,
+    submit_directory,
+    submit_file,
+    submit_from_stage,
+)
+
+__all__ = [
+    "remote",
+    "submit_file",
+    "submit_directory",
+    "list_jobs",
+    "get_job",
+    "delete_job",
+    "MLJob",
+    "JOB_STATUS",
+    "submit_from_stage",
+]
