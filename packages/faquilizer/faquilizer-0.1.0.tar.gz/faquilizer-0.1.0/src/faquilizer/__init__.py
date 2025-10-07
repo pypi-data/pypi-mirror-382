dc7184aeddbddc36303f68e79989f664143aa77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:05:51 +0000

    Add package faquilizer/0.1.0/faquilizer-0.1.0.tar.gz

---
+++ packages/faquilizer/faquilizer-0.1.0.tar.gz/faquilizer-0.1.0/src/faquilizer/__init__.py
@@ -0,0 +1,9 @@
+# faquilizer/src/faquilizer/__init__.py
+
+from .template import NOTEBOOK_TEMPLATE
+
+print("✅ Faquilizer template loaded.")
+print("📋 Copy the entire block below and paste it into a new, empty notebook cell.")
+print("👇" * 20)
+print(NOTEBOOK_TEMPLATE)
+print("👆" * 20)
