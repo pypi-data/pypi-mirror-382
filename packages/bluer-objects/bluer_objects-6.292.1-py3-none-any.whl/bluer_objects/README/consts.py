ac2522719803b35e0f71ace76c9e663d582b1cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:31 +0000

    Add package bluer-objects/6.292.1/bluer_objects-6.292.1-py3-none-any.whl

---
+++ packages/bluer-objects/bluer_objects-6.292.1-py3-none-any.whl/bluer_objects/README/consts.py
@@ -0,0 +1,26 @@
+from typing import Union
+
+github_kamangir = "https://github.com/kamangir"
+designs_repo = f"{github_kamangir}/bluer-designs/"
+
+
+def designs_url(suffix: str) -> str:
+    return f"{designs_repo}/blob/main/{suffix}"
+
+
+def asset_volume(volume: Union[str, int] = "") -> str:
+    return f"{github_kamangir}/assets{str(volume)}/raw/main"
+
+
+assets = asset_volume(volume="")
+assets2 = asset_volume(volume="2")
+
+
+def assets_path(
+    suffix: str,
+    volume: Union[str, int] = "",
+) -> str:
+    return "{}/{}".format(
+        asset_volume(volume=volume),
+        suffix,
+    )
