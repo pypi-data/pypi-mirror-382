b9691bb88e5b127adf78abfc7a19d09a2daad854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:38 +0000

    Add package leaguepedia_parser_thomasbarrepitous/0.0.5/leaguepedia_parser_thomasbarrepitous-0.0.5-py3-none-any.whl

---
+++ packages/leaguepedia_parser_thomasbarrepitous/leaguepedia_parser_thomasbarrepitous-0.0.5-py3-none-any.whl/leaguepedia_parser_thomasbarrepitous/transmuters/picks_bans.py
@@ -0,0 +1,22 @@
+from typing import List
+from lol_dto.classes.game.lol_game import LolPickBan
+import lol_id_tools as lit
+
+from leaguepedia_parser_thomasbarrepitous.transmuters.field_names import (
+    picks_bans_fields,
+)
+
+
+def transmute_picks_bans(input_dict) -> List[LolPickBan]:
+    pb_list = []
+
+    for field in picks_bans_fields:
+        pb_list.append(
+            LolPickBan(
+                championId=lit.get_id(input_dict[field], object_type="champion"),
+                isBan="Ban" in field,
+                team="BLUE" if "Team1" in field else "RED",
+            )
+        )
+
+    return pb_list
