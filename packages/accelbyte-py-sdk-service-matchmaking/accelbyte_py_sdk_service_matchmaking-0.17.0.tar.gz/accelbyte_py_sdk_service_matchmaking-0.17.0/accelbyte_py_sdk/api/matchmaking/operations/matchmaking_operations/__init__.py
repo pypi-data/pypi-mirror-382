3f49adb8fcd1898fa3f21e17ff79642550c72d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:27 +0000

    Add package accelbyte-py-sdk-service-matchmaking/0.17.0/accelbyte_py_sdk_service_matchmaking-0.17.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-matchmaking/accelbyte_py_sdk_service_matchmaking-0.17.0.tar.gz/accelbyte_py_sdk_service_matchmaking-0.17.0/accelbyte_py_sdk/api/matchmaking/operations/matchmaking_operations/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Matchmaking Service."""
+
+__version__ = "2.30.7"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .get_healthcheck_info import GetHealthcheckInfo
+from .handler_v3_healthz import HandlerV3Healthz
+from .public_get_messages import PublicGetMessages
+from .version_check_handler import VersionCheckHandler
