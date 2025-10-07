2f07db19c4807a9e3971c556366dc825e7afab6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:04:39 +0000

    Add package accelbyte-py-sdk-service-inventory/0.17.0/accelbyte_py_sdk_service_inventory-0.17.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-inventory/accelbyte_py_sdk_service_inventory-0.17.0.tar.gz/accelbyte_py_sdk_service_inventory-0.17.0/accelbyte_py_sdk/api/inventory/operations/admin_tags/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Inventory Service."""
+
+__version__ = "0.2.15"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .admin_create_tag import AdminCreateTag
+from .admin_delete_tag import AdminDeleteTag
+from .admin_list_tags import AdminListTags
+from .admin_list_tags import (
+    SortByEnum as AdminListTagsSortByEnum,
+)
