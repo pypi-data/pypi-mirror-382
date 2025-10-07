a345422b52a0c4a660bb9ce489362af3f9863e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:15 +0000

    Add package browsercookie/0.8.2/browsercookie-0.8.2.tar.gz

---
+++ packages/browsercookie/browsercookie-0.8.2.tar.gz/browsercookie-0.8.2/setup.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from distutils.core import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name='browsercookie', 
+    version='0.8.2',
+    python_requires='>=3.4',
+    packages=['browsercookie'],
+    author='Richard Penman',
+    author_email='richard.penman@gmail.com',
+    description='Loads cookies from your browser into a cookiejar object so can download with urllib and other libraries the same content you see in the web browser.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/richardpenman/browsercookie',
+    install_requires=['cryptography', 'keyring', 'lz4',
+                      'pywin32; sys_platform == "win32"'],
+    license='lgpl'
+)
