80a1cbe46e3aa6bb73fe85b6bbe32820f2050323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:05:53 +0000

    Add package django-whatsapp-api-wrapper/0.5.3/django_whatsapp_api_wrapper-0.5.3.tar.gz

---
+++ packages/django-whatsapp-api-wrapper/django_whatsapp_api_wrapper-0.5.3.tar.gz/django_whatsapp_api_wrapper-0.5.3/src/django_whatsapp_api_wrapper/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import WhatsAppCloudApiBusiness, WhatsAppEmbeddedSignUp
+# Register your models here.
+
+admin.site.register(WhatsAppCloudApiBusiness)
+admin.site.register(WhatsAppEmbeddedSignUp)
+
+
+    
+
+    
