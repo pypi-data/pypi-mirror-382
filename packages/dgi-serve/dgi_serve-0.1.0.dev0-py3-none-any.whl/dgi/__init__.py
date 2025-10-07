8489aa3fcd9a01daaef2487294618810c4611726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:04:43 +0000

    Add package dgi-serve/0.1.0.dev0/dgi_serve-0.1.0.dev0-py3-none-any.whl

---
+++ packages/dgi-serve/dgi_serve-0.1.0.dev0-py3-none-any.whl/dgi/__init__.py
@@ -0,0 +1,16 @@
+"""
+DGI: The Deep-neural-network General Inference framework.
+"""
+
+__version__ = "0.1.0-dev"
+__author__ = "Wilhelm Jung"
+
+# Import main functionality here
+# from .core import main_function
+# from .utils import helper_function
+
+__all__ = [
+    # List your public API here
+    # "main_function",
+    # "helper_function",
+]
---
+++ packages/dgi-serve/dgi_serve-0.1.0.dev0-py3-none-any.whl/dgi/core.py
@@ -0,0 +1,24 @@
+"""
+Core functionality for the DGI package.
+"""
+
+
+def hello_world():
+    """
+    A simple hello world function.
+
+    Returns:
+        str: A greeting message.
+    """
+    return "Hello from DGI!"
+
+
+def main():
+    """
+    Main entry point for the DGI package.
+    """
+    print(hello_world())
+
+
+if __name__ == "__main__":
+    main()
