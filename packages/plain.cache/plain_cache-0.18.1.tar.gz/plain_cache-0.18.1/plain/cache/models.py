69050ae2a4681ef742f1440505c50c15b55168bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:21 +0000

    Add package plain.cache/0.18.1/plain_cache-0.18.1.tar.gz

---
+++ packages/plain.cache/plain_cache-0.18.1.tar.gz/plain_cache-0.18.1/plain/cache/models.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from plain import models
+from plain.utils import timezone
+
+
+class CachedItemQuerySet(models.QuerySet):
+    def expired(self) -> CachedItemQuerySet:
+        return self.filter(expires_at__lt=timezone.now())
+
+    def unexpired(self) -> CachedItemQuerySet:
+        return self.filter(expires_at__gte=timezone.now())
+
+    def forever(self) -> CachedItemQuerySet:
+        return self.filter(expires_at=None)
+
+
+@models.register_model
+class CachedItem(models.Model):
+    key = models.CharField(max_length=255)
+    value = models.JSONField(required=False, allow_null=True)
+    expires_at = models.DateTimeField(required=False, allow_null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        queryset_class = CachedItemQuerySet
+        indexes = [
+            models.Index(fields=["expires_at"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["key"], name="plaincache_cacheditem_unique_key"
+            ),
+        ]
+
+    def __str__(self) -> str:
+        return self.key
