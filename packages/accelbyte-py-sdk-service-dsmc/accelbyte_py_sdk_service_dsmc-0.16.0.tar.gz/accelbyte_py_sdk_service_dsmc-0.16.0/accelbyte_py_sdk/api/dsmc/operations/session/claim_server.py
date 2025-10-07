4c7d34a377dd8271f2f7bbd2dc83325c411e52b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:24 +0000

    Add package accelbyte-py-sdk-service-dsmc/0.16.0/accelbyte_py_sdk_service_dsmc-0.16.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-dsmc/accelbyte_py_sdk_service_dsmc-0.16.0.tar.gz/accelbyte_py_sdk_service_dsmc-0.16.0/accelbyte_py_sdk/api/dsmc/operations/session/claim_server.py
@@ -0,0 +1,429 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Dsm Controller Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ModelsClaimSessionRequest
+from ...models import ResponseError
+
+
+class ClaimServer(Operation):
+    """Claim a DS for a game session (ClaimServer)
+
+    Required permission: NAMESPACE:{namespace}:DSM:SESSION [UPDATE]
+
+    Required scope: social
+
+    This endpoint is intended to be called by game session manager (matchmaker, lobby, etc.) to claim a dedicated server. The dedicated server cannot be claimed unless the status is READY
+
+    Properties:
+        url: /dsmcontroller/namespaces/{namespace}/sessions/claim
+
+        method: POST
+
+        tags: ["Session"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        body: (body) REQUIRED ModelsClaimSessionRequest in body
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        204: No Content - (DS claimed for session)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        404: Not Found - ResponseError (session not found)
+
+        409: Conflict - ResponseError (DS is already claimed)
+
+        425: Too Early - ResponseError (DS is not ready to be claimed)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+
+        503: Service Unavailable - ResponseError (DS is unreachable)
+    """
+
+    # region fields
+
+    _url: str = "/dsmcontroller/namespaces/{namespace}/sessions/claim"
+    _path: str = "/dsmcontroller/namespaces/{namespace}/sessions/claim"
+    _base_path: str = ""
+    _method: str = "POST"
+    _consumes: List[str] = ["application/json"]
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "dsmc"
+
+    body: ModelsClaimSessionRequest  # REQUIRED in [body]
+    namespace: str  # REQUIRED in [path]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "body": self.get_body_params(),
+            "path": self.get_path_params(),
+        }
+
+    def get_body_params(self) -> Any:
+        if not hasattr(self, "body") or self.body is None:
+            return None
+        return self.body.to_dict()
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_body(self, value: ModelsClaimSessionRequest) -> ClaimServer:
+        self.body = value
+        return self
+
+    def with_namespace(self, value: str) -> ClaimServer:
+        self.namespace = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "body") and self.body:
+            result["body"] = self.body.to_dict(include_empty=include_empty)
+        elif include_empty:
+            result["body"] = ModelsClaimSessionRequest()
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_204: Optional[HttpResponse] = None
+        error_401: Optional[ResponseError] = None
+        error_404: Optional[ResponseError] = None
+        error_409: Optional[ResponseError] = None
+        error_425: Optional[ResponseError] = None
+        error_500: Optional[ResponseError] = None
+        error_503: Optional[ResponseError] = None
+
+        def ok(self) -> ClaimServer.Response:
+            if self.error_401 is not None:
+                err = self.error_401.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_404 is not None:
+                err = self.error_404.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_409 is not None:
+                err = self.error_409.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_425 is not None:
+                err = self.error_425.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_500 is not None:
+                err = self.error_500.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_503 is not None:
+                err = self.error_503.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_204 is not None:
+                yield self.data_204
+                yield None
+            elif self.error_401 is not None:
+                yield None
+                yield self.error_401
+            elif self.error_404 is not None:
+                yield None
+                yield self.error_404
+            elif self.error_409 is not None:
+                yield None
+                yield self.error_409
+            elif self.error_425 is not None:
+                yield None
+                yield self.error_425
+            elif self.error_500 is not None:
+                yield None
+                yield self.error_500
+            elif self.error_503 is not None:
+                yield None
+                yield self.error_503
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        204: No Content - (DS claimed for session)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        404: Not Found - ResponseError (session not found)
+
+        409: Conflict - ResponseError (DS is already claimed)
+
+        425: Too Early - ResponseError (DS is not ready to be claimed)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+
+        503: Service Unavailable - ResponseError (DS is unreachable)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = ClaimServer.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 204:
+                result.data_204 = None
+            elif code == 401:
+                result.error_401 = ResponseError.create_from_dict(content)
+                result.error = result.error_401.translate_to_api_error()
+            elif code == 404:
+                result.error_404 = ResponseError.create_from_dict(content)
+                result.error = result.error_404.translate_to_api_error()
+            elif code == 409:
+                result.error_409 = ResponseError.create_from_dict(content)
+                result.error = result.error_409.translate_to_api_error()
+            elif code == 425:
+                result.error_425 = ResponseError.create_from_dict(content)
+                result.error = result.error_425.translate_to_api_error()
+            elif code == 500:
+                result.error_500 = ResponseError.create_from_dict(content)
+                result.error = result.error_500.translate_to_api_error()
+            elif code == 503:
+                result.error_503 = ResponseError.create_from_dict(content)
+                result.error = result.error_503.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[None, Union[None, HttpResponse, ResponseError]]:
+        """Parse the given response.
+
+        204: No Content - (DS claimed for session)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        404: Not Found - ResponseError (session not found)
+
+        409: Conflict - ResponseError (DS is already claimed)
+
+        425: Too Early - ResponseError (DS is not ready to be claimed)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+
+        503: Service Unavailable - ResponseError (DS is unreachable)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 204:
+            return None, None
+        if code == 401:
+            return None, ResponseError.create_from_dict(content)
+        if code == 404:
+            return None, ResponseError.create_from_dict(content)
+        if code == 409:
+            return None, ResponseError.create_from_dict(content)
+        if code == 425:
+            return None, ResponseError.create_from_dict(content)
+        if code == 500:
+            return None, ResponseError.create_from_dict(content)
+        if code == 503:
+            return None, ResponseError.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, body: ModelsClaimSessionRequest, namespace: str, **kwargs
+    ) -> ClaimServer:
+        instance = cls()
+        instance.body = body
+        instance.namespace = namespace
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(cls, dict_: dict, include_empty: bool = False) -> ClaimServer:
+        instance = cls()
+        if "body" in dict_ and dict_["body"] is not None:
+            instance.body = ModelsClaimSessionRequest.create_from_dict(
+                dict_["body"], include_empty=include_empty
+            )
+        elif include_empty:
+            instance.body = ModelsClaimSessionRequest()
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "body": "body",
+            "namespace": "namespace",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "body": True,
+            "namespace": True,
+        }
+
+    # endregion static methods
