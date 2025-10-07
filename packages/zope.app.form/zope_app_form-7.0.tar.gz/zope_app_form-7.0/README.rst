cd3b5a67975bcedfb5b36941ced15988c7d30f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:26 +0000

    Add package zope.app.form/7.0/zope_app_form-7.0.tar.gz

---
+++ packages/zope.app.form/zope_app_form-7.0.tar.gz/zope_app_form-7.0/README.rst
@@ -0,0 +1,14 @@
+.. caution::
+
+    This repository is no longer maintained and thus it got archived.
+
+    If you want to work on it please open a ticket in
+    https://github.com/zopefoundation/meta/issues requesting its unarchival.
+
+This package provides the old form framework for Zope 3. It also
+implements a few high-level ZCML directives for declaring forms. More
+advanced alternatives are implemented in ``zope.formlib`` and
+``z3c.form``. The widgets that were defined in here were moved to
+``zope.formlib``. Version 4.0 and newer are maintained for backwards
+compatibility reasons only.
+
---
+++ packages/zope.app.form/zope_app_form-7.0.tar.gz/zope_app_form-7.0/setup.py
@@ -0,0 +1,102 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# https://zopetoolkit.readthedocs.io/
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.app.form package
+"""
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+tests_require = [
+    'zc.sourcefactory',
+    'zope.container',
+    'zope.principalregistry',
+    'zope.site',
+    'zope.traversing',
+    'zope.app.appsetup',
+    'zope.testing',
+    'zope.testrunner >= 6.4',
+    'zope.app.wsgi >= 5.3',
+    'webtest',
+]
+
+setup(name='zope.app.form',
+      version='7.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.dev',
+      description='The Original Zope 3 Form Framework',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          'Detailed documentation:\n'
+          + '\n\n' +
+          read('CHANGES.rst')
+      ),
+      keywords="zope3 form widget zcml",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope :: 3',
+      ],
+      url='https://github.com/zopefoundation/zope.app.form',
+      license='ZPL-2.1',
+      python_requires='>=3.9',
+      extras_require={
+          'test': tests_require
+      },
+      install_requires=[
+          "setuptools",
+          "transaction",
+          "zope.formlib >= 4.3",
+          "zope.browser >= 2.1",
+          "zope.browserpage >= 3.10.1",
+          "zope.browsermenu",
+          "zope.component",
+          "zope.configuration >= 4.4.0",
+          "zope.datetime",
+          "zope.exceptions",
+          "zope.i18n",
+          "zope.interface",
+          "zope.proxy",
+          "zope.publisher >= 4.3.1",
+          "zope.schema >= 4.4.2",
+          "zope.security",
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
