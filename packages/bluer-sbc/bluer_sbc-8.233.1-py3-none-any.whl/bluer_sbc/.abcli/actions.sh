829680a4c43d5ec95b4419a785c3589cf8b49e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:39 +0000

    Add package bluer-sbc/8.233.1/bluer_sbc-8.233.1-py3-none-any.whl

---
+++ packages/bluer-sbc/bluer_sbc-8.233.1-py3-none-any.whl/bluer_sbc/.abcli/actions.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+function bluer_sbc_action_git_before_push() {
+    bluer_sbc build_README
+    [[ $? -ne 0 ]] && return 1
+
+    [[ "$(bluer_ai_git get_branch)" != "main" ]] &&
+        return 0
+
+    bluer_sbc pypi build
+}
