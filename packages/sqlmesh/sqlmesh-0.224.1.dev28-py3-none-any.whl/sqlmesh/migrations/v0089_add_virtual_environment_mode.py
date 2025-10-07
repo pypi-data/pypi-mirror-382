6a9baf20d4c4fd494f1add34a332d86f33bbba42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:04:49 +0000

    Add package sqlmesh/0.224.1.dev28/sqlmesh-0.224.1.dev28-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev28-py3-none-any.whl/sqlmesh/migrations/v0089_add_virtual_environment_mode.py
@@ -0,0 +1,9 @@
+"""Add virtual_environment_mode to the model definition."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
