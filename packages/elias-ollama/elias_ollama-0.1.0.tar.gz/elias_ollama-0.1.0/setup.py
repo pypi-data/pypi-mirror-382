638a0f73a09d3fef7f71ffa93af8f36400aed6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:16 +0000

    Add package elias-ollama/0.1.0/elias_ollama-0.1.0.tar.gz

---
+++ packages/elias-ollama/elias_ollama-0.1.0.tar.gz/elias_ollama-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="elias_ollama",
+    version="0.1.0",
+    author="Elias Liu",
+    author_email="elias.liu120@gmail.com",
+    description="一个用于调用Ollama模型的Python工具包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tenbj/elias_ollama",  # 替换为实际的仓库URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests>=2.25.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'elias-ollama=elias_ollama.cli:main',
+        ],
+    },
+)
