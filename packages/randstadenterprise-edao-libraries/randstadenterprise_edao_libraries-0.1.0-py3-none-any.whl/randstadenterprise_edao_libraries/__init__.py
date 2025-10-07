da5dd01b0ba7d187185abd9b7696f79585e29579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:04:08 +0000

    Add package randstadenterprise-edao-libraries/0.1.0/randstadenterprise_edao_libraries-0.1.0-py3-none-any.whl

---
+++ packages/randstadenterprise-edao-libraries/randstadenterprise_edao_libraries-0.1.0-py3-none-any.whl/randstadenterprise_edao_libraries/__init__.py
@@ -0,0 +1,8 @@
+# randstadenterprise_edao_libraries/__init__.py
+
+from . import users
+from . import datasets
+from . import groups
+from . import roles
+
+# ... other code ...
---
+++ packages/randstadenterprise-edao-libraries/randstadenterprise_edao_libraries-0.1.0-py3-none-any.whl/randstadenterprise_edao_libraries/datasets.py
@@ -0,0 +1,7 @@
+# com_randstadenterprise/users.py
+
+def get_dataset_id_by_name(inst_url, name):
+    """Placeholder for function to retrieve a Dataset ID."""
+    print(f"Retrieving Dataset ID for dataset name: {inst_url} : {name}")
+    # Logic to query a system for the user ID would go here
+    return 12345 
