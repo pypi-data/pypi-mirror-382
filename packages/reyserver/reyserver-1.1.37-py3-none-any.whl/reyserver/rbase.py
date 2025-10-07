a6635bcabe17380fd4c4b2a0b51131648187f7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:51 +0000

    Add package reyserver/1.1.37/reyserver-1.1.37-py3-none-any.whl

---
+++ packages/reyserver/reyserver-1.1.37-py3-none-any.whl/reyserver/rbase.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2025-07-17 22:32:37
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Base methods.
+"""
+
+
+from reykit.rbase import Base
+
+
+__all__ = (
+    'ServerBase',
+    'ServerAPI'
+)
+
+
+class ServerBase(Base):
+    """
+    Server base type.
+    """
+
+
+class ServerAPI(ServerBase):
+    """
+    Server API type.
+    """
