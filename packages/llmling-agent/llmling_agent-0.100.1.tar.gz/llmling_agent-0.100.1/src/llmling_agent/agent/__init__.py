8f64880ed93c51d612c9dc4fdd9edf1fd5f6d283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 05:04:10 +0000

    Add package llmling-agent/0.100.1/llmling_agent-0.100.1.tar.gz

---
+++ packages/llmling-agent/llmling_agent-0.100.1.tar.gz/llmling_agent-0.100.1/src/llmling_agent/agent/__init__.py
@@ -0,0 +1,37 @@
+"""CLI commands for llmling-agent."""
+
+from __future__ import annotations
+from typing_extensions import TypeVar
+
+from llmling_agent.agent.agent import Agent
+from llmling_agent.agent.structured import StructuredAgent
+from llmling_agent.agent.context import AgentContext
+from llmling_agent.agent.process_manager import (
+    ProcessManager,
+    ProcessOutput,
+    RunningProcess,
+)
+
+
+TDeps = TypeVar("TDeps", default=None)
+TResult = TypeVar("TResult", default=str)
+
+type AnyAgent[TDeps, TResult] = Agent[TDeps] | StructuredAgent[TDeps, TResult]
+
+from llmling_agent.agent.conversation import ConversationManager
+from llmling_agent.agent.interactions import Interactions
+from llmling_agent.agent.sys_prompts import SystemPrompts
+
+
+__all__ = [
+    "Agent",
+    "AgentContext",
+    "AnyAgent",
+    "ConversationManager",
+    "Interactions",
+    "ProcessManager",
+    "ProcessOutput",
+    "RunningProcess",
+    "StructuredAgent",
+    "SystemPrompts",
+]
