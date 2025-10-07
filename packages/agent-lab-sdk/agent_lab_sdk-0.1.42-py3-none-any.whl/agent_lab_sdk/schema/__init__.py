118e9206a66072738ceea1c9d4bae720dd515b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:42 +0000

    Add package agent-lab-sdk/0.1.42/agent_lab_sdk-0.1.42-py3-none-any.whl

---
+++ packages/agent-lab-sdk/agent_lab_sdk-0.1.42-py3-none-any.whl/agent_lab_sdk/schema/__init__.py
@@ -0,0 +1,29 @@
+from .log_message import LogMessage
+from .input_types import (
+    MainInput,
+    StringInput,
+    StringArrayInput,
+    StringArrayInputInline,
+    NumberInput,
+    SelectInput,
+    CheckboxInput,
+    SwitchInput,
+    FileInput,
+    FilesInput,
+    SelectOption
+)
+
+__all__ = [
+    "LogMessage",
+    "MainInput",
+    "StringInput", 
+    "StringArrayInput",
+    "StringArrayInputInline",
+    "NumberInput",
+    "SelectInput",
+    "CheckboxInput",
+    "SwitchInput",
+    "FileInput",
+    "FilesInput",
+    "SelectOption"
+]
