a8038608ec45ed7c5b0651d286304636f62f46a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:05:38 +0000

    Add package accelbyte-py-sdk-service-challenge/0.15.0/accelbyte_py_sdk_service_challenge-0.15.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-challenge/accelbyte_py_sdk_service_challenge-0.15.0.tar.gz/accelbyte_py_sdk_service_challenge-0.15.0/accelbyte_py_sdk/api/challenge/operations/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operations-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Challenge Service."""
+
+__version__ = "1.0.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
