1099d1bc379e3a1cc4f7db1d3cde789b94f915a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:18 +0000

    Add package sigllm/0.1.0/sigllm-0.1.0.tar.gz

---
+++ packages/sigllm/sigllm-0.1.0.tar.gz/sigllm-0.1.0/sigllm/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for sigllm."""
+
+__author__ = 'MIT Data To AI Lab'
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.1.0'
+
+import os
+
+from sigllm.core import SigLLM  # noqa: F401
+
+_BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+MLBLOCKS_PRIMITIVES = os.path.join(_BASE_PATH, 'primitives', 'jsons')
+MLBLOCKS_PIPELINES = tuple([
+    os.path.join(_BASE_PATH, 'pipelines', 'prompter'),
+    os.path.join(_BASE_PATH, 'pipelines', 'detector'),
+])
