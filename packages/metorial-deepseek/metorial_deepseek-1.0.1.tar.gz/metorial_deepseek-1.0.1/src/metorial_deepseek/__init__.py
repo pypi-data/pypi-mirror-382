625298c3c42c06ad088498c552cce8db9817d393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:03 +0000

    Add package metorial-deepseek/1.0.1/metorial_deepseek-1.0.1.tar.gz

---
+++ packages/metorial-deepseek/metorial_deepseek-1.0.1.tar.gz/metorial_deepseek-1.0.1/src/metorial_deepseek/__init__.py
@@ -0,0 +1,7 @@
+from .metorial_deepseek import (
+  MetorialDeepSeekSession,
+  build_deepseek_tools,
+  call_deepseek_tools,
+)
+
+__all__ = ["MetorialDeepSeekSession", "build_deepseek_tools", "call_deepseek_tools"]
