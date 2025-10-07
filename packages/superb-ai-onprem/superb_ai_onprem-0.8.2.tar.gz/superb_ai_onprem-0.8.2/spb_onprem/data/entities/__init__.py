0bac24898b8ed1b74e74da577dcbb83057a06878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:04:45 +0000

    Add package superb-ai-onprem/0.8.2/superb_ai_onprem-0.8.2.tar.gz

---
+++ packages/superb-ai-onprem/superb_ai_onprem-0.8.2.tar.gz/superb_ai_onprem-0.8.2/spb_onprem/data/entities/__init__.py
@@ -0,0 +1,22 @@
+from .annotation import Annotation, AnnotationVersion
+from .comment import Comment, Reply
+from .data_meta import DataMeta
+from .data import Data
+from .prediction import Prediction
+from .scene import Scene
+from .data_slice import DataSlice
+from .frame import Frame
+
+
+__all__ = (
+    "Frame",
+    "Data",
+    "Scene",
+    "Annotation",
+    "AnnotationVersion",
+    "Comment",
+    "Reply",
+    "Prediction",
+    "DataMeta",
+    "DataSlice",
+)
