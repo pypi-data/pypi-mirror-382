7977c8dc36874537adcb4667ccc838fcb99001f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:23 +0000

    Add package unique_toolkit/1.9.1/unique_toolkit-1.9.1-py3-none-any.whl

---
+++ packages/unique_toolkit/unique_toolkit-1.9.1-py3-none-any.whl/unique_toolkit/__init__.py
@@ -0,0 +1,24 @@
+# Re-export commonly used classes for easier imports
+from unique_toolkit.chat import ChatService
+from unique_toolkit.content import ContentService
+from unique_toolkit.embedding import EmbeddingService
+from unique_toolkit.language_model import (
+    LanguageModelMessages,
+    LanguageModelName,
+    LanguageModelService,
+    LanguageModelToolDescription,
+)
+from unique_toolkit.short_term_memory import ShortTermMemoryService
+
+# You can add other classes you frequently use here as well
+
+__all__ = [
+    "LanguageModelService",
+    "LanguageModelMessages",
+    "LanguageModelName",
+    "LanguageModelToolDescription",
+    "ChatService",
+    "ContentService",
+    "EmbeddingService",
+    "ShortTermMemoryService",
+]
