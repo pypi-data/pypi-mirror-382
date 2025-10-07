bbf558514d36b78c31ce19b5a44023a3d6cc542a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:11 +0000

    Add package python-whois/0.9.6/python_whois-0.9.6.tar.gz

---
+++ packages/python-whois/python_whois-0.9.6.tar.gz/python_whois-0.9.6/test/test_nicclient.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+import unittest
+
+from whois.whois import NICClient
+
+
+class TestNICClient(unittest.TestCase):
+    def setUp(self):
+        self.client = NICClient()
+
+    def test_choose_server(self):
+        domain = "рнидс.срб"
+        chosen = self.client.choose_server(domain)
+        correct = "whois.rnids.rs"
+        self.assertEqual(chosen, correct)
