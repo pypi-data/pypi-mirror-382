ed88cc8c8233d718d5b1912d4a2db9bd31d89885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:22 +0000

    Add package gpmanager/0.3.0/gpmanager-0.3.0-py3-none-any.whl

---
+++ packages/gpmanager/gpmanager-0.3.0-py3-none-any.whl/gpm/model.py
@@ -0,0 +1,38 @@
+import hashlib
+
+import tensorflow as tf
+
+# DEFAULT META #################################################################
+
+N_CONTEXT_DIM = 8
+N_EMBEDDING_DIM = 128
+
+# HYPER PARAMETERS #############################################################
+
+def seed(key: str) -> int:
+    __hash = hashlib.sha256(string=key.encode('utf-8')).hexdigest()
+    return int(__hash[:8], 16) # take the first 4 bytes: the seed is lower than 2 ** 32
+
+# MODEL ########################################################################
+
+def create(
+    seed: int,
+    n_input_dim: int,
+    n_output_dim: int,
+    n_context_dim: int=N_CONTEXT_DIM,
+    n_embedding_dim: int=N_EMBEDDING_DIM,
+) -> tf.keras.Model:
+    __model = tf.keras.Sequential()
+    # initialize the weights
+    __embedding_init = tf.keras.initializers.GlorotNormal(seed=seed)
+    __dense_init = tf.keras.initializers.GlorotNormal(seed=(seed ** 2) % (2 ** 32)) # different values
+    # embedding
+    __model.add(tf.keras.layers.Embedding(input_dim=n_input_dim, output_dim=n_embedding_dim, embeddings_initializer=__embedding_init, name='embedding'))
+    # head
+    __model.add(tf.keras.layers.Reshape(target_shape=(n_context_dim * n_embedding_dim,), name='reshape'))
+    __model.add(tf.keras.layers.Dense(units=n_output_dim, activation='tanh', use_bias=False, kernel_initializer=__dense_init, name='head'))
+    # compile
+    __model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True, label_smoothing=0., axis=-1, reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE, name='loss'))
+    return __model
---
+++ packages/gpmanager/gpmanager-0.3.0-py3-none-any.whl/gpm/pipeline.py
@@ -0,0 +1,102 @@
+import itertools
+import re
+
+import tensorflow as tf
+
+import gpm.model
+import gpm.vocabulary
+
+# DEFAULT VOCABULARIES #########################################################
+
+INPUT_VOCABULARY = ''.join(chr(__i) for __i in range(128)) # all ASCII characters
+OUTPUT_VOCABULARY = INPUT_VOCABULARY # placeholder
+
+# DEFAULT META #################################################################
+
+N_INPUT_DIM = len(INPUT_VOCABULARY) # all ASCII characters
+N_OUTPUT_DIM = N_INPUT_DIM # placeholder, it depends on the user settings
+
+N_CONTEXT_DIM = 8
+N_EMBEDDING_DIM = 128
+
+N_PASSWORD_DIM = 16
+N_PASSWORD_NONCE = 1
+
+# PREPROCESS ##################################################################
+
+def remove_prefix(text: str) -> str: # "github.com" and "https://github.com"
+    __r = r'^((?:ftp|https?):\/\/)'
+    return re.sub(pattern=__r, repl='', string=text, flags=re.IGNORECASE)
+
+def remove_suffix(text: str) -> str:
+    __r = r'(\/+)$'
+    return re.sub(pattern=__r, repl='', string=text, flags=re.IGNORECASE)
+
+def remove_spaces(text: str) -> str:
+    return text.replace(' ', '').replace('\t', '')
+
+def filter_ascii(text: str) -> str:
+    return ''.join(__c for __c in text if ord(__c) < 128)
+
+def preprocess(target: str, login: str) -> list:
+    __left = remove_suffix(remove_prefix(remove_spaces(filter_ascii(target.lower()))))
+    __right = remove_spaces(filter_ascii(login.lower()))
+    return __left + '|' + __right
+
+# ENTROPY #####################################################################
+
+def accumulate(x: int, y: int, n: int) -> int:
+    return (x + y) % n
+
+def feed(source: list, nonce: int, dimension: int) -> iter:
+    __func = lambda __x, __y: accumulate(x=__x, y=__y + nonce, n=dimension) # add entropy by accumulating the encodings
+    return itertools.accumulate(iterable=itertools.cycle(source), func=__func) # infinite iterable
+
+# INPUTS ######################################################################
+
+def tensor(feed: 'Iterable[int]', length: int, context: int) -> tf.Tensor:
+    __x = [[next(feed) for _ in range(context)] for _ in range(length)]
+    return tf.constant(tf.convert_to_tensor(value=__x, dtype=tf.dtypes.int32))
+
+# OUTPUTS #####################################################################
+
+def password(model: tf.keras.Model, x: tf.Tensor, itos: callable) -> str:
+    __y = tf.squeeze(model(x, training=False))
+    __p = list(tf.argmax(__y, axis=-1).numpy())
+    return gpm.vocabulary.decode(__p, itos=itos)
+
+# PROCESS #####################################################################
+
+def process(
+    master_key: str,
+    login_target: str,
+    login_id: str,
+    password_length: int,
+    password_nonce: int,
+    include_lowers: bool,
+    include_uppers: bool,
+    include_digits: bool,
+    include_symbols: bool,
+    include_words: bool,
+    input_vocabulary: str=INPUT_VOCABULARY,
+    model_context_dim: int=N_CONTEXT_DIM,
+    model_embedding_dim: int=N_EMBEDDING_DIM
+) -> str:
+    # seed to generate the model weights randomly
+    __seed = gpm.model.seed(key=master_key)
+    # input vocabulary
+    __input_mappings = gpm.vocabulary.mappings(vocabulary=input_vocabulary)
+    __input_dim = len(input_vocabulary)
+    # output vocabulary
+    __output_vocabulary = gpm.vocabulary.compose(lowers=include_lowers, uppers=include_uppers, digits=include_digits, symbols=include_symbols, words=include_words)
+    __output_mappings = gpm.vocabulary.mappings(vocabulary=__output_vocabulary)
+    __output_dim = len(__output_vocabulary)
+    # inputs
+    __inputs = preprocess(target=login_target, login=login_id)
+    __source = gpm.vocabulary.encode(text=__inputs, stoi=__input_mappings['encode'])
+    __feed = feed(source=__source, nonce=password_nonce, dimension=__input_dim)
+    __x = tensor(feed=__feed, length=password_length, context=model_context_dim)
+    # model
+    __model = gpm.model.create(seed=__seed, n_input_dim=__input_dim, n_output_dim=__output_dim, n_context_dim=model_context_dim, n_embedding_dim=model_embedding_dim)
+    # password
+    return password(model=__model, x=__x, itos=__output_mappings['decode'])
