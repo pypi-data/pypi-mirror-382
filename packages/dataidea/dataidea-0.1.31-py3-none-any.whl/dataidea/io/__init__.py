eed5614d350f4ca2a1a97dc264d6d64136dde627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:04:52 +0000

    Add package dataidea/0.1.31/dataidea-0.1.31-py3-none-any.whl

---
+++ packages/dataidea/dataidea-0.1.31-py3-none-any.whl/dataidea/io/__init__.py
@@ -0,0 +1,7 @@
+"""
+Input/Output functionality for loading and saving data
+"""
+from dataidea.io.datasets import load_dataset
+from dataidea.io.youtube import download as download_youtube
+
+__all__ = ['load_dataset', 'download_youtube'] 
---
+++ packages/dataidea/dataidea-0.1.31-py3-none-any.whl/dataidea/sklearn/__init__.py
@@ -0,0 +1,6 @@
+"""
+Model management functionality
+"""
+from dataidea.sklearn.models import load_model, save_model, regression_report, classification_report
+
+__all__ = ['load_model', 'save_model', 'regression_report', 'classification_report'] 
