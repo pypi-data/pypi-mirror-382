0a080b2939ee7ebf00826803a241be553a0c8d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:35 +0000

    Add package gp-core/6.0.1/gp_core-6.0.1-cp313-cp313t-manylinux_2_34_x86_64.whl

---
+++ packages/gp-core/gp_core-6.0.1-cp313-cp313t-manylinux_2_34_x86_64.whl/gp_core/__init__.py
@@ -0,0 +1,8 @@
+import gp_core.gp_core  # type: ignore[import]
+from gp_core.gp_core import algorithms, literal_matchers, models
+
+__all__ = [
+    "algorithms",
+    "literal_matchers",
+    "models",
+]
