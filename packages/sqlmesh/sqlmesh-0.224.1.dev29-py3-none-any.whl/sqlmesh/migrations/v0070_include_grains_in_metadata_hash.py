371b38db44033dcf70631b4d566f029594a8787a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:36 +0000

    Add package sqlmesh/0.224.1.dev29/sqlmesh-0.224.1.dev29-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev29-py3-none-any.whl/sqlmesh/migrations/v0070_include_grains_in_metadata_hash.py
@@ -0,0 +1,9 @@
+"""Include grains in the metadata hash."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
