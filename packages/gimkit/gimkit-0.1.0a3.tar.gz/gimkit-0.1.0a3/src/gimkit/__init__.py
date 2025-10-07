5e88072d0d40434316d3e9f3d37934f258425a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:04:47 +0000

    Add package gimkit/0.1.0a3/gimkit-0.1.0a3.tar.gz

---
+++ packages/gimkit/gimkit-0.1.0a3.tar.gz/gimkit-0.1.0a3/src/gimkit/__init__.py
@@ -0,0 +1,25 @@
+from importlib.metadata import PackageNotFoundError, version
+
+from gimkit.contexts import Query, Response, Result
+from gimkit.guides import guide
+from gimkit.models import from_openai, from_vllm, from_vllm_offline
+from gimkit.schemas import MaskedTag, validate
+
+
+try:
+    __version__ = version("gimkit")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+
+__all__ = [
+    "MaskedTag",
+    "Query",
+    "Response",
+    "Result",
+    "from_openai",
+    "from_vllm",
+    "from_vllm_offline",
+    "guide",
+    "validate",
+]
