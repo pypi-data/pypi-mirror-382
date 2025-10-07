a5e5f7a985ae214d44103a56b1b8f969c8e60e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:04:40 +0000

    Add package reydb/1.2.18/reydb-1.2.18-py3-none-any.whl

---
+++ packages/reydb/reydb-1.2.18-py3-none-any.whl/reydb/__init__.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024-01-07 20:51:57
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Backend database method set.
+
+Modules
+-------
+rall : All methods.
+rbase : Base methods.
+rbuild : Database build methods.
+rconfig : Database config methods.
+rconn : Database connection methods.
+rdb : Database methods.
+rerror : Database error methods.
+rexec : Database execute methods.
+rorm : Database ORM methods.
+rinfo : Database information methods.
+"""
+
+
+from .rdb import Database, DatabaseAsync
