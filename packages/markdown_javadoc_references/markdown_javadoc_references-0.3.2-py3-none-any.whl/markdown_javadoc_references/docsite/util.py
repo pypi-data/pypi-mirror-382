d9299faff4170cc316591047d8b47219018642b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:46 +0000

    Add package markdown_javadoc_references/0.3.2/markdown_javadoc_references-0.3.2-py3-none-any.whl

---
+++ packages/markdown_javadoc_references/markdown_javadoc_references-0.3.2-py3-none-any.whl/markdown_javadoc_references/docsite/util.py
@@ -0,0 +1,34 @@
+import requests
+
+from markdown_javadoc_references.entities import Klass, Type
+from markdown_javadoc_references.util import get_logger
+
+logger = get_logger(__name__)
+
+def read_url(url: str) -> str:
+    resp = requests.get(url)
+    resp.raise_for_status()
+    return resp.text
+
+
+def check_url(url: str) -> requests.Response:
+    resp = requests.head(url)
+    return resp
+
+def find_class_type(text: str, klass: Klass) -> Type | None:
+    match text:
+        case s if "Enum" in s:
+            return Type.ENUM
+        case s if "Annotation" in s:
+            return Type.ANN_INTERFACE
+        case s if "Interface" in s:
+            return Type.INTERFACE
+        case s if "Record" in s:
+            return Type.RECORD
+        case s if "Class" in s:
+            return Type.CLASS
+        case _:
+            logger.error(f"Unknown class type in title {text} of {klass.url}")
+            return None
+
+    logger.debug(f"Set type {klass.type} for {klass.url}")
