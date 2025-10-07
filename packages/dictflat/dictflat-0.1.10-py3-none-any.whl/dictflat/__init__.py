d86bf2e252021526428e090c7f189fde51079210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:42 +0000

    Add package dictflat/0.1.10/dictflat-0.1.10-py3-none-any.whl

---
+++ packages/dictflat/dictflat-0.1.10-py3-none-any.whl/dictflat/__init__.py
@@ -0,0 +1,16 @@
+from typing import Final, List
+
+from .dictflat import (
+    ALL,
+    CHANGE_ROOT,
+    CONTEXT_DEPTH,
+    CONTEXT_ELEMENT,
+    CONTEXT_PATH,
+    CONTEXT_ROOT_REF,
+    DictFlat,
+)
+
+__author__: Final[str] = 'Arnaud Valmary'
+__email__: Final[str] = 'github@valmary.eu'
+__version_parts__: Final[List[str]] = ['0', '1', '10']
+__version__: Final[str] = '.'.join(__version_parts__)
