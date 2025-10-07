1b7b76b181d4502f57a224a80e51c8333adb4d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:13 +0000

    Add package pydantic-apply/0.8.0/pydantic_apply-0.8.0-py3-none-any.whl

---
+++ packages/pydantic-apply/pydantic_apply-0.8.0-py3-none-any.whl/pydantic_apply/apply.py
@@ -0,0 +1,87 @@
+import warnings
+from typing import Any, cast
+
+import pydantic
+
+from pydantic_apply.utils import assignment_validation_context, is_pydantic_apply_annotation
+
+
+class ApplyModelMixin(pydantic.BaseModel):
+    """Mixin to allow models to apply partly changes to their data."""
+
+    def model_apply(
+        self,
+        changes: pydantic.BaseModel | dict[str, Any],
+    ) -> None:
+        """Apply (partly) changes to the model data."""
+
+
+        if isinstance(changes, pydantic.BaseModel):
+            # Convert model to dict. Do not use `changes.model_dump()` because it
+            # would convert nested models to dicts, too. We don't want that yet.
+            changes = {
+                key: value
+                for key, value
+                in changes.__dict__.items()
+                if key in changes.__pydantic_fields_set__
+            }
+
+        # Prepare the changes
+        prepared_changes = {}
+        for field_name, model_field in self.__class__.model_fields.items():
+            # Make sure the field exists in the changes and
+            # get changed field value
+            if field_name in changes:
+                changed_field_value = changes[field_name]
+            elif model_field.alias in changes:
+                changed_field_value = changes[model_field.alias]
+            else:
+                # Field will not be changed
+                continue
+
+            # Handle ApplyModelMixin fields:
+            # Field type must be ApplyModelMixin....
+            field_annotation = model_field.annotation
+            if field_annotation is None:
+                raise RuntimeError("model field has not typing annotation")
+
+            if is_pydantic_apply_annotation(field_annotation):
+                current_value = getattr(self, field_name)
+                if (
+                    # ...AND current attribute value must be a instance
+                    #        of ApplyModelMixin (not None)...
+                    isinstance(current_value, ApplyModelMixin)
+                    # ...AND type of changed value must allow patching.
+                    and (
+                        isinstance(changed_field_value, dict)
+                        or isinstance(changed_field_value, pydantic.BaseModel)
+                    )
+                ):
+                    # When validation on assignment is enabled we need to
+                    # copy the current value first. Otherwise, the validation
+                    # might have issues, see below.
+                    if self.model_config.get("validate_assignment", None):
+                        current_value = current_value.model_copy()
+
+                    # ...then use `.apply(...)` on the current value to prepare changes
+                    cast(ApplyModelMixin, current_value).model_apply(changed_field_value)
+                    prepared_changes[field_name] = current_value
+                    continue
+
+            # Default apply: Just set new value
+            prepared_changes[field_name] = changed_field_value
+
+        with assignment_validation_context(self, prepared_changes) as changes_in_context:
+            for field_name, field_value in changes_in_context.items():
+                setattr(self, field_name, field_value)
+
+    def apply(
+        self,
+        changes: pydantic.BaseModel | dict[str, Any],
+    ) -> None:
+        warnings.warn(
+            "apply(...) is deprecated, use model_apply(...) instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self.model_apply(changes)
