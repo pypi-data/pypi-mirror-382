58005765bc4fc20205fdd939821cb9157516c2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:50 +0000

    Add package qflux/0.0.2/qflux-0.0.2-py3-none-any.whl

---
+++ packages/qflux/qflux-0.0.2-py3-none-any.whl/qflux/closed_systems/__init__.py
@@ -0,0 +1,17 @@
+"""
+
+qflux v.0.1.0
+
+__version__ = 0.1.0
+
+Description: 
+
+classical_methods.py --> DynamicsCS : Closed-System dynamics 
+qubit_methods.py     --> QubitDynamicsCS : Qubit-based dynamics
+spin_dynamics_oo.py  --> SpinDynamicsS, SpinDynamicsH : Statevector and hadamard-test implementations (respectively) for spin-chain systems
+
+"""
+
+from .classical_methods import DynamicsCS
+from .qubit_methods import QubitDynamicsCS
+from .spin_dynamics_oo import SpinDynamicsS, SpinDynamicsH
