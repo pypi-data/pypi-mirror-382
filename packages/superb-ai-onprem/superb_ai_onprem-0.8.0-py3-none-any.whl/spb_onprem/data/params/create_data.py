0aef12771c8a01ca8f2e88f0d170c70097dbc47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:04:41 +0000

    Add package superb-ai-onprem/0.8.0/superb_ai_onprem-0.8.0-py3-none-any.whl

---
+++ packages/superb-ai-onprem/superb_ai_onprem-0.8.0-py3-none-any.whl/spb_onprem/data/params/create_data.py
@@ -0,0 +1,60 @@
+from spb_onprem.data.entities import Data
+
+
+def create_params(
+    data: Data,
+):
+    """Make the variables for the createData query.
+
+    Args:
+        data (Data): The data object what you want to create.
+    """
+    return {
+        "datasetId": data.dataset_id,
+        "key": data.key,
+        "type": data.type.value,
+        "slices": data.slice_ids if data.slice_ids is not None else [],
+        "scene": [
+            {
+                "type": scene.type.value,
+                "content": {
+                    "id": scene.content.id,
+                },
+                "meta": scene.meta,
+            }
+            for scene in data.scene
+        ] if data.scene is not None else None,
+        "thumbnail": {
+            "id": data.thumbnail.id,
+        } if data.thumbnail is not None else None,
+        "annotation": {
+            "versions": [
+                {
+                    "content": {
+                        "id": version.content.id,
+                    },
+                    "meta": version.meta,
+                }
+                for version in data.annotation.versions
+            ] if data.annotation.versions is not None else [],
+            "meta": data.annotation.meta
+        } if data.annotation is not None else None,
+        "predictions": [
+            {
+                "setId": prediction.set_id,
+                "content": {
+                    "id": prediction.content.id,
+                },
+                "meta": prediction.meta,
+            }
+            for prediction in data.predictions
+        ] if data.predictions is not None else [],
+        "meta": [
+            {
+                "key": meta.key,
+                "type": meta.type.value,
+                "value": meta.value,
+            }
+            for meta in data.meta
+        ] if data.meta is not None else [],
+    }
