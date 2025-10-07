5e8e38e2cf24f1c84ef9d1b326697b6bb97535f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:19 +0000

    Add package accelbyte-py-sdk-service-challenge/0.13.0/accelbyte_py_sdk_service_challenge-0.13.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-challenge/accelbyte_py_sdk_service_challenge-0.13.0.tar.gz/accelbyte_py_sdk_service_challenge-0.13.0/accelbyte_py_sdk/api/challenge/operations/challenge_list/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Challenge Service."""
+
+__version__ = "1.0.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .get_challenges import GetChallenges
+from .get_challenges import (
+    SortByEnum as GetChallengesSortByEnum,
+    StatusEnum as GetChallengesStatusEnum,
+)
+from .public_get_scheduled_goals import PublicGetScheduledGoals
+from .public_get_scheduled_goals import (
+    SortByEnum as PublicGetScheduledGoalsSortByEnum,
+)
