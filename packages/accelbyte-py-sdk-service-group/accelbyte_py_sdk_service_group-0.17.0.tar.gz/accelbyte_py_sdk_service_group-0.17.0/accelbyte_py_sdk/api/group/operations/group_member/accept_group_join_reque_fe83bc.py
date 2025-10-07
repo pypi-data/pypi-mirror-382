aaa0ec623323125a88371f0a3cecb3d6ec134b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:20 +0000

    Add package accelbyte-py-sdk-service-group/0.17.0/accelbyte_py_sdk_service_group-0.17.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-group/accelbyte_py_sdk_service_group-0.17.0.tar.gz/accelbyte_py_sdk_service_group-0.17.0/accelbyte_py_sdk/api/group/operations/group_member/accept_group_join_reque_fe83bc.py
@@ -0,0 +1,456 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Group Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ModelsMemberRequestGroupResponseV1
+from ...models import ResponseErrorResponse
+
+
+class AcceptGroupJoinRequestPublicV2(Operation):
+    """Accept Group Join Request (acceptGroupJoinRequestPublicV2)
+
+    Required valid user authentication
+
+    Required Member Role Permission: "GROUP:JOIN [CREATE]"
+
+    This endpoint is used to accept group join request.
+
+    Accept group join request. If specific user is not asked to join the specific group ID, it will show the the error to show if the user is not asked to join yet.
+
+    This endpoint will also check if the specific user is already joined to specific group
+
+    Action Code: 73407
+
+    Properties:
+        url: /group/v2/public/namespaces/{namespace}/users/{userId}/groups/{groupId}/join/accept
+
+        method: POST
+
+        tags: ["Group Member"]
+
+        consumes: []
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        group_id: (groupId) REQUIRED str in path
+
+        namespace: (namespace) REQUIRED str in path
+
+        user_id: (userId) REQUIRED str in path
+
+    Responses:
+        200: OK - ModelsMemberRequestGroupResponseV1 (OK)
+
+        400: Bad Request - ResponseErrorResponse (20002: validation error)
+
+        401: Unauthorized - ResponseErrorResponse (20001: unauthorized access)
+
+        403: Forbidden - ResponseErrorResponse (20022: token is not user token | 73036: insufficient member role permission)
+
+        404: Not Found - ResponseErrorResponse (73443: member request not found)
+
+        409: Conflict - ResponseErrorResponse (73442: user already joined in another group)
+
+        500: Internal Server Error - ResponseErrorResponse (Internal Server Error)
+    """
+
+    # region fields
+
+    _url: str = "/group/v2/public/namespaces/{namespace}/users/{userId}/groups/{groupId}/join/accept"
+    _path: str = "/group/v2/public/namespaces/{namespace}/users/{userId}/groups/{groupId}/join/accept"
+    _base_path: str = ""
+    _method: str = "POST"
+    _consumes: List[str] = []
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "group"
+
+    group_id: str  # REQUIRED in [path]
+    namespace: str  # REQUIRED in [path]
+    user_id: str  # REQUIRED in [path]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "path": self.get_path_params(),
+        }
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "group_id"):
+            result["groupId"] = self.group_id
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        if hasattr(self, "user_id"):
+            result["userId"] = self.user_id
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_group_id(self, value: str) -> AcceptGroupJoinRequestPublicV2:
+        self.group_id = value
+        return self
+
+    def with_namespace(self, value: str) -> AcceptGroupJoinRequestPublicV2:
+        self.namespace = value
+        return self
+
+    def with_user_id(self, value: str) -> AcceptGroupJoinRequestPublicV2:
+        self.user_id = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "group_id") and self.group_id:
+            result["groupId"] = str(self.group_id)
+        elif include_empty:
+            result["groupId"] = ""
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        if hasattr(self, "user_id") and self.user_id:
+            result["userId"] = str(self.user_id)
+        elif include_empty:
+            result["userId"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_200: Optional[ModelsMemberRequestGroupResponseV1] = None
+        error_400: Optional[ResponseErrorResponse] = None
+        error_401: Optional[ResponseErrorResponse] = None
+        error_403: Optional[ResponseErrorResponse] = None
+        error_404: Optional[ResponseErrorResponse] = None
+        error_409: Optional[ResponseErrorResponse] = None
+        error_500: Optional[ResponseErrorResponse] = None
+
+        def ok(self) -> AcceptGroupJoinRequestPublicV2.Response:
+            if self.error_400 is not None:
+                err = self.error_400.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_401 is not None:
+                err = self.error_401.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_403 is not None:
+                err = self.error_403.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_404 is not None:
+                err = self.error_404.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_409 is not None:
+                err = self.error_409.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_500 is not None:
+                err = self.error_500.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_200 is not None:
+                yield self.data_200
+                yield None
+            elif self.error_400 is not None:
+                yield None
+                yield self.error_400
+            elif self.error_401 is not None:
+                yield None
+                yield self.error_401
+            elif self.error_403 is not None:
+                yield None
+                yield self.error_403
+            elif self.error_404 is not None:
+                yield None
+                yield self.error_404
+            elif self.error_409 is not None:
+                yield None
+                yield self.error_409
+            elif self.error_500 is not None:
+                yield None
+                yield self.error_500
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        200: OK - ModelsMemberRequestGroupResponseV1 (OK)
+
+        400: Bad Request - ResponseErrorResponse (20002: validation error)
+
+        401: Unauthorized - ResponseErrorResponse (20001: unauthorized access)
+
+        403: Forbidden - ResponseErrorResponse (20022: token is not user token | 73036: insufficient member role permission)
+
+        404: Not Found - ResponseErrorResponse (73443: member request not found)
+
+        409: Conflict - ResponseErrorResponse (73442: user already joined in another group)
+
+        500: Internal Server Error - ResponseErrorResponse (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = AcceptGroupJoinRequestPublicV2.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 200:
+                result.data_200 = ModelsMemberRequestGroupResponseV1.create_from_dict(
+                    content
+                )
+            elif code == 400:
+                result.error_400 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_400.translate_to_api_error()
+            elif code == 401:
+                result.error_401 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_401.translate_to_api_error()
+            elif code == 403:
+                result.error_403 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_403.translate_to_api_error()
+            elif code == 404:
+                result.error_404 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_404.translate_to_api_error()
+            elif code == 409:
+                result.error_409 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_409.translate_to_api_error()
+            elif code == 500:
+                result.error_500 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_500.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[
+        Union[None, ModelsMemberRequestGroupResponseV1],
+        Union[None, HttpResponse, ResponseErrorResponse],
+    ]:
+        """Parse the given response.
+
+        200: OK - ModelsMemberRequestGroupResponseV1 (OK)
+
+        400: Bad Request - ResponseErrorResponse (20002: validation error)
+
+        401: Unauthorized - ResponseErrorResponse (20001: unauthorized access)
+
+        403: Forbidden - ResponseErrorResponse (20022: token is not user token | 73036: insufficient member role permission)
+
+        404: Not Found - ResponseErrorResponse (73443: member request not found)
+
+        409: Conflict - ResponseErrorResponse (73442: user already joined in another group)
+
+        500: Internal Server Error - ResponseErrorResponse (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 200:
+            return ModelsMemberRequestGroupResponseV1.create_from_dict(content), None
+        if code == 400:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 401:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 403:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 404:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 409:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 500:
+            return None, ResponseErrorResponse.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, group_id: str, namespace: str, user_id: str, **kwargs
+    ) -> AcceptGroupJoinRequestPublicV2:
+        instance = cls()
+        instance.group_id = group_id
+        instance.namespace = namespace
+        instance.user_id = user_id
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> AcceptGroupJoinRequestPublicV2:
+        instance = cls()
+        if "groupId" in dict_ and dict_["groupId"] is not None:
+            instance.group_id = str(dict_["groupId"])
+        elif include_empty:
+            instance.group_id = ""
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        if "userId" in dict_ and dict_["userId"] is not None:
+            instance.user_id = str(dict_["userId"])
+        elif include_empty:
+            instance.user_id = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "groupId": "group_id",
+            "namespace": "namespace",
+            "userId": "user_id",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "groupId": True,
+            "namespace": True,
+            "userId": True,
+        }
+
+    # endregion static methods
