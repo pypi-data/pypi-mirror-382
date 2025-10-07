10ed186f11c7f68bc5deaee38b969b7cf5d40aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 17:04:37 +0000

    Add package python-gmp/0.5.0a4/python_gmp-0.5.0a4.tar.gz

---
+++ packages/python-gmp/python_gmp-0.5.0a4.tar.gz/python_gmp-0.5.0a4/scripts/cibw_before_all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e -x
+
+GMP_VERSION=6.3.0
+
+PREFIX="$(pwd)/.local/"
+
+# -- build GMP --
+curl -s -O https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.xz
+tar -xf gmp-${GMP_VERSION}.tar.xz
+cd gmp-${GMP_VERSION}
+patch -N -Z -p1 < ../scripts/fat_build_fix.diff
+patch -N -Z -p0 < ../scripts/dll-importexport.diff
+patch -N -Z -p1 < ../scripts/gcc15.diff
+autoreconf -fi
+
+unset CFLAGS
+
+# config.guess uses microarchitecture and configfsf.guess doesn't
+# We replace config.guess with configfsf.guess to avoid microarchitecture
+# specific code in common code.
+rm config.guess && mv configfsf.guess config.guess && chmod +x config.guess
+./configure --enable-fat \
+            --enable-shared \
+            --disable-static \
+            --with-pic \
+            --disable-alloca \
+            --prefix=$PREFIX -q
+make -j6 -s
+make -s install
+cd ../
