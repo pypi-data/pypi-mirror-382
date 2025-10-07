f890ab08c80b4a63ec5e921ac48055d4306696c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:05:10 +0000

    Add package edq-lms-toolkit/1.0.0/edq_lms_toolkit-1.0.0.tar.gz

---
+++ packages/edq-lms-toolkit/edq_lms_toolkit-1.0.0.tar.gz/edq_lms_toolkit-1.0.0/lms/model/constants.py
@@ -0,0 +1,23 @@
+import typing
+
+import lms.util.net
+
+BACKEND_TYPE_CANVAS: str = 'canvas'
+
+BACKEND_TYPES: typing.List[str] = [
+    BACKEND_TYPE_CANVAS,
+]
+
+OUTPUT_FORMAT_JSON: str = 'json'
+OUTPUT_FORMAT_TABLE: str = 'table'
+OUTPUT_FORMAT_TEXT: str = 'text'
+
+OUTPUT_FORMATS: typing.List[str] = [
+    OUTPUT_FORMAT_JSON,
+    OUTPUT_FORMAT_TABLE,
+    OUTPUT_FORMAT_TEXT,
+]
+
+BACKEND_REQUEST_CLEANING_FUNCS: typing.Dict[str, typing.Callable] = {
+    BACKEND_TYPE_CANVAS: lms.util.net.clean_canvas_response,
+}
