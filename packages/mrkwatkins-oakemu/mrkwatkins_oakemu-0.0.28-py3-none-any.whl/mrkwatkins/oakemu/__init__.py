b3a30a1383802ae41299845346fe7858515dd13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 04:04:42 +0000

    Add package mrkwatkins-oakemu/0.0.28/mrkwatkins_oakemu-0.0.28-py3-none-any.whl

---
+++ packages/mrkwatkins-oakemu/mrkwatkins_oakemu-0.0.28-py3-none-any.whl/mrkwatkins/oakemu/__init__.py
@@ -0,0 +1,28 @@
+from os import path
+
+from pythonnet import load
+
+# Explicitly load all the necessary assemblies up front to help catch any errors early.
+assembles = [
+    "MrKWatkins.Ast",
+    "MrKWatkins.OakAsm",
+    "MrKWatkins.OakAsm.Disassembly",
+    "MrKWatkins.OakAsm.Disassembly.Z80",
+    "MrKWatkins.OakAsm.Formatting",
+    "MrKWatkins.OakAsm.IO",
+    "MrKWatkins.OakAsm.IO.ZXSpectrum",
+    "MrKWatkins.OakAsm.Z80",
+    "MrKWatkins.OakEmu",
+    "MrKWatkins.OakEmu.Cpus",
+    "MrKWatkins.OakEmu.Cpus.Z80",
+    "MrKWatkins.OakEmu.Machines.ZXSpectrum",
+]
+
+assemblies_path = path.join(path.dirname(__file__), "assemblies")
+
+load("coreclr")
+
+import clr  # noqa: E402
+
+for assembly in assembles:
+    clr.AddReference(path.join(assemblies_path, assembly))  # noqa
