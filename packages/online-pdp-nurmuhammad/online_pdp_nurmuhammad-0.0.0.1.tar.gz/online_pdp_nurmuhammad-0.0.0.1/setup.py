f74876e0ae24dd6d47da793c740b1595e8413146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:04:51 +0000

    Add package online-pdp-nurmuhammad/0.0.0.1/online_pdp_nurmuhammad-0.0.0.1.tar.gz

---
+++ packages/online-pdp-nurmuhammad/online_pdp_nurmuhammad-0.0.0.1.tar.gz/online_pdp_nurmuhammad-0.0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="online_pdp_nurmuhammad",
+    version="0.0.0.1",
+    author="nurmuhammad",
+    author_email="nurmuhammadhasanov0@gmail.com",
+    description="Online PDP learning",
+    packages=find_packages(),
+    requires=[],
+    license="MIT",
+)
