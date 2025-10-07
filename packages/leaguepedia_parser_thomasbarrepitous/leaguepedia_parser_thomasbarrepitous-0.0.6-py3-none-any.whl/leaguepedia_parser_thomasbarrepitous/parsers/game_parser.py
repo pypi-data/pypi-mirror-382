7dee34ee7341568c3c8018a084dd304be3cbafb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:45 +0000

    Add package leaguepedia_parser_thomasbarrepitous/0.0.6/leaguepedia_parser_thomasbarrepitous-0.0.6-py3-none-any.whl

---
+++ packages/leaguepedia_parser_thomasbarrepitous/leaguepedia_parser_thomasbarrepitous-0.0.6-py3-none-any.whl/leaguepedia_parser_thomasbarrepitous/parsers/game_parser.py
@@ -0,0 +1,169 @@
+from concurrent.futures.thread import ThreadPoolExecutor
+from typing import List, Optional
+
+from lol_dto.classes.game import LolGame
+from lol_dto.classes.game.lol_game import LolPickBan
+
+from leaguepedia_parser_thomasbarrepitous.site.leaguepedia import leaguepedia
+from leaguepedia_parser_thomasbarrepitous.transmuters.field_names import (
+    game_fields,
+    tournaments_fields,
+    game_players_fields,
+)
+from leaguepedia_parser_thomasbarrepitous.transmuters.game import transmute_game
+from leaguepedia_parser_thomasbarrepitous.transmuters.game_players import add_players
+from leaguepedia_parser_thomasbarrepitous.transmuters.picks_bans import (
+    picks_bans_fields,
+    transmute_picks_bans,
+)
+from leaguepedia_parser_thomasbarrepitous.transmuters.tournament import (
+    transmute_tournament,
+    LeaguepediaTournament,
+)
+
+
+def get_regions() -> List[str]:
+    """Returns a list of all regions that appear in the Tournaments table.
+
+    Returns:
+        The list of all region names, simply strings.
+    """
+    regions_dicts_list = leaguepedia.query(
+        tables="Tournaments", fields="Region", group_by="Region"
+    )
+
+    return [row["Region"] for row in regions_dicts_list]
+
+
+def get_tournaments(
+    region: str = None,
+    year: int = None,
+    tournament_level: str = "Primary",
+    is_playoffs: bool = None,
+    **kwargs,
+) -> List[LeaguepediaTournament]:
+    """Returns a list of tournaments.
+
+    Typical usage example:
+        get_tournaments('China', 2020)
+
+    Args:
+        region: Recommended to get it from get_tournament_regions().
+        year: Year to filter on. Defaults to None.
+        tournament_level: Primary, Secondary, Major, Secondary, Showmatch. Defaults to Primary.
+        is_playoffs: Can be used to filter between playoffs and regular season tournaments.
+
+    Returns:
+        A list of tournaments dictionaries.
+    """
+    # We need to cast is_playoffs as an integer for the cargoquery
+    if is_playoffs is not None:
+        is_playoffs = 1 if is_playoffs else 0
+
+    # This generates the WHERE part of the cargoquery
+    where_conditions = []
+    for field_name, value in [
+        ("Region", region),
+        ("Year", year),
+        ("TournamentLevel", tournament_level),
+        ("IsPlayoffs", is_playoffs),
+    ]:
+        # We don't filter on variables that are None
+        if value is not None:
+            # Escape single quotes by doubling them to prevent SQL injection
+            escaped_value = str(value).replace("'", "''")
+            where_conditions.append(f"Tournaments.{field_name}='{escaped_value}'")
+
+    where = " AND ".join(where_conditions)
+
+    result = leaguepedia.query(
+        tables="Tournaments, Leagues",
+        join_on="Tournaments.League = Leagues.League",
+        fields=f"Leagues.League_Short, {', '.join(f'Tournaments.{field}' for field in tournaments_fields)}",
+        where=where,
+        **kwargs,
+    )
+
+    return [transmute_tournament(tournament) for tournament in result]
+
+
+def get_games(tournament_overview_page=None, **kwargs) -> List[LolGame]:
+    """Returns the list of games played in a tournament.
+
+    Returns basic information about all games played in a tournament.
+
+    Args:
+        tournament_overview_page: tournament overview page, acquired from get_tournaments().
+
+    Returns:
+        A list of LolGame with basic game information.
+    """
+
+    games = leaguepedia.query(
+        tables="ScoreboardGames",
+        fields=", ".join(game_fields),
+        where=f"ScoreboardGames.OverviewPage ='{tournament_overview_page}'",
+        order_by="ScoreboardGames.DateTime_UTC",
+        **kwargs,
+    )
+
+    return [transmute_game(game) for game in games]
+
+
+def get_game_details(game: LolGame, add_page_id=False) -> LolGame:
+    # TODO Add more scoreboard information in this step
+    """Gets most game information available on Leaguepedia.
+
+    Args:
+        game: A LolGame with Leaguepedia IDs in its 'sources' dict.
+        add_page_id: whether or not to link the player page ID to their object. Mostly for debugging.
+
+    Returns:
+        The LolGame with all information available on Leaguepedia.
+    """
+    try:
+        assert game.sources.leaguepedia.gameId
+    except AssertionError:
+        raise ValueError(
+            f"Leaguepedia GameId not present in the input object, joins cannot be performed to get details"
+        )
+
+    with ThreadPoolExecutor() as executor:
+        picks_bans_future = executor.submit(_get_picks_bans, game)
+        game_future = executor.submit(_add_game_players, game, add_page_id)
+
+    game = game_future.result()
+    game.picksBans = picks_bans_future.result()
+
+    return game
+
+
+def _get_picks_bans(game: LolGame) -> Optional[List[LolPickBan]]:
+    """Returns the picks and bans for the game."""
+    # Double join as required by Leaguepedia
+    picks_bans = leaguepedia.query(
+        tables="PicksAndBansS7, ScoreboardGames",
+        join_on="PicksAndBansS7.GameId = ScoreboardGames.GameId",
+        fields=", ".join(picks_bans_fields),
+        where=f"ScoreboardGames.GameId = '{game.sources.leaguepedia.gameId}'",
+    )
+
+    if not picks_bans:
+        return None
+
+    return transmute_picks_bans(picks_bans[0])
+
+
+def _add_game_players(game: LolGame, add_page_id: bool) -> LolGame:
+    """Joins on PlayersRedirect to get all players information."""
+
+    players = leaguepedia.query(
+        tables=f"ScoreboardGames, ScoreboardPlayers, PlayerRedirects, Players",
+        join_on="ScoreboardGames.GameId = ScoreboardPlayers.GameId, "
+        "ScoreboardPlayers.Link = PlayerRedirects.AllName, "
+        "PlayerRedirects.OverviewPage = Players.OverviewPage",
+        fields=", ".join(game_players_fields) + ", Players._pageID=pageId",
+        where=f"ScoreboardGames.GameId = '{game.sources.leaguepedia.gameId}'",
+    )
+
+    return add_players(game, players, add_page_id=add_page_id)
---
+++ packages/leaguepedia_parser_thomasbarrepitous/leaguepedia_parser_thomasbarrepitous-0.0.6-py3-none-any.whl/leaguepedia_parser_thomasbarrepitous/transmuters/game.py
@@ -0,0 +1,125 @@
+import urllib.parse
+from dataclasses import dataclass
+from datetime import datetime, timezone
+
+import lol_id_tools as lit
+from lol_dto.classes.game import (
+    LolGame,
+    LolGamePlayer,
+    LolGameTeamEndOfGameStats,
+)
+from lol_dto.classes.sources.riot_lol_api import RiotGameSource
+
+from leaguepedia_parser_thomasbarrepitous.transmuters.game_players import (
+    LeaguepediaPlayerIdentifier,
+)
+
+
+@dataclass
+class LeaguepediaGameIdentifier:
+    gameId: str
+    matchId: str
+
+    matchHistoryUrl: str
+    overviewPage: str
+
+    tournamentName: str
+
+
+@dataclass
+class LeaguepediaTeamIdentifier:
+    name: str = None
+
+
+def transmute_game(source_dict: dict) -> LolGame:
+    """
+    Transforms a ScoreboardGames row into a LolGame
+
+    Some fields like team gold and kills are not present. Get_game_details should be used for it.
+    """
+
+    game = LolGame(
+        start=datetime.fromisoformat(source_dict["DateTime UTC"])
+        .replace(tzinfo=timezone.utc)
+        .isoformat(timespec="seconds"),
+        gameInSeries=int(source_dict["N GameInMatch"]),
+        patch=source_dict["Patch"],
+        duration=int(float(source_dict["Gamelength Number"] or 0) * 60),
+        vod=source_dict["VOD"],
+        winner="BLUE" if source_dict["Winner"] == "1" else "RED",
+    )
+
+    setattr(
+        game.sources,
+        "leaguepedia",
+        LeaguepediaGameIdentifier(
+            gameId=source_dict["GameId"],
+            matchId=source_dict["MatchId"],
+            matchHistoryUrl=source_dict["MatchHistory"],
+            overviewPage=source_dict["OverviewPage"],
+            tournamentName=source_dict["Tournament"],
+        ),
+    )
+
+    for team, idx in [(game.teams.BLUE, 1), (game.teams.RED, 2)]:
+        team.bans = [
+            lit.get_id(champion_name, object_type="champion")
+            for champion_name in source_dict[f"Team{idx}Bans"].split(",")
+        ]
+
+        team.endOfGameStats = LolGameTeamEndOfGameStats(
+            turretKills=int(source_dict[f"Team{idx}Towers"] or 0),
+            dragonKills=int(source_dict[f"Team{idx}Dragons"] or 0),
+            riftHeraldKills=int(source_dict[f"Team{idx}RiftHeralds"] or 0),
+            baronKills=int(source_dict[f"Team{idx}Barons"] or 0),
+        )
+
+        for player_idx, champion_name in enumerate(
+            source_dict[f"Team{idx}Picks"].split(",")
+        ):
+            player_object = LolGamePlayer(
+                championId=lit.get_id(champion_name, object_type="champion"),
+            )
+
+            setattr(
+                player_object.sources,
+                "leaguepedia",
+                LeaguepediaPlayerIdentifier(
+                    gameName=source_dict[f"Team{idx}Players"].split(",")[idx]
+                ),
+            )
+
+            team.players.append(player_object)
+
+        setattr(
+            team.sources,
+            "leaguepedia",
+            LeaguepediaTeamIdentifier(name=source_dict[f"Team{idx}"]),
+        )
+
+    # For Riot API games, I directly parse the URL for the game to have its actual identifiers.
+    if source_dict.get("MatchHistory") and "gameHash" in source_dict["MatchHistory"]:
+        parsed_url = urllib.parse.urlparse(
+            urllib.parse.urlparse(source_dict["MatchHistory"]).fragment
+        )
+
+        query = urllib.parse.parse_qs(parsed_url.query)
+        platform_id, game_id = parsed_url.path.split("/")[1:]
+        game_hash = query["gameHash"][0]
+
+        setattr(
+            game.sources,
+            "riotLolApi",
+            RiotGameSource(gameId=game_id, platformId=platform_id, gameHash=game_hash),
+        )
+    # For new tournaments, where the ID is directly input in the wiki instead of the match history URL.
+    elif not source_dict.get("MatchHistory") and source_dict.get("RiotPlatformGameId"):
+        platform_id, game_id = source_dict["RiotPlatformGameId"].split("_")
+
+        setattr(
+            game.sources,
+            "riotLolApi",
+            RiotGameSource(gameId=game_id, platformId=platform_id),
+        )
+
+    return game
