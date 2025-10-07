6d7eab01bd570f581d42a34d0015fb9eb999f263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:10 +0000

    Add package nurmuhammadown-package/0.0.0.1/nurmuhammadown_package-0.0.0.1.tar.gz

---
+++ packages/nurmuhammadown-package/nurmuhammadown_package-0.0.0.1.tar.gz/nurmuhammadown_package-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="nurmuhammadown-package",
+    version="0.0.0.1",
+    author="nurmuhammad",
+    author_email="nurmuhammadhasanov0@gmail.com",
+    description="Online PDP learning",
+    packages=find_packages(),
+    requires=[],
+    license="MIT",
+)
