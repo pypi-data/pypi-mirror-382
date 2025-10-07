fc3f6bfea218ea2184cc30fdce138cb14eaabf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:03 +0000

    Add package dbnd-snowflake/1.0.30.1/dbnd_snowflake-1.0.30.1.tar.gz

---
+++ packages/dbnd-snowflake/dbnd_snowflake-1.0.30.1.tar.gz/dbnd_snowflake-1.0.30.1/setup.py
@@ -0,0 +1,31 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import os
+
+import setuptools
+
+from setuptools.config import read_configuration
+
+
+BASE_PATH = os.path.dirname(__file__)
+CFG_PATH = os.path.join(BASE_PATH, "setup.cfg")
+
+config = read_configuration(CFG_PATH)
+version = config["metadata"]["version"]
+
+setuptools.setup(
+    name="dbnd-snowflake",
+    package_dir={"": "src"},
+    install_requires=["sqlparse", "dbnd==" + version],
+    extras_require={
+        "tests": [
+            'pandas==1.3.5;python_version<"3.8"',  # M1 is supported
+            'pandas==1.4.0;python_version>="3.8" and python_version<"3.12"',
+            'pandas==2.2.2;python_version>="3.12"',
+            'numpy==1.21.6;python_version<"3.8"',  # M1 is supported
+            'numpy==1.22.4;python_version>="3.8" and python_version<"3.12"',
+            'numpy==1.26.4;python_version>="3.12"',
+            "snowflake-connector-python",
+        ]
+    },
+)
