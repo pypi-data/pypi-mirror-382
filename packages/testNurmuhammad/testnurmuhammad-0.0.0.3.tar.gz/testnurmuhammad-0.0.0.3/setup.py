f68a118bd22d2a8b639699b4f09c33805b5773f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:04:34 +0000

    Add package testNurmuhammad/0.0.0.3/testnurmuhammad-0.0.0.3.tar.gz

---
+++ packages/testNurmuhammad/testnurmuhammad-0.0.0.3.tar.gz/testnurmuhammad-0.0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="testNurmuhammad",
+    version="0.0.0.3",
+    py_modules=["core"],  
+    install_requires=[],
+    author="Nurmuhammad Hasanov",
+    author_email="nurmuhammad@example.com",
+    description="My example package",
+)
