9598073911c06cfac0e7e9c14b3424ec6ea74a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:28 +0000

    Add package accelbyte-py-sdk-service-reporting/0.14.0/accelbyte_py_sdk_service_reporting-0.14.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-reporting/accelbyte_py_sdk_service_reporting-0.14.0.tar.gz/accelbyte_py_sdk_service_reporting-0.14.0/accelbyte_py_sdk/api/reporting/operations/admin_configurations/get.py
@@ -0,0 +1,344 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Reporting Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import StrEnum
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import RestapiConfigResponse
+from ...models import RestapiErrorResponse
+
+
+class CategoryEnum(StrEnum):
+    ALL = "all"
+    EXTENSION = "extension"
+
+
+class Get(Operation):
+    """Get configuration (Get)
+
+    TimeInterval is in nanoseconds.
+    When there's no configuration set, the response is the default value (configurable through envar).
+
+    Properties:
+        url: /reporting/v1/admin/namespaces/{namespace}/configurations
+
+        method: GET
+
+        tags: ["Admin Configurations"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        namespace: (namespace) REQUIRED str in path
+
+        category: (category) OPTIONAL Union[str, CategoryEnum] in query
+
+    Responses:
+        200: OK - RestapiConfigResponse
+
+        500: Internal Server Error - RestapiErrorResponse
+    """
+
+    # region fields
+
+    _url: str = "/reporting/v1/admin/namespaces/{namespace}/configurations"
+    _path: str = "/reporting/v1/admin/namespaces/{namespace}/configurations"
+    _base_path: str = ""
+    _method: str = "GET"
+    _consumes: List[str] = ["application/json"]
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "reporting"
+
+    namespace: str  # REQUIRED in [path]
+    category: Union[str, CategoryEnum]  # OPTIONAL in [query]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "path": self.get_path_params(),
+            "query": self.get_query_params(),
+        }
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    def get_query_params(self) -> dict:
+        result = {}
+        if hasattr(self, "category"):
+            result["category"] = self.category
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_namespace(self, value: str) -> Get:
+        self.namespace = value
+        return self
+
+    def with_category(self, value: Union[str, CategoryEnum]) -> Get:
+        self.category = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        if hasattr(self, "category") and self.category:
+            result["category"] = str(self.category)
+        elif include_empty:
+            result["category"] = Union[str, CategoryEnum]()
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_200: Optional[RestapiConfigResponse] = None
+        error_500: Optional[RestapiErrorResponse] = None
+
+        def ok(self) -> Get.Response:
+            if self.error_500 is not None:
+                err = self.error_500.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_200 is not None:
+                yield self.data_200
+                yield None
+            elif self.error_500 is not None:
+                yield None
+                yield self.error_500
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        200: OK - RestapiConfigResponse
+
+        500: Internal Server Error - RestapiErrorResponse
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = Get.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 200:
+                result.data_200 = RestapiConfigResponse.create_from_dict(content)
+            elif code == 500:
+                result.error_500 = RestapiErrorResponse.create_from_dict(content)
+                result.error = result.error_500.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[
+        Union[None, RestapiConfigResponse],
+        Union[None, HttpResponse, RestapiErrorResponse],
+    ]:
+        """Parse the given response.
+
+        200: OK - RestapiConfigResponse
+
+        500: Internal Server Error - RestapiErrorResponse
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 200:
+            return RestapiConfigResponse.create_from_dict(content), None
+        if code == 500:
+            return None, RestapiErrorResponse.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls,
+        namespace: str,
+        category: Optional[Union[str, CategoryEnum]] = None,
+        **kwargs,
+    ) -> Get:
+        instance = cls()
+        instance.namespace = namespace
+        if category is not None:
+            instance.category = category
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(cls, dict_: dict, include_empty: bool = False) -> Get:
+        instance = cls()
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        if "category" in dict_ and dict_["category"] is not None:
+            instance.category = str(dict_["category"])
+        elif include_empty:
+            instance.category = Union[str, CategoryEnum]()
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "namespace": "namespace",
+            "category": "category",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "namespace": True,
+            "category": False,
+        }
+
+    @staticmethod
+    def get_enum_map() -> Dict[str, List[Any]]:
+        return {
+            "category": ["all", "extension"],  # in query
+        }
+
+    # endregion static methods
