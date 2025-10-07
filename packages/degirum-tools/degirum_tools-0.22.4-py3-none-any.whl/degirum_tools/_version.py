2ebb7e62c381db253b9873db04fd573477b7a2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:13 +0000

    Add package degirum-tools/0.22.4/degirum_tools-0.22.4-py3-none-any.whl

---
+++ packages/degirum-tools/degirum_tools-0.22.4-py3-none-any.whl/degirum_tools/_version.py
@@ -0,0 +1,11 @@
+#
+# _version.py: degirum_tools package version
+#
+# Copyright DeGirum Corporation 2025
+# All rights reserved
+#
+
+# >>> increment version here vvv
+__version_info__ = ("0", "22", "4")
+__version__ = ".".join(__version_info__)
+
