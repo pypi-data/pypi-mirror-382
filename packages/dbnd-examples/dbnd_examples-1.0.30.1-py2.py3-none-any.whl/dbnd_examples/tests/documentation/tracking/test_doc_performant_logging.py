7b1e55d63b625494da0e46f1c7ff765b81cdc8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:14 +0000

    Add package dbnd-examples/1.0.30.1/dbnd_examples-1.0.30.1-py2.py3-none-any.whl

---
+++ packages/dbnd-examples/dbnd_examples-1.0.30.1-py2.py3-none-any.whl/dbnd_examples/tests/documentation/tracking/test_doc_performant_logging.py
@@ -0,0 +1,17 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+from dbnd import parameter
+from dbnd._core.settings.tracking_config import ValueTrackingLevel
+
+
+class TestDocPerformantLogging:
+    def test_doc(self):
+        #### DOC START
+        value_reporting_strategy = parameter(
+            default=ValueTrackingLevel.SMART,
+            description="Multiple strategies with different limitations on potentially expensive calculation for value_meta."
+            "ALL => no limitations."
+            "SMART (default) => restrictions on lazy evaluation types."
+            "NONE => limit everything.",
+        ).enum(ValueTrackingLevel)
+        #### DOC END
---
+++ packages/dbnd-examples/dbnd_examples-1.0.30.1-py2.py3-none-any.whl/dbnd_examples/tracking/tracking_mlflow.py
@@ -0,0 +1,61 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import datetime
+import logging
+import os
+
+from random import randint, random
+
+from mlflow import (
+    active_run,
+    end_run,
+    get_tracking_uri,
+    log_artifacts,
+    log_metric,
+    log_param,
+    start_run,
+)
+from mlflow.tracking import MlflowClient
+
+from dbnd import task
+from dbnd_mlflow.mlflow_with_dbnd_tracking import enable_dbnd_for_mlflow_tracking
+
+
+logger = logging.getLogger(__name__)
+
+
+@task
+def task_with_mflow(check_time: datetime.datetime = datetime.datetime.now()) -> str:
+    enable_dbnd_for_mlflow_tracking()
+    logger.info("Running MLFlow tracking integration check with mlflow enabled!")
+    logger.info("MLFlow tracking URI: {}".format(get_tracking_uri()))
+
+    start_run()
+
+    # params
+    log_param("param1", randint(0, 100))
+    log_param("param2", randint(0, 100))
+
+    # metrics
+    log_metric("foo1", random())
+    log_metric("foo1", random() + 1)
+    log_metric("foo2", random())
+    log_metric("foo2", random() + 1)
+
+    # artifacts
+    os.makedirs("outputs", exist_ok=True)
+    with open("outputs/test1.txt", "w") as f1, open("outputs/test2.txt", "w") as f2:
+        f1.write("hello")
+        f2.write("world!")
+    log_artifacts("outputs")
+
+    # Get run metadata & data from the tracking server
+    service = MlflowClient()
+    run_id = active_run().info.run_id
+    run = service.get_run(run_id)
+    logger.info("Metadata & data for run with UUID %s: %s" % (run_id, run))
+
+    end_run()
+
+    logger.info("MLFlow tracking integration check completed!")
+    return ""
