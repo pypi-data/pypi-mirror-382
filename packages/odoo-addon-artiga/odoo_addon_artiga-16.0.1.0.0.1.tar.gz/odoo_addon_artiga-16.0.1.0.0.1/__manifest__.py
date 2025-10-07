2344b43c7a54036dcb6f4a1cf2e1afb7be5da102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:04:58 +0000

    Add package odoo-addon-artiga/16.0.1.0.0.1/odoo_addon_artiga-16.0.1.0.0.1.tar.gz

---
+++ packages/odoo-addon-artiga/odoo_addon_artiga-16.0.1.0.0.1.tar.gz/odoo_addon_artiga-16.0.1.0.0.1/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Odoo customizations for Artiga",
+    "version": "16.0.1.0.0",
+    "depends": ["l10n_es_pos"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Project Management",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Odoo customizations for Artiga.
+    """,
+    "data": [],
+    "qweb": ["static/src/xml/pos.xml"],
+    "installable": True,
+}
