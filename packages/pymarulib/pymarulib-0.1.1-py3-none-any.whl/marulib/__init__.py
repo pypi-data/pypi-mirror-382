2be8d3c3354a25932d8d9b2ed9f183e57c97632d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:04:41 +0000

    Add package pymarulib/0.1.1/pymarulib-0.1.1-py3-none-any.whl

---
+++ packages/pymarulib/pymarulib-0.1.1-py3-none-any.whl/marulib/__init__.py
@@ -0,0 +1,13 @@
+"""
+Maru's Personal Utility Library (marulib)
+"""
+
+# Import the Logger class from the async_logger module.
+from .async_logger import Logger
+
+# Define what gets imported with 'from marulib import *'
+__all__ = ['Logger']
+
+
+__version__ = "0.1.0"
+
