600c3c14eda3876f91c6af7e23b7a2b9230acdda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:24 +0000

    Add package unique_stock_ticker/1.0.1/unique_stock_ticker-1.0.1.tar.gz

---
+++ packages/unique_stock_ticker/unique_stock_ticker-1.0.1.tar.gz/unique_stock_ticker-1.0.1/unique_stock_ticker/detection/config.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from pydantic import BaseModel, Field
+from unique_toolkit._common.default_language_model import DEFAULT_GPT_4o
+from unique_toolkit._common.validators import LMI
+from unique_toolkit.agentic.tools.config import get_configuration_dict
+from unique_toolkit.language_model.infos import LanguageModelInfo
+
+from unique_stock_ticker.detection.memory import StockTickerMemoryConfig
+
+
+class StockTickerDetectionConfig(BaseModel):
+    model_config = get_configuration_dict()
+    language_model: LMI = LanguageModelInfo.from_name(
+        DEFAULT_GPT_4o,
+    )
+    additional_llm_options: dict[str, Any] = Field(
+        default={},
+        description="Additional options to pass to the language model.",
+    )
+    memory_config: StockTickerMemoryConfig = StockTickerMemoryConfig()
