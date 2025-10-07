6d3c24382a2298595a85bd7195d73f7b550943be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:48 +0000

    Add package connector-py/4.136.0/connector_py-4.136.0.tar.gz

---
+++ packages/connector-py/connector_py-4.136.0.tar.gz/connector_py-4.136.0/connector/scaffold/templates/tests/common_mock_data.py
@@ -0,0 +1,11 @@
+from connector.generated import AuthCredential, OAuthCredential
+from {name}.settings import {pascal}Settings
+
+VALID_AUTH = AuthCredential(
+    oauth=OAuthCredential(access_token="valid"),  # noqa: S105
+)
+INVALID_AUTH = AuthCredential(
+    oauth=OAuthCredential(access_token="invalid"),  # noqa: S105
+)
+SETTINGS = {pascal}Settings(account_id="test-account-id").model_dump()
+TEST_MAX_PAGE_SIZE = 100
