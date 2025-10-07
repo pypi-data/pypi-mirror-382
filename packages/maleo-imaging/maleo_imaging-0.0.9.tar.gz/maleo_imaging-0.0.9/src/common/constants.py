7a18e5dd23262478066a91755bab6b848ba9e899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:11 +0000

    Add package maleo-imaging/0.0.9/maleo_imaging-0.0.9.tar.gz

---
+++ packages/maleo-imaging/maleo_imaging-0.0.9.tar.gz/maleo_imaging-0.0.9/src/common/constants.py
@@ -0,0 +1,7 @@
+from maleo.schemas.resource import Resource, ResourceIdentifier
+
+
+RESOURCE = Resource(
+    identifiers=[ResourceIdentifier(key="xray", name="XRay", slug="xray")],
+    details=None,
+)
