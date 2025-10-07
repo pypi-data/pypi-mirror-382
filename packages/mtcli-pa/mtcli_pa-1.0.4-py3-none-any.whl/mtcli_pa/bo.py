0a28f87b36aab9085753c7291bfb7810b7b44262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 14:04:30 +0000

    Add package mtcli-pa/1.0.4/mtcli_pa-1.0.4-py3-none-any.whl

---
+++ packages/mtcli-pa/mtcli_pa-1.0.4-py3-none-any.whl/mtcli_pa/bo.py
@@ -0,0 +1,38 @@
+"""
+Calcula pullback e projeções de rompimento
+"""
+
+import click
+
+
+@click.command()
+@click.argument("final", type=float)
+@click.argument("inicio", type=float)
+@click.option(
+    "--digitos",
+    "-d",
+    type=int,
+    default=0,
+    envvar="DIGITOS",
+    help="Digitos apos o separador decimal",
+)
+def bo(final, inicio, digitos):
+    """Calcula pullbacks e projecoes de rompimento"""
+    tamanho = final - inicio
+    click.echo("%.{0}f pt".format(digitos) % abs(tamanho))
+    projecao1 = final + tamanho
+    projecao2 = final + tamanho * 2
+    #    click.echo("%.{0}f 2X".format(digits) % projecao2)
+    click.echo("%.{0}f 1X".format(digitos) % projecao1)
+    click.echo("%.{0}f".format(digitos) % final)
+    niveis = [0.33, 0.50, 0.66]
+    pullback = []
+    i = 0
+    for n in niveis:
+        pullback.append(final - tamanho * n)
+        click.echo("%.{0}f %.2f".format(digitos) % (pullback[i], n))
+        i += 1
+
+
+if __name__ == "__main__":
+    bo()
