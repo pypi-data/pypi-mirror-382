27779cacbbd54510433437c7b93e1a6a400b39df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:22 +0000

    Add package nurmuhammadown-package2/0.0.0.3/nurmuhammadown_package2-0.0.0.3.tar.gz

---
+++ packages/nurmuhammadown-package2/nurmuhammadown_package2-0.0.0.3.tar.gz/nurmuhammadown_package2-0.0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nurmuhammadown_package2",
+    version="0.0.0.3",
+    py_modules=["example"],  
+    install_requires=[],
+    author="Nurmuhammad Hasanov",
+    author_email="nurmuhammad@example.com",
+    description="My example package",
+)
