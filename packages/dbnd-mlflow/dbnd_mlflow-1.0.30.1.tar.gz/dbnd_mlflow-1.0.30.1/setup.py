74d50dbc049f3325feaa113f13a40bb56282804f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:15 +0000

    Add package dbnd-mlflow/1.0.30.1/dbnd_mlflow-1.0.30.1.tar.gz

---
+++ packages/dbnd-mlflow/dbnd_mlflow-1.0.30.1.tar.gz/dbnd_mlflow-1.0.30.1/setup.py
@@ -0,0 +1,29 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+from os import path
+
+import setuptools
+
+from setuptools.config import read_configuration
+
+
+BASE_PATH = path.dirname(__file__)
+CFG_PATH = path.join(BASE_PATH, "setup.cfg")
+
+config = read_configuration(CFG_PATH)
+version = config["metadata"]["version"]
+
+setuptools.setup(
+    name="dbnd-mlflow",
+    package_dir={"": "src"},
+    install_requires=["dbnd==" + version, "six"],
+    extras_require=dict(tests=[]),
+    entry_points={
+        "mlflow.tracking_store": [
+            "dbnd = dbnd_mlflow.tracking_store:get_dbnd_store",
+            "dbnd+s = dbnd_mlflow.tracking_store:get_dbnd_store",
+            "databand = dbnd_mlflow.tracking_store:get_dbnd_store",
+            "databand+s = dbnd_mlflow.tracking_store:get_dbnd_store",
+        ]
+    },
+)
