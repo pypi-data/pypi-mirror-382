9b718a8d94289a06912d15f18e5be7e3078fc39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 03:14:16 +0000

    Add package Geode-SimplexGeosciences/3.2.1rc1/geode_simplexgeosciences-3.2.1rc1-cp39-cp39-win_amd64.whl

---
+++ packages/Geode-SimplexGeosciences/geode_simplexgeosciences-3.2.1rc1-cp39-cp39-win_amd64.whl/geode_simplexgeosciences/model.pyi
@@ -0,0 +1,7 @@
+from __future__ import annotations
+import geode_simplex as geode_simplex
+from geode_simplexgeosciences.bin.geode_simplexgeosciences_py_model import SimplexGeosciencesModelLibrary
+from geode_simplexgeosciences.bin.geode_simplexgeosciences_py_model import structural_model_simplex_remesh
+import opengeode as opengeode
+import opengeode_geosciences as opengeode_geosciences
+__all__: list[str] = ['SimplexGeosciencesModelLibrary', 'geode_simplex', 'opengeode', 'opengeode_geosciences', 'structural_model_simplex_remesh']
