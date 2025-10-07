4d60aeaa5f4d56a32323ef03eebce36c71d23177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 15:05:35 +0000

    Add package vellum-ai/1.7.3/vellum_ai-1.7.3-py3-none-any.whl

---
+++ packages/vellum-ai/vellum_ai-1.7.3-py3-none-any.whl/vellum/workflows/types/core.py
@@ -0,0 +1,45 @@
+from enum import Enum
+from threading import Event as ThreadingEvent
+from typing import (  # type: ignore[attr-defined]
+    Any,
+    Dict,
+    List,
+    Union,
+    _GenericAlias,
+    _SpecialGenericAlias,
+    _UnionGenericAlias,
+)
+
+from vellum.client.core.pydantic_utilities import UniversalBaseModel
+
+JsonArray = List["Json"]
+JsonObject = Dict[str, "Json"]
+Json = Union[None, bool, int, float, str, JsonArray, JsonObject]
+
+CancelSignal = ThreadingEvent
+
+# Unions and Generics inherit from `_GenericAlias` instead of `type`
+# In future versions of python, we'll see `_UnionGenericAlias`
+UnderGenericAlias = _GenericAlias
+SpecialGenericAlias = _SpecialGenericAlias
+UnionGenericAlias = _UnionGenericAlias
+
+
+class VellumSecret(UniversalBaseModel):
+    name: str
+
+
+EntityInputsInterface = Dict[str, Any]
+
+
+class MergeBehavior(Enum):
+    AWAIT_ALL = "AWAIT_ALL"
+    AWAIT_ANY = "AWAIT_ANY"
+    AWAIT_ATTRIBUTES = "AWAIT_ATTRIBUTES"
+    CUSTOM = "CUSTOM"
+
+
+class ConditionType(Enum):
+    IF = "IF"
+    ELIF = "ELIF"
+    ELSE = "ELSE"
