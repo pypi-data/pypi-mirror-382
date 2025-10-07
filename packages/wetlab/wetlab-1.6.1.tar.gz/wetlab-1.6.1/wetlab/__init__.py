71a6690dc3e5d1203deb94207d8b6401b30ca077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-07 16:04:33 +0000

    Add package wetlab/1.6.1/wetlab-1.6.1.tar.gz

---
+++ packages/wetlab/wetlab-1.6.1.tar.gz/wetlab-1.6.1/wetlab/__init__.py
@@ -0,0 +1,97 @@
+"""Basic wetlab entities [`source <https://github.com/laminlabs/wetlab/blob/main/wetlab/models.py>`__].
+
+Install and mount `wetlab` in a new instance:
+
+>>> pip install wetlab
+>>> lamin init --storage ./test-wetlab --modules bionty,wetlab
+
+Import the package:
+
+>>> import wetlab as wl
+
+Create records:
+
+>>> biosample = wl.Biosample(name="Sample 1").save()
+
+Basic registries:
+
+.. autosummary::
+   :toctree: .
+
+   Experiment
+   Biosample
+   Techsample
+   Donor
+
+Perturbation registries:
+
+.. autosummary::
+   :toctree: .
+
+   GeneticPerturbation
+   Biologic
+   Compound
+   CompoundPerturbation
+   EnvironmentalPerturbation
+   CombinationPerturbation
+
+Auxiliary registries:
+
+.. autosummary::
+   :toctree: .
+
+   Well
+   PerturbationTarget
+
+Types:
+
+.. autosummary::
+   :toctree: .
+
+   GeneticPerturbationSystem
+   BiologicType
+
+"""
+
+__version__ = "1.6.1"
+
+from lamindb_setup import _check_instance_setup
+
+from .types import BiologicType, GeneticPerturbationSystem
+
+_check_instance_setup(from_module="wetlab")
+
+
+from .models import (
+    Biologic,
+    Biosample,
+    CombinationPerturbation,
+    Compound,
+    CompoundPerturbation,
+    Donor,
+    EnvironmentalPerturbation,
+    Experiment,
+    GeneticPerturbation,
+    PerturbationTarget,
+    Techsample,
+    Well,
+)
+
+__all__ = [
+    # registries
+    "Biologic",
+    "Biosample",
+    "CombinationPerturbation",
+    "Compound",
+    "CompoundPerturbation",
+    "Donor",
+    "EnvironmentalPerturbation",
+    "Experiment",
+    "GeneticPerturbation",
+    "PerturbationTarget",
+    "Techsample",
+    "Well",
+    # helper types
+    "BiologicType",
+    "GeneticPerturbationSystem",
+]
