a83d292b4fa646e9b81fa9dd15e52577a843511b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:04:49 +0000

    Add package rft1d/0.2.5/rft1d-0.2.5.tar.gz

---
+++ packages/rft1d/rft1d-0.2.5.tar.gz/rft1d-0.2.5/src/rft1d/__init__.py
@@ -0,0 +1,19 @@
+
+__version__ = '0.2.5'  # 2025-10-07
+
+__all__ = ['data', 'distributions', 'geom', 'prob', 'random']
+
+from . import data
+from . import distributions
+from . import geom
+from . import prob
+from . import random
+
+randn1d      = random.randn1d
+multirandn1d = random.multirandn1d
+
+chi2         = distributions.chi2
+f            = distributions.f
+norm         = distributions.norm
+t            = distributions.t
+T2           = distributions.T2
