7accd64204204846afc48c2ab5cedebc2f49b7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 15:06:39 +0000

    Add package vercel-sdk/0.0.5/vercel_sdk-0.0.5-py3-none-any.whl

---
+++ packages/vercel-sdk/vercel_sdk-0.0.5-py3-none-any.whl/vercel/oidc/__init__.py
@@ -0,0 +1,18 @@
+from .token import (
+    VercelOidcTokenError,
+    get_vercel_oidc_token,
+    get_vercel_oidc_token_sync,
+)
+from .credentials import Credentials, get_credentials
+from .token_util import get_token_payload
+from .token import decode_oidc_payload
+
+__all__ = [
+    "VercelOidcTokenError",
+    "get_vercel_oidc_token",
+    "get_vercel_oidc_token_sync",
+    "get_token_payload",
+    "Credentials",
+    "get_credentials",
+    "decode_oidc_payload",
+]
