87433294df776d4a9fd7984e0f9e96eb22e69f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:12 +0000

    Add package Class163-NexT/0.3.10/class163_next-0.3.10-py3-none-any.whl

---
+++ packages/Class163-NexT/class163_next-0.3.10-py3-none-any.whl/Class163_NexT/__init__.py
@@ -0,0 +1,17 @@
+"""
+Class163_NexT - 网易云音乐下载API库
+"""
+
+# 导入主要模块
+from .models import Music, Playlist, Class163
+from .utils import safe_run, selenium_login, playwright_login
+
+__version__ = "0.3.10"
+__all__ = [
+    "Music", 
+    "Playlist", 
+    "Class163",
+    "safe_run",
+    "selenium_login",
+    "playwright_login"
+]
