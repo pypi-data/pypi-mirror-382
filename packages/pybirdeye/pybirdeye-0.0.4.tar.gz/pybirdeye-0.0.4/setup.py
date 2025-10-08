2e28295aa94a7aae584a9585c514ae3e53da7940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:05:14 +0000

    Add package pybirdeye/0.0.4/pybirdeye-0.0.4.tar.gz

---
+++ packages/pybirdeye/pybirdeye-0.0.4.tar.gz/pybirdeye-0.0.4/setup.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+from setuptools import setup, find_packages
+
+# Remove build and dist directories if they exist
+for dir_name in ['build', 'dist']:
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+
+setup(
+    name='pybirdeye',
+    version='0.0.4',
+    packages=find_packages('.'),
+    install_requires=[
+        'httpx',
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dwdwow/birdeye-python',
+)
