f8f392bf303260cbb0d892444c40a4af0b3fe1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:14:54 +0000

    Add package accelbyte-py-sdk-service-achievement/0.23.0/accelbyte_py_sdk_service_achievement-0.23.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-achievement/accelbyte_py_sdk_service_achievement-0.23.0-py3-none-any.whl/accelbyte_py_sdk/api/achievement/operations/anonymization/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Achievement Service."""
+
+__version__ = "2.27.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .admin_anonymize_user_ac_c61ab2 import AdminAnonymizeUserAchievement
