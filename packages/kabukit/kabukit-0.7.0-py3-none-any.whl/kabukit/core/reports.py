ef79b6170604bda735f41fa23b860c37ce45565c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 14:08:04 +0000

    Add package kabukit/0.7.0/kabukit-0.7.0-py3-none-any.whl

---
+++ packages/kabukit/kabukit-0.7.0-py3-none-any.whl/kabukit/core/reports.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from polars import DataFrame
+
+
+class Reports(Base):
+    pass
