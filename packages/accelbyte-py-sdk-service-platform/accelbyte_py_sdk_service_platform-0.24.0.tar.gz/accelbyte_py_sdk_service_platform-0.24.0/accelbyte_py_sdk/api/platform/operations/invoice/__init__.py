ada64a597540fabef1b0b38fdb82f539045ad99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:15:49 +0000

    Add package accelbyte-py-sdk-service-platform/0.24.0/accelbyte_py_sdk_service_platform-0.24.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-platform/accelbyte_py_sdk_service_platform-0.24.0.tar.gz/accelbyte_py_sdk_service_platform-0.24.0/accelbyte_py_sdk/api/platform/operations/invoice/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Platform Service."""
+
+__version__ = "6.7.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .download_invoice_details import DownloadInvoiceDetails
+from .download_invoice_details import (
+    ItemTypeEnum as DownloadInvoiceDetailsItemTypeEnum,
+)
+from .generate_invoice_summary import GenerateInvoiceSummary
+from .generate_invoice_summary import (
+    ItemTypeEnum as GenerateInvoiceSummaryItemTypeEnum,
+)
---
+++ packages/accelbyte-py-sdk-service-platform/accelbyte_py_sdk_service_platform-0.24.0.tar.gz/accelbyte_py_sdk_service_platform-0.24.0/accelbyte_py_sdk/api/platform/operations/wallet/enable_user_wallet.py
@@ -0,0 +1,360 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Platform Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ErrorEntity
+
+
+class EnableUserWallet(Operation):
+    """[DEPRECATED] Enable a user wallet (enableUserWallet)
+
+    enable a user wallet.
+
+    Properties:
+        url: /platform/admin/namespaces/{namespace}/users/{userId}/wallets/{walletId}/enable
+
+        method: PUT
+
+        tags: ["Wallet"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        namespace: (namespace) REQUIRED str in path
+
+        user_id: (userId) REQUIRED str in path
+
+        wallet_id: (walletId) REQUIRED str in path
+
+    Responses:
+        204: No Content - (Successful operation)
+
+        404: Not Found - ErrorEntity (35141: Wallet [{walletId}] does not exist)
+
+        409: Conflict - ErrorEntity (20006: optimistic lock)
+    """
+
+    # region fields
+
+    _url: str = "/platform/admin/namespaces/{namespace}/users/{userId}/wallets/{walletId}/enable"
+    _path: str = "/platform/admin/namespaces/{namespace}/users/{userId}/wallets/{walletId}/enable"
+    _base_path: str = ""
+    _method: str = "PUT"
+    _consumes: List[str] = ["application/json"]
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "platform"
+
+    namespace: str  # REQUIRED in [path]
+    user_id: str  # REQUIRED in [path]
+    wallet_id: str  # REQUIRED in [path]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "path": self.get_path_params(),
+        }
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        if hasattr(self, "user_id"):
+            result["userId"] = self.user_id
+        if hasattr(self, "wallet_id"):
+            result["walletId"] = self.wallet_id
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_namespace(self, value: str) -> EnableUserWallet:
+        self.namespace = value
+        return self
+
+    def with_user_id(self, value: str) -> EnableUserWallet:
+        self.user_id = value
+        return self
+
+    def with_wallet_id(self, value: str) -> EnableUserWallet:
+        self.wallet_id = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        if hasattr(self, "user_id") and self.user_id:
+            result["userId"] = str(self.user_id)
+        elif include_empty:
+            result["userId"] = ""
+        if hasattr(self, "wallet_id") and self.wallet_id:
+            result["walletId"] = str(self.wallet_id)
+        elif include_empty:
+            result["walletId"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_204: Optional[HttpResponse] = None
+        error_404: Optional[ErrorEntity] = None
+        error_409: Optional[ErrorEntity] = None
+
+        def ok(self) -> EnableUserWallet.Response:
+            if self.error_404 is not None:
+                err = self.error_404.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_409 is not None:
+                err = self.error_409.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_204 is not None:
+                yield self.data_204
+                yield None
+            elif self.error_404 is not None:
+                yield None
+                yield self.error_404
+            elif self.error_409 is not None:
+                yield None
+                yield self.error_409
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        204: No Content - (Successful operation)
+
+        404: Not Found - ErrorEntity (35141: Wallet [{walletId}] does not exist)
+
+        409: Conflict - ErrorEntity (20006: optimistic lock)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = EnableUserWallet.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 204:
+                result.data_204 = None
+            elif code == 404:
+                result.error_404 = ErrorEntity.create_from_dict(content)
+                result.error = result.error_404.translate_to_api_error()
+            elif code == 409:
+                result.error_409 = ErrorEntity.create_from_dict(content)
+                result.error = result.error_409.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[None, Union[None, ErrorEntity, HttpResponse]]:
+        """Parse the given response.
+
+        204: No Content - (Successful operation)
+
+        404: Not Found - ErrorEntity (35141: Wallet [{walletId}] does not exist)
+
+        409: Conflict - ErrorEntity (20006: optimistic lock)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 204:
+            return None, None
+        if code == 404:
+            return None, ErrorEntity.create_from_dict(content)
+        if code == 409:
+            return None, ErrorEntity.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, namespace: str, user_id: str, wallet_id: str, **kwargs
+    ) -> EnableUserWallet:
+        instance = cls()
+        instance.namespace = namespace
+        instance.user_id = user_id
+        instance.wallet_id = wallet_id
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> EnableUserWallet:
+        instance = cls()
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        if "userId" in dict_ and dict_["userId"] is not None:
+            instance.user_id = str(dict_["userId"])
+        elif include_empty:
+            instance.user_id = ""
+        if "walletId" in dict_ and dict_["walletId"] is not None:
+            instance.wallet_id = str(dict_["walletId"])
+        elif include_empty:
+            instance.wallet_id = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "namespace": "namespace",
+            "userId": "user_id",
+            "walletId": "wallet_id",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "namespace": True,
+            "userId": True,
+            "walletId": True,
+        }
+
+    # endregion static methods
