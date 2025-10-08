898929051e6723ba5cd662b79d25293d4b2d0460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:28 +0000

    Add package data-ingestion-sdk/0.4.8/data_ingestion_sdk-0.4.8.tar.gz

---
+++ packages/data-ingestion-sdk/data_ingestion_sdk-0.4.8.tar.gz/data_ingestion_sdk-0.4.8/src/trelent_data_ingestion_sdk/__init__.py
@@ -0,0 +1,39 @@
+from .config import SDKConfig
+from .client import DataIngestionClient
+from .models import (
+    S3Connector,
+    S3CreateInput,
+    UrlConnector,
+    BucketOutput,
+    S3SignedUrlOutput,
+    JobInput,
+    ProcessResponse,
+    JobStatusItem,
+    JobStatusResponse,
+    WorkflowSummary,
+    BucketPointer,
+    S3SignedUrlDeliveryItem,
+    BucketDeliveryItem,
+    HealthzResponse,
+)
+
+__all__ = [
+    "SDKConfig",
+    "DataIngestionClient",
+    "S3Connector",
+    "S3CreateInput",
+    "UrlConnector",
+    "BucketOutput",
+    "S3SignedUrlOutput",
+    "JobInput",
+    "ProcessResponse",
+    "JobStatusItem",
+    "JobStatusResponse",
+    "WorkflowSummary",
+    "BucketPointer",
+    "S3SignedUrlDeliveryItem",
+    "BucketDeliveryItem",
+    "HealthzResponse",
+]
+
+
