f0f5fae6496821ffa2fcdfd53dee8fb53d704b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 08:05:54 +0000

    Add package Kurdish-Characters/1.0.1/kurdish_characters-1.0.1.tar.gz

---
+++ packages/Kurdish-Characters/kurdish_characters-1.0.1.tar.gz/kurdish_characters-1.0.1/setup.py
@@ -0,0 +1,40 @@
+"""
+Setup script for the Kurdish Text Handler library
+"""
+
+from setuptools import setup, find_packages    # pyright: ignore[reportMissingModuleSource]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Kurdish_Characters",
+    version="1.0.1",
+    author="Nashwan Taha Nheli",
+    author_email="neshwantaha@gmail.com",
+    description="A Python library to properly display Kurdish characters in tkinter and other interfaces",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neshwantaha/Kurdish_Characters",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # No external dependencies required
+    ],
+    keywords="kurdish, text, tkinter, unicode, font, display",
+)
