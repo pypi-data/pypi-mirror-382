64e6918c017434b7662e9ed354d96cbb86f4d6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 08:05:44 +0000

    Add package openspeleo-core/0.0.3/openspeleo_core-0.0.3-cp310-cp310-win_amd64.whl

---
+++ packages/openspeleo-core/openspeleo_core-0.0.3-cp310-cp310-win_amd64.whl/openspeleo_core/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+OpenSpeleo-Core is a high performance to manipulate cave survey files.
+"""
+
+import importlib.metadata
+
+__author__ = "Jonathan Dekhtiar"
+__version__ = importlib.metadata.version("openspeleo_core")
