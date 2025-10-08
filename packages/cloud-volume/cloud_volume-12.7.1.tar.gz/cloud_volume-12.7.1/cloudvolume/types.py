838571e37c392c0d6095d552540c5ebb1210400c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 08:06:05 +0000

    Add package cloud-volume/12.7.1/cloud_volume-12.7.1.tar.gz

---
+++ packages/cloud-volume/cloud_volume-12.7.1.tar.gz/cloud_volume-12.7.1/cloudvolume/types.py
@@ -0,0 +1,9 @@
+from typing import Union, Optional, List, Tuple
+
+CompressType = Optional[Union[str,bool]]
+ParallelType = Union[int,bool]
+CacheType = Union[bool,str]
+SecretsType = Optional[Union[str,dict]]
+MipType = Union[int, List[int]]
+ShapeType = Tuple[int,int,int]
+ProgressType = Union[bool,str]
