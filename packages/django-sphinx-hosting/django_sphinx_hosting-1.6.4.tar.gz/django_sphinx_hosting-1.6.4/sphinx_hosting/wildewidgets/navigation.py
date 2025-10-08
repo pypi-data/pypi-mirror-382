fbe1294d36a1cb79f963857ff6180560e172d10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 14:07:49 +0000

    Add package django-sphinx-hosting/1.6.4/django_sphinx_hosting-1.6.4.tar.gz

---
+++ packages/django-sphinx-hosting/django_sphinx_hosting-1.6.4.tar.gz/django_sphinx_hosting-1.6.4/sphinx_hosting/wildewidgets/navigation.py
@@ -0,0 +1,126 @@
+from typing import TYPE_CHECKING, ClassVar, Final, List, Optional, cast  # noqa: UP035
+
+from crequest.middleware import CrequestMiddleware
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse_lazy
+from wildewidgets import (
+    Block,
+    BreadcrumbBlock,
+    BreadcrumbItem,
+    LinkedImage,
+    Menu,
+    MenuItem,
+    TablerVerticalNavbar,
+)
+
+from ..settings import LOGO_IMAGE, LOGO_URL, LOGO_WIDTH, SITE_NAME
+from .search import GlobalSearchFormWidget
+
+if TYPE_CHECKING:
+    from django.http.request import HttpRequest
+
+
+class SphinxHostingMainMenu(Menu):
+    """
+    The primary menu that appears in :py:class:`SphinxHostingSidebar`
+    directly underneath the "Search" form,
+    :py:class:`sphinx_hosting.wildewidgets.search.GlobalSearchFormWidget`.
+
+    It gives access to all the views that normal, non privileged users should be
+    allowed to use.
+    """
+
+    title: str = "Main"
+    items: Final[List[MenuItem]] = [
+        MenuItem(
+            text="Projects",
+            icon="stack",
+            url=reverse_lazy("sphinx_hosting:project--list"),
+        )
+    ]
+
+    def __init__(self, *items, **kwargs) -> None:
+        self.active_item: Optional[str] = None  # noqa: FA100
+        super().__init__(*items, **kwargs)
+
+    def build_menu(self, items: List[MenuItem]) -> None:
+        """
+        Programatically build our menu here.  We have this code because the
+        presence of some items in this menu depends on the user's permissions.
+
+        We have to do it here because if we do it in ``__init__``, that's too
+        early in the Django boostrap and the global Django ``urlpatterns`` have
+        not finished building, and even ``reverse_lazy`` fails.
+        """
+        request: HttpRequest = CrequestMiddleware.get_request()
+        user: AbstractUser = cast(AbstractUser, request.user)
+        if user.has_perm("sphinxhostingcore.view_classifier"):
+            item = MenuItem(
+                text="Classifiers",
+                icon="sliders",
+                url=reverse_lazy("sphinx_hosting:classifier--index"),
+            )
+            items.append(item)
+        for item in items:
+            if self.active_item is not None:
+                item.set_active(self.active_item)
+        super().build_menu(items)
+
+    def activate(self, text: str) -> bool:
+        """
+        Normally, how activate works is that it looks through our menu items,
+        finds the one whose ``text`` or ``url`` matches ``text``.
+
+        In our case, we're building the menu items in :py:meth:`build_menu`,
+        which occurs after :py:meth:`activate` would be called, so we have to
+        save the ``text`` here, and use it later in :py:meth:`build_menu`.
+
+        Args:
+            text: the text to search for among our :py:attr:`items`
+
+        Returns:
+            We always return ``True`` here, since we won't know if we match
+            until later.
+
+        """
+        self.active_item = text
+        return True
+
+
+class SphinxHostingSidebar(TablerVerticalNavbar):
+    """
+    The vertical menu area on the left of the page.  It houses our search form,
+    :py:class:`sphinx_hosting.wildewidgets.search.GlobalSearchFormWidget`, our
+    main menu :py:class:`SphinxHostingMainMenu`, possibly our utility menu
+    :py:class:`SphinxHostingLookupsMenu` and finally the global navigation for a
+    :py:class:`sphinx_hosting.models.Version` when we're reading our
+    documentation.
+    """
+
+    hide_below_viewport: str = "xl"
+    branding: Block = Block(
+        LinkedImage(
+            image_src=LOGO_IMAGE,
+            image_width=LOGO_WIDTH,
+            image_alt=SITE_NAME,
+            css_class="d-flex justify-content-center",
+            url=LOGO_URL,
+        ),
+        GlobalSearchFormWidget(css_class="ms-auto ms-xl-0 align-self-center mt-3"),
+        css_class=(
+            "d-flex flex-row flex-xl-column justify-content-between "
+            "flex-grow-1 flex-xl-grow-0"
+        ),
+    )
+    contents: Final[List[Block]] = [
+        SphinxHostingMainMenu(),
+    ]
+    wide: bool = True
+
+
+class SphinxHostingBreadcrumbs(BreadcrumbBlock):
+    items: ClassVar[List[BreadcrumbItem]] = [
+        BreadcrumbItem(
+            title=SITE_NAME, url=reverse_lazy("sphinx_hosting:project--list")
+        )
+    ]
