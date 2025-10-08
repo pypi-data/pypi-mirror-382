f6564b75ca5204baed496124e4a655e6e0474db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:52 +0000

    Add package stimulsoft-data-adapters/2025.4.1/stimulsoft_data_adapters-2025.4.1.tar.gz

---
+++ packages/stimulsoft-data-adapters/stimulsoft_data_adapters-2025.4.1.tar.gz/stimulsoft_data_adapters-2025.4.1/stimulsoft_data_adapters/classes/StiFunctions.py
@@ -0,0 +1,32 @@
+import re
+import uuid
+from enum import Enum
+
+
+class StiFunctions:
+
+### String
+
+    def getJavaScriptValue(value) -> str:
+        if value == None: return 'null'
+        if isinstance(value, Enum): return 'null' if value.value == None else value.value
+        if type(value) == list: return str(value)
+        if type(value) == str: return f"'{value}'"
+        return str(value).lower()
+
+    def isNullOrEmpty(value) -> bool:
+        return len(value or '') == 0
+    
+    def newGuid(length = 16) -> str:
+        return uuid.uuid4().hex[:length]
+
+    def isJavaScriptFunctionName(value) -> bool:
+        return re.search("^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$", value)
+    
+    def isDashboardsProduct() -> bool:
+        try:
+            from stimulsoft_dashboards.report.StiDashboard import StiDashboard
+        except Exception as e:
+            return False
+        
+        return True
