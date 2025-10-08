bc54103dbfe4bf0c8808dccac6c9366cdc5b1930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 14:08:08 +0000

    Add package metorial-openai-compatible/1.0.2/metorial_openai_compatible-1.0.2.tar.gz

---
+++ packages/metorial-openai-compatible/metorial_openai_compatible-1.0.2.tar.gz/metorial_openai_compatible-1.0.2/src/metorial_openai_compatible/__init__.py
@@ -0,0 +1,11 @@
+from .metorial_openai_compatible import (
+  MetorialOpenAICompatibleSession,
+  build_openai_compatible_tools,
+  call_openai_compatible_tools,
+)
+
+__all__ = [
+  "MetorialOpenAICompatibleSession",
+  "build_openai_compatible_tools",
+  "call_openai_compatible_tools",
+]
