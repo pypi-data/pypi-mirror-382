dc4797c6c76c846613d6405718eece506f39eecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:05:14 +0000

    Add package openlineage-sql/1.39.0/openlineage_sql-1.39.0.tar.gz

---
+++ packages/openlineage-sql/openlineage_sql-1.39.0.tar.gz/openlineage_sql-1.39.0/iface-py/tests/python/test_small.py
@@ -0,0 +1,10 @@
+# Copyright 2018-2025 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
+from openlineage_sql import parse
+
+
+def test_parse_small():
+    metadata = parse("SELECT * FROM test1")
+    assert metadata.inputs == ["test1"]
+    assert metadata.output is None
