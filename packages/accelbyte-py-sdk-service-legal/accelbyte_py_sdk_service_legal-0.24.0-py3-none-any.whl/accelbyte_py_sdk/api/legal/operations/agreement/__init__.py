123c5cced920ef2bc1864c4014c9a3371a0e3909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:38 +0000

    Add package accelbyte-py-sdk-service-legal/0.24.0/accelbyte_py_sdk_service_legal-0.24.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-legal/accelbyte_py_sdk_service_legal-0.24.0-py3-none-any.whl/accelbyte_py_sdk/api/legal/operations/agreement/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Legal Service."""
+
+__version__ = "4.4.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .accept_versioned_policy import AcceptVersionedPolicy
+from .bulk_accept_versioned_policy import BulkAcceptVersionedPolicy
+from .change_preference_consent import ChangePreferenceConsent
+from .change_preference_consent_1 import ChangePreferenceConsent1
+from .indirect_bulk_accept_ve_f4345a import IndirectBulkAcceptVersionedPolicy1
+from .indirect_bulk_accept_ve_34e753 import IndirectBulkAcceptVersionedPolicyV2
+from .retrieve_accepted_agreements import RetrieveAcceptedAgreements
+from .retrieve_agreements_public import RetrieveAgreementsPublic
+from .retrieve_all_users_by_p_90a012 import RetrieveAllUsersByPolicyVersion
---
+++ packages/accelbyte-py-sdk-service-legal/accelbyte_py_sdk_service_legal-0.24.0-py3-none-any.whl/accelbyte_py_sdk/api/legal/operations/localized_policy_versions_with_namespace/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Legal Service."""
+
+__version__ = "4.4.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .create_localized_policy_b58242 import CreateLocalizedPolicyVersion1
+from .delete_localized_policy import DeleteLocalizedPolicy
+from .request_presigned_url_1 import RequestPresignedURL1
+from .retrieve_localized_poli_4a4859 import RetrieveLocalizedPolicyVersions1
+from .retrieve_single_localiz_f21ac4 import RetrieveSingleLocalizedPolicyVersion1
+from .retrieve_single_localiz_480659 import RetrieveSingleLocalizedPolicyVersion3
+from .set_default_policy_1 import SetDefaultPolicy1
+from .update_localized_policy_b28b13 import UpdateLocalizedPolicyVersion1
