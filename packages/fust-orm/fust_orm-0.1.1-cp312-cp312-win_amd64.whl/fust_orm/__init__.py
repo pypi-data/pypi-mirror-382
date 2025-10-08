3f53afbabb47f14ad39c09ee44585c340b642f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:15:12 +0000

    Add package fust-orm/0.1.1/fust_orm-0.1.1-cp312-cp312-win_amd64.whl

---
+++ packages/fust-orm/fust_orm-0.1.1-cp312-cp312-win_amd64.whl/fust_orm/__init__.py
@@ -0,0 +1,18 @@
+from fust_orm.fust_orm import (
+    Database,
+    QueryBuilder,
+    WhereCondition,
+    ColumnField,
+    Model,
+    select,
+)
+
+
+__all__ = (
+    "Database",
+    "QueryBuilder",
+    "WhereCondition",
+    "ColumnField",
+    "Model",
+    "select",
+)
