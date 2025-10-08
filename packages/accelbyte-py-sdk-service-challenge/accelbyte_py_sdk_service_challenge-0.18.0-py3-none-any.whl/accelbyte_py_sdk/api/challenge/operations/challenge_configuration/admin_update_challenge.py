638327c8f278ce14cafa41898c21f0a62516161c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:06:12 +0000

    Add package accelbyte-py-sdk-service-challenge/0.18.0/accelbyte_py_sdk_service_challenge-0.18.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-challenge/accelbyte_py_sdk_service_challenge-0.18.0-py3-none-any.whl/accelbyte_py_sdk/api/challenge/operations/challenge_configuration/admin_update_challenge.py
@@ -0,0 +1,473 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Challenge Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ModelChallengeResponse
+from ...models import ModelUpdateChallengeRequest
+from ...models import ResponseError
+
+
+class AdminUpdateChallenge(Operation):
+    """Update a Challenge (adminUpdateChallenge)
+
+    - Required permission: ADMIN:NAMESPACE:{namespace}:CHALLENGE [UPDATE]
+    Request body:
+    - name: name of the challenge
+    - description: text describing about the challenge (optional)
+    - startDate: timestamp of when the challenge is started
+    - endDate: timestamp of when the challenge is ended (optional)
+    - endAfter: describe number of period challenge will be retired after (optional). To configure challenge that never end, leave the endDate and endAfter field null/empty.
+    - repeatAfter: describe number of period challenge's goals will be repeated after. Leave it empty if you don't want to repeat the challenge.
+    - rotation: describe how long goals in a challenge will be available for players to progress before rotated with another goals. (DAILY|WEEKLY|MONTHLY|NONE)
+    - activeGoalsPerRotation: number of goals per rotation (currently only applicable for RANDOMIZE assignment)
+    - assignmentRule: describe how the goals will be assigned and scheduled to users. (FIXED|RANDOMIZED|UNSCHEDULED|CUSTOM)
+    - goalsVisibility: describe whether users can see all goals under challenge, or only active goal in one rotation period only. (SHOWALL|PERIODONLY)
+    - resetConfig: describe when rotation reset will happen (optional).
+    - resetTime: Reset time must follow hours:minutes in 24 hours format (e.g. 01:30, 23:15) and in UTC timezone. Default to "00:00"
+    - resetDay: Reset Day follows the ISO-8601 standard, from 1 (Monday) to 7 (Sunday). Default to 1 in WEEKLY rotation.
+    - resetDate: Reset Date must be a number 1 - 31. Default to 1 in MONTHLY rotation.
+    - randomizedPerRotation:
+    - true: each goal will be randomly assigned to multiple periods
+    - false: a goal will only be assigned to one period
+    - tags: challenge's labels.
+
+    Properties:
+        url: /challenge/v1/admin/namespaces/{namespace}/challenges/{challengeCode}
+
+        method: PUT
+
+        tags: ["Challenge Configuration"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        body: (body) REQUIRED ModelUpdateChallengeRequest in body
+
+        challenge_code: (challengeCode) REQUIRED str in path
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        200: OK - ModelChallengeResponse (OK)
+
+        400: Bad Request - ResponseError (20018: bad request: {{message}})
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        422: Unprocessable Entity - ResponseError (Unprocessable Entity)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+    """
+
+    # region fields
+
+    _url: str = "/challenge/v1/admin/namespaces/{namespace}/challenges/{challengeCode}"
+    _path: str = "/challenge/v1/admin/namespaces/{namespace}/challenges/{challengeCode}"
+    _base_path: str = ""
+    _method: str = "PUT"
+    _consumes: List[str] = ["application/json"]
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "challenge"
+
+    body: ModelUpdateChallengeRequest  # REQUIRED in [body]
+    challenge_code: str  # REQUIRED in [path]
+    namespace: str  # REQUIRED in [path]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "body": self.get_body_params(),
+            "path": self.get_path_params(),
+        }
+
+    def get_body_params(self) -> Any:
+        if not hasattr(self, "body") or self.body is None:
+            return None
+        return self.body.to_dict()
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "challenge_code"):
+            result["challengeCode"] = self.challenge_code
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_body(self, value: ModelUpdateChallengeRequest) -> AdminUpdateChallenge:
+        self.body = value
+        return self
+
+    def with_challenge_code(self, value: str) -> AdminUpdateChallenge:
+        self.challenge_code = value
+        return self
+
+    def with_namespace(self, value: str) -> AdminUpdateChallenge:
+        self.namespace = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "body") and self.body:
+            result["body"] = self.body.to_dict(include_empty=include_empty)
+        elif include_empty:
+            result["body"] = ModelUpdateChallengeRequest()
+        if hasattr(self, "challenge_code") and self.challenge_code:
+            result["challengeCode"] = str(self.challenge_code)
+        elif include_empty:
+            result["challengeCode"] = ""
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_200: Optional[ModelChallengeResponse] = None
+        error_400: Optional[ResponseError] = None
+        error_401: Optional[ResponseError] = None
+        error_403: Optional[ResponseError] = None
+        error_404: Optional[ResponseError] = None
+        error_422: Optional[ResponseError] = None
+        error_500: Optional[ResponseError] = None
+
+        def ok(self) -> AdminUpdateChallenge.Response:
+            if self.error_400 is not None:
+                err = self.error_400.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_401 is not None:
+                err = self.error_401.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_403 is not None:
+                err = self.error_403.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_404 is not None:
+                err = self.error_404.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_422 is not None:
+                err = self.error_422.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_500 is not None:
+                err = self.error_500.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_200 is not None:
+                yield self.data_200
+                yield None
+            elif self.error_400 is not None:
+                yield None
+                yield self.error_400
+            elif self.error_401 is not None:
+                yield None
+                yield self.error_401
+            elif self.error_403 is not None:
+                yield None
+                yield self.error_403
+            elif self.error_404 is not None:
+                yield None
+                yield self.error_404
+            elif self.error_422 is not None:
+                yield None
+                yield self.error_422
+            elif self.error_500 is not None:
+                yield None
+                yield self.error_500
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        200: OK - ModelChallengeResponse (OK)
+
+        400: Bad Request - ResponseError (20018: bad request: {{message}})
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        422: Unprocessable Entity - ResponseError (Unprocessable Entity)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = AdminUpdateChallenge.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 200:
+                result.data_200 = ModelChallengeResponse.create_from_dict(content)
+            elif code == 400:
+                result.error_400 = ResponseError.create_from_dict(content)
+                result.error = result.error_400.translate_to_api_error()
+            elif code == 401:
+                result.error_401 = ResponseError.create_from_dict(content)
+                result.error = result.error_401.translate_to_api_error()
+            elif code == 403:
+                result.error_403 = ResponseError.create_from_dict(content)
+                result.error = result.error_403.translate_to_api_error()
+            elif code == 404:
+                result.error_404 = ResponseError.create_from_dict(content)
+                result.error = result.error_404.translate_to_api_error()
+            elif code == 422:
+                result.error_422 = ResponseError.create_from_dict(content)
+                result.error = result.error_422.translate_to_api_error()
+            elif code == 500:
+                result.error_500 = ResponseError.create_from_dict(content)
+                result.error = result.error_500.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[
+        Union[None, ModelChallengeResponse], Union[None, HttpResponse, ResponseError]
+    ]:
+        """Parse the given response.
+
+        200: OK - ModelChallengeResponse (OK)
+
+        400: Bad Request - ResponseError (20018: bad request: {{message}})
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        422: Unprocessable Entity - ResponseError (Unprocessable Entity)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 200:
+            return ModelChallengeResponse.create_from_dict(content), None
+        if code == 400:
+            return None, ResponseError.create_from_dict(content)
+        if code == 401:
+            return None, ResponseError.create_from_dict(content)
+        if code == 403:
+            return None, ResponseError.create_from_dict(content)
+        if code == 404:
+            return None, ResponseError.create_from_dict(content)
+        if code == 422:
+            return None, ResponseError.create_from_dict(content)
+        if code == 500:
+            return None, ResponseError.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls,
+        body: ModelUpdateChallengeRequest,
+        challenge_code: str,
+        namespace: str,
+        **kwargs,
+    ) -> AdminUpdateChallenge:
+        instance = cls()
+        instance.body = body
+        instance.challenge_code = challenge_code
+        instance.namespace = namespace
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> AdminUpdateChallenge:
+        instance = cls()
+        if "body" in dict_ and dict_["body"] is not None:
+            instance.body = ModelUpdateChallengeRequest.create_from_dict(
+                dict_["body"], include_empty=include_empty
+            )
+        elif include_empty:
+            instance.body = ModelUpdateChallengeRequest()
+        if "challengeCode" in dict_ and dict_["challengeCode"] is not None:
+            instance.challenge_code = str(dict_["challengeCode"])
+        elif include_empty:
+            instance.challenge_code = ""
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "body": "body",
+            "challengeCode": "challenge_code",
+            "namespace": "namespace",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "body": True,
+            "challengeCode": True,
+            "namespace": True,
+        }
+
+    # endregion static methods
