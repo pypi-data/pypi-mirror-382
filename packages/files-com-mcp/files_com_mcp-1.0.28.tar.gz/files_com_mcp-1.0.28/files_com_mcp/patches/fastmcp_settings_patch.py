3628095edd7a7cab12e4f75d7f5aab2d7c5a5557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 15:06:21 +0000

    Add package files-com-mcp/1.0.28/files_com_mcp-1.0.28.tar.gz

---
+++ packages/files-com-mcp/files_com_mcp-1.0.28.tar.gz/files_com_mcp-1.0.28/files_com_mcp/patches/fastmcp_settings_patch.py
@@ -0,0 +1,14 @@
+from mcp.server.session import ServerSession
+import os
+
+_original_init = ServerSession.__init__
+
+
+def patched_init(self, *args, **kwargs):
+    _original_init(self, *args, **kwargs)
+
+    # Inject your custom attributes
+    self._files_com_api_key = os.getenv("FILES_COM_API_KEY", "").strip()
+
+
+ServerSession.__init__ = patched_init
