c7084613222f5f90c8f03ebf97aa518dd408612b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:39 +0000

    Add package sqlmesh/0.224.1.dev31/sqlmesh-0.224.1.dev31-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev31-py3-none-any.whl/sqlmesh/migrations/v0080_add_batch_size_to_scd_type_2_models.py
@@ -0,0 +1,9 @@
+"""Add batch_size to SCD Type 2 models and add updated_at_name to by time which changes their data hash."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
