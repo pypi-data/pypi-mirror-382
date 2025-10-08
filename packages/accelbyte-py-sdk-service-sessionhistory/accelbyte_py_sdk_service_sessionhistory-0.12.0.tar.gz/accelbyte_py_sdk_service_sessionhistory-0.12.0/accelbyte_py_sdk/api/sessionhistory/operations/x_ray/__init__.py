5fcf6a5cb750c8c9f0e2f7e5dd2d98ba733c687e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:15:54 +0000

    Add package accelbyte-py-sdk-service-sessionhistory/0.12.0/accelbyte_py_sdk_service_sessionhistory-0.12.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-sessionhistory/accelbyte_py_sdk_service_sessionhistory-0.12.0.tar.gz/accelbyte_py_sdk_service_sessionhistory-0.12.0/accelbyte_py_sdk/api/sessionhistory/operations/x_ray/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Session History Service."""
+
+__version__ = "1.14.2"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .create_xray_bulk_ticket_cacbf6 import CreateXrayBulkTicketObservability
+from .create_xray_ticket_obse_a825bd import CreateXrayTicketObservability
