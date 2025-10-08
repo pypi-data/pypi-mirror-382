0ab9f17d1f7c6e51f6c3bdc8a289400e208136a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:41 +0000

    Add package arm-test-data/0.1.2/arm_test_data-0.1.2.tar.gz

---
+++ packages/arm-test-data/arm_test_data-0.1.2.tar.gz/arm_test_data-0.1.2/arm_test_data/dataset.py
@@ -0,0 +1,27 @@
+import importlib.resources
+import pooch
+
+DATASETS = pooch.create(
+    path=pooch.os_cache('arm-test-data'),
+    base_url='https://github.com/ARM-DOE/arm-test-data/raw/main/data/',
+    env='ARM_TEST_DATA_DIR',
+)
+
+with open(importlib.resources.files('arm_test_data') / 'registry.txt') as registry_file:
+    DATASETS.load_registry(registry_file)
+
+
+def locate():
+    """The absolute path to the sample data storage location on disk.
+    This is where the data are saved on your computer. The location is
+    dependent on the operating system. The folder locations are defined by the
+    ``appdirs``  package (see the `appdirs documentation
+    <https://github.com/ActiveState/appdirs>`__).
+    The location can be overwritten by the ``PYTHIA_DATASETS_DIR`` environment
+    variable to the desired destination.
+    Returns
+    -------
+    path : str
+        The local data storage location.
+    """
+    return str(DATASETS.abspath)
