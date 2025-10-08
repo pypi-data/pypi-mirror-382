52c303df6fe1ff294c80fb9fcf9465535e1b75f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:37 +0000

    Add package snowdrop-adjudicators/0.0.4/snowdrop_adjudicators-0.0.4-py3-none-any.whl

---
+++ packages/snowdrop-adjudicators/snowdrop_adjudicators-0.0.4-py3-none-any.whl/snowdrop_adjudicators/__init__.py
@@ -0,0 +1,12 @@
+from snowdrop_adjudicators.adjudicators.schrodinger import SchrodingerEquationAdjudicator
+from snowdrop_adjudicators.adjudicators.simulated_annealing import SimulatedAnnealingAdjudicator
+from snowdrop_adjudicators.adjudicators.adjudicator import (GameState, AdjudicationResult, Adjudicator, IsingModel,
+                                                            evaluate_winner)
+
+__all__ = ['SimulatedAnnealingAdjudicator',
+           'SchrodingerEquationAdjudicator',
+           'GameState',
+           'AdjudicationResult',
+           'Adjudicator',
+           'IsingModel',
+           'evaluate_winner']
