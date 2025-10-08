b1f6982a90e2ac32e1d5e8437b8014c32bbf0e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:29 +0000

    Add package certbot-dns-cloudflare/5.1.0/certbot_dns_cloudflare-5.1.0.tar.gz

---
+++ packages/certbot-dns-cloudflare/certbot_dns_cloudflare-5.1.0.tar.gz/certbot_dns_cloudflare-5.1.0/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup
+
+version = '5.1.0'
+
+install_requires = [
+    # for now, do not upgrade to cloudflare>=2.20 to avoid deprecation warnings and the breaking
+    # changes in version 3.0. see https://github.com/certbot/certbot/issues/9938
+    'cloudflare>=2.19, <2.20',
+]
+
+if os.environ.get('SNAP_BUILD'):
+    install_requires.append('packaging')
+else:
+    install_requires.extend([
+        # We specify the minimum acme and certbot version as the current plugin
+        # version for simplicity. See
+        # https://github.com/certbot/certbot/issues/8761 for more info.
+        f'acme>={version}',
+        f'certbot>={version}',
+    ])
+
+
+setup(
+    version=version,
+    install_requires=install_requires,
+)
