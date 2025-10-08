30bc8bd02b651aa7aba09416759c0229fb9aa777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:19 +0000

    Add package accelbyte-py-sdk-service-legal/0.23.0/accelbyte_py_sdk_service_legal-0.23.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-legal/accelbyte_py_sdk_service_legal-0.23.0.tar.gz/accelbyte_py_sdk_service_legal-0.23.0/accelbyte_py_sdk/api/legal/operations/policy_versions/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Legal Service."""
+
+__version__ = "4.4.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .create_policy_version import CreatePolicyVersion
+from .publish_policy_version import PublishPolicyVersion
+from .retrieve_single_policy_version import RetrieveSinglePolicyVersion
+from .update_policy_version import UpdatePolicyVersion
