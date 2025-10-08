ae669c3f52ccefb2df551979015e62e1f4a7bb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 14:07:59 +0000

    Add package pyIGRF14/1.0.4/pyigrf14-1.0.4-py3-none-any.whl

---
+++ packages/pyIGRF14/pyigrf14-1.0.4-py3-none-any.whl/pyIGRF14/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+This is a package of IGRF-14 (International Geomagnetic Reference Field) about python version.
+It don't need any Fortran compiler.
+"""
+
+__author__ = "zzyztyy"
+__version__ = "1.0.4"
+
+from pyIGRF14 import calculate, loadCoeffs  # noqa: F401
+from pyIGRF14.value import igrf_value, igrf_variation  # noqa: F401
