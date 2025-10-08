3c277c3d35533f54b84b0ace2f48b8f7776cec85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 08:05:46 +0000

    Add package Kurdish-Characters/1.0.0/kurdish_characters-1.0.0-py3-none-any.whl

---
+++ packages/Kurdish-Characters/kurdish_characters-1.0.0-py3-none-any.whl/Kurdish_Characters/__init__.py
@@ -0,0 +1,10 @@
+"""
+Init file for the Kurdish Text Handler package
+فایلی دەستپێکردن بۆ کتێبخانەی نامەی کوردی
+"""
+
+from .Kurdish_Characters import KurdishTextHandler, create_kurdish_window, display_kurdish_message, KURDISH_CHARS
+
+__version__ = "1.0.0"
+__author__ = "Kurdish Developer Community"
+# کۆمەڵگەی پەرەپێدەرە کوردەکان
