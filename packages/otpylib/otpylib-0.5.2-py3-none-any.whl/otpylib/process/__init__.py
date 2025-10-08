b6a444ff5861c0a760eeb58e2e923fec2468e984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:05 +0000

    Add package otpylib/0.5.2/otpylib-0.5.2-py3-none-any.whl

---
+++ packages/otpylib/otpylib-0.5.2-py3-none-any.whl/otpylib/process/__init__.py
@@ -0,0 +1,55 @@
+"""
+Process Module
+
+High-level process management API for otpylib.
+"""
+
+from otpylib.process.api import (
+    spawn,
+    spawn_link,
+    spawn_monitor,
+    send,
+    send_after,
+    read_timer,
+    cancel_timer,
+    receive,
+    register,
+    unregister,
+    whereis,
+    whereis_name,
+    self,
+    link,
+    unlink,
+    monitor,
+    demonitor,
+    exit,
+    is_alive,
+    processes,
+    registered,
+    use_runtime,
+)
+
+__all__ = [
+    'spawn',
+    'spawn_link',
+    'spawn_monitor',
+    'send',
+    'send_after',
+    'read_timer',
+    'cancel_timer',
+    'receive',
+    'register',
+    'unregister',
+    'whereis',
+    'whereis_name',
+    'self',
+    'link',
+    'unlink',
+    'monitor',
+    'demonitor',
+    'exit',
+    'is_alive',
+    'processes',
+    'registered',
+    'use_runtime'
+]
