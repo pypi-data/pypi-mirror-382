ac6a8483705acc8299235ebb6b1abb42dec8a94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:14 +0000

    Add package accelbyte-py-sdk-service-inventory/0.19.0/accelbyte_py_sdk_service_inventory-0.19.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-inventory/accelbyte_py_sdk_service_inventory-0.19.0-py3-none-any.whl/accelbyte_py_sdk/api/inventory/operations/public_item_types/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Inventory Service."""
+
+__version__ = "0.2.17"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .public_list_item_types import PublicListItemTypes
+from .public_list_item_types import (
+    SortByEnum as PublicListItemTypesSortByEnum,
+)
