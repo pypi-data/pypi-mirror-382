c38db8e66412e69425e55503c6ac783a26117493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 17:06:00 +0000

    Add package sqlmesh/0.224.1.dev36/sqlmesh-0.224.1.dev36.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev36.tar.gz/sqlmesh-0.224.1.dev36/sqlmesh/migrations/v0067_add_tsql_date_full_precision.py
@@ -0,0 +1,9 @@
+"""Add full precision for tsql to support nanoseconds."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
