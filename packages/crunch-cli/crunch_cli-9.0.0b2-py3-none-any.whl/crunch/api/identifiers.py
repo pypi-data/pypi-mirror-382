d74c37d6425395d5bfa81e617ec9ce1b2b63e1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:05:16 +0000

    Add package crunch-cli/9.0.0b2/crunch_cli-9.0.0b2-py3-none-any.whl

---
+++ packages/crunch-cli/crunch_cli-9.0.0b2-py3-none-any.whl/crunch/api/identifiers.py
@@ -0,0 +1,44 @@
+import typing
+
+import typing_extensions
+
+CompetitionIdentifierType = typing.Union[
+    int,
+    str
+]
+
+RoundIdentifierType = typing.Union[
+    int,
+    typing_extensions.Literal[
+        "@current",
+        "@last",
+    ]
+]
+
+PhaseIdentifierType = typing.Union[
+    str,
+    typing_extensions.Literal[
+        "submission",
+        "out-of-sample",
+    ],
+    typing_extensions.Literal[
+        "@current",
+    ]
+]
+
+CrunchIdentifierType = typing.Union[
+    int,
+    typing_extensions.Literal[
+        "@current",
+        "@next",
+        "@published",
+    ]
+]
+
+LeaderboardIdentifierType = typing.Union[
+    str,
+    typing_extensions.Literal[
+        "@default",
+        "@mine",
+    ]
+]
