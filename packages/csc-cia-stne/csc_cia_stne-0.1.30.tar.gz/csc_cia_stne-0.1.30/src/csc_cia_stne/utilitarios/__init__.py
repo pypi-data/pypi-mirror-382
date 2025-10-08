ab1e54b27c988dbbc8bddf4466f2b5262d07c293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:05 +0000

    Add package csc-cia-stne/0.1.30/csc_cia_stne-0.1.30.tar.gz

---
+++ packages/csc-cia-stne/csc_cia_stne-0.1.30.tar.gz/csc_cia_stne-0.1.30/src/csc_cia_stne/utilitarios/__init__.py
@@ -0,0 +1,15 @@
+from .functions import titulo, recriar_pasta, b64encode, b64decode, convert_bquery_result_to_json,get_config, get_secret, now_sp, validate_json, delete_file, delete_folder
+
+__all__ = [
+    "titulo", 
+    "recriar_pasta", 
+    "b64encode", 
+    "b64decode", 
+    "convert_bquery_result_to_json",
+    "get_config",
+    "get_secret",
+    "now_sp",
+    "delete_file",
+    "delete_folder",
+    "validate_json"
+    ]
