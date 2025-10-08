fa5f31517507b2b1497d4bc4f12c4f35f49ab768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:04:36 +0000

    Add package accelbyte-py-sdk-service-social/0.24.0/accelbyte_py_sdk_service_social-0.24.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-social/accelbyte_py_sdk_service_social-0.24.0.tar.gz/accelbyte_py_sdk_service_social-0.24.0/accelbyte_py_sdk/api/social/operations/user_statistic_cycle/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Statistics Service."""
+
+__version__ = "3.0.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .get_user_stat_cycle_items import GetUserStatCycleItems
+from .get_user_stat_cycle_items_1 import GetUserStatCycleItems1
+from .public_list_my_stat_cyc_a54f9a import PublicListMyStatCycleItems
