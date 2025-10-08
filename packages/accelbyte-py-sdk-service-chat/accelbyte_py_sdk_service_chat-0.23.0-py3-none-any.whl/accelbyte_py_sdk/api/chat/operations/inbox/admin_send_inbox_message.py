6c32da23ccb8afab64aa6a764c6828b63a8b61f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:15:04 +0000

    Add package accelbyte-py-sdk-service-chat/0.23.0/accelbyte_py_sdk_service_chat-0.23.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-chat/accelbyte_py_sdk_service_chat-0.23.0-py3-none-any.whl/accelbyte_py_sdk/api/chat/operations/inbox/admin_send_inbox_message.py
@@ -0,0 +1,417 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Chat Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ModelsSendInboxMessageRequest
+from ...models import ModelsSendInboxMessageResponse
+from ...models import RestapiErrorResponseBody
+
+
+class AdminSendInboxMessage(Operation):
+    """admin send inbox message (adminSendInboxMessage)
+
+    Send inbox message
+
+    Properties:
+        url: /chat/v1/admin/inbox/namespaces/{namespace}/messages/{messageId}/send
+
+        method: POST
+
+        tags: ["inbox"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        body: (body) REQUIRED ModelsSendInboxMessageRequest in body
+
+        message_id: (messageId) REQUIRED str in path
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        200: OK - ModelsSendInboxMessageResponse (OK)
+
+        400: Bad Request - RestapiErrorResponseBody (Bad Request)
+
+        401: Unauthorized - RestapiErrorResponseBody (Unauthorized)
+
+        403: Forbidden - RestapiErrorResponseBody (Forbidden)
+
+        500: Internal Server Error - RestapiErrorResponseBody (Internal Server Error)
+    """
+
+    # region fields
+
+    _url: str = "/chat/v1/admin/inbox/namespaces/{namespace}/messages/{messageId}/send"
+    _path: str = "/chat/v1/admin/inbox/namespaces/{namespace}/messages/{messageId}/send"
+    _base_path: str = ""
+    _method: str = "POST"
+    _consumes: List[str] = ["application/json"]
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "chat"
+
+    body: ModelsSendInboxMessageRequest  # REQUIRED in [body]
+    message_id: str  # REQUIRED in [path]
+    namespace: str  # REQUIRED in [path]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "body": self.get_body_params(),
+            "path": self.get_path_params(),
+        }
+
+    def get_body_params(self) -> Any:
+        if not hasattr(self, "body") or self.body is None:
+            return None
+        return self.body.to_dict()
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "message_id"):
+            result["messageId"] = self.message_id
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_body(self, value: ModelsSendInboxMessageRequest) -> AdminSendInboxMessage:
+        self.body = value
+        return self
+
+    def with_message_id(self, value: str) -> AdminSendInboxMessage:
+        self.message_id = value
+        return self
+
+    def with_namespace(self, value: str) -> AdminSendInboxMessage:
+        self.namespace = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "body") and self.body:
+            result["body"] = self.body.to_dict(include_empty=include_empty)
+        elif include_empty:
+            result["body"] = ModelsSendInboxMessageRequest()
+        if hasattr(self, "message_id") and self.message_id:
+            result["messageId"] = str(self.message_id)
+        elif include_empty:
+            result["messageId"] = ""
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_200: Optional[ModelsSendInboxMessageResponse] = None
+        error_400: Optional[RestapiErrorResponseBody] = None
+        error_401: Optional[RestapiErrorResponseBody] = None
+        error_403: Optional[RestapiErrorResponseBody] = None
+        error_500: Optional[RestapiErrorResponseBody] = None
+
+        def ok(self) -> AdminSendInboxMessage.Response:
+            if self.error_400 is not None:
+                err = self.error_400.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_401 is not None:
+                err = self.error_401.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_403 is not None:
+                err = self.error_403.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_500 is not None:
+                err = self.error_500.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_200 is not None:
+                yield self.data_200
+                yield None
+            elif self.error_400 is not None:
+                yield None
+                yield self.error_400
+            elif self.error_401 is not None:
+                yield None
+                yield self.error_401
+            elif self.error_403 is not None:
+                yield None
+                yield self.error_403
+            elif self.error_500 is not None:
+                yield None
+                yield self.error_500
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        200: OK - ModelsSendInboxMessageResponse (OK)
+
+        400: Bad Request - RestapiErrorResponseBody (Bad Request)
+
+        401: Unauthorized - RestapiErrorResponseBody (Unauthorized)
+
+        403: Forbidden - RestapiErrorResponseBody (Forbidden)
+
+        500: Internal Server Error - RestapiErrorResponseBody (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = AdminSendInboxMessage.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 200:
+                result.data_200 = ModelsSendInboxMessageResponse.create_from_dict(
+                    content
+                )
+            elif code == 400:
+                result.error_400 = RestapiErrorResponseBody.create_from_dict(content)
+                result.error = result.error_400.translate_to_api_error()
+            elif code == 401:
+                result.error_401 = RestapiErrorResponseBody.create_from_dict(content)
+                result.error = result.error_401.translate_to_api_error()
+            elif code == 403:
+                result.error_403 = RestapiErrorResponseBody.create_from_dict(content)
+                result.error = result.error_403.translate_to_api_error()
+            elif code == 500:
+                result.error_500 = RestapiErrorResponseBody.create_from_dict(content)
+                result.error = result.error_500.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[
+        Union[None, ModelsSendInboxMessageResponse],
+        Union[None, HttpResponse, RestapiErrorResponseBody],
+    ]:
+        """Parse the given response.
+
+        200: OK - ModelsSendInboxMessageResponse (OK)
+
+        400: Bad Request - RestapiErrorResponseBody (Bad Request)
+
+        401: Unauthorized - RestapiErrorResponseBody (Unauthorized)
+
+        403: Forbidden - RestapiErrorResponseBody (Forbidden)
+
+        500: Internal Server Error - RestapiErrorResponseBody (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 200:
+            return ModelsSendInboxMessageResponse.create_from_dict(content), None
+        if code == 400:
+            return None, RestapiErrorResponseBody.create_from_dict(content)
+        if code == 401:
+            return None, RestapiErrorResponseBody.create_from_dict(content)
+        if code == 403:
+            return None, RestapiErrorResponseBody.create_from_dict(content)
+        if code == 500:
+            return None, RestapiErrorResponseBody.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls,
+        body: ModelsSendInboxMessageRequest,
+        message_id: str,
+        namespace: str,
+        **kwargs,
+    ) -> AdminSendInboxMessage:
+        instance = cls()
+        instance.body = body
+        instance.message_id = message_id
+        instance.namespace = namespace
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> AdminSendInboxMessage:
+        instance = cls()
+        if "body" in dict_ and dict_["body"] is not None:
+            instance.body = ModelsSendInboxMessageRequest.create_from_dict(
+                dict_["body"], include_empty=include_empty
+            )
+        elif include_empty:
+            instance.body = ModelsSendInboxMessageRequest()
+        if "messageId" in dict_ and dict_["messageId"] is not None:
+            instance.message_id = str(dict_["messageId"])
+        elif include_empty:
+            instance.message_id = ""
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "body": "body",
+            "messageId": "message_id",
+            "namespace": "namespace",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "body": True,
+            "messageId": True,
+            "namespace": True,
+        }
+
+    # endregion static methods
