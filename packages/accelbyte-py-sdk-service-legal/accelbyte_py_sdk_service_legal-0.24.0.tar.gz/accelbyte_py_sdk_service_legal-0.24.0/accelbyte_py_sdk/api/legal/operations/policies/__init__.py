1bc096053df90f318e3f81bdb163e3275ef8c096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:38 +0000

    Add package accelbyte-py-sdk-service-legal/0.24.0/accelbyte_py_sdk_service_legal-0.24.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-legal/accelbyte_py_sdk_service_legal-0.24.0.tar.gz/accelbyte_py_sdk_service_legal-0.24.0/accelbyte_py_sdk/api/legal/operations/policies/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Legal Service."""
+
+__version__ = "4.4.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .retrieve_country_list_w_8c69ff import RetrieveCountryListWithPolicies
+from .retrieve_latest_policies import RetrieveLatestPolicies
+from .retrieve_latest_policies import (
+    PolicyTypeEnum as RetrieveLatestPoliciesPolicyTypeEnum,
+)
+from .retrieve_latest_policie_95ffb6 import (
+    RetrieveLatestPoliciesByNamespaceAndCountryPublic,
+)
+from .retrieve_latest_policie_95ffb6 import (
+    PolicyTypeEnum as RetrieveLatestPoliciesByNamespaceAndCountryPublicPolicyTypeEnum,
+)
+from .retrieve_latest_policie_29f65f import RetrieveLatestPoliciesPublic
+from .retrieve_latest_policie_29f65f import (
+    PolicyTypeEnum as RetrieveLatestPoliciesPublicPolicyTypeEnum,
+)
+from .retrieve_policies import RetrievePolicies
+from .set_default_policy_2 import SetDefaultPolicy2
+from .update_policy import UpdatePolicy
---
+++ packages/accelbyte-py-sdk-service-legal/accelbyte_py_sdk_service_legal-0.24.0.tar.gz/accelbyte_py_sdk_service_legal-0.24.0/accelbyte_py_sdk/api/legal/operations/policy_versions_with_namespace/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Legal Service."""
+
+__version__ = "4.4.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .create_policy_version_1 import CreatePolicyVersion1
+from .delete_policy_version import DeletePolicyVersion
+from .publish_policy_version_1 import PublishPolicyVersion1
+from .retrieve_single_policy__f63224 import RetrieveSinglePolicyVersion1
+from .unpublish_policy_version import UnpublishPolicyVersion
+from .update_policy_version_1 import UpdatePolicyVersion1
