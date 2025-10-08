50d27091d6b5fc50202239ed354c632ac99c92c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 09:06:01 +0000

    Add package open-webui/0.6.33/open_webui-0.6.33-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.6.33-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/5YBeov5O.js
@@ -0,0 +1,2 @@
+import{s as e}from"./t6__rHm-.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
+//# sourceMappingURL=5YBeov5O.js.map
---
+++ packages/open-webui/open_webui-0.6.33-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.DnNgDEeQ.js
@@ -0,0 +1,2 @@
+import{l as o,c as r}from"../chunks/t6__rHm-.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.DnNgDEeQ.js.map
