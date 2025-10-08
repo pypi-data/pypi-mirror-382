e5fe6106c41d9b6693a61db3ae57d288acbc7574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 14:08:19 +0000

    Add package nuclear-rose/1.1.6/nuclear_rose-1.1.6-py3-none-any.whl

---
+++ packages/nuclear-rose/nuclear_rose-1.1.6-py3-none-any.whl/rose/energized_interaction_eim.py
@@ -0,0 +1,199 @@
+"""
+Defines a class for "affinizing" Interactions using the Empirical Interpolation
+Method (EIM).
+"""
+
+from typing import Callable
+import numpy as np
+from scipy.stats import qmc
+
+from .interaction import couplings
+from .interaction_eim import InteractionEIM, InteractionEIMSpace
+from .constants import HBARC, DEFAULT_RHO_MESH
+from .spin_orbit import SpinOrbitTerm
+
+
+class EnergizedInteractionEIM(InteractionEIM):
+    r"""
+    Extension of InteractionEIM that supports energy, mu and k as parameters. Expected format
+    for alpha is [energy, mu, k, *rest_of_params]
+    """
+
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        r"""
+        Parameters:
+            kwargs (dict): arguments to InteractionEIM. Note; the `energy` argument should not
+            be given, it will be ignored. Energy is the first element of alpha. If `mu is None`,
+            the reduced mass will expected to be the second element of alpha.
+
+        Attributes:
+            singular_values (ndarray): `S` in `U, S, Vt = numpy.linalg.svd(...)`
+            snapshots (ndarray): pillars, columns of `U`
+            match_indices (ndarray): indices of points in $\rho$ mesh that are
+                matched to the true potential
+            match_points (ndarray): points in $\rho$ mesh that are matched to
+                the true potential
+            Ainv (ndarray): inverse of A matrix (Ax = b)
+        """
+        super().__init__(**kwargs)
+
+    def tilde(self, s: float, alpha: np.array):
+        r"""Computes the energy-scaled interaction.
+
+        Parameters:
+            s (float): mesh point
+            alpha (ndarray): interaction parameters
+
+        Returns:
+            u_tilde (float | complex): energy-scaled interaction
+
+        """
+        energy = self.E(alpha)
+        k = self.momentum(alpha)
+        alpha_truncated = alpha[3:]
+        vr = self.v_r(
+            s / k, alpha_truncated
+        ) + self.spin_orbit_term.spin_orbit_potential(s / k, alpha_truncated)
+        return 1.0 / energy * vr
+
+    def coefficients(self, alpha: np.array):  # interaction parameters
+        r"""Computes the EIM expansion coefficients.
+
+        Parameters:
+            alpha (ndarray): interaction parameters
+
+        Returns:
+            coefficients (ndarray): EIM expansion coefficients
+
+        """
+        k = self.momentum(alpha)
+        u_true = self.tilde(self.r_i, alpha)
+        return 1 / k, self.Ainv @ u_true
+
+    def eta(self, alpha: np.array):
+        r"""Returns the Sommerfeld parameter.
+
+        Parameters:
+            alpha (ndarray): interaction parameters
+
+        Returns:
+            eta (float): Sommerfeld parameter
+
+        """
+        return self.k_c / self.momentum(alpha)
+
+    def tilde_emu(self, s: float, alpha: np.array):
+        r"""Emulated interaction = $\hat{U}(s, \alpha, E)$
+
+        Parameters:
+            alpha (ndarray): interaction parameters
+
+        Returns:
+            u_hat (ndarray): emulated interaction
+
+        """
+        _, x = self.coefficients(alpha)
+        emu = np.sum(x * self.snapshots, axis=1)
+        return emu
+
+    def basis_functions(self, s_mesh: np.array):
+        r"""$u_j$ in $\tilde{U} \approx \hat{U} \equiv \sum_j \beta_j(\alpha) u_j$
+
+        Parameters:
+            s_mesh (ndarray): $s$ mesh points
+
+        Returns:
+            u_j (ndarray): "pillars" (MxN matrix; M = number of mesh points; N = number of pillars)
+
+        """
+        return np.copy(self.snapshots)
+
+    def momentum(self, alpha: np.array):
+        r"""Center-of-mass, scattering momentum. Implemented as a function to support energy
+        emulation (where k could be a part of the parameter vector, `alpha`).
+
+        Parameters:
+            alpha (ndarray): interaction parameters
+
+        Returns:
+            k (float): momentum
+        """
+        return alpha[2]
+
+    def E(self, alpha: np.array):
+        r"""Energy. Implemented as a function to support energy emulation (where the energy
+        could be a part of the parameter vector, `alpha`).
+
+        Parameters:
+            alpha (ndarray): parameter vector
+
+        Returns:
+            Energy (float): in [MeV]
+        """
+        return alpha[0]
+
+    def reduced_mass(self, alpha: np.array):
+        r"""Mu. Implemented as a function to support energy emulation (where mu could be a
+        part of the parameter vector, `alpha`).
+
+        Parameters:
+            alpha (ndarray): parameter vector
+
+        Returns:
+            Mu (float): in [MeV/c^2]
+        """
+        return alpha[1]
+
+    def bundle_gcoeff_args(self, alpha: np.array):
+        r"""Bundles parameters for the Schrödinger equation
+
+        Returns:
+            args (tuple) : all the arguments to g_coeff except for $s$
+
+        Parameters:
+            alpha (ndarray) : the parameters for the interaction
+        """
+        k = self.momentum(alpha)
+        S_C = self.coulomb_cutoff(alpha) * k
+        E = self.E(alpha)
+        eta = self.eta(alpha)
+        l = self.ell
+        v_r = self.v_r
+        l_dot_s = self.spin_orbit_term.l_dot_s
+
+        # remove the energy term for alpha, so we return just the parameters that plug into v_r
+        return (alpha[3:], k, S_C, E, eta, l, v_r, self.spin_orbit_term.v_so, l_dot_s)
+
+
+class EnergizedInteractionEIMSpace(InteractionEIMSpace):
+    def __init__(
+        self,
+        l_max: int = 15,
+        interaction_type=EnergizedInteractionEIM,
+        **kwargs,
+    ):
+        r"""Generates a list of $\ell$-specific interactions.
+
+        Parameters:
+            interaction_args (list): positional arguments for constructor of `interaction_type`
+            interaction_kwargs (dict): arguments to constructor of `interaction_type`
+            l_max (int): maximum angular momentum
+            interaction_type (Type): type of `Interaction` to construct
+
+        Returns:
+            instance (EnergizedInteractionEIMSpaceInteractionSpace):
+                instance of EnergizedInteractionEIMSpace
+
+        Attributes:
+            interaction (list): list of `Interaction`s
+            l_max (int): partial wave cutoff
+            type (Type): interaction type
+        """
+        super().__init__(
+            interaction_type=interaction_type,
+            l_max=l_max,
+            **kwargs,
+        )
---
+++ packages/nuclear-rose/nuclear_rose-1.1.6-py3-none-any.whl/rose/schroedinger.py
@@ -0,0 +1,246 @@
+r"""`SchroedingerEquation` is a high-fidelity (HF), Schrödinger-equation solver for
+local, complex interactions.
+
+By default, `rose` will provide HF solution using `scipy.integrate.solve_ivp`.
+For details about providing your own solutions, see [Basis
+documentation](basis.md).
+
+"""
+
+import numpy as np
+from scipy.integrate import solve_ivp
+
+from .interaction import Interaction
+from .free_solutions import H_minus, H_plus, H_minus_prime, H_plus_prime
+from .utility import regular_inverse_s, Gamow_factor, g_coeff
+
+
+class SchroedingerEquation:
+    """
+    Solver for the single-channel, reduced, radial Schrödinger equation using scipy.integrate
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html
+
+    All other solvers for the single-channel, reduced, radial Schrödinger equation inherit from
+    this class
+    """
+
+    # Default values for solving the SE.
+    DEFAULT_R_MIN = 1e-2  # fm
+    DEFAULT_R_MAX = 30.0  # fm
+    DEFAULT_S_MIN = 1e-2  # dimensionless
+    DEFAULT_S_MAX = 8 * np.pi  # dimensionless
+    DEFAULT_S_EPSILON = 1.0e-4
+    DEFAULT_R_0 = 20.0  # fm
+    DEFAULT_NUM_PTS = 2000
+    MAX_STEPS = 20000
+    PHI_THRESHOLD = 1e-10
+
+    @classmethod
+    def make_base_solver(
+        cls,
+        rk_tols=[1e-9, 1e-9],
+        s_0=None,
+        domain=None,
+    ):
+        return SchroedingerEquation(None, rk_tols, s_0, domain)
+
+    def __init__(
+        self,
+        interaction: Interaction,
+        rk_tols: list = [1e-9, 1e-9],
+        s_0=None,
+        domain=None,
+    ):
+        r"""Solves the Shrödinger equation for local, complex potentials.
+
+        Parameters:
+            interaction (Interaction): See [Interaction documentation](interaction.md).
+            rk_tols (list): 2-element list of numbers specifying tolerances for the
+                Runge-Kutta solver: the relative tolerance and the  absolute tolerance
+            s_0 (float) :
+            domain (ndarray) :
+
+        Returns:
+            solver (SchroedingerEquation): instance of `SchroedingerEquation`
+
+        """
+        if domain is None:
+            domain = np.array([self.DEFAULT_S_MIN, self.DEFAULT_S_MAX])
+
+        if s_0 is None:
+            s_0 = domain[1] - np.pi
+            assert s_0 > 0
+
+        assert domain[1] > s_0
+
+        self.s_0 = s_0
+        self.domain = domain.copy()
+        self.interaction = interaction
+        self.rk_tols = rk_tols
+
+        if self.interaction is not None:
+            if self.interaction.k_c == 0:
+                self.eta = 0
+            else:
+                # There is Coulomb, but we haven't (yet) worked out how to emulate
+                # across energies, so we can precompute H+ and H- stuff.
+                self.eta = self.interaction.eta(None)
+
+            self.Hm = H_minus(self.s_0, self.interaction.ell, self.eta)
+            self.Hp = H_plus(self.s_0, self.interaction.ell, self.eta)
+            self.Hmp = H_minus_prime(self.s_0, self.interaction.ell, self.eta)
+            self.Hpp = H_plus_prime(self.s_0, self.interaction.ell, self.eta)
+
+            self.domain[0], self.init_cond = self.initial_conditions(
+                self.eta,
+                self.PHI_THRESHOLD,
+                self.interaction.ell,
+                self.domain[0],
+            )
+
+            assert self.domain[0] < self.s_0
+
+    def clone_for_new_interaction(self, interaction: Interaction):
+        return SchroedingerEquation(
+            interaction, self.rk_tols, s_0=self.s_0, domain=self.domain.copy()
+        )
+
+    def initial_conditions(self, eta: float, phi_threshold: float, l: int, rho_0=None):
+        r"""
+        Returns:
+            initial_conditions (tuple) : initial conditions [phi, phi'] at rho_0
+
+        Parameters:
+            eta (float): sommerfield param
+            phi_threshold (float): minimum $\phi$ value; The wave function is
+                considered zero below this value.
+            rho_0 (float): starting point for the solver
+        """
+
+        C_l = Gamow_factor(l, eta)
+        min_rho_0 = (phi_threshold / C_l) ** (1 / (l + 1))
+
+        if min_rho_0 > rho_0:
+            rho_0 = min_rho_0
+
+        phi_0 = C_l * rho_0 ** (l + 1)
+        phi_prime_0 = C_l * (l + 1) * rho_0**l
+
+        if self.interaction.is_complex:
+            initial_conditions = np.array([phi_0 * (1 + 0j), phi_prime_0 * (1 + 0j)])
+        else:
+            initial_conditions = np.array([phi_0, phi_prime_0])
+
+        return rho_0, initial_conditions
+
+    def solve_se(
+        self,
+        alpha: np.array,
+        **kwargs,
+    ):
+        r"""Solves the reduced, radial Schrödinger equation using the builtin in Runge-Kutta
+            solver in scipy.integrate.solve_ivp
+
+        Parameters:
+            alpha (ndarray): parameter vector
+            kwargs (dict) : passed to scipy.integrate.solve_ivp
+
+        Returns:
+            sol  (scipy.integrate.OdeSolution) : the radial wavefunction
+            and its first derivative; see https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.OdeSolution.html#scipy.integrate.OdeSolution
+        """
+
+        args = self.interaction.bundle_gcoeff_args(alpha)
+        sol = solve_ivp(
+            lambda s, phi: np.array(
+                [
+                    phi[1],
+                    -1 * g_coeff(s, *args) * phi[0],
+                ]
+            ),
+            self.domain,
+            self.init_cond,
+            rtol=self.rk_tols[0],
+            atol=self.rk_tols[1],
+            dense_output=True,
+            **kwargs,
+        )
+
+        return sol.sol
+
+    def rmatrix(
+        self,
+        alpha: np.array,
+        **kwargs,
+    ):
+        r"""Calculates the $\ell$-th partial wave R-matrix element at the specified energy.
+            using the Runge-Kutta method for integrating the Radial SE. kwargs are passed to
+            solve_se.
+
+        Parameters:
+            alpha (ndarray): parameter vector
+            kwargs (dict) : passed to scipy.integrate.solve_ivp
+
+        Returns:
+            rl (float)  : r-matrix element, or logarithmic derivative of wavefunction at the channel
+                radius; s_0
+
+        """
+        solution = self.solve_se(alpha, **kwargs)
+        u = solution(self.s_0)
+        rl = 1 / self.s_0 * (u[0] / u[1])
+        return rl
+
+    def phi(
+        self,
+        alpha: np.array,
+        s_mesh: np.array,
+        **kwargs,
+    ):
+        r"""Computes the reduced, radial wave function $\phi$ (or $u$) on `s_mesh` using the
+        Runge-Kutta method
+
+        Parameters:
+            alpha (ndarray): parameter vector
+            s_mesh (ndarray): values of $s$ at which $\phi$ is calculated
+            kwargs (dict) : passed to scipy.integrate.solve_ivp
+
+        Returns:
+            phi (ndarray): reduced, radial wave function
+
+        """
+        solution = np.zeros_like(s_mesh, dtype=np.complex128)
+        mask = np.logical_and(s_mesh >= self.domain[0], s_mesh <= self.domain[1])
+        phi = self.solve_se(alpha, **kwargs)
+        solution[mask] = phi(s_mesh)[0][mask]
+        return solution
+
+    def smatrix(
+        self,
+        alpha: np.array,
+        **kwargs,
+    ):
+        rl = self.rmatrix(alpha, **kwargs)
+
+        return (self.Hm - self.s_0 * rl * self.Hmp) / (
+            self.Hp - self.s_0 * rl * self.Hpp
+        )
+
+    def delta(
+        self,
+        alpha: np.array,
+        **kwargs,
+    ):
+        r"""Calculates the $\ell$-th partial wave phase shift
+
+        Parameters:
+            alpha (ndarray): parameter vector
+
+        Returns:
+            delta (float): phase shift extracted from the reduced, radial
+                wave function
+
+        """
+        sl = self.smatrix(alpha, **kwargs)
+
+        return np.log(sl) / 2j
