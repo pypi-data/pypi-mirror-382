cb2b070738584df61e51ac0419e0cdd7ae5d3b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 16:05:25 +0000

    Add package paperwrite/0.1.0/paperwrite-0.1.0.tar.gz

---
+++ packages/paperwrite/paperwrite-0.1.0.tar.gz/paperwrite-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paperwrite',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='Your name',
+    author_email='zhc20150305@sina.com',
+    description='你可以在里面写字',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
