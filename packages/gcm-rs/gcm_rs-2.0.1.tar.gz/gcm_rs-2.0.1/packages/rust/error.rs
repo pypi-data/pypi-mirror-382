782febf9dcc59e78ad7b64948e64f75976a9addb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:05:16 +0000

    Add package gcm-rs/2.0.1/gcm_rs-2.0.1.tar.gz

---
+++ packages/gcm-rs/gcm_rs-2.0.1.tar.gz/gcm_rs-2.0.1/packages/rust/error.rs
@@ -0,0 +1,30 @@
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    InvalidNonceSize { expected_size: usize },
+    InvalidKeySize,
+    InvalidTag,
+}
+
+impl Display for Error {
+    fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
+        match self {
+            Error::InvalidNonceSize { expected_size } => {
+                write!(
+                    fmt,
+                    "invalid nonce size, expected {}",
+                    expected_size
+                )
+            }
+            Error::InvalidKeySize => {
+                write!(fmt, "invalid key size")
+            }
+            Error::InvalidTag => {
+                write!(fmt, "invalid authentication tag")
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {}
