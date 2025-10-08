c89452f95d2c4cfb92ad21511ffb442201b35c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:05:10 +0000

    Add package ssm-cli/0.0.8/ssm_cli-0.0.8.tar.gz

---
+++ packages/ssm-cli/ssm_cli-0.0.8.tar.gz/ssm_cli-0.0.8/ssm_cli/xdg.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from xdg_base_dirs import xdg_config_home
+
+# We just put everything in the same directory as there is so little
+
+def get_conf_root(check=True) -> Path:
+    root = xdg_config_home() / 'ssm-cli'
+    if check and not root.exists():
+        from ssm_cli.commands.setup import create_conf_dir
+        create_conf_dir()
+    return root
+
+def get_conf_file(check=True) -> Path:
+    path = get_conf_root(check) / 'ssm.yaml'
+    if check and not path.exists():
+        raise EnvironmentError(f"{path} missing, run `ssm setup` to create")
+    return path
+
+def get_log_file(check=False) -> Path:
+    path = get_conf_root(True) / 'ssm.log'
+    if check and not path.exists():
+        raise EnvironmentError(f"{path} missing, run `ssm setup` to create")
+    return path
+
+def get_ssh_hostkey(check=True) -> Path:
+    path = get_conf_root(check) / 'hostkey.pem'
+    if check and not path.exists():
+        raise EnvironmentError(f"{path} missing, run `ssm setup` to create")
+    return path
