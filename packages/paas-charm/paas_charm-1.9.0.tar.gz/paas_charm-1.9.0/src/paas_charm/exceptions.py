cb2959ba4c058bc76e93ca8a277b66055450b9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:24 +0000

    Add package paas-charm/1.9.0/paas_charm-1.9.0.tar.gz

---
+++ packages/paas-charm/paas_charm-1.9.0.tar.gz/paas_charm-1.9.0/src/paas_charm/exceptions.py
@@ -0,0 +1,38 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Exceptions used by charms."""
+
+
+class CharmConfigInvalidError(Exception):
+    """Exception raised when a charm configuration is found to be invalid.
+
+    Attrs:
+        msg (str): Explanation of the error.
+    """
+
+    def __init__(self, msg: str):
+        """Initialize a new instance of the CharmConfigInvalidError exception.
+
+        Args:
+            msg (str): Explanation of the error.
+        """
+        self.msg = msg
+
+
+class PebbleNotReadyError(Exception):
+    """Exception raised when accessing pebble while it isn't ready."""
+
+
+class MissingCharmLibraryError(Exception):
+    """Raised when a required charm library is missing."""
+
+
+class InvalidRelationDataError(Exception):
+    """Represents an invalid relation data.
+
+    Attributes:
+        relation: The name of the relation with invalid data.
+    """
+
+    relation: str
