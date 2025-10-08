141f90fc9993bc7a2429220798d44bdbc9b090a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:44 +0000

    Add package bits-bitsdb/2.0.0/bits_bitsdb-2.0.0.tar.gz

---
+++ packages/bits-bitsdb/bits_bitsdb-2.0.0.tar.gz/bits_bitsdb-2.0.0/bits/bitsdb/firestore.py
@@ -0,0 +1,7 @@
+"""Firestore class file."""
+
+from bits.google import Firestore as FirestoreBase
+
+
+class Firestore(FirestoreBase):
+    """Firestore class."""
---
+++ packages/bits-bitsdb/bits_bitsdb-2.0.0.tar.gz/bits_bitsdb-2.0.0/docs/README.md
@@ -0,0 +1,3 @@
+# bits-bitsdb Documentation
+
+This documentation provides an overview and usage instructions for the bits-bitsdb API and utilities.
