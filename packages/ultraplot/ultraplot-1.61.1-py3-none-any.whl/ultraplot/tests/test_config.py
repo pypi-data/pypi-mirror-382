a562edd275da61648539a2eb4cf0b11c300cb799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 15:06:08 +0000

    Add package ultraplot/1.61.1/ultraplot-1.61.1-py3-none-any.whl

---
+++ packages/ultraplot/ultraplot-1.61.1-py3-none-any.whl/ultraplot/tests/test_config.py
@@ -0,0 +1,19 @@
+import ultraplot as uplt, pytest
+
+
+def test_wrong_keyword_reset():
+    """
+    The context should reset after a failed attempt.
+    """
+    # Init context
+    uplt.rc.context()
+    config = uplt.rc
+    # Set a wrong key
+    with pytest.raises(KeyError):
+        config._get_item_dicts("non_existing_key", "non_existing_value")
+    # Set a known good value
+    config._get_item_dicts("coastcolor", "black")
+    # Confirm we can still plot
+    fig, ax = uplt.subplots(proj="cyl")
+    ax.format(coastcolor="black")
+    fig.canvas.draw()
