b4bba284016be80e8b7256079f6e98a2c481564e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:37 +0000

    Add package accelbyte-py-sdk-service-inventory/0.20.0/accelbyte_py_sdk_service_inventory-0.20.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-inventory/accelbyte_py_sdk_service_inventory-0.20.0-py3-none-any.whl/accelbyte_py_sdk/api/inventory/operations/public_items/public_consume_my_item.py
@@ -0,0 +1,392 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Inventory Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ApimodelsConsumeItemReq
+from ...models import ApimodelsErrorResponse
+from ...models import ApimodelsItemResp
+
+
+class PublicConsumeMyItem(Operation):
+    """To consume item (PublicConsumeMyItem)
+
+    Consume user's own item.
+    Client should pass item ID in options if item type is OPTIONBOX
+
+    Properties:
+        url: /inventory/v1/public/namespaces/{namespace}/users/me/inventories/{inventoryId}/consume
+
+        method: POST
+
+        tags: ["Public Items"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        body: (body) REQUIRED ApimodelsConsumeItemReq in body
+
+        inventory_id: (inventoryId) REQUIRED str in path
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        200: OK - ApimodelsItemResp (OK)
+
+        400: Bad Request - ApimodelsErrorResponse (Bad Request)
+
+        404: Not Found - ApimodelsErrorResponse (Not Found)
+
+        500: Internal Server Error - ApimodelsErrorResponse (Internal Server Error)
+    """
+
+    # region fields
+
+    _url: str = "/inventory/v1/public/namespaces/{namespace}/users/me/inventories/{inventoryId}/consume"
+    _path: str = "/inventory/v1/public/namespaces/{namespace}/users/me/inventories/{inventoryId}/consume"
+    _base_path: str = ""
+    _method: str = "POST"
+    _consumes: List[str] = ["application/json"]
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "inventory"
+
+    body: ApimodelsConsumeItemReq  # REQUIRED in [body]
+    inventory_id: str  # REQUIRED in [path]
+    namespace: str  # REQUIRED in [path]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "body": self.get_body_params(),
+            "path": self.get_path_params(),
+        }
+
+    def get_body_params(self) -> Any:
+        if not hasattr(self, "body") or self.body is None:
+            return None
+        return self.body.to_dict()
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "inventory_id"):
+            result["inventoryId"] = self.inventory_id
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_body(self, value: ApimodelsConsumeItemReq) -> PublicConsumeMyItem:
+        self.body = value
+        return self
+
+    def with_inventory_id(self, value: str) -> PublicConsumeMyItem:
+        self.inventory_id = value
+        return self
+
+    def with_namespace(self, value: str) -> PublicConsumeMyItem:
+        self.namespace = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "body") and self.body:
+            result["body"] = self.body.to_dict(include_empty=include_empty)
+        elif include_empty:
+            result["body"] = ApimodelsConsumeItemReq()
+        if hasattr(self, "inventory_id") and self.inventory_id:
+            result["inventoryId"] = str(self.inventory_id)
+        elif include_empty:
+            result["inventoryId"] = ""
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_200: Optional[ApimodelsItemResp] = None
+        error_400: Optional[ApimodelsErrorResponse] = None
+        error_404: Optional[ApimodelsErrorResponse] = None
+        error_500: Optional[ApimodelsErrorResponse] = None
+
+        def ok(self) -> PublicConsumeMyItem.Response:
+            if self.error_400 is not None:
+                err = self.error_400.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_404 is not None:
+                err = self.error_404.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_500 is not None:
+                err = self.error_500.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_200 is not None:
+                yield self.data_200
+                yield None
+            elif self.error_400 is not None:
+                yield None
+                yield self.error_400
+            elif self.error_404 is not None:
+                yield None
+                yield self.error_404
+            elif self.error_500 is not None:
+                yield None
+                yield self.error_500
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        200: OK - ApimodelsItemResp (OK)
+
+        400: Bad Request - ApimodelsErrorResponse (Bad Request)
+
+        404: Not Found - ApimodelsErrorResponse (Not Found)
+
+        500: Internal Server Error - ApimodelsErrorResponse (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = PublicConsumeMyItem.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 200:
+                result.data_200 = ApimodelsItemResp.create_from_dict(content)
+            elif code == 400:
+                result.error_400 = ApimodelsErrorResponse.create_from_dict(content)
+                result.error = result.error_400.translate_to_api_error()
+            elif code == 404:
+                result.error_404 = ApimodelsErrorResponse.create_from_dict(content)
+                result.error = result.error_404.translate_to_api_error()
+            elif code == 500:
+                result.error_500 = ApimodelsErrorResponse.create_from_dict(content)
+                result.error = result.error_500.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[
+        Union[None, ApimodelsItemResp],
+        Union[None, ApimodelsErrorResponse, HttpResponse],
+    ]:
+        """Parse the given response.
+
+        200: OK - ApimodelsItemResp (OK)
+
+        400: Bad Request - ApimodelsErrorResponse (Bad Request)
+
+        404: Not Found - ApimodelsErrorResponse (Not Found)
+
+        500: Internal Server Error - ApimodelsErrorResponse (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 200:
+            return ApimodelsItemResp.create_from_dict(content), None
+        if code == 400:
+            return None, ApimodelsErrorResponse.create_from_dict(content)
+        if code == 404:
+            return None, ApimodelsErrorResponse.create_from_dict(content)
+        if code == 500:
+            return None, ApimodelsErrorResponse.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, body: ApimodelsConsumeItemReq, inventory_id: str, namespace: str, **kwargs
+    ) -> PublicConsumeMyItem:
+        instance = cls()
+        instance.body = body
+        instance.inventory_id = inventory_id
+        instance.namespace = namespace
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> PublicConsumeMyItem:
+        instance = cls()
+        if "body" in dict_ and dict_["body"] is not None:
+            instance.body = ApimodelsConsumeItemReq.create_from_dict(
+                dict_["body"], include_empty=include_empty
+            )
+        elif include_empty:
+            instance.body = ApimodelsConsumeItemReq()
+        if "inventoryId" in dict_ and dict_["inventoryId"] is not None:
+            instance.inventory_id = str(dict_["inventoryId"])
+        elif include_empty:
+            instance.inventory_id = ""
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "body": "body",
+            "inventoryId": "inventory_id",
+            "namespace": "namespace",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "body": True,
+            "inventoryId": True,
+            "namespace": True,
+        }
+
+    # endregion static methods
