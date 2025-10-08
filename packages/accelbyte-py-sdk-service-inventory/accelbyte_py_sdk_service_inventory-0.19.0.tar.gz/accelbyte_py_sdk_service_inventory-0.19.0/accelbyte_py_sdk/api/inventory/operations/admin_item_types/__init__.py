060faff21c5c32fd91aceaaf6e1d21d0a4432ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:15 +0000

    Add package accelbyte-py-sdk-service-inventory/0.19.0/accelbyte_py_sdk_service_inventory-0.19.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-inventory/accelbyte_py_sdk_service_inventory-0.19.0.tar.gz/accelbyte_py_sdk_service_inventory-0.19.0/accelbyte_py_sdk/api/inventory/operations/admin_item_types/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Inventory Service."""
+
+__version__ = "0.2.17"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .admin_create_item_type import AdminCreateItemType
+from .admin_delete_item_type import AdminDeleteItemType
+from .admin_list_item_types import AdminListItemTypes
+from .admin_list_item_types import (
+    SortByEnum as AdminListItemTypesSortByEnum,
+)
---
+++ packages/accelbyte-py-sdk-service-inventory/accelbyte_py_sdk_service_inventory-0.19.0.tar.gz/accelbyte_py_sdk_service_inventory-0.19.0/accelbyte_py_sdk/api/inventory/operations/admin_item_types/admin_delete_item_type.py
@@ -0,0 +1,343 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Inventory Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ApimodelsErrorResponse
+
+
+class AdminDeleteItemType(Operation):
+    """To delete an item type (AdminDeleteItemType)
+
+    This endpoint will delete a item type by itemtypeName in a specified namespace.
+    If the itemtypeName doesn't exist in a namespace, it'll return not found.
+
+    Permission: ADMIN:NAMESPACE:{namespace}:INVENTORY:ITEMTYPE [DELETE]
+
+    Properties:
+        url: /inventory/v1/admin/namespaces/{namespace}/itemtypes/{itemTypeName}
+
+        method: DELETE
+
+        tags: ["Admin Item Types"]
+
+        consumes: []
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        item_type_name: (itemTypeName) REQUIRED str in path
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        204: No Content - (No Content)
+
+        404: Not Found - ApimodelsErrorResponse (Not Found)
+
+        500: Internal Server Error - ApimodelsErrorResponse (Internal Server Error)
+    """
+
+    # region fields
+
+    _url: str = "/inventory/v1/admin/namespaces/{namespace}/itemtypes/{itemTypeName}"
+    _path: str = "/inventory/v1/admin/namespaces/{namespace}/itemtypes/{itemTypeName}"
+    _base_path: str = ""
+    _method: str = "DELETE"
+    _consumes: List[str] = []
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "inventory"
+
+    item_type_name: str  # REQUIRED in [path]
+    namespace: str  # REQUIRED in [path]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "path": self.get_path_params(),
+        }
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "item_type_name"):
+            result["itemTypeName"] = self.item_type_name
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_item_type_name(self, value: str) -> AdminDeleteItemType:
+        self.item_type_name = value
+        return self
+
+    def with_namespace(self, value: str) -> AdminDeleteItemType:
+        self.namespace = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "item_type_name") and self.item_type_name:
+            result["itemTypeName"] = str(self.item_type_name)
+        elif include_empty:
+            result["itemTypeName"] = ""
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_204: Optional[HttpResponse] = None
+        error_404: Optional[ApimodelsErrorResponse] = None
+        error_500: Optional[ApimodelsErrorResponse] = None
+
+        def ok(self) -> AdminDeleteItemType.Response:
+            if self.error_404 is not None:
+                err = self.error_404.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_500 is not None:
+                err = self.error_500.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_204 is not None:
+                yield self.data_204
+                yield None
+            elif self.error_404 is not None:
+                yield None
+                yield self.error_404
+            elif self.error_500 is not None:
+                yield None
+                yield self.error_500
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        204: No Content - (No Content)
+
+        404: Not Found - ApimodelsErrorResponse (Not Found)
+
+        500: Internal Server Error - ApimodelsErrorResponse (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = AdminDeleteItemType.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 204:
+                result.data_204 = None
+            elif code == 404:
+                result.error_404 = ApimodelsErrorResponse.create_from_dict(content)
+                result.error = result.error_404.translate_to_api_error()
+            elif code == 500:
+                result.error_500 = ApimodelsErrorResponse.create_from_dict(content)
+                result.error = result.error_500.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[None, Union[None, ApimodelsErrorResponse, HttpResponse]]:
+        """Parse the given response.
+
+        204: No Content - (No Content)
+
+        404: Not Found - ApimodelsErrorResponse (Not Found)
+
+        500: Internal Server Error - ApimodelsErrorResponse (Internal Server Error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 204:
+            return None, None
+        if code == 404:
+            return None, ApimodelsErrorResponse.create_from_dict(content)
+        if code == 500:
+            return None, ApimodelsErrorResponse.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, item_type_name: str, namespace: str, **kwargs
+    ) -> AdminDeleteItemType:
+        instance = cls()
+        instance.item_type_name = item_type_name
+        instance.namespace = namespace
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> AdminDeleteItemType:
+        instance = cls()
+        if "itemTypeName" in dict_ and dict_["itemTypeName"] is not None:
+            instance.item_type_name = str(dict_["itemTypeName"])
+        elif include_empty:
+            instance.item_type_name = ""
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "itemTypeName": "item_type_name",
+            "namespace": "namespace",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "itemTypeName": True,
+            "namespace": True,
+        }
+
+    # endregion static methods
