b86e34960dd5a29147778416d8e4ff7501fd15b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 17:06:14 +0000

    Add package microsoft-agents-hosting-core/0.4.0.dev18/microsoft_agents_hosting_core-0.4.0.dev18.tar.gz

---
+++ packages/microsoft-agents-hosting-core/microsoft_agents_hosting_core-0.4.0.dev18.tar.gz/microsoft_agents_hosting_core-0.4.0.dev18/microsoft_agents/hosting/core/connector/user_token_base.py
@@ -0,0 +1,44 @@
+from abc import abstractmethod
+from typing import Protocol
+
+from microsoft_agents.activity import TokenResponse, TokenStatus
+
+
+class UserTokenBase(Protocol):
+    @abstractmethod
+    async def get_token(
+        self,
+        user_id: str,
+        connection_name: str,
+        channel_id: str = None,
+        code: str = None,
+    ) -> TokenResponse:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def get_aad_tokens(
+        self,
+        user_id: str,
+        connection_name: str,
+        channel_id: str = None,
+        body: dict = None,
+    ) -> dict[str, TokenResponse]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def sign_out(
+        self, user_id: str, connection_name: str = None, channel_id: str = None
+    ) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def get_token_status(
+        self, user_id: str, channel_id: str = None, include: str = None
+    ) -> list[TokenStatus]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def exchange_token(
+        self, user_id: str, connection_name: str, channel_id: str, body: dict = None
+    ) -> TokenResponse:
+        raise NotImplementedError()
