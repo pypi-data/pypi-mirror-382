36302937087cc59adbafb16c99b41e36c8ea387c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:15:57 +0000

    Add package aece/2.0.1/aece-2.0.1.tar.gz

---
+++ packages/aece/aece-2.0.1.tar.gz/aece-2.0.1/README.md
@@ -0,0 +1,10 @@
+# The aece library in Python prints the text written inside it, like the print function.  
+
+# Telegram  :  @LAEGER_MO
+# The installation method is like this 
+pip install aece
+#After installation, how to use it 
+
+python```
+aece("Hello world ")
+```
---
+++ packages/aece/aece-2.0.1.tar.gz/aece-2.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aece",
+    version="2.0.1",
+    packages=find_packages(),
+    description="Use 'aece()' instead of 'print()' in Python",
+    long_description=open("README.md", encoding="utf-8").read(),  
+    long_description_content_type="text/markdown",
+    author="Programmer Seo Hook : @LAEGER_MO ",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+)
