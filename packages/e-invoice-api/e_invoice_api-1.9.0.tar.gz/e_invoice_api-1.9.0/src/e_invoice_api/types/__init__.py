1f61763e4e89343140b223458f38f4e34c65ef4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 17:06:42 +0000

    Add package e-invoice-api/1.9.0/e_invoice_api-1.9.0.tar.gz

---
+++ packages/e-invoice-api/e_invoice_api-1.9.0.tar.gz/e_invoice_api-1.9.0/src/e_invoice_api/types/__init__.py
@@ -0,0 +1,40 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .certificate import Certificate as Certificate
+from .currency_code import CurrencyCode as CurrencyCode
+from .document_type import DocumentType as DocumentType
+from .document_state import DocumentState as DocumentState
+from .webhook_response import WebhookResponse as WebhookResponse
+from .document_response import DocumentResponse as DocumentResponse
+from .inbox_list_params import InboxListParams as InboxListParams
+from .document_direction import DocumentDirection as DocumentDirection
+from .document_send_params import DocumentSendParams as DocumentSendParams
+from .unit_of_measure_code import UnitOfMeasureCode as UnitOfMeasureCode
+from .webhook_create_params import WebhookCreateParams as WebhookCreateParams
+from .webhook_list_response import WebhookListResponse as WebhookListResponse
+from .webhook_update_params import WebhookUpdateParams as WebhookUpdateParams
+from .document_create_params import DocumentCreateParams as DocumentCreateParams
+from .lookup_retrieve_params import LookupRetrieveParams as LookupRetrieveParams
+from .ubl_document_validation import UblDocumentValidation as UblDocumentValidation
+from .webhook_delete_response import WebhookDeleteResponse as WebhookDeleteResponse
+from .document_delete_response import DocumentDeleteResponse as DocumentDeleteResponse
+from .lookup_retrieve_response import LookupRetrieveResponse as LookupRetrieveResponse
+from .inbox_list_invoices_params import InboxListInvoicesParams as InboxListInvoicesParams
+from .paginated_document_response import PaginatedDocumentResponse as PaginatedDocumentResponse
+from .payment_detail_create_param import PaymentDetailCreateParam as PaymentDetailCreateParam
+from .validate_validate_ubl_params import ValidateValidateUblParams as ValidateValidateUblParams
+from .validate_validate_json_params import ValidateValidateJsonParams as ValidateValidateJsonParams
+from .inbox_list_credit_notes_params import InboxListCreditNotesParams as InboxListCreditNotesParams
+from .document_attachment_create_param import DocumentAttachmentCreateParam as DocumentAttachmentCreateParam
+from .outbox_list_draft_documents_params import OutboxListDraftDocumentsParams as OutboxListDraftDocumentsParams
+from .validate_validate_peppol_id_params import ValidateValidatePeppolIDParams as ValidateValidatePeppolIDParams
+from .lookup_retrieve_participants_params import LookupRetrieveParticipantsParams as LookupRetrieveParticipantsParams
+from .validate_validate_peppol_id_response import ValidateValidatePeppolIDResponse as ValidateValidatePeppolIDResponse
+from .lookup_retrieve_participants_response import (
+    LookupRetrieveParticipantsResponse as LookupRetrieveParticipantsResponse,
+)
+from .outbox_list_received_documents_params import (
+    OutboxListReceivedDocumentsParams as OutboxListReceivedDocumentsParams,
+)
---
+++ packages/e-invoice-api/e_invoice_api-1.9.0.tar.gz/e_invoice_api-1.9.0/src/e_invoice_api/types/documents/__init__.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .ubl_get_response import UblGetResponse as UblGetResponse
+from .document_attachment import DocumentAttachment as DocumentAttachment
+from .attachment_add_params import AttachmentAddParams as AttachmentAddParams
+from .attachment_list_response import AttachmentListResponse as AttachmentListResponse
+from .attachment_delete_response import AttachmentDeleteResponse as AttachmentDeleteResponse
