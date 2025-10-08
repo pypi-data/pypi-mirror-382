0495e2a7e8d6a47872d062760d31a0ef09132e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:06:09 +0000

    Add package accelbyte-py-sdk-service-basic/0.24.0/accelbyte_py_sdk_service_basic-0.24.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-basic/accelbyte_py_sdk_service_basic-0.24.0.tar.gz/accelbyte_py_sdk_service_basic-0.24.0/accelbyte_py_sdk/api/basic/operations/user_profile/public_get_user_profile_cdd77a.py
@@ -0,0 +1,336 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# AccelByte Gaming Services Basic Service
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import UserProfilePublicInfo
+from ...models import ValidationErrorEntity
+
+
+class PublicGetUserProfilePublicInfoByIds(Operation):
+    """Get user profile public info by ids (publicGetUserProfilePublicInfoByIds)
+
+    Get user public profile by ids.
+    Other detail info:
+
+      * Action code : 11405
+      *  Returns : user public profiles
+
+    Properties:
+        url: /basic/v1/public/namespaces/{namespace}/profiles/public
+
+        method: GET
+
+        tags: ["UserProfile"]
+
+        consumes: []
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+
+        namespace: (namespace) REQUIRED str in path
+
+        user_ids: (userIds) REQUIRED str in query
+
+    Responses:
+        200: OK - List[UserProfilePublicInfo] (Successful operation)
+
+        400: Bad Request - ValidationErrorEntity (20002: validation error)
+    """
+
+    # region fields
+
+    _url: str = "/basic/v1/public/namespaces/{namespace}/profiles/public"
+    _path: str = "/basic/v1/public/namespaces/{namespace}/profiles/public"
+    _base_path: str = ""
+    _method: str = "GET"
+    _consumes: List[str] = []
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "basic"
+
+    namespace: str  # REQUIRED in [path]
+    user_ids: str  # REQUIRED in [query]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "path": self.get_path_params(),
+            "query": self.get_query_params(),
+        }
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    def get_query_params(self) -> dict:
+        result = {}
+        if hasattr(self, "user_ids"):
+            result["userIds"] = self.user_ids
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_namespace(self, value: str) -> PublicGetUserProfilePublicInfoByIds:
+        self.namespace = value
+        return self
+
+    def with_user_ids(self, value: str) -> PublicGetUserProfilePublicInfoByIds:
+        self.user_ids = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        if hasattr(self, "user_ids") and self.user_ids:
+            result["userIds"] = str(self.user_ids)
+        elif include_empty:
+            result["userIds"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_200: Optional[List[UserProfilePublicInfo]] = None
+        error_400: Optional[ValidationErrorEntity] = None
+
+        def ok(self) -> PublicGetUserProfilePublicInfoByIds.Response:
+            if self.error_400 is not None:
+                err = self.error_400.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_200 is not None:
+                yield self.data_200
+                yield None
+            elif self.error_400 is not None:
+                yield None
+                yield self.error_400
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        200: OK - List[UserProfilePublicInfo] (Successful operation)
+
+        400: Bad Request - ValidationErrorEntity (20002: validation error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = PublicGetUserProfilePublicInfoByIds.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 200:
+                result.data_200 = [
+                    UserProfilePublicInfo.create_from_dict(i) for i in content
+                ]
+            elif code == 400:
+                result.error_400 = ValidationErrorEntity.create_from_dict(content)
+                result.error = result.error_400.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[
+        Union[None, List[UserProfilePublicInfo]],
+        Union[None, HttpResponse, ValidationErrorEntity],
+    ]:
+        """Parse the given response.
+
+        200: OK - List[UserProfilePublicInfo] (Successful operation)
+
+        400: Bad Request - ValidationErrorEntity (20002: validation error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 200:
+            return [UserProfilePublicInfo.create_from_dict(i) for i in content], None
+        if code == 400:
+            return None, ValidationErrorEntity.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, namespace: str, user_ids: str, **kwargs
+    ) -> PublicGetUserProfilePublicInfoByIds:
+        instance = cls()
+        instance.namespace = namespace
+        instance.user_ids = user_ids
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> PublicGetUserProfilePublicInfoByIds:
+        instance = cls()
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        if "userIds" in dict_ and dict_["userIds"] is not None:
+            instance.user_ids = str(dict_["userIds"])
+        elif include_empty:
+            instance.user_ids = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "namespace": "namespace",
+            "userIds": "user_ids",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "namespace": True,
+            "userIds": True,
+        }
+
+    # endregion static methods
