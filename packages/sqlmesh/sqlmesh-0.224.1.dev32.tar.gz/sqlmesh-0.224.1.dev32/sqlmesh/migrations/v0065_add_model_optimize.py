e344c5e8e7981bea00cd4d3f77f0a2bb81a313ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 08:05:34 +0000

    Add package sqlmesh/0.224.1.dev32/sqlmesh-0.224.1.dev32.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev32.tar.gz/sqlmesh-0.224.1.dev32/sqlmesh/migrations/v0065_add_model_optimize.py
@@ -0,0 +1,9 @@
+"""Add the optimize_query model attribute."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev32.tar.gz/sqlmesh-0.224.1.dev32/web/common/src/index.ts
@@ -0,0 +1,76 @@
+// Components
+export { Badge, type BadgeProps } from '@/components/Badge/Badge'
+export {
+  Button,
+  type ButtonProps,
+  type ButtonVariant,
+} from '@/components/Button/Button'
+export {
+  CopyButton,
+  type CopyButtonProps,
+} from '@/components/CopyButton/CopyButton'
+export {
+  HorizontalContainer,
+  type HorizontalContainerProps,
+} from '@/components/HorizontalContainer/HorizontalContainer'
+export {
+  ScrollContainer,
+  type ScrollContainerProps,
+} from '@/components/ScrollContainer/ScrollContainer'
+export {
+  VerticalContainer,
+  type VerticalContainerProps,
+} from '@/components/VerticalContainer/VerticalContainer'
+export {
+  ModelName,
+  type ModelNameProps,
+} from '@/components/ModelName/ModelName'
+export { Tooltip, type TooltipProps } from '@/components/Tooltip/Tooltip'
+export {
+  VirtualList,
+  type VirtualListProps,
+} from '@/components/VirtualList/VirtualList'
+export {
+  FilterableList,
+  type FilterableListProps,
+} from '@/components/VirtualList/FilterableList'
+export {
+  MessageContainer,
+  type MessageContainerProps,
+} from '@/components/MessageContainer/MessageContainer'
+export { Input, type InputProps } from '@/components/Input/Input'
+export {
+  LoadingContainer,
+  type LoadingContainerProps,
+} from '@/components/LoadingContainer/LoadingContainer'
+export { Metadata, type MetadataProps } from '@/components/Metadata/Metadata'
+export {
+  Description,
+  type DescriptionProps,
+} from '@/components/Typography/Description'
+export { Headline, type HeadlineProps } from '@/components/Typography/Headline'
+export {
+  Information,
+  type InformationProps,
+} from '@/components/Typography/Information'
+export { Tagline, type TaglineProps } from '@/components/Typography/Tagline'
+export { Text, type TextProps } from '@/components/Typography/Text'
+
+// Hooks
+export { useCopyClipboard } from '@/hooks/useCopyClipboard'
+
+// Utils
+export { cn, truncate } from '@/utils'
+
+// Types
+export type {
+  Brand,
+  Branded,
+  Size,
+  HeadlineLevel,
+  Side,
+  LayoutDirection,
+  Shape,
+  Position,
+  Callback,
+} from '@/types'
