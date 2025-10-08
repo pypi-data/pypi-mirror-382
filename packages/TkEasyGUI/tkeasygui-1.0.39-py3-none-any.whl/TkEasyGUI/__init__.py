2f27d8cd2f0308974c53b8e8bc4f08363e6092e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:06:07 +0000

    Add package TkEasyGUI/1.0.39/tkeasygui-1.0.39-py3-none-any.whl

---
+++ packages/TkEasyGUI/tkeasygui-1.0.39-py3-none-any.whl/TkEasyGUI/__init__.py
@@ -0,0 +1,12 @@
+# pylint: disable=invalid-name, unused-import, unused-wildcard-import, wildcard-import
+"""TkEasyGUI - A simple GUI library for Python using tkinter."""
+
+from .dialogs import *  # noqa: F403, I001 # pylint: disable=redefined-builtin
+from .locale_easy import *  # noqa: F403, I001
+from .utils import *  # noqa: F403, I001
+from .version import __version__  # noqa: F401, I001
+from .widgets import *  # noqa: F403, I001
+
+if __name__ == "__main__":
+    print(__doc__)
+    print("Version: ", __version__)
