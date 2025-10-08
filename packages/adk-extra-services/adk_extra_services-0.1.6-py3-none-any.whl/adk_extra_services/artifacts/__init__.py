ac5d3246d98b6a6c3829c0a2280b4a410b87c569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:04:59 +0000

    Add package adk-extra-services/0.1.6/adk_extra_services-0.1.6-py3-none-any.whl

---
+++ packages/adk-extra-services/adk_extra_services-0.1.6-py3-none-any.whl/adk_extra_services/artifacts/__init__.py
@@ -0,0 +1,11 @@
+"""Artifact service implementations for Google ADK."""
+
+from .azure_artifact_service import AzureBlobArtifactService
+from .local_folder_artifact_service import LocalFolderArtifactService
+from .s3_artifact_service import S3ArtifactService
+
+__all__ = [
+    "AzureBlobArtifactService",
+    "S3ArtifactService",
+    "LocalFolderArtifactService",
+]
