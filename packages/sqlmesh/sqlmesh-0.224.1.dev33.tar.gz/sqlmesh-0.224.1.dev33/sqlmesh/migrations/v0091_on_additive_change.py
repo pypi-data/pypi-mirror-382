a4f0a050c56a04df458cd402452ce6c105323144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 08:05:37 +0000

    Add package sqlmesh/0.224.1.dev33/sqlmesh-0.224.1.dev33.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev33.tar.gz/sqlmesh-0.224.1.dev33/sqlmesh/migrations/v0091_on_additive_change.py
@@ -0,0 +1,9 @@
+"""Add on_additive_change to incremental model metadata hash."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
