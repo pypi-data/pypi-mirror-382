6daea3a7f1715ca91a801dfea53d261afe05da50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 14:08:26 +0000

    Add package sqlmesh/0.224.1.dev35/sqlmesh-0.224.1.dev35-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev35-py3-none-any.whl/sqlmesh/migrations/v0093_use_raw_sql_in_fingerprint.py
@@ -0,0 +1,9 @@
+"""Use the raw SQL when computing the model fingerprint."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
