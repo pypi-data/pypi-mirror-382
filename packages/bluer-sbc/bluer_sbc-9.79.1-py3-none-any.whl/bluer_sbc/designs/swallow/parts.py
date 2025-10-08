9e38a7ee67de81213f07999d7ef694215bc6e2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 17:05:42 +0000

    Add package bluer-sbc/9.79.1/bluer_sbc-9.79.1-py3-none-any.whl

---
+++ packages/bluer-sbc/bluer_sbc-9.79.1-py3-none-any.whl/bluer_sbc/designs/swallow/parts.py
@@ -0,0 +1,7 @@
+dict_of_parts = {
+    "BTS7960": "2 x",
+    "connector": "2 females",
+    "white-terminal": "8 x",
+    "nuts-bolts-spacers": "🚧",
+    "solid-cable-1-15": "20 cm x (red + black/blue)",
+}
