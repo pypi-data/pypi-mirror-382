2161f45f356f5787bde5dbf8c2a0c45619ed4e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 15:06:09 +0000

    Add package plain.redirection/0.22.0/plain_redirection-0.22.0.tar.gz

---
+++ packages/plain.redirection/plain_redirection-0.22.0.tar.gz/plain_redirection-0.22.0/plain/redirection/chores.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from plain.chores import register_chore
+from plain.runtime import settings
+from plain.utils import timezone
+
+from .models import NotFoundLog, RedirectLog
+
+
+@register_chore("redirection")
+def delete_logs() -> str:
+    """
+    Delete logs older than REDIRECTION_LOG_RETENTION_TIMEDELTA.
+    """
+    cutoff = timezone.now() - settings.REDIRECTION_LOG_RETENTION_TIMEDELTA
+
+    result = RedirectLog.query.filter(created_at__lt=cutoff).delete()
+    output = f"{result[0]} redirect logs deleted"
+
+    result = NotFoundLog.query.filter(created_at__lt=cutoff).delete()
+    output += f", {result[0]} not found logs deleted"
+
+    return output
