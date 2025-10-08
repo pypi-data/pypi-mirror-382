6e58c2bccfc1467e5e964b65b5c1f61408dd0403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:14:52 +0000

    Add package b5/1.5.1/b5-1.5.1.tar.gz

---
+++ packages/b5/b5-1.5.1.tar.gz/b5-1.5.1/b5/modules/pipenv.py
@@ -0,0 +1,123 @@
+import os
+import shlex
+from typing import ClassVar
+
+from . import BaseModule
+
+
+class PipenvModule(BaseModule):
+    '''Pipenv module
+    '''
+
+    DEFAULT_CONFIG: ClassVar = {
+        'base_path': '.',
+        'pipenv_bin': 'pipenv',
+        'pyenv_bin': 'pyenv',
+        'use_pyenv': True,
+        'install_dev': True,
+        'store_venv_in_project': True,  # Sets PIPENV_VENV_IN_PROJECT to 1
+        'pipfile': 'Pipfile',  # Sets PIPENV_PIPFILE
+    }
+
+    def prepare_config(self) -> None:
+        self.config['base_path'] = os.path.realpath(os.path.join(
+            self.state.run_path,
+            self.config['base_path'],
+        ))
+
+    def _pipenv_environment(self) -> str:
+        return '''
+            {pyenv_init}
+            export PIPENV_VENV_IN_PROJECT="{store_venv_in_project}"
+            export PIPENV_PIPFILE={pipfile}
+        '''.format(
+            pyenv_init=(
+                'eval "$( {pyenv_bin} init - )"'.format(pyenv_bin=shlex.quote(self.config['pyenv_bin']))
+                if self.config['use_pyenv']
+                else ''
+            ),
+            store_venv_in_project='1' if self.config['store_venv_in_project'] else '',
+            pipfile=shlex.quote(self.config['pipfile']),
+        )
+
+    def is_installed_script(self) -> str:
+        """
+        Add a check to evaluate whether the pipenv module bin is installed or not
+        Returns: str
+        """
+        return self.create_is_installed_script(module=self.name, module_bin=self.config['pipenv_bin'])
+
+    def get_script(self) -> str:
+        script = [super().get_script()]
+
+        script.append(self._script_config_vars())
+
+        script.append(self._script_function_source('install', '''
+            (
+                {environment}
+                cd {base_path} && \\
+                {pipenv_bin} install {install_dev}
+            )
+        '''.format(
+            environment=self._pipenv_environment(),
+            base_path=shlex.quote(self.config['base_path']),
+            pipenv_bin=shlex.quote(self.config['pipenv_bin']),
+            install_dev='--dev' if self.config['install_dev'] else '',
+        )))
+
+        script.append(self._script_function_source('update', f'''
+            {self.name}:install
+        '''))
+
+        script.append(self._script_function_source('run', '''
+            (
+                {environment}
+                local initial_path="$( pwd )"
+                cd "{base_path}" && \\
+                source "$( {pipenv_bin} --venv )/bin/activate" && \\
+                cd "$initial_path" && \\
+                "$@"
+            )
+        '''.format(
+            environment=self._pipenv_environment(),
+            base_path=shlex.quote(self.config['base_path']),
+            pipenv_bin=shlex.quote(self.config['pipenv_bin']),
+        )))
+
+        script.append(self._script_function_source('pipenv', '''
+            (
+                {environment}
+                cd {base_path} && \\
+                {pipenv_bin} "$@"
+            )
+        '''.format(
+            environment=self._pipenv_environment(),
+            base_path=shlex.quote(self.config['base_path']),
+            pipenv_bin=self.config['pipenv_bin'],
+        )))
+
+        script.append(self._script_function_source('pyenv', '''
+            (
+                {environment}
+                cd {base_path} && \\
+                {pyenv_bin} "$@"
+            )
+        '''.format(
+            environment=self._pipenv_environment(),
+            base_path=shlex.quote(self.config['base_path']),
+            pyenv_bin=self.config['pyenv_bin'],
+        )))
+
+        script.append(self._script_function_source('shell', '''
+            (
+                {environment}
+                cd {base_path} && \\
+                {pipenv_bin} shell "$@"
+            )
+        '''.format(
+            environment=self._pipenv_environment(),
+            base_path=shlex.quote(self.config['base_path']),
+            pipenv_bin=self.config['pipenv_bin'],
+        )))
+
+        return '\n'.join(script)
---
+++ packages/b5/b5-1.5.1.tar.gz/b5-1.5.1/b5/modules/virtualenv.py
@@ -0,0 +1,88 @@
+import os
+import shlex
+from typing import ClassVar
+
+from . import BaseModule
+
+
+class VirtualenvModule(BaseModule):
+    '''Virtualenv module
+    '''
+
+    DEFAULT_CONFIG: ClassVar = {
+        'base_path': '.',
+        'virtualenv_bin': 'virtualenv',
+        'python_bin': 'python3',
+        'env_path': 'ENV',
+        'requirements_file': 'requirements.txt',
+    }
+
+    def prepare_config(self) -> None:
+        self.config['base_path'] = os.path.realpath(os.path.join(
+            self.state.run_path,
+            self.config['base_path'],
+        ))
+        self.config['env_path'] = os.path.realpath(os.path.join(
+            self.config['base_path'],
+            self.config['env_path'],
+        ))
+        self.config['requirements_file'] = os.path.realpath(os.path.join(
+            self.config['base_path'],
+            self.config['requirements_file'],
+        ))
+
+    def is_installed_script(self) -> str:
+        """
+        Add a check to evaluate whether the virtualenv module bin is installed or not
+        Returns: str
+        """
+        return self.create_is_installed_script(module=self.name, module_bin=self.config['virtualenv_bin'])
+
+    def get_script(self) -> str:
+        script = [super().get_script()]
+
+        script.append(self._script_config_vars())
+
+        script.append(self._script_function_source('install', '''
+            (
+                cd {base_path} && \\
+                {virtualenv_bin} --python={python_bin} {env_path} && \\
+                {name}:update
+            )
+        '''.format(
+            base_path=shlex.quote(self.config['base_path']),
+            virtualenv_bin=shlex.quote(self.config['virtualenv_bin']),
+            python_bin=shlex.quote(self.config['python_bin']),
+            env_path=shlex.quote(self.config['env_path']),
+            name=self.name,
+        )))
+
+        script.append(self._script_function_source('update', '''
+            (
+                cd {base_path} && \\
+                {name}:pip install -U -r {requirements_file}
+            )
+        '''.format(
+            base_path=shlex.quote(self.config['base_path']),
+            requirements_file=shlex.quote(self.config['requirements_file']),
+            name=self.name,
+        )))
+
+        script.append(self._script_function_source('run', '''
+            (
+                source {activate_path} && \\
+                "$@"
+            )
+        '''.format(
+            activate_path=shlex.quote(os.path.join(
+                self.config['env_path'],
+                'bin',
+                'activate',
+            )),
+        )))
+
+        script.append(self._script_function_source('pip', f'''
+            {self.name}:run pip "$@"
+        '''))
+
+        return '\n'.join(script)
