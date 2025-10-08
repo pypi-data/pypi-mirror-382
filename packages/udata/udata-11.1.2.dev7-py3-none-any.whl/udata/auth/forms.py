3af8f23bf39fc24b76aa86ec7a8cad228b8db730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:15:37 +0000

    Add package udata/11.1.2.dev7/udata-11.1.2.dev7-py3-none-any.whl

---
+++ packages/udata/udata-11.1.2.dev7-py3-none-any.whl/udata/auth/forms.py
@@ -0,0 +1,79 @@
+import datetime
+
+from flask import current_app
+from flask_login import current_user
+from flask_security.forms import Form, LoginForm, RegisterForm, ResetPasswordForm
+
+from udata.forms import fields, validators
+from udata.i18n import lazy_gettext as _
+
+
+class ExtendedRegisterForm(RegisterForm):
+    first_name = fields.StringField(
+        _("First name"),
+        [
+            validators.DataRequired(_("First name is required")),
+            validators.NoURLs(_("URLs not allowed in this field")),
+        ],
+    )
+    last_name = fields.StringField(
+        _("Last name"),
+        [
+            validators.DataRequired(_("Last name is required")),
+            validators.NoURLs(_("URLs not allowed in this field")),
+        ],
+    )
+
+    def validate(self, **kwargs):
+        # no register allowed when read only mode is on
+        if not super().validate(**kwargs) or current_app.config.get("READ_ONLY_MODE"):
+            return False
+
+        return True
+
+
+class ExtendedLoginForm(LoginForm):
+    def validate(self, **kwargs):
+        if not super().validate(**kwargs):
+            return False
+
+        if self.user.password_rotation_demanded:
+            self.password.errors.append(_("Password must be changed for security reasons"))
+            return False
+
+        return True
+
+
+class ExtendedResetPasswordForm(ResetPasswordForm):
+    def validate(self, **kwargs):
+        if not super().validate(**kwargs):
+            return False
+
+        if self.user.password_rotation_demanded:
+            self.user.password_rotation_demanded = None
+            self.user.password_rotation_performed = datetime.datetime.utcnow()
+            self.user.save()
+
+        return True
+
+
+class ChangeEmailForm(Form):
+    new_email = fields.StringField(_("New email"), [validators.DataRequired(), validators.Email()])
+    new_email_confirm = fields.StringField(
+        _("Retype email"),
+        [validators.EqualTo("new_email", message=_("Email does not match")), validators.Email()],
+    )
+    submit = fields.SubmitField(_("Change email"))
+
+    def validate(self, **kwargs):
+        if not super().validate(**kwargs):
+            return False
+
+        self.user = current_user
+
+        if self.user.email.strip() == self.new_email.data.strip():
+            self.new_email.errors.append(
+                "Your new email must be different than your previous email"
+            )
+            return False
+        return True
---
+++ packages/udata/udata-11.1.2.dev7-py3-none-any.whl/udata/auth/views.py
@@ -0,0 +1,191 @@
+from flask import current_app, redirect, url_for
+from flask_login import current_user, login_required
+from flask_security.utils import (
+    check_and_get_token_status,
+    get_within_delta,
+    hash_data,
+    login_user,
+    logout_user,
+    send_mail,
+    verify_hash,
+)
+from flask_security.views import (
+    change_password,
+    confirm_email,
+    forgot_password,
+    login,
+    logout,
+    register,
+    reset_password,
+    send_confirmation,
+    send_login,
+    token_login,
+)
+from werkzeug.local import LocalProxy
+
+from udata.i18n import lazy_gettext as _
+from udata.uris import homepage_url
+
+from .forms import ChangeEmailForm
+
+_security = LocalProxy(lambda: current_app.extensions["security"])
+_datastore = LocalProxy(lambda: _security.datastore)
+
+
+def slash_url_suffix(url, suffix):
+    """Adds a slash either to the beginning or the end of a suffix
+    (which is to be appended to a URL), depending on whether or not
+    the URL ends with a slash.
+    """
+
+    return url.endswith("/") and ("%s/" % suffix) or ("/%s" % suffix)
+
+
+def send_change_email_confirmation_instructions(user, new_email):
+    data = [str(current_user.fs_uniquifier), hash_data(current_user.email), new_email]
+    token = _security.confirm_serializer.dumps(data)
+    confirmation_link = url_for("security.confirm_change_email", token=token, _external=True)
+
+    subject = _("Confirm change of email instructions")
+    send_mail(
+        subject=subject,
+        recipient=new_email,
+        template="confirmation_instructions",
+        user=current_user,
+        confirmation_link=confirmation_link,
+    )
+
+
+def confirm_change_email_token_status(token):
+    expired, invalid, token_data = check_and_get_token_status(
+        token, "confirm", get_within_delta("CONFIRM_EMAIL_WITHIN")
+    )
+    new_email = None
+
+    if not invalid and token_data:
+        user, token_email_hash, new_email = token_data
+        user = _datastore.find_user(fs_uniquifier=user)
+        invalid = not verify_hash(token_email_hash, user.email)
+
+    return expired, invalid, user, new_email
+
+
+def confirm_change_email(token):
+    expired, invalid, user, new_email = confirm_change_email_token_status(token)
+
+    flash = None
+    flash_data = None
+    if expired:
+        send_change_email_confirmation_instructions(user, new_email)
+        flash = "change_email_expired"
+        flash_data = {
+            "email_within": _security.confirm_email_within,
+            "new_email": new_email,
+        }
+    elif not user or invalid:
+        flash = "change_email_invalid"
+
+    if flash:
+        return redirect(homepage_url(flash=flash, flash_data=flash_data))
+
+    if user != current_user:
+        logout_user()
+        login_user(user)
+
+    user.email = new_email
+    _datastore.put(user)
+
+    return redirect(homepage_url(flash="change_email_confirmed"))
+
+
+@login_required
+def change_email():
+    """Change email page."""
+
+    form = ChangeEmailForm()
+
+    if form.validate_on_submit():
+        new_email = form.new_email.data
+        send_change_email_confirmation_instructions(current_user, new_email)
+        return redirect(
+            homepage_url(
+                flash="change_email",
+                flash_data={
+                    "email": new_email,
+                },
+            )
+        )
+
+    return _security.render_template("security/change_email.html", change_email_form=form)
+
+
+def create_security_blueprint(app, state, import_name):
+    from udata.i18n import I18nBlueprint
+
+    """
+    Creates the security extension blueprint
+    This creates an I18nBlueprint to use as a base.
+    """
+    bp = I18nBlueprint(
+        state.blueprint_name,
+        import_name,
+        url_prefix=state.url_prefix,
+        subdomain=state.subdomain,
+        template_folder="templates",
+    )
+
+    bp.route(app.config["SECURITY_LOGOUT_URL"], endpoint="logout")(logout)
+
+    if state.passwordless:
+        bp.route(app.config["SECURITY_LOGIN_URL"], methods=["GET", "POST"], endpoint="login")(
+            send_login
+        )
+        bp.route(
+            app.config["SECURITY_LOGIN_URL"]
+            + slash_url_suffix(app.config["SECURITY_LOGIN_URL"], "<token>"),
+            endpoint="token_login",
+        )(token_login)
+    else:
+        bp.route(app.config["SECURITY_LOGIN_URL"], methods=["GET", "POST"], endpoint="login")(login)
+
+    if state.registerable:
+        bp.route(app.config["SECURITY_REGISTER_URL"], methods=["GET", "POST"], endpoint="register")(
+            register
+        )
+
+    if state.recoverable:
+        bp.route(
+            app.config["SECURITY_RESET_URL"], methods=["GET", "POST"], endpoint="forgot_password"
+        )(forgot_password)
+        bp.route(
+            app.config["SECURITY_RESET_URL"]
+            + slash_url_suffix(app.config["SECURITY_RESET_URL"], "<token>"),
+            methods=["GET", "POST"],
+            endpoint="reset_password",
+        )(reset_password)
+
+    if state.changeable:
+        bp.route(
+            app.config["SECURITY_CHANGE_URL"], methods=["GET", "POST"], endpoint="change_password"
+        )(change_password)
+
+    if state.confirmable:
+        bp.route(
+            app.config["SECURITY_CONFIRM_URL"],
+            methods=["GET", "POST"],
+            endpoint="send_confirmation",
+        )(send_confirmation)
+        bp.route(
+            app.config["SECURITY_CONFIRM_URL"]
+            + slash_url_suffix(app.config["SECURITY_CONFIRM_URL"], "<token>"),
+            methods=["GET", "POST"],
+            endpoint="confirm_email",
+        )(confirm_email)
+
+    bp.route(
+        "/confirm-change-email/<token>", methods=["GET", "POST"], endpoint="confirm_change_email"
+    )(confirm_change_email)
+
+    bp.route("/change-email", methods=["GET", "POST"], endpoint="change_email")(change_email)
+
+    return bp
