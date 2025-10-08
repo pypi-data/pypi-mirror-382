8002d0f1dc2002e7ce7217c08d1369f1e7489566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 06:05:04 +0000

    Add package qtapputils/0.9.3/qtapputils-0.9.3-py3-none-any.whl

---
+++ packages/qtapputils/qtapputils-0.9.3-py3-none-any.whl/qtapputils/managers/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © QtAppUtils Project Contributors
+# https://github.com/jnsebgosselin/apputils
+#
+# This file is part of QtAppUtils.
+# Licensed under the terms of the MIT License.
+# -----------------------------------------------------------------------------
+from .taskmanagers import WorkerBase, TaskManagerBase, LIFOTaskManager
+from .fileio import SaveFileManager
