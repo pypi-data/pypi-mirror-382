93873cdcdbee7099073dc4621c2ec0c832f9f4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 16:05:21 +0000

    Add package machineconfig/5.39/machineconfig-5.39.tar.gz

---
+++ packages/machineconfig/machineconfig-5.39.tar.gz/machineconfig-5.39/src/machineconfig/setup_linux/__init__.py
@@ -0,0 +1,11 @@
+
+from pathlib import Path
+
+APPS = Path(__file__).parent.joinpath("apps.sh")
+MACHINECONFIG = Path(__file__).parent.joinpath("machineconfig.sh")
+UV = Path(__file__).parent.joinpath("uv.sh")
+
+APPS_DESKTOP = Path(__file__).parent.joinpath("apps_desktop.sh")
+APPS_GUI = Path(__file__).parent.joinpath("apps_gui.sh")
+
+SSH_SERVER = Path(__file__).parent.joinpath("ssh/openssh_server.sh")
