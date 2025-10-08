f02711d69b3b8f05113c179b37ebf637e0efbfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 09:06:14 +0000

    Add package PySide6-GlobalHotkeys/0.0.1/pyside6_globalhotkeys-0.0.1.tar.gz

---
+++ packages/PySide6-GlobalHotkeys/pyside6_globalhotkeys-0.0.1.tar.gz/pyside6_globalhotkeys-0.0.1/src/PySide6_GlobalHotkeys/notifications.py
@@ -0,0 +1,76 @@
+from PySide6 import QtCore
+from PySide6.QtCore import QObject, Slot, QCoreApplication
+from PySide6.QtDBus import (
+    QDBusConnection,
+    QDBusInterface,
+    QDBusMessage,
+    QDBusObjectPath,
+)
+
+from .dbuspaths import (
+    BUS_NAME,
+    OBJECT_PATH,
+    REGISTRY_IFACE,
+    SHORTCUT_IFACE,
+    HANDLE_TOKEN,
+    SESSION_HANDLE_TOKEN,
+)
+
+
+class Listener(QObject):
+    hotkeyPressed = QtCore.Signal(str)
+    hotkeyReleased = QtCore.Signal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.session: QDBusObjectPath | None = None
+
+        self.bus = QDBusConnection.sessionBus()
+
+        # Make it have the correct name in the UI.
+        self.registry = QDBusInterface(BUS_NAME, OBJECT_PATH, REGISTRY_IFACE, self.bus)
+        msg = self.registry.call("Register", QCoreApplication.applicationName(), {})
+        if msg.type() == QDBusMessage.ErrorMessage:
+            raise RuntimeError(msg.errorMessage())
+
+        # Connect the hotkey signals.
+        self.portal = QDBusInterface(BUS_NAME, OBJECT_PATH, SHORTCUT_IFACE, self.bus)
+        options = {
+            "handle_token": HANDLE_TOKEN,
+            "session_handle_token": SESSION_HANDLE_TOKEN,
+        }
+        msg = self.portal.call("CreateSession", options)
+        if msg.type() == QDBusMessage.ErrorMessage:
+            raise RuntimeError(msg.errorMessage())
+
+        self.portal.connect(
+            QtCore.SIGNAL("Activated(QDBusObjectPath,QString,qulonglong,QVariantMap)"),
+            self.onKeyActivated,
+        )
+        self.portal.connect(
+            QtCore.SIGNAL(
+                "Deactivated(QDBusObjectPath,QString,qulonglong,QVariantMap)"
+            ),
+            self.onKeyDeactivated,
+        )
+
+    @Slot("o", "s", "t", "a{sv}")
+    def onKeyActivated(
+        self,
+        session_handle: QDBusObjectPath,
+        shortcutId: str,
+        timestamp: int,
+        options: dict,
+    ):
+        self.hotkeyPressed.emit(shortcutId)
+
+    @Slot("o", "s", "u", "a{sv}")
+    def onKeyDeactivated(
+        self,
+        session_handle: QDBusObjectPath,
+        shortcutId: str,
+        timestamp: int,
+        options: dict,
+    ):
+        self.hotkeyReleased.emit(shortcutId)
