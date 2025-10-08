66a24a1625c92bfe2511bfb07bffe659ce82d644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:20 +0000

    Add package accelbyte-py-sdk-service-lobby/0.22.0/accelbyte_py_sdk_service_lobby-0.22.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-lobby/accelbyte_py_sdk_service_lobby-0.22.0.tar.gz/accelbyte_py_sdk_service_lobby-0.22.0/accelbyte_py_sdk/api/lobby/operations/presence/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Lobby Server."""
+
+__version__ = "3.39.2"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .users_presence_handler_v1 import UsersPresenceHandlerV1
+from .users_presence_handler_v2 import UsersPresenceHandlerV2
