76cdbe715cd7d2a387f2cf881863f95a626e26a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:20 +0000

    Add package accelbyte-py-sdk-service-lobby/0.22.0/accelbyte_py_sdk_service_lobby-0.22.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-lobby/accelbyte_py_sdk_service_lobby-0.22.0-py3-none-any.whl/accelbyte_py_sdk/api/lobby/operations/lobby_operations/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Lobby Server."""
+
+__version__ = "3.39.2"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .admin_join_party_v1 import AdminJoinPartyV1
+from .admin_update_party_attr_920eb3 import AdminUpdatePartyAttributesV1
+from .public_get_messages import PublicGetMessages
