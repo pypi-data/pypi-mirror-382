a52e20ef6db27320f33a3589eb8c8639b3579005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 06:04:36 +0000

    Add package sequenzo/0.1.19/sequenzo-0.1.19-cp312-cp312-macosx_10_13_universal2.whl

---
+++ packages/sequenzo/sequenzo-0.1.19-cp312-cp312-macosx_10_13_universal2.whl/sequenzo/dissimilarity_measures/utils/seqdss.pyx
@@ -0,0 +1,33 @@
+import numpy as np
+cimport numpy as cnp
+from sequenzo.define_sequence_data import SequenceData
+from libc.stdint cimport int32_t
+
+def seqdss(seqdata):
+    if not isinstance(seqdata, SequenceData):
+        raise ValueError("[!] data is NOT a sequence object, see SequenceData to create one.")
+
+    cdef cnp.ndarray[int32_t, ndim=2] seqdatanum = seqdata.values.astype(np.int32, copy=False)
+    cdef int n = seqdatanum.shape[0], m = seqdatanum.shape[1]
+    cdef int i, j
+
+    cdef cnp.ndarray[int32_t, ndim=2] ffill = seqdatanum
+    cdef cnp.ndarray[char, ndim=2] boundaries = np.ones((n, 1), dtype=bool)  # Fixed here
+
+    for j in range(1, m):
+        mask = (ffill[:, j] < 0)
+        ffill[mask, j] = ffill[mask, j - 1]
+
+    boundaries = np.concatenate([boundaries, ffill[:, 1:] != ffill[:, :-1]], axis=1)
+
+    cdef list groups = [row[boundary & (row >= 0)] for row, boundary in zip(ffill, boundaries)]
+    cdef int max_groups = max(len(g) for g in groups)
+
+    # 使用int32能表示的缺失值标记，避免np.nan转换为int32时的警告
+    cdef cnp.ndarray[int32_t, ndim=2] result = np.full((n, max_groups), -999, dtype=np.int32)
+
+    for i in range(n):
+        g = groups[i]
+        result[i, :len(g)] = g
+
+    return result
