005755948e7b6d271b46ff2904e622fd5730f75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 03:14:56 +0000

    Add package bluer-sbc/9.31.1/bluer_sbc-9.31.1-py3-none-any.whl

---
+++ packages/bluer-sbc/bluer_sbc-9.31.1-py3-none-any.whl/bluer_sbc/README/design.py
@@ -0,0 +1,32 @@
+from typing import List, Dict
+
+from bluer_objects import markdown
+
+from bluer_sbc.parts.db import db_of_parts
+
+
+def design(
+    items: List[str],
+    dict_of_parts: Dict = {},
+) -> Dict:
+    output = {
+        "items": items,
+    }
+
+    if dict_of_parts:
+        output["macros"] = {
+            "parts_images:::": markdown.generate_table(
+                db_of_parts.as_images(
+                    dict_of_parts,
+                    reference="../../../parts",
+                ),
+                cols=10,
+            ),
+            "parts_list:::": db_of_parts.as_list(
+                dict_of_parts,
+                reference="../../../parts",
+                log=False,
+            ),
+        }
+
+    return output
