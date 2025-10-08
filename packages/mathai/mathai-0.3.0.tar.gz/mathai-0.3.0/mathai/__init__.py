e1455880f1fae5ceb6972c11e73fbbb98b8b1f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:05:23 +0000

    Add package mathai/0.3.0/mathai-0.3.0.tar.gz

---
+++ packages/mathai/mathai-0.3.0.tar.gz/mathai-0.3.0/mathai/__init__.py
@@ -0,0 +1,16 @@
+from .expand import expand
+from .parser import parse
+from .printeq import printeq, printeq_log, printeq_str
+from .simplify import solve, simplify, solve2
+from .integrate import integrate, sqint, byparts
+from .diff import diff
+from .factor import factor, factor2
+from .fraction import fraction
+from .inverse import inverse
+from .trig import trig0, trig1, trig2, trig3, trig4
+from .logic import logic0, logic1, logic2, logic3
+from .apart import apart
+from .console import console
+from .limit import limit
+from .univariate_inequality import wavycurvy, absolute, domain, handle_sqrt
+from .base import *
