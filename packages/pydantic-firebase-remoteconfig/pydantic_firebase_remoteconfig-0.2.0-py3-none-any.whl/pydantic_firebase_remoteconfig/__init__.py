2177b4f03a0dc28a2769d3cb79220a5e6b826bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 06:05:00 +0000

    Add package pydantic-firebase-remoteconfig/0.2.0/pydantic_firebase_remoteconfig-0.2.0-py3-none-any.whl

---
+++ packages/pydantic-firebase-remoteconfig/pydantic_firebase_remoteconfig-0.2.0-py3-none-any.whl/pydantic_firebase_remoteconfig/__init__.py
@@ -0,0 +1,9 @@
+from .client import FirebaseRemoteConfigClient
+from .models import BaseRemoteConfigModel
+from .models import RemoteConfigConfigDict
+
+__all__ = [
+    "BaseRemoteConfigModel",
+    "FirebaseRemoteConfigClient",
+    "RemoteConfigConfigDict",
+]
