8c75811654846f1cd0e7ed1bed520e2a5acd3802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:05:18 +0000

    Add package reasoning-from-scratch/0.1.7/reasoning_from_scratch-0.1.7.tar.gz

---
+++ packages/reasoning-from-scratch/reasoning_from_scratch-0.1.7.tar.gz/reasoning_from_scratch-0.1.7/tests/test_appendix_c.py
@@ -0,0 +1,30 @@
+# Copyright (c) Sebastian Raschka under Apache License 2.0 (see LICENSE.txt)
+# Source for "Build a Reasoning Model (From Scratch)": https://mng.bz/lZ5B
+# Code repository: https://github.com/rasbt/reasoning-from-scratch
+
+from pathlib import Path
+import sys
+import pytest
+
+from reasoning_from_scratch.ch02 import (
+    generate_text_basic,
+    generate_text_basic_cache,
+)
+# Local imports
+from test_qwen3 import test_model
+from conftest import import_definitions_from_notebook
+
+
+ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(ROOT))
+
+
+nb_path = ROOT / "chC" / "01_main-chapter-code" / "chC_main.ipynb"
+mod = import_definitions_from_notebook(nb_path, "chC_chC_main_defs")
+Qwen3Model = getattr(mod, "Qwen3Model")
+
+
+@pytest.mark.parametrize("ModelClass", [Qwen3Model])
+@pytest.mark.parametrize("generate_fn", [generate_text_basic, generate_text_basic_cache])
+def test_model_here_too(ModelClass, qwen3_weights_path, generate_fn):
+    test_model(ModelClass, qwen3_weights_path, generate_fn)
