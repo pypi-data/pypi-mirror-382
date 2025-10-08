11e00c36b0ab561b0c69ba976e1373bccc2c609b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:44 +0000

    Add package accelbyte-py-sdk-service-loginqueue/0.8.0/accelbyte_py_sdk_service_loginqueue-0.8.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-loginqueue/accelbyte_py_sdk_service_loginqueue-0.8.0-py3-none-any.whl/accelbyte_py_sdk/api/loginqueue/operations/ticket_v1/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Login Queue Service."""
+
+__version__ = "2.3.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .cancel_ticket import CancelTicket
+from .refresh_ticket import RefreshTicket
