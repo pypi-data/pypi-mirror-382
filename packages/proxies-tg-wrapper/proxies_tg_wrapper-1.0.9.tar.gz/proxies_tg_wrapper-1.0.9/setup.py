710f05ffe4697686e46420151a9b569e2985636d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 16:05:29 +0000

    Add package proxies-tg-wrapper/1.0.9/proxies_tg_wrapper-1.0.9.tar.gz

---
+++ packages/proxies-tg-wrapper/proxies_tg_wrapper-1.0.9.tar.gz/proxies_tg_wrapper-1.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="proxies-tg-wrapper",
+    version="1.0.9",
+    packages=find_packages(),
+    install_requires=["python-telegram==0.18.0"],
+    author="Ilia Abolhasani",
+    author_email="abolhasani.eliya@gmail.com",
+    description="A package designed to facilitate interaction with the Telegram API, specifically for managing MTProto proxies.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ilia-Abolhasani/proxies-tg-wrapper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
