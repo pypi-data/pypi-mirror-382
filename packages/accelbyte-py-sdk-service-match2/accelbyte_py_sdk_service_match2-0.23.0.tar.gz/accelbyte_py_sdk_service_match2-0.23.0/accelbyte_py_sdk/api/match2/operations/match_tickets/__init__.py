9a8ebd88e92081611f72a06779fc683e1ca2ce46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:45 +0000

    Add package accelbyte-py-sdk-service-match2/0.23.0/accelbyte_py_sdk_service_match2-0.23.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-match2/accelbyte_py_sdk_service_match2-0.23.0.tar.gz/accelbyte_py_sdk_service_match2-0.23.0/accelbyte_py_sdk/api/match2/operations/match_tickets/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Match Service V2."""
+
+__version__ = "2.36.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .create_match_ticket import CreateMatchTicket
+from .delete_match_ticket import DeleteMatchTicket
+from .get_my_match_tickets import GetMyMatchTickets
+from .match_ticket_details import MatchTicketDetails
