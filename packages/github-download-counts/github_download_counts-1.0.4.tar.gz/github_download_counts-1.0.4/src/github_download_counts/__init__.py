f06be4633ffbd088dc6b23240e94a732af472fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 08:06:30 +0000

    Add package github-download-counts/1.0.4/github_download_counts-1.0.4.tar.gz

---
+++ packages/github-download-counts/github_download_counts-1.0.4.tar.gz/github_download_counts-1.0.4/src/github_download_counts/__init__.py
@@ -0,0 +1,7 @@
+"""a simple Python wrapper around the GitHub REST API to get download counts for release assets and ghcr.io image pulls for organizations' repositories"""
+
+__version__ = "1.0.4"
+__author__ = "Seth Grover <mero.mero.guero@gmail.com>"
+__all__ = []
+
+from github_download_counts.github_download_counts import *
