94047452ca01536e0f176af412c2acdd75db3b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:16 +0000

    Add package TkThemes/0.1.2/tkthemes-0.1.2-py3-none-any.whl

---
+++ packages/TkThemes/tkthemes-0.1.2-py3-none-any.whl/TkThemes/__init__.py
@@ -0,0 +1,24 @@
+# TkThemes/__init__.py
+
+from .themes import Themes
+
+__all__ = [
+    "Themes",
+    "Dracula",
+    "SolarizedLight",
+    "Nord",
+    "MonokaiPro",
+    "GruvboxDark",
+    "OneDarkPro",
+    "GitHubLight",
+    "OceanicNext",
+    "CrimsonRed",
+    "ForestGreen",
+    "RoyalPurple",
+    "ClassicWhite",
+    "SepiaTone",
+    "Cyberpunk",
+    "PastelSunset"
+]  
+
+__version__ = "0.1.2"
