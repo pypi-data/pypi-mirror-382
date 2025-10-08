e56cddc5b747c613f8b8169740288397dc9ffb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 07:05:09 +0000

    Add package optibioseq/0.2.1/optibioseq-0.2.1-py3-none-any.whl

---
+++ packages/optibioseq/optibioseq-0.2.1-py3-none-any.whl/optibioseq/__main__.py
@@ -0,0 +1,12 @@
+
+from . import bce
+from . import abc
+from . import bepi
+from . import iedb1
+from . import iedb2
+
+#exec(open("abc.py").read())
+#exec(open("bce.py").read())
+#exec(open("bepi.py").read())
+#exec(open("iedb1.py").read())
+#exec(open("iedb2.py").read())
