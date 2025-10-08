8f2aea8692e4cf8ce475b4a42fdcbd6b6b92c062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:54 +0000

    Add package prseq/0.0.9/prseq-0.0.9-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/prseq/prseq-0.0.9-cp313-cp313-macosx_11_0_arm64.whl/prseq/__init__.py
@@ -0,0 +1,7 @@
+from .fasta import FastaRecord, FastaReader, read_fasta
+from .fastq import FastqRecord, FastqReader, read_fastq
+
+__version__ = "0.0.9"
+__all__ = ["FastaRecord", "FastaReader", "read_fasta", "FastqRecord", "FastqReader", "read_fastq"]
+
+
