209e07ae2c1f0339b06c49fb1f6b5cb782186beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:19 +0000

    Add package accelbyte-py-sdk-service-legal/0.23.0/accelbyte_py_sdk_service_legal-0.23.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-legal/accelbyte_py_sdk_service_legal-0.23.0-py3-none-any.whl/accelbyte_py_sdk/api/legal/operations/agreement_with_namespace/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Legal Service."""
+
+__version__ = "4.4.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .download_exported_agree_a56e5b import DownloadExportedAgreementsInCSV
+from .initiate_export_agreeme_d92c31 import InitiateExportAgreementsToCSV
+from .retrieve_accepted_agreements_1 import RetrieveAcceptedAgreements1
+from .retrieve_accepted_agree_8c230d import RetrieveAcceptedAgreementsForMultiUsers
+from .retrieve_all_users_by_p_3f6faa import RetrieveAllUsersByPolicyVersion1
---
+++ packages/accelbyte-py-sdk-service-legal/accelbyte_py_sdk_service_legal-0.23.0-py3-none-any.whl/accelbyte_py_sdk/api/legal/operations/localized_policy_versions/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Legal Service."""
+
+__version__ = "4.4.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .create_localized_policy_19e4a3 import CreateLocalizedPolicyVersion
+from .request_presigned_url import RequestPresignedURL
+from .retrieve_localized_poli_24a671 import RetrieveLocalizedPolicyVersions
+from .retrieve_single_localiz_2f6231 import RetrieveSingleLocalizedPolicyVersion
+from .retrieve_single_localiz_6ecc81 import RetrieveSingleLocalizedPolicyVersion2
+from .set_default_policy import SetDefaultPolicy
+from .update_localized_policy_a8a90f import UpdateLocalizedPolicyVersion
