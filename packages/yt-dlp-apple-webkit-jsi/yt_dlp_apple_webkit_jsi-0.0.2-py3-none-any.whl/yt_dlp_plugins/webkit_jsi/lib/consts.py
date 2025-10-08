924eb1518516241ad7ee5b7cbc9ee639d3cd765a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 14:08:10 +0000

    Add package yt-dlp-apple-webkit-jsi/0.0.2/yt_dlp_apple_webkit_jsi-0.0.2-py3-none-any.whl

---
+++ packages/yt-dlp-apple-webkit-jsi/yt_dlp_apple_webkit_jsi-0.0.2-py3-none-any.whl/yt_dlp_plugins/webkit_jsi/lib/consts.py
@@ -0,0 +1,34 @@
+SCRIPT_PHOLDER = r'/*__ACTUAL_SCRIPT_CONTENT_PLACEHOLDER__*/'
+SCRIPT_TEMPL = r'''
+let __webkit = window.webkit;
+const res = await (async ()=>{  // IIAFE
+const communicate = (()=>{
+if (!window?.webkit?.messageHandlers) throw new Error('No message handlers set up');
+function __postmsg(x, channel) {
+    window.webkit = __webkit;
+    const ret = window.webkit.messageHandlers[channel].postMessage(x);
+    __webkit = window.webkit;
+    window.webkit = undefined;
+    return ret;
+}
+Object.entries({
+    trace: 0,  // TRACE
+    debug: 1,  // DIAG
+    log: 2,  // INFO
+    info: 2,  // INFO
+    warn: 3,  // WARN
+    assert: 4,  // ASSERT
+    error: 5,  // ERR
+}).forEach(([fn, logType])=>{
+    console[fn] = function() {
+        __postmsg({logType, argsArr: Array.from(arguments)}, 'wkjs_log');
+    };
+});
+window.webkit = undefined;
+return x=>__postmsg(x, 'wkjs_com');
+})();
+/*__ACTUAL_SCRIPT_CONTENT_PLACEHOLDER__*/
+})();
+window.webkit = __webkit;
+return res;
+'''
