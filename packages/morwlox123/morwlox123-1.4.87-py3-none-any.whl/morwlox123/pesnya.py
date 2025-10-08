80151be8d821b1248561d951b8babdcf44e34564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:52 +0000

    Add package morwlox123/1.4.87/morwlox123-1.4.87-py3-none-any.whl

---
+++ packages/morwlox123/morwlox123-1.4.87-py3-none-any.whl/morwlox123/pesnya.py
@@ -0,0 +1,9 @@
+class петь():
+    def песня_про_какашку_и_говняшку():
+        return 'govno kakashkaaaaaaaaa jopa jopa piska'
+
+    def klazermp3():
+        return ''
+
+    def рустам():
+        return ''
