8b9b87388dfb136e2cbc66b8d0cf8276e2e509ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 04:05:56 +0000

    Add package waymap/6.2.11/waymap-6.2.11.tar.gz

---
+++ packages/waymap/waymap-6.2.11.tar.gz/waymap-6.2.11/setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, find_packages # type: ignore
+
+setup(
+    name='waymap',
+    version='6.2.11',  # Updated version
+    packages=[
+        'extras',
+        'lib',
+        'lib.core',
+        'lib.injection',
+        'lib.injection.sqlin',
+        'lib.parse',
+        'lib.ProfileCritical',
+        'lib.ProfileCritical.drupal',
+        'lib.ProfileCritical.Generic',
+        'lib.ProfileCritical.wordpress',
+        'lib.ProfileDeepScan',
+        'lib.ProfileHigh',
+        'lib.ProfileHigh.drupal',
+        'lib.ProfileHigh.wordpress',
+        'lib.waymapcrawlers',
+    ],
+    py_modules=['waymap'],  # Include standalone waymap.py module
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'termcolor',
+        'packaging',
+        'bs4',
+        'urllib3',
+        'colorama',
+        'tqdm',
+        'argparse',
+    ],
+    include_package_data=True,
+    package_data={
+        '': ['data/*', 'doc/*', 'extras/*', 'sessions/*'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Security',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'waymap = waymap:main',
+        ],
+    },
+    author="Trix Cyrus",
+    author_email="trixcyrus666@gmail.com",
+    description="A powerful web security tool for automated scanning.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TrixSec/waymap",
+    project_urls={
+        "Source Code": "https://github.com/TrixSec/waymap",
+        "Bug Tracker": "https://github.com/TrixSec/waymap/issues",
+    },
+    keywords=["security", "pentesting", "vulnerability-scanning", "cybersecurity"],
+    license="GPL-3.0",  
+    license_files=('LICENSE',), 
+)
