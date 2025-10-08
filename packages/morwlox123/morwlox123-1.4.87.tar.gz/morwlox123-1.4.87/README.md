0d66a0e854bdd9e59da02cbddabe7e7ce7c05c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-08 05:04:52 +0000

    Add package morwlox123/1.4.87/morwlox123-1.4.87.tar.gz

---
+++ packages/morwlox123/morwlox123-1.4.87.tar.gz/morwlox123-1.4.87/README.md
@@ -0,0 +1,17 @@
+# ti lox suck kok #
+
+## ты лох ##
+
+говнищеееееееееееее
+
+---
+
+    ты говнооооооооооооооййоойооойй
+    пук пук пук пук
+
+###
+xyuuuuuuuuuuuuuuuuuu
+
+----
+
+idi nax lol lox morw daun
---
+++ packages/morwlox123/morwlox123-1.4.87.tar.gz/morwlox123-1.4.87/morwlox123/fart.py
@@ -0,0 +1,5 @@
+def fart(colicshestvo):
+    return f'напреееедеелел {colicshestvo}ййййййцуууущщщщщщщщщщщщщ раз'
+
+def mega_farting(colicshestvo):
+    return f'супер ьупф напреееедеелел {colicshestvo}йщщ раз'
