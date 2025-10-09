5b5c3112afee61af1b072e8c9a8e98b8bcb27741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:18:00 +0000

    Add package tidecv-light/1.0.2/tidecv_light-1.0.2-py3-none-any.whl

---
+++ packages/tidecv-light/tidecv_light-1.0.2-py3-none-any.whl/tidecv/__init__.py
@@ -0,0 +1,9 @@
+"""Based on https://github.com/dbolya/tide
+
+Copyright (c) 2020 Daniel Bolya
+"""
+
+from .data import Data
+from .errors.qualifiers import *
+from .quantify import *
+from .quantify import TIDE
