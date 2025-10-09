8ef72e42c368e89ff9bf9c65b7a2d9b998d198cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 07:05:09 +0000

    Add package financial-scraper/1.0.2/financial_scraper-1.0.2.tar.gz

---
+++ packages/financial-scraper/financial_scraper-1.0.2.tar.gz/financial_scraper-1.0.2/financial_scraper/config/selenium.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.chrome.options import Options
+
+class Selenium():
+
+    @staticmethod
+    def get_options(download_path: str) -> Options:
+        options = Options()
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+        prefs = {
+            "download.default_directory": download_path,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True,
+        }
+        options.add_experimental_option('prefs', prefs)
+        return options
