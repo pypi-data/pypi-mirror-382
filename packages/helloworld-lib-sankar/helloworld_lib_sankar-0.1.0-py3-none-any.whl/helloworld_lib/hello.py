95ea252fef8830ed20da23e441cae39b8833a907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 16:09:32 +0000

    Add package helloworld-lib-sankar/0.1.0/helloworld_lib_sankar-0.1.0-py3-none-any.whl

---
+++ packages/helloworld-lib-sankar/helloworld_lib_sankar-0.1.0-py3-none-any.whl/helloworld_lib/hello.py
@@ -0,0 +1,9 @@
+# helloworld-lib/hello.py
+
+def say_hello(name="World"):
+    """Return a friendly greeting."""
+    return f"Hello, {name}!"
+
+def say_bye(name="World"):
+    """Return a friendly greeting."""
+    return f"Bye, {name}!"
