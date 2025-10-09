bae5fb0bda9812aec41324869a88d758509b5d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 06:05:58 +0000

    Add package afnio/0.3.1/afnio-0.3.1-py3-none-any.whl

---
+++ packages/afnio/afnio-0.3.1-py3-none-any.whl/afnio/__init__.py
@@ -0,0 +1,43 @@
+# afnio/__init__.py
+
+from afnio import autodiff, cognitive, optim, utils
+from afnio._model_client import (
+    get_backward_model_client,
+    set_backward_model_client,
+)
+from afnio._variable import Variable
+from afnio.autodiff import is_grad_enabled, no_grad, set_grad_enabled
+from afnio.autodiff.graph import (
+    GradientEdge,
+    Node,
+)
+from afnio.cognitive import functional
+from afnio.serialization import load, save
+
+from ._utils import (
+    _validate_multi_turn_messages,
+    _validate_typed_sequence,
+)
+
+__all__ = [
+    "GradientEdge",
+    "Node",
+    "Variable",
+    "_validate_multi_turn_messages",
+    "_validate_typed_sequence",
+    "autodiff",
+    "cognitive",
+    "functional",
+    "get_backward_model_client",
+    "is_grad_enabled",
+    "load",
+    "no_grad",
+    "optim",
+    "save",
+    "set_backward_model_client",
+    "set_grad_enabled",
+    "utils",
+]
+
+# Please keep this list sorted
+assert __all__ == sorted(__all__)
