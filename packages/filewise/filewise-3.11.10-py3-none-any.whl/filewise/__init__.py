0b0eff36d3b8f76f54bdbb6be5ce622432d50848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 05:06:39 +0000

    Add package filewise/3.11.10/filewise-3.11.10-py3-none-any.whl

---
+++ packages/filewise/filewise-3.11.10-py3-none-any.whl/filewise/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# filewise/__init__.py
+
+__version__ = "3.11.10"
+
+# Define what should be available when using 'from filewise import *'
+__all__ = [
+    'file_operations',
+    'format_converters',
+    'general',
+    'json_utils',
+    'pandas_utils',
+    'scripts',
+    'xarray_utils'
+]
