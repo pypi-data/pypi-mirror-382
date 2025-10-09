6248c2141502a71cb3cbf7c040ec04ac12c44379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 04:06:01 +0000

    Add package pawlib/0.0.0.post13/pawlib-0.0.0.post13-py3-none-any.whl

---
+++ packages/pawlib/pawlib-0.0.0.post13-py3-none-any.whl/pawlib/ai/summarize/summarize_manager.py
@@ -0,0 +1,22 @@
+from pawlib.ai.summarize.textrank.textrank_sentence import TextRankSentence
+from loguru import logger
+
+
+class SummarizeManager:
+    @classmethod
+    def summarize(cls, model_protocal: str, content: str) -> str:
+        """
+            文本摘要,支持的模型:
+                1. local:textrank - 本地TextRank模型
+        """
+        if model_protocal.startswith('local:'):
+            local_model = model_protocal.split(':')[1]
+            if local_model == 'textrank':
+                text_rank_model = TextRankSentence()
+                text_rank_model.analyze(
+                    text=content, lower=True, source='all_filters')
+                items = text_rank_model.get_key_sentences(num=1)
+                return items[0].sentence
+
+        logger.warning(f"没有找到对应的摘要模型: {model_protocal}, 使用默认的内容返回")
+        return content
