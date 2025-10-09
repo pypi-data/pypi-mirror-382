225a876379f98490a49f7c5c8b37f4bf99893d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:30:17 +0000

    Add package tap-pardot/1.4.9/tap_pardot-1.4.9.tar.gz

---
+++ packages/tap-pardot/tap_pardot-1.4.9.tar.gz/tap_pardot-1.4.9/README.md
@@ -0,0 +1,6 @@
+# tap-pardot
+Singer tap for replicating Pardot data.
+
+---
+
+Copyright &copy; 2019 Stitch
---
+++ packages/tap-pardot/tap_pardot-1.4.9.tar.gz/tap_pardot-1.4.9/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-pardot",
+    version="1.4.9",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_pardot"],
+    install_requires=["singer-python==5.13.2", "requests==2.32.4", "backoff==1.10.0"],
+    entry_points="""
+    [console_scripts]
+    tap-pardot=tap_pardot:main
+    """,
+    packages=["tap_pardot"],
+    package_data={"schemas": ["tap_pardot/schemas/*.json"]},
+    include_package_data=True,
+)
