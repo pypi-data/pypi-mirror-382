d0f67c600229618a39b7217f380d9b069791d309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 14:07:00 +0000

    Add package cudf-polars-cu12/25.10.0/cudf_polars_cu12-25.10.0-py3-none-any.whl

---
+++ packages/cudf-polars-cu12/cudf_polars_cu12-25.10.0-py3-none-any.whl/cudf_polars/containers/dataframe.py
@@ -0,0 +1,361 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-License-Identifier: Apache-2.0
+
+"""A dataframe, with some properties."""
+
+from __future__ import annotations
+
+from functools import cached_property
+from typing import TYPE_CHECKING, cast
+
+import polars as pl
+
+import pylibcudf as plc
+
+from cudf_polars.containers import Column, DataType
+from cudf_polars.utils import conversion
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable, Mapping, Sequence, Set
+
+    from typing_extensions import Any, CapsuleType, Self
+
+    from cudf_polars.typing import ColumnOptions, DataFrameHeader, PolarsDataType, Slice
+
+
+__all__: list[str] = ["DataFrame"]
+
+
+def _create_polars_column_metadata(
+    name: str, dtype: PolarsDataType
+) -> plc.interop.ColumnMetadata:
+    """Create ColumnMetadata preserving dtype attributes not supported by libcudf."""
+    children_meta = []
+    timezone = ""
+    precision: int | None = None
+
+    if isinstance(dtype, pl.Struct):
+        children_meta = [
+            _create_polars_column_metadata(field.name, field.dtype)
+            for field in dtype.fields
+        ]
+    elif isinstance(dtype, pl.Datetime):
+        timezone = dtype.time_zone or timezone
+    elif isinstance(dtype, pl.Decimal):
+        precision = dtype.precision
+
+    return plc.interop.ColumnMetadata(
+        name=name,
+        timezone=timezone,
+        precision=precision,
+        children_meta=children_meta,
+    )
+
+
+# This is also defined in pylibcudf.interop
+class _ObjectWithArrowMetadata:
+    def __init__(
+        self, obj: plc.Table | plc.Column, metadata: list[plc.interop.ColumnMetadata]
+    ) -> None:
+        self.obj = obj
+        self.metadata = metadata
+
+    def __arrow_c_array__(
+        self, requested_schema: None = None
+    ) -> tuple[CapsuleType, CapsuleType]:
+        return self.obj._to_schema(self.metadata), self.obj._to_host_array()
+
+
+# Pacify the type checker. DataFrame init asserts that all the columns
+# have a string name, so let's narrow the type.
+class NamedColumn(Column):
+    name: str
+
+
+class DataFrame:
+    """A representation of a dataframe."""
+
+    column_map: dict[str, Column]
+    table: plc.Table
+    columns: list[NamedColumn]
+
+    def __init__(self, columns: Iterable[Column]) -> None:
+        columns = list(columns)
+        if any(c.name is None for c in columns):
+            raise ValueError("All columns must have a name")
+        self.columns = [cast(NamedColumn, c) for c in columns]
+        self.dtypes = [c.dtype for c in self.columns]
+        self.column_map = {c.name: c for c in self.columns}
+        self.table = plc.Table([c.obj for c in self.columns])
+
+    def copy(self) -> Self:
+        """Return a shallow copy of self."""
+        return type(self)(c.copy() for c in self.columns)
+
+    def to_polars(self) -> pl.DataFrame:
+        """Convert to a polars DataFrame."""
+        # If the arrow table has empty names, from_arrow produces
+        # column_$i. But here we know there is only one such column
+        # (by construction) and it should have an empty name.
+        # https://github.com/pola-rs/polars/issues/11632
+        # To guarantee we produce correct names, we therefore
+        # serialise with names we control and rename with that map.
+        name_map = {f"column_{i}": name for i, name in enumerate(self.column_map)}
+        metadata = [
+            _create_polars_column_metadata(name, dtype.polars)
+            for name, dtype in zip(name_map, self.dtypes, strict=True)
+        ]
+        table_with_metadata = _ObjectWithArrowMetadata(self.table, metadata)
+        df = pl.DataFrame(table_with_metadata)
+        return df.rename(name_map).with_columns(
+            pl.col(c.name).set_sorted(descending=c.order == plc.types.Order.DESCENDING)
+            if c.is_sorted
+            else pl.col(c.name)
+            for c in self.columns
+        )
+
+    @cached_property
+    def column_names_set(self) -> frozenset[str]:
+        """Return the column names as a set."""
+        return frozenset(self.column_map)
+
+    @cached_property
+    def column_names(self) -> list[str]:
+        """Return a list of the column names."""
+        return list(self.column_map)
+
+    @cached_property
+    def num_columns(self) -> int:
+        """Number of columns."""
+        return len(self.column_map)
+
+    @cached_property
+    def num_rows(self) -> int:
+        """Number of rows."""
+        return self.table.num_rows() if self.column_map else 0
+
+    @classmethod
+    def from_polars(cls, df: pl.DataFrame) -> Self:
+        """
+        Create from a polars dataframe.
+
+        Parameters
+        ----------
+        df
+            Polars dataframe to convert
+
+        Returns
+        -------
+        New dataframe representing the input.
+        """
+        plc_table = plc.Table.from_arrow(df)
+        return cls(
+            Column(d_col, name=name, dtype=DataType(h_col.dtype)).copy_metadata(h_col)
+            for d_col, h_col, name in zip(
+                plc_table.columns(), df.iter_columns(), df.columns, strict=True
+            )
+        )
+
+    @classmethod
+    def from_table(
+        cls, table: plc.Table, names: Sequence[str], dtypes: Sequence[DataType]
+    ) -> Self:
+        """
+        Create from a pylibcudf table.
+
+        Parameters
+        ----------
+        table
+            Pylibcudf table to obtain columns from
+        names
+            Names for the columns
+        dtypes
+            Dtypes for the columns
+
+        Returns
+        -------
+        New dataframe sharing data with the input table.
+
+        Raises
+        ------
+        ValueError
+            If the number of provided names does not match the
+            number of columns in the table.
+        """
+        if table.num_columns() != len(names):
+            raise ValueError("Mismatching name and table length.")
+        return cls(
+            Column(c, name=name, dtype=dtype)
+            for c, name, dtype in zip(table.columns(), names, dtypes, strict=True)
+        )
+
+    @classmethod
+    def deserialize(
+        cls, header: DataFrameHeader, frames: tuple[memoryview, plc.gpumemoryview]
+    ) -> Self:
+        """
+        Create a DataFrame from a serialized representation returned by `.serialize()`.
+
+        Parameters
+        ----------
+        header
+            The (unpickled) metadata required to reconstruct the object.
+        frames
+            Two-tuple of frames (a memoryview and a gpumemoryview).
+
+        Returns
+        -------
+        DataFrame
+            The deserialized DataFrame.
+        """
+        packed_metadata, packed_gpu_data = frames
+        table = plc.contiguous_split.unpack_from_memoryviews(
+            packed_metadata, packed_gpu_data
+        )
+        return cls(
+            Column(c, **Column.deserialize_ctor_kwargs(kw))
+            for c, kw in zip(table.columns(), header["columns_kwargs"], strict=True)
+        )
+
+    def serialize(
+        self,
+    ) -> tuple[DataFrameHeader, tuple[memoryview, plc.gpumemoryview]]:
+        """
+        Serialize the table into header and frames.
+
+        Follows the Dask serialization scheme with a picklable header (dict) and
+        a tuple of frames (in this case a contiguous host and device buffer).
+
+        To enable dask support, dask serializers must be registered
+
+            >>> from cudf_polars.experimental.dask_serialize import register
+            >>> register()
+
+        Returns
+        -------
+        header
+            A dict containing any picklable metadata required to reconstruct the object.
+        frames
+            Two-tuple of frames suitable for passing to `plc.contiguous_split.unpack_from_memoryviews`
+        """
+        packed = plc.contiguous_split.pack(self.table)
+
+        # Keyword arguments for `Column.__init__`.
+        columns_kwargs: list[ColumnOptions] = [
+            col.serialize_ctor_kwargs() for col in self.columns
+        ]
+        header: DataFrameHeader = {
+            "columns_kwargs": columns_kwargs,
+            "frame_count": 2,
+        }
+        return header, packed.release()
+
+    def sorted_like(
+        self, like: DataFrame, /, *, subset: Set[str] | None = None
+    ) -> Self:
+        """
+        Return a shallow copy with sortedness copied from like.
+
+        Parameters
+        ----------
+        like
+            The dataframe to copy from
+        subset
+            Optional subset of columns from which to copy data.
+
+        Returns
+        -------
+        Shallow copy of self with metadata set.
+
+        Raises
+        ------
+        ValueError
+            If there is a name mismatch between self and like.
+        """
+        if like.column_names != self.column_names:
+            raise ValueError("Can only copy from identically named frame")
+        subset = self.column_names_set if subset is None else subset
+        return type(self)(
+            c.sorted_like(other) if c.name in subset else c
+            for c, other in zip(self.columns, like.columns, strict=True)
+        )
+
+    def with_columns(
+        self, columns: Iterable[Column], *, replace_only: bool = False
+    ) -> Self:
+        """
+        Return a new dataframe with extra columns.
+
+        Parameters
+        ----------
+        columns
+            Columns to add
+        replace_only
+            If true, then only replacements are allowed (matching by name).
+
+        Returns
+        -------
+        New dataframe
+
+        Notes
+        -----
+        If column names overlap, newer names replace older ones, and
+        appear in the same order as the original frame.
+        """
+        new = {c.name: c for c in columns}
+        if replace_only and not self.column_names_set.issuperset(new.keys()):
+            raise ValueError("Cannot replace with non-existing names")
+        return type(self)((self.column_map | new).values())
+
+    def discard_columns(self, names: Set[str]) -> Self:
+        """Drop columns by name."""
+        return type(self)(column for column in self.columns if column.name not in names)
+
+    def select(self, names: Sequence[str] | Mapping[str, Any]) -> Self:
+        """Select columns by name returning DataFrame."""
+        try:
+            return type(self)(self.column_map[name] for name in names)
+        except KeyError as e:
+            raise ValueError("Can't select missing names") from e
+
+    def rename_columns(self, mapping: Mapping[str, str]) -> Self:
+        """Rename some columns."""
+        return type(self)(c.rename(mapping.get(c.name, c.name)) for c in self.columns)
+
+    def select_columns(self, names: Set[str]) -> list[Column]:
+        """Select columns by name."""
+        return [c for c in self.columns if c.name in names]
+
+    def filter(self, mask: Column) -> Self:
+        """Return a filtered table given a mask."""
+        table = plc.stream_compaction.apply_boolean_mask(self.table, mask.obj)
+        return (
+            type(self)
+            .from_table(table, self.column_names, self.dtypes)
+            .sorted_like(self)
+        )
+
+    def slice(self, zlice: Slice | None) -> Self:
+        """
+        Slice a dataframe.
+
+        Parameters
+        ----------
+        zlice
+            optional, tuple of start and length, negative values of start
+            treated as for python indexing. If not provided, returns self.
+
+        Returns
+        -------
+        New dataframe (if zlice is not None) otherwise self (if it is)
+        """
+        if zlice is None:
+            return self
+        (table,) = plc.copying.slice(
+            self.table, conversion.from_polars_slice(zlice, num_rows=self.num_rows)
+        )
+        return (
+            type(self)
+            .from_table(table, self.column_names, self.dtypes)
+            .sorted_like(self)
+        )
---
+++ packages/cudf-polars-cu12/cudf_polars_cu12-25.10.0-py3-none-any.whl/cudf_polars/dsl/expressions/boolean.py
@@ -0,0 +1,326 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-License-Identifier: Apache-2.0
+# TODO: Document BooleanFunction to remove noqa
+# ruff: noqa: D101
+"""Boolean DSL nodes."""
+
+from __future__ import annotations
+
+from enum import IntEnum, auto
+from functools import partial, reduce
+from typing import TYPE_CHECKING, Any, ClassVar
+
+import pylibcudf as plc
+
+from cudf_polars.containers import Column, DataType
+from cudf_polars.dsl.expressions.base import (
+    ExecutionContext,
+    Expr,
+)
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+    import polars.type_aliases as pl_types
+    from polars.polars import _expr_nodes as pl_expr
+
+    from cudf_polars.containers import DataFrame
+
+__all__ = ["BooleanFunction"]
+
+
+class BooleanFunction(Expr):
+    class Name(IntEnum):
+        """Internal and picklable representation of polars' `BooleanFunction`."""
+
+        All = auto()
+        AllHorizontal = auto()
+        Any = auto()
+        AnyHorizontal = auto()
+        IsBetween = auto()
+        IsClose = auto()
+        IsDuplicated = auto()
+        IsFinite = auto()
+        IsFirstDistinct = auto()
+        IsIn = auto()
+        IsInfinite = auto()
+        IsLastDistinct = auto()
+        IsNan = auto()
+        IsNotNan = auto()
+        IsNotNull = auto()
+        IsNull = auto()
+        IsUnique = auto()
+        Not = auto()
+
+        @classmethod
+        def from_polars(cls, obj: pl_expr.BooleanFunction) -> Self:
+            """Convert from polars' `BooleanFunction`."""
+            try:
+                function, name = str(obj).split(".", maxsplit=1)
+            except ValueError:
+                # Failed to unpack string
+                function = None
+            if function != "BooleanFunction":
+                raise ValueError("BooleanFunction required")
+            return getattr(cls, name)
+
+    __slots__ = ("name", "options")
+    _non_child = ("dtype", "name", "options")
+
+    def __init__(
+        self,
+        dtype: DataType,
+        name: BooleanFunction.Name,
+        options: tuple[Any, ...],
+        *children: Expr,
+    ) -> None:
+        self.dtype = dtype
+        self.options = options
+        self.name = name
+        self.children = children
+        self.is_pointwise = self.name not in (
+            BooleanFunction.Name.All,
+            BooleanFunction.Name.Any,
+            BooleanFunction.Name.IsDuplicated,
+            BooleanFunction.Name.IsFirstDistinct,
+            BooleanFunction.Name.IsLastDistinct,
+            BooleanFunction.Name.IsUnique,
+        )
+        if self.name in {
+            BooleanFunction.Name.IsClose,
+        }:
+            raise NotImplementedError(
+                f"Boolean function {self.name}"
+            )  # pragma: no cover
+
+    @staticmethod
+    def _distinct(
+        column: Column,
+        dtype: DataType,
+        *,
+        keep: plc.stream_compaction.DuplicateKeepOption,
+        source_value: plc.Scalar,
+        target_value: plc.Scalar,
+    ) -> Column:
+        table = plc.Table([column.obj])
+        indices = plc.stream_compaction.distinct_indices(
+            table,
+            keep,
+            # TODO: polars doesn't expose options for these
+            plc.types.NullEquality.EQUAL,
+            plc.types.NanEquality.ALL_EQUAL,
+        )
+        return Column(
+            plc.copying.scatter(
+                [source_value],
+                indices,
+                plc.Table([plc.Column.from_scalar(target_value, table.num_rows())]),
+            ).columns()[0],
+            dtype=dtype,
+        )
+
+    _BETWEEN_OPS: ClassVar[
+        dict[
+            pl_types.ClosedInterval,
+            tuple[plc.binaryop.BinaryOperator, plc.binaryop.BinaryOperator],
+        ]
+    ] = {
+        "none": (
+            plc.binaryop.BinaryOperator.GREATER,
+            plc.binaryop.BinaryOperator.LESS,
+        ),
+        "left": (
+            plc.binaryop.BinaryOperator.GREATER_EQUAL,
+            plc.binaryop.BinaryOperator.LESS,
+        ),
+        "right": (
+            plc.binaryop.BinaryOperator.GREATER,
+            plc.binaryop.BinaryOperator.LESS_EQUAL,
+        ),
+        "both": (
+            plc.binaryop.BinaryOperator.GREATER_EQUAL,
+            plc.binaryop.BinaryOperator.LESS_EQUAL,
+        ),
+    }
+
+    def do_evaluate(
+        self, df: DataFrame, *, context: ExecutionContext = ExecutionContext.FRAME
+    ) -> Column:
+        """Evaluate this expression given a dataframe for context."""
+        if self.name in (
+            BooleanFunction.Name.IsFinite,
+            BooleanFunction.Name.IsInfinite,
+        ):
+            # Avoid evaluating the child if the dtype tells us it's unnecessary.
+            (child,) = self.children
+            needles = child.evaluate(df, context=context)
+            is_float = needles.obj.type().id() in (
+                plc.TypeId.FLOAT32,
+                plc.TypeId.FLOAT64,
+            )
+            is_finite = self.name is BooleanFunction.Name.IsFinite
+            if not is_float:
+                base = plc.Column.from_scalar(
+                    plc.Scalar.from_py(py_val=is_finite), needles.size
+                )
+                out = base.with_mask(needles.obj.null_mask(), needles.null_count)
+                return Column(out, dtype=self.dtype)
+            to_search = [-float("inf"), float("inf")]
+            if is_finite:
+                # NaN is neither finite not infinite
+                to_search.append(float("nan"))
+            haystack = plc.Column.from_iterable_of_py(
+                to_search,
+                dtype=needles.obj.type(),
+            )
+            result = plc.search.contains(haystack, needles.obj)
+            if is_finite:
+                result = plc.unary.unary_operation(result, plc.unary.UnaryOperator.NOT)
+            return Column(
+                result.with_mask(needles.obj.null_mask(), needles.null_count),
+                dtype=self.dtype,
+            )
+        columns = [child.evaluate(df, context=context) for child in self.children]
+        # Kleene logic for Any (OR) and All (AND) if ignore_nulls is
+        # False
+        if self.name in (BooleanFunction.Name.Any, BooleanFunction.Name.All):
+            (ignore_nulls,) = self.options
+            (column,) = columns
+            is_any = self.name is BooleanFunction.Name.Any
+            agg = plc.aggregation.any() if is_any else plc.aggregation.all()
+            result = plc.reduce.reduce(column.obj, agg, self.dtype.plc)
+            if not ignore_nulls and column.null_count > 0:
+                #      Truth tables
+                #     Any         All
+                #   | F U T     | F U T
+                # --+------   --+------
+                # F | F U T   F | F F F
+                # U | U U T   U | F U U
+                # T | T T T   T | F U T
+                #
+                # If the input null count was non-zero, we must
+                # post-process the result to insert the correct value.
+                h_result = result.to_py()
+                if (is_any and not h_result) or (not is_any and h_result):
+                    # Any                     All
+                    # False || Null => Null   True && Null => Null
+                    return Column(
+                        plc.Column.all_null_like(column.obj, 1), dtype=self.dtype
+                    )
+            return Column(plc.Column.from_scalar(result, 1), dtype=self.dtype)
+        if self.name is BooleanFunction.Name.IsNull:
+            (column,) = columns
+            return Column(plc.unary.is_null(column.obj), dtype=self.dtype)
+        elif self.name is BooleanFunction.Name.IsNotNull:
+            (column,) = columns
+            return Column(plc.unary.is_valid(column.obj), dtype=self.dtype)
+        elif self.name in (BooleanFunction.Name.IsNan, BooleanFunction.Name.IsNotNan):
+            (column,) = columns
+            is_float = column.obj.type().id() in (
+                plc.TypeId.FLOAT32,
+                plc.TypeId.FLOAT64,
+            )
+            if is_float:
+                op = (
+                    plc.unary.is_nan
+                    if self.name is BooleanFunction.Name.IsNan
+                    else plc.unary.is_not_nan
+                )
+                base = op(column.obj)
+            else:
+                base = plc.Column.from_scalar(
+                    plc.Scalar.from_py(
+                        py_val=self.name is not BooleanFunction.Name.IsNan
+                    ),
+                    column.size,
+                )
+            out = base.with_mask(column.obj.null_mask(), column.null_count)
+            return Column(out, dtype=self.dtype)
+        elif self.name is BooleanFunction.Name.IsFirstDistinct:
+            (column,) = columns
+            return self._distinct(
+                column,
+                dtype=self.dtype,
+                keep=plc.stream_compaction.DuplicateKeepOption.KEEP_FIRST,
+                source_value=plc.Scalar.from_py(py_val=True, dtype=self.dtype.plc),
+                target_value=plc.Scalar.from_py(py_val=False, dtype=self.dtype.plc),
+            )
+        elif self.name is BooleanFunction.Name.IsLastDistinct:
+            (column,) = columns
+            return self._distinct(
+                column,
+                dtype=self.dtype,
+                keep=plc.stream_compaction.DuplicateKeepOption.KEEP_LAST,
+                source_value=plc.Scalar.from_py(py_val=True, dtype=self.dtype.plc),
+                target_value=plc.Scalar.from_py(py_val=False, dtype=self.dtype.plc),
+            )
+        elif self.name is BooleanFunction.Name.IsUnique:
+            (column,) = columns
+            return self._distinct(
+                column,
+                dtype=self.dtype,
+                keep=plc.stream_compaction.DuplicateKeepOption.KEEP_NONE,
+                source_value=plc.Scalar.from_py(py_val=True, dtype=self.dtype.plc),
+                target_value=plc.Scalar.from_py(py_val=False, dtype=self.dtype.plc),
+            )
+        elif self.name is BooleanFunction.Name.IsDuplicated:
+            (column,) = columns
+            return self._distinct(
+                column,
+                dtype=self.dtype,
+                keep=plc.stream_compaction.DuplicateKeepOption.KEEP_NONE,
+                source_value=plc.Scalar.from_py(py_val=False, dtype=self.dtype.plc),
+                target_value=plc.Scalar.from_py(py_val=True, dtype=self.dtype.plc),
+            )
+        elif self.name is BooleanFunction.Name.AllHorizontal:
+            return Column(
+                reduce(
+                    partial(
+                        plc.binaryop.binary_operation,
+                        op=plc.binaryop.BinaryOperator.NULL_LOGICAL_AND,
+                        output_type=self.dtype.plc,
+                    ),
+                    (c.obj for c in columns),
+                ),
+                dtype=self.dtype,
+            )
+        elif self.name is BooleanFunction.Name.AnyHorizontal:
+            return Column(
+                reduce(
+                    partial(
+                        plc.binaryop.binary_operation,
+                        op=plc.binaryop.BinaryOperator.NULL_LOGICAL_OR,
+                        output_type=self.dtype.plc,
+                    ),
+                    (c.obj for c in columns),
+                ),
+                dtype=self.dtype,
+            )
+        elif self.name is BooleanFunction.Name.IsIn:
+            needles, haystack = columns
+            if haystack.obj.type().id() == plc.TypeId.LIST:
+                # Unwrap values from the list column
+                # the type: ignore is safe because we know that the type ID is LIST,
+                # which always has an inner attribute.
+                haystack = Column(
+                    haystack.obj.children()[1],
+                    dtype=DataType(haystack.dtype.polars.inner),  # type: ignore[attr-defined]
+                ).astype(needles.dtype)
+            if haystack.size:
+                return Column(
+                    plc.search.contains(haystack.obj, needles.obj), dtype=self.dtype
+                )
+            return Column(
+                plc.Column.from_scalar(plc.Scalar.from_py(py_val=False), needles.size),
+                dtype=self.dtype,
+            )
+        elif self.name is BooleanFunction.Name.Not:
+            (column,) = columns
+            return Column(
+                plc.unary.unary_operation(column.obj, plc.unary.UnaryOperator.NOT),
+                dtype=self.dtype,
+            )
+        else:
+            raise NotImplementedError(
+                f"BooleanFunction {self.name}"
+            )  # pragma: no cover; handled by init raising
