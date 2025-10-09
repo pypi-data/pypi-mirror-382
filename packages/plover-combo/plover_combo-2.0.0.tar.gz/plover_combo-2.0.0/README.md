9e1cfcba98c8038e11836ef78a0e659a8f62672e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 16:09:14 +0000

    Add package plover-combo/2.0.0/plover_combo-2.0.0.tar.gz

---
+++ packages/plover-combo/plover_combo-2.0.0.tar.gz/plover_combo-2.0.0/README.md
@@ -0,0 +1,27 @@
+# Plover Combo
+[![PyPI](https://img.shields.io/pypi/v/plover-combo)](https://pypi.org/project/plover-combo/)
+![GitHub](https://img.shields.io/github/license/Kaoffie/plover_combo)
+
+A plugin for the open-source stenography software [Plover](https://www.openstenoproject.org) that adds a Combo counter. Inspired by the [Code in the Dark Editor](https://github.com/codeinthedark/editor).
+
+![combo](https://user-images.githubusercontent.com/30435273/131253406-878f1d0b-6f83-4d31-8f2b-b919f5f9424a.gif)
+
+## Usage Notes
+
+- Shortcuts:
+    - `Ctrl/Cmd + S` to open settings
+    - `Ctrl/Cmd + X` to close widget. 
+
+- Left click on the counter number and drag to move the widget around.
+- After adjusting the settings, you might have to restart the plugin for all the changes to fully take effect.
+- On Windows, the widget cannot be moved around by dragging on empty areas. To fix this, change the background opacity to 1.
+- On macOS, you might experience repainting issues where a ghost image appears behind the counter. To fix this, turn on the "Force Repaint" option in the settings dialog.
+- The window width is controlled by the width of the number currently displayed; to adjust the left and right padding, change the "Horizontal Margin" setting.
+
+## Installation
+
+You can install this plugin directly using the Plugin Manager, under the name `plover-combo`.
+
+## Credits
+
+Huge thanks to /usr, Emily and Jen on the Plover Discord for testing the plugin out on macOS!
---
+++ packages/plover-combo/plover_combo-2.0.0.tar.gz/plover_combo-2.0.0/plover_combo/config_ui.py
@@ -0,0 +1,149 @@
+from PySide6.QtWidgets import (
+    QDialog, QWidget, QLabel, QSpinBox, 
+    QComboBox, QDialogButtonBox, QGridLayout,
+    QGroupBox, QCheckBox, QVBoxLayout,
+    QLineEdit, QScrollArea, QSizePolicy,
+    QPlainTextEdit
+)
+from PySide6.QtCore import Qt
+
+from plover_combo.combo_config import (
+    ComboAlignment, ComboConfig, CONFIG_NAMES, CONFIG_ORDER, 
+    CONFIG_TYPES, CONFIG_RANGES, ALIGNMENT_OPTIONS
+)
+from plover_combo.combo_colors import COLOR_FORMAT
+
+
+FIELD_DATA_WIDTH = 250
+PLAIN_TEXT_DATA_HEIGHT = 200
+
+
+class ConfigUI(QDialog):
+
+    def __init__(self, temp_config: ComboConfig, parent: QWidget = None) -> None:
+        super().__init__(parent)
+        self.temp_config = temp_config
+        self.setup_window()
+
+    def setup_window(self) -> None:
+        self.scroll_widget = QWidget()
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_layout = QVBoxLayout()
+        self.fields = dict()
+
+        current_grid_row = 0
+        current_grid_layout: QGridLayout = None
+        current_groupbox: QGroupBox = None
+
+        for config_name in CONFIG_ORDER:
+            if config_name not in CONFIG_NAMES:
+                if current_groupbox is not None:
+                    current_groupbox.setLayout(current_grid_layout)
+                    self.scroll_layout.addWidget(current_groupbox)
+                
+                current_grid_row = 0
+                current_groupbox = QGroupBox()
+                current_groupbox.setTitle(config_name)
+                current_grid_layout = QGridLayout()
+                continue
+            
+            field_label = QLabel()
+            field_label.setText(CONFIG_NAMES[config_name])
+
+            field_type = CONFIG_TYPES[config_name]
+            field_data = None
+
+            if field_type == bool:
+                field_data = QCheckBox()
+                field_data.setChecked(getattr(self.temp_config, config_name))
+
+            elif field_type == int:
+                low, high, step, suffix = CONFIG_RANGES[config_name]
+                field_data = QSpinBox()
+                field_data.setRange(low, high)
+                field_data.setSingleStep(step)
+                field_data.setSuffix(suffix)
+                field_data.setValue(getattr(self.temp_config, config_name))
+
+            elif field_type == str:
+                field_data = QLineEdit()
+                field_data.setText(getattr(self.temp_config, config_name))
+
+            elif field_type == ComboAlignment:
+                field_data = QComboBox()
+                field_data.addItems(ALIGNMENT_OPTIONS)
+                field_data.setCurrentIndex(int(getattr(self.temp_config, config_name).value))
+
+            if field_data is not None:
+                field_data.setMinimumWidth(FIELD_DATA_WIDTH)
+                current_grid_layout.addWidget(
+                    field_label, current_grid_row, 0, 1, 1, Qt.AlignmentFlag.AlignRight
+                )
+                current_grid_layout.addWidget(
+                    field_data, current_grid_row, 1, 1, 1, Qt.AlignmentFlag.AlignLeft
+                )
+
+                self.fields[config_name] = field_data
+                current_grid_row += 1
+
+        if current_groupbox is not None:
+            current_groupbox.setLayout(current_grid_layout)
+            self.scroll_layout.addWidget(current_groupbox)
+
+        combo_color_group = QGroupBox()
+        combo_color_group.setTitle("Combo Colors")
+        combo_color_layout = QVBoxLayout()
+
+        combo_color_label = QLabel()
+        combo_color_label.setText(COLOR_FORMAT)
+        combo_color_layout.addWidget(combo_color_label)
+
+        combo_color_data = QPlainTextEdit()
+        combo_color_data.setPlainText(self.temp_config.combo_colors)
+        combo_color_data.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+        combo_color_data.setFixedHeight(PLAIN_TEXT_DATA_HEIGHT)
+        combo_color_layout.addWidget(combo_color_data)
+        self.fields["combo_colors"] = combo_color_data
+
+        combo_color_group.setLayout(combo_color_layout)
+        self.scroll_layout.addWidget(combo_color_group)
+
+        self.button_box = QDialogButtonBox(
+            (
+                QDialogButtonBox.StandardButton.Cancel | 
+                QDialogButtonBox.StandardButton.Ok
+            ),
+            parent=self
+        )
+        self.button_box.rejected.connect(self.reject)
+        self.button_box.accepted.connect(self.save_settings)
+
+        self.scroll_widget.setLayout(self.scroll_layout)
+        self.scroll_area.setWidget(self.scroll_widget)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.scroll_area)
+        self.layout.addWidget(self.button_box)
+        self.setLayout(self.layout)
+
+    def save_settings(self) -> None:
+        for config_name in CONFIG_NAMES.keys():
+            field_type = CONFIG_TYPES[config_name]
+            field_data = self.fields[config_name]
+            field_value = None
+
+            if field_type == bool:
+                field_value = field_data.isChecked()
+            elif field_type == int:
+                field_value = field_data.value()
+            elif field_type == str:
+                field_value = field_data.text()
+            elif field_type == ComboAlignment:
+                field_value = ComboAlignment(field_data.currentIndex())
+
+            if field_value is not None:
+                setattr(self.temp_config, config_name, field_value)
+        
+        self.temp_config.combo_colors = self.fields["combo_colors"].toPlainText()
+        self.accept()
