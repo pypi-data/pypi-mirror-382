ec06cdac8a9a2361a8f463ef8ff075ab3feeecb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 16:08:57 +0000

    Add package eiogram/2.1.1.dev20251009065049/eiogram-2.1.1.dev20251009065049-py3-none-any.whl

---
+++ packages/eiogram/eiogram-2.1.1.dev20251009065049-py3-none-any.whl/eiogram/filters/__init__.py
@@ -0,0 +1,26 @@
+from ._base import Filter
+from ._chat_type import IsSuperGroup, IsChannel, IsForum, IsGroup, IsPrivate
+from ._command import Command, StartCommand
+from ._data import Data
+from ._photo import Photo
+from ._regax import Regex
+from ._text import Text
+from ._state import StateFilter, IgnoreStateFilter
+
+__all__ = [
+    "BaseTextFilter",
+    "Filter",
+    "IsSuperGroup",
+    "IsChannel",
+    "IsForum",
+    "IsGroup",
+    "IsPrivate",
+    "Command",
+    "StartCommand",
+    "Data",
+    "Photo",
+    "Regex",
+    "Text",
+    "StateFilter",
+    "IgnoreStateFilter",
+]
---
+++ packages/eiogram/eiogram-2.1.1.dev20251009065049-py3-none-any.whl/eiogram/types/_user.py
@@ -0,0 +1,64 @@
+from typing import Optional, Union
+from pydantic import BaseModel
+from ..client import Bot
+
+
+class User(BaseModel):
+    id: int
+    is_bot: bool
+    first_name: str
+    last_name: Optional[str] = None
+    username: Optional[str] = None
+    bot: Optional[Bot] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}" if self.last_name else self.first_name
+
+    @property
+    def chatid(self) -> int:
+        return self.id
+
+    @property
+    def mention(self) -> Optional[str]:
+        return f"@{self.username}" if self.username else None
+
+    def __str__(self) -> str:
+        return f"User(id={self.id}, name={self.full_name}, username={self.username or 'N/A'})"
+
+    async def is_join(
+        self,
+        chat_id: Union[str, int],
+    ) -> bool:
+        from ._chat import ChatMemberStatus
+
+        try:
+            status = await self.bot.get_chat_member(chat_id=chat_id, user_id=self.id)
+            if status and status in [
+                ChatMemberStatus.ADMINISTRATOR,
+                ChatMemberStatus.CREATOR,
+                ChatMemberStatus.MEMBER,
+                ChatMemberStatus.RESTRICTED,
+            ]:
+                return True
+        except Exception:
+            return False
+
+    async def is_admin(
+        self,
+        chat_id: Union[str, int],
+    ) -> bool:
+        from ._chat import ChatMemberStatus
+
+        try:
+            status = await self.bot.get_chat_member(chat_id=chat_id, user_id=self.id)
+            if status and status in [
+                ChatMemberStatus.ADMINISTRATOR,
+                ChatMemberStatus.CREATOR,
+            ]:
+                return True
+        except Exception:
+            return False
