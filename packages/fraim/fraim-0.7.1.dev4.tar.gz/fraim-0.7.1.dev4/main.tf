07cd09fa19df3d39984f2cdf9c3901e3f7f6937d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 07:05:45 +0000

    Add package fraim/0.7.1.dev4/fraim-0.7.1.dev4.tar.gz

---
+++ packages/fraim/fraim-0.7.1.dev4.tar.gz/fraim-0.7.1.dev4/main.tf
@@ -0,0 +1,12 @@
+resource aws_security_group "example" {
+    name = "example_security_group"
+    description = "Example Description"
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        from_port = 5432
+        to_port = 5432
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
