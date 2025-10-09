a40789805738dd2d727298fa83eda02f94c9c428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 14:07:11 +0000

    Add package qupled/1.4.1/qupled-1.4.1-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/qupled/qupled-1.4.1-cp310-cp310-manylinux_2_28_x86_64.whl/qupled/esa.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from . import hf
+from . import native
+from . import serialize
+
+
+class Solver(hf.Solver):
+    """
+    Class used to solve the ESA scheme.
+    """
+
+    # Native classes used to solve the scheme
+    native_scheme_cls = native.ESA
+
+    def __init__(self):
+        super().__init__()
+        self.results: hf.Result = hf.Result()
+        # Undocumented properties
+        self.native_scheme_cls = native.ESA
+
+
+@serialize.serializable_dataclass
+class Input(hf.Input):
+    """
+    Class used to manage the input for the :obj:`qupled.esa.ESA` class.
+    """
+
+    theory: str = "ESA"
+
+
+if __name__ == "__main__":
+    Solver.run_mpi_worker(Input, hf.Result)
