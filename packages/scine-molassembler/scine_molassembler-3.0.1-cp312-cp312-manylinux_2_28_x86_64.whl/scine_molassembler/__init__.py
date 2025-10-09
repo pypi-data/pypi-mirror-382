5196b0947d143ebc63ac082ea5bcc285796c35a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:30:36 +0000

    Add package scine-molassembler/3.0.1/scine_molassembler-3.0.1-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/scine-molassembler/scine_molassembler-3.0.1-cp312-cp312-manylinux_2_28_x86_64.whl/scine_molassembler/__init__.py
@@ -0,0 +1,35 @@
+__copyright__ = """This code is licensed under the 3-clause BSD license.
+Copyright ETH Zurich, Department of Chemistry and Applied Biosciences, Reiher Group.
+See LICENSE.txt for details.
+"""
+
+from importlib.util import (
+    spec_from_file_location,
+    module_from_spec,
+)
+from importlib.abc import Loader
+import os
+import sys
+from pathlib import Path
+import sysconfig
+
+expected_suffix = sysconfig.get_config_var("EXT_SUFFIX")
+if not isinstance(expected_suffix, str):
+    msg = "Unexpected sysconfig return type for EXT_SUFFIX: {}"
+    raise ImportError(msg.format(type(expected_suffix)))
+
+expected_name = __name__ + expected_suffix
+this_file_dir = Path(__file__).parent.absolute()
+python_module_path = os.path.join(this_file_dir, expected_name)
+if not os.path.exists(python_module_path):
+    raise ImportError(f"Could not find {expected_name}")
+
+# This is more or less copy-paste from importlib's docs, makes the pybind11
+# module wild-importable (e.g. from `scine_molassembler.io import *`)
+spec = spec_from_file_location(__name__, python_module_path)
+module = module_from_spec(spec)
+sys.modules[__name__] = module
+if not isinstance(spec.loader, Loader):
+    raise ImportError("Type mismatch for generated loader!")
+
+spec.loader.exec_module(module)
