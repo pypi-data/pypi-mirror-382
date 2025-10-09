69e416d436e042d0989748638e9ba081d5ad85a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:29:45 +0000

    Add package mangoautomation/1.1.2/mangoautomation-1.1.2-py3-none-any.whl

---
+++ packages/mangoautomation/mangoautomation-1.1.2-py3-none-any.whl/mangoautomation/models/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description:# @Time   : 2023-09-09 23:17
+# @Author : 毛鹏
+import sys
+
+from ..models._ui_model import ElementModel, ElementResultModel, ElementListResultModel
+
+
+__all__ = [
+    'ElementModel',
+    'ElementResultModel',
+    'ElementListResultModel'
+]
