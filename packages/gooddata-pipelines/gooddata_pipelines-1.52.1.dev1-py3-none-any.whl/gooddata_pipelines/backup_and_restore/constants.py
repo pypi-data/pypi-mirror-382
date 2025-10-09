36f4a3bedc89c4617117bf20acab4b6b71eb48d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:17:45 +0000

    Add package gooddata-pipelines/1.52.1.dev1/gooddata_pipelines-1.52.1.dev1-py3-none-any.whl

---
+++ packages/gooddata-pipelines/gooddata_pipelines-1.52.1.dev1-py3-none-any.whl/gooddata_pipelines/backup_and_restore/constants.py
@@ -0,0 +1,39 @@
+# (C) 2025 GoodData Corporation
+import datetime
+
+import attrs
+from gooddata_sdk._version import __version__ as sdk_version
+
+
+@attrs.frozen
+class DirNames:
+    """
+    Folder names used in the SDK backup process:
+    - LAYOUTS - GoodData Layouts
+    - LDM - Logical Data Model
+    - AM - Analytics Model
+    - UDF - User Data Filters
+    """
+
+    LAYOUTS = "gooddata_layouts"
+    LDM = "ldm"
+    AM = "analytics_model"
+    UDF = "user_data_filters"
+
+
+@attrs.frozen
+class ApiDefaults:
+    DEFAULT_PAGE_SIZE = 100
+    DEFAULT_BATCH_SIZE = 100
+    DEFAULT_API_CALLS_PER_SECOND = 1.0
+
+
+@attrs.frozen
+class BackupSettings(ApiDefaults):
+    MAX_RETRIES = 3
+    RETRY_DELAY = 5  # seconds
+    TIMESTAMP_SDK_FOLDER = (
+        str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+        + "-"
+        + sdk_version.replace(".", "_")
+    )
