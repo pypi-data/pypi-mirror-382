93136bbd1efbe7ab6ad2641f4d9774bd2a66928e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:17:33 +0000

    Add package xi.sdk.resellers/1.3.0/xi_sdk_resellers-1.3.0.tar.gz

---
+++ packages/xi.sdk.resellers/xi_sdk_resellers-1.3.0.tar.gz/xi_sdk_resellers-1.3.0/xi/sdk/resellers/api/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+# import apis into api package
+from xi.sdk.resellers.api.accesstoken_api import AccesstokenApi
+from xi.sdk.resellers.api.deals_api import DealsApi
+from xi.sdk.resellers.api.freight_estimate_api import FreightEstimateApi
+from xi.sdk.resellers.api.invoices_api import InvoicesApi
+from xi.sdk.resellers.api.order_status_api import OrderStatusApi
+from xi.sdk.resellers.api.orders_api import OrdersApi
+from xi.sdk.resellers.api.product_catalog_api import ProductCatalogApi
+from xi.sdk.resellers.api.quotes_api import QuotesApi
+from xi.sdk.resellers.api.renewals_api import RenewalsApi
+from xi.sdk.resellers.api.returns_api import ReturnsApi
+from xi.sdk.resellers.api.stock_update_api import StockUpdateApi
+
