976d3457c8eb74951a01620ad73e09679c587e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:06:59 +0000

    Add package qerent-keyring/0.1.0/qerent_keyring-0.1.0.tar.gz

---
+++ packages/qerent-keyring/qerent_keyring-0.1.0.tar.gz/qerent_keyring-0.1.0/src/qerent_keyring/__init__.py
@@ -0,0 +1,61 @@
+__author__ = "BSC Global"
+__version__ = "0.1.0"
+
+import warnings
+from urllib.parse import urlsplit
+
+import keyring.backend
+import keyring.credentials
+from keyring.compat import properties
+
+from .plugin import CredentialProvider
+
+
+class QerentKeyringBackend(keyring.backend.KeyringBackend):
+    def __init__(self):
+        self._cache = {}
+
+    @properties.classproperty
+    def priority(self) -> float:
+        return 9.9
+
+    def get_credential(
+        self, service: str, username: str | None
+    ) -> keyring.credentials.Credential | None:
+        try:
+            parsed = urlsplit(service)
+        except Exception as e:
+            warnings.warn(str(e))
+            return None
+
+        netloc = parsed.netloc.rpartition("@")[-1]
+
+        if netloc is None or not netloc.endswith("distribution.qerent.ai"):
+            return None
+
+        provider = CredentialProvider()
+        username, password = provider.get_credentials(service)
+
+        if username and password:
+            self._cache[service, username] = password
+            return keyring.credentials.SimpleCredential(username, password)
+
+    def get_password(self, service: str, username: str) -> str | None:
+        password = self._cache.get((service, username), None)
+        if password is not None:
+            return password
+
+        creds = self.get_credential(service, None)
+
+        if creds and username == creds.username:
+            return creds.password
+
+        return None
+
+    def set_password(self, service: str, username: str, password: str) -> None:
+        # Defer setting a password to the next backend
+        raise NotImplementedError()
+
+    def delete_password(self, service: str, username: str) -> None:
+        # Defer deleting a password to the next backend
+        raise NotImplementedError()
---
+++ packages/qerent-keyring/qerent_keyring-0.1.0.tar.gz/qerent_keyring-0.1.0/src/qerent_keyring/plugin.py
@@ -0,0 +1,50 @@
+import subprocess
+import sys
+from subprocess import Popen
+
+
+class CredentialProvider:
+    def get_credentials(self, url: str) -> tuple[str, str]:
+        proc = Popen(
+            ["qerent", "login", "--return-token"],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        if not proc.stderr:
+            raise RuntimeError("proc.stderr not available for some reason")
+
+        if not proc.stdout:
+            raise RuntimeError("proc.stdout not available for some reason")
+
+        for stderr_line in iter(proc.stderr.readline, b""):
+            line = stderr_line.decode(errors="ignore")
+            sys.stdout.write(line)
+            sys.stdout.flush()
+
+        proc.wait()
+
+        if proc.returncode != 0:
+            stderr = proc.stderr.read().decode("utf-8", "ignore")
+
+            error_msg = "Failed to get credentials: process with PID {pid} exited with code {code}".format(
+                pid=proc.pid, code=proc.returncode
+            )
+            if stderr.strip():
+                error_msg += f"; additional error message: {stderr}"
+
+            raise RuntimeError(error_msg)
+
+        try:
+            # stdout is expected to be UTF-8 encoded text, so decoding errors are not ignored here.
+            payload = proc.stdout.read().decode("utf-8").strip()
+        except ValueError:
+            raise RuntimeError(
+                "Failed to get credentials: the Qerent CLI's output could not be decoded using UTF-8."
+            )
+
+        username = "jwt"
+        password = payload
+
+        return username, password
