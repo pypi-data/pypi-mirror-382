767d3c1f657ddd8cbbf5610d6f0bbd3b02e993f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 14:07:16 +0000

    Add package osl-dynamics/2.1.7/osl_dynamics-2.1.7.tar.gz

---
+++ packages/osl-dynamics/osl_dynamics-2.1.7.tar.gz/osl_dynamics-2.1.7/osl_dynamics/analysis/__init__.py
@@ -0,0 +1,15 @@
+"""A collection of modules to perform post-hoc analysis of data."""
+
+from osl_dynamics.analysis import (
+    connectivity,
+    fisher_kernel,
+    gmm,
+    modes,
+    power,
+    regression,
+    spectral,
+    static,
+    statistics,
+    workbench,
+    prediction,
+)
