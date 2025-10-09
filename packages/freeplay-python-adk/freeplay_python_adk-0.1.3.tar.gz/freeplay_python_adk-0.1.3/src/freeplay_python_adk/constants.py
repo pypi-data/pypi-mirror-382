9f48ee216496c4b01abfa74ad8f2480610f61ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 09:05:35 +0000

    Add package freeplay-python-adk/0.1.3/freeplay_python_adk-0.1.3.tar.gz

---
+++ packages/freeplay-python-adk/freeplay_python_adk-0.1.3.tar.gz/freeplay_python_adk-0.1.3/src/freeplay_python_adk/constants.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class FreeplayOTelAttributes(Enum):
+    FREEPLAY_INPUT_VARIABLES = "freeplay.input-variables"
+    FREEPLAY_PROMPT_TEMPLATE_VERSION_ID = "freeplay.prompt-template-version-id"
+    FREEPLAY_ENVIRONMENT = "freeplay.environment"
