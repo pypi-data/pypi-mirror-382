6b30087fda2fedae88dc9f4f1a87a001a7168b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:30:12 +0000

    Add package prevectorchunks-core/0.1.12/prevectorchunks_core-0.1.12.tar.gz

---
+++ packages/prevectorchunks-core/prevectorchunks_core-0.1.12.tar.gz/prevectorchunks_core-0.1.12/prevectorchunks_core/test_loader.py
@@ -0,0 +1,26 @@
+import json
+import pytest
+
+from core.prevectorchunks_core.config.splitter_config import SplitterConfig
+from core.prevectorchunks_core.services import chunk_documents_crud_vdb
+
+
+# Create a temporary JSON file to test with
+@pytest.fixture
+def temp_json_file(tmp_path):
+    file_path = tmp_path / "test.json"
+    content = [{"id": 1, "text": "hello world"}]
+    with open(file_path, "w") as f:
+        json.dump(content, f)
+    return file_path
+
+
+def test_load_file_and_upsert_chunks_to_vdb(temp_json_file):
+    #dataset = chunk_and_upsert_to_vdb("dl-doc-search","instructions", file_path="content_playground/content.json")
+    #dataset=chunk_documents_crud_vdb.fetch_vdb_chunks_grouped_by_document_name("dl-doc-search")
+    splitter_config = SplitterConfig(chunk_size= 300, chunk_overlap= 0,separators=["\n"],split_type="RecursiveCharacterTextSplitter")
+
+    dataset=chunk_documents_crud_vdb.chunk_documents("extract", file_name=None, file_path=temp_json_file,splitter_config=splitter_config)
+    #dataset=chunk_documents_crud_vdb.chunk_documents("Extract doco",file_path="content_playground/content.json",file_name=None)
+    # Assertions
+
