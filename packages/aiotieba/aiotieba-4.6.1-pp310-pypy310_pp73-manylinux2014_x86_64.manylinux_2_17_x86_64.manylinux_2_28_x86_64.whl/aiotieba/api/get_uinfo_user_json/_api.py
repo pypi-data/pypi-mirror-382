0bc046e1d52aa8a62e41d6cac67f20558ffa25cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:13 +0000

    Add package aiotieba/4.6.1/aiotieba-4.6.1-pp310-pypy310_pp73-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/aiotieba/aiotieba-4.6.1-pp310-pypy310_pp73-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/aiotieba/api/get_uinfo_user_json/_api.py
@@ -0,0 +1,34 @@
+import yarl
+
+from ...const import WEB_BASE_HOST
+from ...core import HttpCore
+from ...exception import TiebaValueError
+from ...helper import parse_json
+from ._classdef import UserInfo_json
+
+
+def parse_body(body: bytes) -> UserInfo_json:
+    if not body:
+        raise TiebaValueError("Empty body")
+
+    text = body.decode("utf-8", errors="ignore")
+    res_json = parse_json(text)
+
+    user_dict = res_json["creator"]
+    user = UserInfo_json.from_tbdata(user_dict)
+
+    return user
+
+
+async def request(http_core: HttpCore, user_name: str) -> UserInfo_json:
+    params = [
+        ("un", user_name),
+        ("ie", "utf-8"),
+    ]
+
+    request = http_core.pack_web_get_request(
+        yarl.URL.build(scheme="http", host=WEB_BASE_HOST, path="/i/sys/user_json"), params
+    )
+
+    body = await http_core.net_core.send_request(request, read_bufsize=2 * 1024)
+    return parse_body(body)
