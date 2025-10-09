77ef3daa7d1a53a2c6273c483416f6bcd4d57d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:17:59 +0000

    Add package modusa/0.3.96/modusa-0.3.96.tar.gz

---
+++ packages/modusa/modusa-0.3.96.tar.gz/modusa-0.3.96/tests/test_io/plotter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+#--------Meta Information----------
+class_name = "Plotter"
+author_name = "Ankit Anand"
+author_email = "ankit0.anand0@gmail.com"
+created_at = "2025-07-06"
+#----------------------------------
+	
+from modusa.ios import Plotter
