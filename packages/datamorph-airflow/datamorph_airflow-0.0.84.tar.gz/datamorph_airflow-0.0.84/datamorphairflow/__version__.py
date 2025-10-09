23857170979e3c62d024a23c3fb829dec95540bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 08:07:29 +0000

    Add package datamorph-airflow/0.0.84/datamorph_airflow-0.0.84.tar.gz

---
+++ packages/datamorph-airflow/datamorph_airflow-0.0.84.tar.gz/datamorph_airflow-0.0.84/datamorphairflow/__version__.py
@@ -0,0 +1,9 @@
+"""
+Module contains the version of datamorph-airflow,
+ChangeLog.md with the release details for the version change needs to be updated
+"""
+__version__ = "0.0.84"
+
+"""
+0.0.83 - loadparams action introduced and updated s3resources to read json file
+"""
