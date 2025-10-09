906751a6dd9e12fee2a1d32469bc0b886cddb760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:12 +0000

    Add package aiotieba/4.6.1/aiotieba-4.6.1-cp313-cp313t-macosx_10_13_x86_64.whl

---
+++ packages/aiotieba/aiotieba-4.6.1-cp313-cp313t-macosx_10_13_x86_64.whl/aiotieba/api/get_fid/_api.py
@@ -0,0 +1,31 @@
+import yarl
+
+from ...const import WEB_BASE_HOST
+from ...core import HttpCore
+from ...exception import TiebaServerError, TiebaValueError
+from ...helper import parse_json
+
+
+def parse_body(body: bytes) -> int:
+    res_json = parse_json(body)
+    if code := res_json["no"]:
+        raise TiebaServerError(code, res_json["error"])
+
+    if not (fid := res_json["data"]["fid"]):
+        raise TiebaValueError("fid is 0")
+
+    return fid
+
+
+async def request(http_core: HttpCore, fname: str) -> int:
+    params = [
+        ("fname", fname),
+        ("ie", "utf-8"),
+    ]
+
+    request = http_core.pack_web_get_request(
+        yarl.URL.build(scheme="http", host=WEB_BASE_HOST, path="/f/commit/share/fnameShareApi"), params
+    )
+
+    body = await http_core.net_core.send_request(request, read_bufsize=2 * 1024)
+    return parse_body(body)
