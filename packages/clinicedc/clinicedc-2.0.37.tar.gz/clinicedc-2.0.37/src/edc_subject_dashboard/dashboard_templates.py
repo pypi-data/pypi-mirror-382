122df0b2b55d40040fe17666e576d29f899d783b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 06:05:52 +0000

    Add package clinicedc/2.0.37/clinicedc-2.0.37.tar.gz

---
+++ packages/clinicedc/clinicedc-2.0.37.tar.gz/clinicedc-2.0.37/src/edc_subject_dashboard/dashboard_templates.py
@@ -0,0 +1,7 @@
+"""To customize any of the values below,
+use settings.DASHBOARD_BASE_TEMPLATES.
+"""
+
+dashboard_templates = dict(
+    edc_subject_dashboard_template="edc_subject_dashboard/dashboard.html"
+)
