d2988f1dd8ae916725d2a65dff2469563c8614d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 07:05:50 +0000

    Add package reinforcenow/0.0.11/reinforcenow-0.0.11.tar.gz

---
+++ packages/reinforcenow/reinforcenow-0.0.11.tar.gz/reinforcenow-0.0.11/reinforcenow/templates/reward.py
@@ -0,0 +1,6 @@
+# Template for reward function
+# Define your custom reward logic here
+
+def calculate_reward(state, action, next_state):
+    """Calculate reward for the given transition"""
+    return 0.0
