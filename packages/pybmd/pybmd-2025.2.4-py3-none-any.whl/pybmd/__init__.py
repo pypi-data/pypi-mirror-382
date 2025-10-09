c137478b1798018a2b88edd356dedc93ee3bfe69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:19:08 +0000

    Add package pybmd/2025.2.4/pybmd-2025.2.4-py3-none-any.whl

---
+++ packages/pybmd/pybmd-2025.2.4-py3-none-any.whl/pybmd/__init__.py
@@ -0,0 +1,12 @@
+from .resolve import Resolve
+from .ui_dispather import UI_Dispather
+
+__name__ = 'pybmd'
+
+# __major_version__ = "2025"
+# __minor_version__ = "2"
+# __change_version__ = "0"
+# __version__ = ".".join(
+#     (__major_version__, __minor_version__, __change_version__)
+# )
+# __version__="2025.2.0"
