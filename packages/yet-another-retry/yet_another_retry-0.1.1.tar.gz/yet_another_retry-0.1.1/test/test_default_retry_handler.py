9f4d1f9d557473334327d65f355bfe099e01cc02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:16 +0000

    Add package yet-another-retry/0.1.1/yet_another_retry-0.1.1.tar.gz

---
+++ packages/yet-another-retry/yet_another_retry-0.1.1.tar.gz/yet_another_retry-0.1.1/test/test_default_retry_handler.py
@@ -0,0 +1,7 @@
+from yet_another_retry.retry_handlers import default_retry
+
+
+def test_default_retry_handler():
+    delay_seconds = default_retry(e=Exception, retry_config={})
+
+    assert delay_seconds == 0
