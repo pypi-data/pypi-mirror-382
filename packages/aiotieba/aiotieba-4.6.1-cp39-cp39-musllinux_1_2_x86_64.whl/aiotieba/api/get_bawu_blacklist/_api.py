dfd7fbf5ec88a7b73ad70a18a00f02ea175b76f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:13 +0000

    Add package aiotieba/4.6.1/aiotieba-4.6.1-cp39-cp39-musllinux_1_2_x86_64.whl

---
+++ packages/aiotieba/aiotieba-4.6.1-cp39-cp39-musllinux_1_2_x86_64.whl/aiotieba/api/get_bawu_blacklist/_api.py
@@ -0,0 +1,27 @@
+import bs4
+import yarl
+
+from ...const import WEB_BASE_HOST
+from ...core import HttpCore
+from ._classdef import BawuBlacklistUsers
+
+
+def parse_body(body: bytes) -> BawuBlacklistUsers:
+    soup = bs4.BeautifulSoup(body, "lxml")
+    bawu_blacklist_users = BawuBlacklistUsers.from_tbdata(soup)
+
+    return bawu_blacklist_users
+
+
+async def request(http_core: HttpCore, fname: str, pn: int) -> BawuBlacklistUsers:
+    params = [
+        ("word", fname),
+        ("pn", pn),
+    ]
+
+    request = http_core.pack_web_get_request(
+        yarl.URL.build(scheme="http", host=WEB_BASE_HOST, path="/bawu2/platform/listBlackUser"), params
+    )
+
+    body = await http_core.net_core.send_request(request, read_bufsize=16 * 1024)
+    return parse_body(body)
---
+++ packages/aiotieba/aiotieba-4.6.1-cp39-cp39-musllinux_1_2_x86_64.whl/aiotieba/api/login/_classdef.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import dataclasses as dcs
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+
+
+@dcs.dataclass
+class UserInfo_login:
+    """
+    用户信息
+
+    Attributes:
+        user_id (int): user_id
+        portrait (str): portrait
+        user_name (str): 用户名
+    """
+
+    user_id: int = 0
+    portrait: str = ""
+    user_name: str = ""
+
+    @staticmethod
+    def from_tbdata(data_map: Mapping) -> UserInfo_login:
+        user_id = int(data_map["id"])
+        portrait = data_map["portrait"]
+        user_name = data_map["name"]
+        return UserInfo_login(user_id, portrait, user_name)
+
+    def __str__(self) -> str:
+        return self.user_name or self.portrait or str(self.user_id)
+
+    def __hash__(self) -> int:
+        return self.user_id
+
+    def __bool__(self) -> bool:
+        return bool(self.user_id)
