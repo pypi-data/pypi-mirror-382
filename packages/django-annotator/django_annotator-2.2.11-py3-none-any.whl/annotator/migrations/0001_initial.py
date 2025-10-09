02aa877ea57417221c49536299f18c70a68feb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:19:28 +0000

    Add package django-annotator/2.2.11/django_annotator-2.2.11-py3-none-any.whl

---
+++ packages/django-annotator/django_annotator-2.2.11-py3-none-any.whl/annotator/migrations/0001_initial.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import uuid
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Annotation",
+            fields=[
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4, serialize=False, primary_key=True
+                    ),
+                ),
+                (
+                    "annotator_schema_version",
+                    models.CharField(default="v1.0", max_length=8),
+                ),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+                ("text", models.TextField()),
+                ("quote", models.TextField()),
+                ("uri", models.CharField(max_length=4096, blank=True)),
+                ("user", models.CharField(max_length=128, blank=True)),
+                ("consumer", models.CharField(max_length=64, blank=True)),
+            ],
+            options={"ordering": ("created",)},
+        ),
+        migrations.CreateModel(
+            name="Range",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        verbose_name="ID",
+                        serialize=False,
+                        auto_created=True,
+                        primary_key=True,
+                    ),
+                ),
+                ("start", models.CharField(max_length=128)),
+                ("end", models.CharField(max_length=128)),
+                ("startOffset", models.IntegerField()),
+                ("endOffset", models.IntegerField()),
+                (
+                    "annotation",
+                    models.ForeignKey(
+                        to="annotator.Annotation",
+                        on_delete=models.CASCADE,
+                        related_name="ranges",
+                    ),
+                ),
+            ],
+        ),
+    ]
---
+++ packages/django-annotator/django_annotator-2.2.11-py3-none-any.whl/annotator/urls.py
@@ -0,0 +1,28 @@
+from django.urls import re_path
+from rest_framework.routers import format_suffix_patterns
+
+from annotator import views
+
+annotations_list = views.AnnotationViewSet.as_view({"get": "list", "post": "create"})
+
+annotations_detail = views.AnnotationViewSet.as_view(
+    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
+)
+
+annotations_search = views.AnnotationViewSet.as_view({"get": "search"})
+
+annotations_root = views.AnnotationViewSet.as_view({"get": "root"})
+
+urlpatterns = format_suffix_patterns(
+    [
+        re_path(r"^demo/?$", views.DemoView.as_view(), name="demo"),
+        re_path(r"^$", annotations_root, name="root"),
+        re_path(r"annotations/?$", annotations_list, name="annotations-list"),
+        re_path(
+            r"annotations/(?P<pk>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/?$",
+            annotations_detail,
+            name="annotations-detail",
+        ),
+        re_path(r"search/?$", annotations_search, name="annotations-search"),
+    ]
+)
