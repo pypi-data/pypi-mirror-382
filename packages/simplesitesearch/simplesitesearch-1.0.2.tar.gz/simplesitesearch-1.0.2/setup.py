48311fa89d7a3ea10aaf10ffb47fb205a608e6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 08:06:42 +0000

    Add package simplesitesearch/1.0.2/simplesitesearch-1.0.2.tar.gz

---
+++ packages/simplesitesearch/simplesitesearch-1.0.2.tar.gz/simplesitesearch-1.0.2/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simplesitesearch",
+    version="1.0.2",
+    author="Reptile Tech",
+    author_email="flouis@reptile.tech",
+    description="Reptile Simple Site Search django app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reptiletech/simplesitesearch",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "Django>=3.2",
+        "django-cms>=3.2",
+        "requests>=2.25.0",
+    ],
+    include_package_data=True,
+    package_data={
+        "simplesitesearch": [
+            "templates/simplesitesearch/*.html",
+        ],
+    },
+    zip_safe=False,
+)
