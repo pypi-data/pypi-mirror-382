eface19cd846a9475eadc069141a1eb5794c08ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 16:09:52 +0000

    Add package reyserver/1.1.44/reyserver-1.1.44-py3-none-any.whl

---
+++ packages/reyserver/reyserver-1.1.44-py3-none-any.whl/reyserver/__init__.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-02-19
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Backend server method set.
+
+Modules
+-------
+rall : All methods.
+rbase : Base methods.
+rclient : Client methods.
+rfile : File methods.
+rserver : Server methods.
+"""
+
+
+from .rserver import Server
