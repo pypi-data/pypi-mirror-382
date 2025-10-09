c3ad545b7296beebf75bce85ddf95dcac2681546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:19:29 +0000

    Add package castor-extractor/0.25.7/castor_extractor-0.25.7.tar.gz

---
+++ packages/castor-extractor/castor_extractor-0.25.7.tar.gz/castor_extractor-0.25.7/castor_extractor/visualization/count/assets.py
@@ -0,0 +1,11 @@
+from ...types import ExternalAsset
+
+
+class CountAsset(ExternalAsset):
+    """Count assets"""
+
+    CANVASES = "canvases"
+    CANVAS_PERMISSIONS = "canvas_permissions"
+    CELLS = "cells"
+    PROJECTS = "projects"
+    USERS = "users"
