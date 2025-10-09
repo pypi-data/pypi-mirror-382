5ee9c99542f330e71bea43d637e9116a1e92e235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 07:05:38 +0000

    Add package ilt-deep-phonemizer/0.0.20/ilt_deep_phonemizer-0.0.20.tar.gz

---
+++ packages/ilt-deep-phonemizer/ilt_deep_phonemizer-0.0.20.tar.gz/ilt_deep_phonemizer-0.0.20/tests/test_decorators.py
@@ -0,0 +1,14 @@
+import unittest
+
+from deep_phonemizer.training.decorators import ignore_exception
+
+
+@ignore_exception
+def test_function() -> None:
+    raise ValueError("Exception should be ignored.")
+
+
+class TestDecorators(unittest.TestCase):
+
+    def test_ignore_exception(self):
+        test_function()
