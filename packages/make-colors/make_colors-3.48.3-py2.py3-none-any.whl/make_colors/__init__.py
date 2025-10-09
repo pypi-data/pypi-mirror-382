e9455fcd266ed4f534ff44cf8ba0936c79849f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:10 +0000

    Add package make-colors/3.48.3/make_colors-3.48.3-py2.py3-none-any.whl

---
+++ packages/make-colors/make_colors-3.48.3-py2.py3-none-any.whl/make_colors/__init__.py
@@ -0,0 +1,7 @@
+# from .make_colors import make_colors, MakeColors, make_color, make, getSort, print, color_map, MakeColor, convert, translate
+# from .colors import *
+from .make_colors import *
+from . import __version__ as version
+__version__ 	= version.version
+__email__		= "cumulus13@gmail.com"
+__author__		= "Hadi Cahyadi"
