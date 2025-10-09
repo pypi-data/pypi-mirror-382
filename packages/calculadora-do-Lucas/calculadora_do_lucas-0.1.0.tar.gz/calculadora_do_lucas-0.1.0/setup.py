4fcb67421fcde8f175989599be6018e82870360b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 04:05:55 +0000

    Add package calculadora-do-Lucas/0.1.0/calculadora_do_lucas-0.1.0.tar.gz

---
+++ packages/calculadora-do-Lucas/calculadora_do_lucas-0.1.0.tar.gz/calculadora_do_lucas-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Lendo o README.md para o PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="calculadora_do_Lucas",  # ⚡ Nome com hífen para o PyPI
+    version="0.1.0",
+    description="Biblioteca de operações matemáticas básicas em Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Lucas Mariani Gomes",
+    author_email="lucas2607.gomes@gmail.com",
+    url="https://github.com/LucasMarianiG/Lib_Calculadora.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
