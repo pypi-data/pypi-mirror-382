28b1a77dd62b7a8b629d9e42b797003d406cedd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 06:06:08 +0000

    Add package lambdalib/0.4.0/lambdalib-0.4.0-py3-none-any.whl

---
+++ packages/lambdalib/lambdalib-0.4.0-py3-none-any.whl/lambdalib/ramlib/__init__.py
@@ -0,0 +1,22 @@
+from siliconcompiler import Design
+
+from .la_asyncfifo.la_asyncfifo import Asyncfifo
+from .la_syncfifo.la_syncfifo import Syncfifo
+from .la_dpram.la_dpram import Dpram
+from .la_spram.la_spram import Spram
+
+__all__ = ['Asyncfifo',
+           'Syncfifo',
+           'Dpram',
+           'Spram']
+
+
+class RAMLib(Design):
+    def __init__(self):
+        super().__init__("la_ramlib")
+
+        with self.active_fileset("rtl"):
+            self.add_depfileset(Asyncfifo(), depfileset="rtl")
+            self.add_depfileset(Syncfifo(), depfileset="rtl")
+            self.add_depfileset(Dpram(), depfileset="rtl")
+            self.add_depfileset(Spram(), depfileset="rtl")
