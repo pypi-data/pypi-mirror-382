3e44a3f8668f9a3647bb493455b2f613ea970cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 16:08:56 +0000

    Add package modusa/0.3.98/modusa-0.3.98.tar.gz

---
+++ packages/modusa/modusa-0.3.98.tar.gz/modusa-0.3.98/tests/test_signals/time_domain_signal.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+#--------Meta Information----------
+_class_name = "TimeDomainSignal"
+_author_name = "Ankit Anand"
+_author_email = "ankit0.anand0@gmail.com"
+_created_at = "2025-07-09"
+#----------------------------------
+	
+from modusa.signals import TimeDomainSignal
