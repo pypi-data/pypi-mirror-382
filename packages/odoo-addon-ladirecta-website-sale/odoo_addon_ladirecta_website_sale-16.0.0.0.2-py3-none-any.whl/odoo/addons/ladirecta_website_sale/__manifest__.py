97d1d53624490b7da837f4f21c6cd602ea1ef5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:17:28 +0000

    Add package odoo-addon-ladirecta-website-sale/16.0.0.0.2/odoo_addon_ladirecta_website_sale-16.0.0.0.2-py3-none-any.whl

---
+++ packages/odoo-addon-ladirecta-website-sale/odoo_addon_ladirecta_website_sale-16.0.0.0.2-py3-none-any.whl/odoo/addons/ladirecta_website_sale/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "version": "16.0.0.0.2",
+    "name": "La Directa Website Sale customizations",
+    "summary": """
+    """,
+    "depends": [
+        "website_sale",
+        "ladirecta",
+        "partner_firstname",
+        "partner_contact_birthdate",
+        "partner_contact_birthplace",
+        "partner_contact_gender",
+    ],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Website/Website",
+    "website": "https://git.coopdevs.org/talaios/addons/odoo-directa#",
+    "license": "AGPL-3",
+    "data": [
+        "views/website_templates.xml",
+        "data/data.xml",
+    ],
+    "demo": [],
+    "application": False,
+    "installable": True,
+}
