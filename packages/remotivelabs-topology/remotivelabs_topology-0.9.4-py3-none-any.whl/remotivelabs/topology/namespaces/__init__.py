43a3e625f106faaf55957692b256e8498155cac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 16:09:01 +0000

    Add package remotivelabs-topology/0.9.4/remotivelabs_topology-0.9.4-py3-none-any.whl

---
+++ packages/remotivelabs-topology/remotivelabs_topology-0.9.4-py3-none-any.whl/remotivelabs/topology/namespaces/__init__.py
@@ -0,0 +1,12 @@
+__doc__ = """
+Namespace access module for RemotiveBroker.
+
+Provides an interface to a namespace configured in a RemotiveBroker.
+Supported types include:
+    - `someip`: Enables sending requests and subscribing to events.
+    - `generic`: Enables Restbus access, signal subscriptions, and more.
+    - `can`: Same as generic
+
+Namespaces can be used standalone or injected into a BehavioralModel for simulation or testing.
+See individual module documentation for protocol-specific details.
+"""
