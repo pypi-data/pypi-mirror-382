5738d8e8da179749fee5d47a2e69faad55637c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 06:05:47 +0000

    Add package vivarium/3.5.4/vivarium-3.5.4-py3-none-any.whl

---
+++ packages/vivarium/vivarium-3.5.4-py3-none-any.whl/vivarium/examples/boids/population.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+
+from vivarium import Component
+from vivarium.framework.engine import Builder
+from vivarium.framework.population import SimulantData
+
+
+class Population(Component):
+    ##############
+    # Properties #
+    ##############
+    configuration_defaults = {
+        "population": {
+            "colors": ["red", "blue"],
+        }
+    }
+    columns_created = ["color", "entrance_time"]
+
+    #####################
+    # Lifecycle methods #
+    #####################
+
+    def setup(self, builder: Builder) -> None:
+        self.colors = builder.configuration.population.colors
+
+    ########################
+    # Event-driven methods #
+    ########################
+
+    def on_initialize_simulants(self, pop_data: SimulantData) -> None:
+        new_population = pd.DataFrame(
+            {
+                "color": np.random.choice(self.colors, len(pop_data.index)),
+                "entrance_time": pop_data.creation_time,
+            },
+            index=pop_data.index,
+        )
+        self.population_view.update(new_population)
