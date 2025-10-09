4fe6c5c9f8c897cb07febb93499a91f93322b0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:19:43 +0000

    Add package anothertestproject/0.1.7/anothertestproject-0.1.7.tar.gz

---
+++ packages/anothertestproject/anothertestproject-0.1.7.tar.gz/anothertestproject-0.1.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="anothertestproject",
+    version="0.1.7",
+    author="Ваше Имя",
+    author_email="your@email.com",
+    description="Описание вашей библиотеки",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_data={
+        "anothertestproject": [
+            "*.dll",
+            "*.exe",
+            "binaries/*.exe",
+            "binaries/*.dll",
+        ]
+    },
+    include_package_data=True,
+    python_requires=">=3.7",
+)
