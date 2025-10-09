8f13aaa050742a92cb5837cd607553a69b469eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:09 +0000

    Add package modusa/0.4.1/modusa-0.4.1.tar.gz

---
+++ packages/modusa/modusa-0.4.1.tar.gz/modusa-0.4.1/tests/test_tools/test_fourier_tranform.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+#--------Meta Information----------
+_class_name = "FourierTransform"
+_author_name = "Ankit Anand"
+_author_email = "ankit0.anand0@gmail.com"
+_created_at = "2025-07-11"
+#----------------------------------
+
