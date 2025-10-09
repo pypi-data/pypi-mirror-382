9fc37a82eab6785d3aa439cb2e604ffefa8a0a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:06:32 +0000

    Add package hla-compass/1.6.5/hla_compass-1.6.5.tar.gz

---
+++ packages/hla-compass/hla_compass-1.6.5.tar.gz/hla_compass-1.6.5/hla_compass/templates/ui-template/frontend/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.tsx',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'ModuleUI',
+      type: 'umd',
+      export: 'default',
+    },
+    globalObject: "typeof self !== 'undefined' ? self : this",
+    clean: true,
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    antd: 'antd',
+    '@ant-design/icons': 'icons',
+    'plotly.js': 'Plotly',
+    'plotly.js-dist-min': 'Plotly',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+};
