63a82d7e35d82ff6c4c5c83508f0001d4bfddb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 04:06:03 +0000

    Add package pyroll-core/3.1.0/pyroll_core-3.1.0.tar.gz

---
+++ packages/pyroll-core/pyroll_core-3.1.0.tar.gz/pyroll_core-3.1.0/pyroll/core/engine/engine.py
@@ -0,0 +1,53 @@
+from typing import Union, Set, List
+import numpy as np
+
+
+from ..hooks import HookHost, Hook
+
+__all__ = ["Engine"]
+
+
+class Engine(HookHost):
+    """Represents an engine of a roll pass."""
+
+    rotational_frequency = Hook[float]()
+    """Rotational frequency (revolutions per time)."""
+
+    torque = Hook[float]()
+    """Engine torque."""
+
+    idle_torque = Hook[float]()
+    """Engine torque in idle state"""
+
+    power = Hook[float]()
+    """Power of the engine."""
+
+    gear_ratio = Hook[float]()
+    """Gear ratio of the engines gear box."""
+
+    spindle_efficiency = Hook[float]()
+    """Efficiency of the spindle that drives the rolls."""
+
+    gear_box_efficiency = Hook[float]()
+    """Efficiency of the gear box that drives the rolls."""
+
+    maximum_power = Hook[float]()
+    """Maximum power of the engine."""
+
+    maximum_rotational_frequency = Hook[float]()
+    """Maximum rotational frequency of the engine."""
+
+    base_speed = Hook[float]()
+    """Knee point were a DC engine switches from anchor range to field weakening range."""
+
+
+    def __init__(self, **kwargs):
+        """
+        :param kwargs: additional hook values as keyword arguments to set explicitly
+        """
+        self.__dict__.update(kwargs)
+
+        super().__init__()
+
+    def reevaluate_cache(self):
+        super().reevaluate_cache()
