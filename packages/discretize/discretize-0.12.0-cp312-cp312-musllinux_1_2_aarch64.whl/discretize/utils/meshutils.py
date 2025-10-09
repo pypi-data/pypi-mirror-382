c6e45f2b34d4c997a9a341d920e7a6a1a1fe92ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:30:22 +0000

    Add package discretize/0.12.0/discretize-0.12.0-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/discretize/discretize-0.12.0-cp312-cp312-musllinux_1_2_aarch64.whl/discretize/utils/meshutils.py
@@ -0,0 +1,6 @@
+from discretize.utils.mesh_utils import *  # NOQA F401,F403
+
+raise ImportError(
+    "Importing from discretize.meshutils is deprecated behavoir. Please import "
+    "from discretize.utils. This message will be removed in version 1.0.0 of discretize.",
+)
