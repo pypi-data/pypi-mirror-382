24136bd9ec27f7a3c4fe48dbbbfa86a0858de94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 06:06:16 +0000

    Add package accelon/0.0.1/accelon-0.0.1.tar.gz

---
+++ packages/accelon/accelon-0.0.1.tar.gz/accelon-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="accelon",
+    version="0.0.1",
+    author="Muhammad Usman",
+    author_email="usmank.personal@outlook.com",
+    description="A simple 3d rendering engine for python.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/m-usman-k/accelon",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
