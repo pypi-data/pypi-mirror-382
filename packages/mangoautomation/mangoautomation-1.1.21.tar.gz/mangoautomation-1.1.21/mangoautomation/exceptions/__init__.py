5d9893b274f1a47dcb68100371a9cb6fd6ac0f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:14 +0000

    Add package mangoautomation/1.1.21/mangoautomation-1.1.21.tar.gz

---
+++ packages/mangoautomation/mangoautomation-1.1.21.tar.gz/mangoautomation-1.1.21/mangoautomation/exceptions/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description:
+# @Time   : 2023-07-07 10:14
+# @Author : 毛鹏
+import sys
+
+from ._exceptions import MangoAutomationError
+
+
+
+__all__ = ['MangoAutomationError']
---
+++ packages/mangoautomation/mangoautomation-1.1.21.tar.gz/mangoautomation-1.1.21/mangoautomation/tools/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2025-04-15 11:54
+# @Author : 毛鹏
+import sys
+
+from ..tools._mate import Meta
+from ..tools._uiautodev import start_uiautodev, stop_uiautodev
+
+
+__all__ = ['Meta', 'start_uiautodev', 'stop_uiautodev']
