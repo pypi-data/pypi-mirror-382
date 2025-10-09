de9e76797ebb25b4ca0b19d4f7e1fcfa30ab1e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 05:06:52 +0000

    Add package osz2/1.0.3/osz2-1.0.3-py3-none-any.whl

---
+++ packages/osz2/osz2-1.0.3-py3-none-any.whl/osz2/types.py
@@ -0,0 +1,27 @@
+
+from enum import IntEnum
+
+class MetadataType(IntEnum):
+    Title = 0
+    Artist = 1
+    Creator = 2
+    Version = 3
+    Source = 4
+    Tags = 5
+    VideoDataOffset = 6
+    VideoDataLength = 7
+    VideoHash = 8
+    BeatmapSetID = 9
+    Genre = 10
+    Language = 11
+    TitleUnicode = 12
+    ArtistUnicode = 13
+    Difficulty = 14
+    PreviewTime = 15
+    ArtistFullName = 16
+    ArtistTwitter = 17
+    SourceUnicode = 18
+    ArtistURL = 19
+    Revision = 20
+    PackID = 21
+    Unknown = 9999
