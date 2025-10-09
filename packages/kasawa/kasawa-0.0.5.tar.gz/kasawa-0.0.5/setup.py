8b733573cc7027a5750fdc2b64205808fbcbe7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 08:06:24 +0000

    Add package kasawa/0.0.5/kasawa-0.0.5.tar.gz

---
+++ packages/kasawa/kasawa-0.0.5.tar.gz/kasawa-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("./README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="kasawa",
+    version="0.0.5",
+    description="I Make module this for using for easy to work",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/x2super/kasawa",
+    author="x2super",
+    author_email="kasawaonecap@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+        "cloudscraper",
+        "pystyle"
+    ],
+    python_requires=">=3.10",
+)
+
