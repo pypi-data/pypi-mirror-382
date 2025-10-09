af65190ef5ff175e46676d664a24f2f9aeb3963c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:01 +0000

    Add package lumivox/0.4.1/lumivox-0.4.1.tar.gz

---
+++ packages/lumivox/lumivox-0.4.1.tar.gz/lumivox-0.4.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="lumivox",
+    version="0.4.1",
+    author="Arjun",
+    description="Offline voice-controlled LED system for students and robotics labs",
+    packages=find_packages(),
+    install_requires=["vosk", "sounddevice", "pyserial"],
+    include_package_data=True,
+    python_requires=">=3.8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
