6338c4ba0be41a1ec6a88153227c8f8e7c054979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:18:00 +0000

    Add package cdebug/0.0.1a0/cdebug-0.0.1a0.tar.gz

---
+++ packages/cdebug/cdebug-0.0.1a0.tar.gz/cdebug-0.0.1a0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup,find_packages
+
+# print(find_packages())
+with open("README",encoding="utf-8") as f:
+    md = f.read()
+
+
+setup(
+    name="cdebug",
+    version="0.0.1a0",
+    author="wayne931121",
+    author_email="",
+    description="debug",
+    long_description=md,
+    long_description_content_type="text/markdown",
+    license="CC-BY-NC 4.0",
+    url="https://github.com/wayne931121",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+)
