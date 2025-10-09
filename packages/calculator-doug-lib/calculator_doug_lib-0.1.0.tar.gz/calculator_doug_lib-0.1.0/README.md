d19c1603e5f949e2102226a57d041e3106e0d6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 04:05:48 +0000

    Add package calculator-doug-lib/0.1.0/calculator_doug_lib-0.1.0.tar.gz

---
+++ packages/calculator-doug-lib/calculator_doug_lib-0.1.0.tar.gz/calculator_doug_lib-0.1.0/README.md
@@ -0,0 +1,1 @@
+Primeira lib
---
+++ packages/calculator-doug-lib/calculator_doug_lib-0.1.0.tar.gz/calculator_doug_lib-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# Lendo o README.md para o PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="calculator-doug-lib",  # ⚡ Nome com hífen para o PyPI
+    version="0.1.0",
+    description="Biblioteca de operações matemáticas básicas em Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Douglas Lima",
+    author_email="douglasbolislima@gmail.com",
+    url="https://github.com/douglasbolis/ps_calculadora_lib",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
