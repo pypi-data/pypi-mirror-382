22ebcea314d7275cb88f6d81628280f6dc1f6df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 05:06:15 +0000

    Add package maclidsensor/1.0.3/maclidsensor-1.0.3-py2.py3-none-any.whl

---
+++ packages/maclidsensor/maclidsensor-1.0.3-py2.py3-none-any.whl/maclidsensor/constants.py
@@ -0,0 +1,6 @@
+# Hardware constants (from reverse engineering)
+VENDOR_ID = 0x05AC  # Apple
+PRODUCT_ID = 0x8104  # MacBook sensor hub
+USAGE_PAGE = 0x0020  # HID Sensor page
+USAGE = 0x008A  # Orientation sensor
+FEATURE_REPORT_ID = 1  # Feature report containing angle data
