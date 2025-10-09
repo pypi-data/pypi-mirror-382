b05eb80a544d727d8369964f916f8911ffb0d446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:06:33 +0000

    Add package grimp/3.12/grimp-3.12-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/grimp/grimp-3.12-cp313-cp313-macosx_11_0_arm64.whl/grimp/main.py
@@ -0,0 +1,19 @@
+__all__ = ["build_graph"]
+
+from .adaptors.caching import Cache
+from .adaptors.filesystem import FileSystem
+from .adaptors.graph import ImportGraph
+from .adaptors.modulefinder import ModuleFinder
+from .adaptors.packagefinder import ImportLibPackageFinder
+from .adaptors.timing import SystemClockTimer
+from .application.config import settings
+from .application.usecases import build_graph
+
+settings.configure(
+    MODULE_FINDER=ModuleFinder(),
+    FILE_SYSTEM=FileSystem(),
+    IMPORT_GRAPH_CLASS=ImportGraph,
+    PACKAGE_FINDER=ImportLibPackageFinder(),
+    CACHE_CLASS=Cache,
+    TIMER=SystemClockTimer(),
+)
