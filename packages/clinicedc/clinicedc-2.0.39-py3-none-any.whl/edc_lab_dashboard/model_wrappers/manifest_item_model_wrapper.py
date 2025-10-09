6d3fb1cec0fd4aa472dd35b0903a6eaad6487dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 14:07:07 +0000

    Add package clinicedc/2.0.39/clinicedc-2.0.39-py3-none-any.whl

---
+++ packages/clinicedc/clinicedc-2.0.39-py3-none-any.whl/edc_lab_dashboard/model_wrappers/manifest_item_model_wrapper.py
@@ -0,0 +1,21 @@
+from edc_model_wrapper import ModelWrapper
+
+from edc_lab.models import Box, ManifestItem
+
+
+class ManifestItemModelWrapper(ModelWrapper):
+    model_cls = ManifestItem
+    next_url_name = "manage_manifest_listboard_url"
+    action_name = "manage"
+
+    @property
+    def manifest_identifier(self):
+        return self.object.manifest.manifest_identifier
+
+    @property
+    def box_identifier(self):
+        return self.object.identifier
+
+    @property
+    def box(self):
+        return Box.objects.get(box_identifier=self.object.identifier)
