68858a5f8c3ddf00e37626d453bf1db4933182d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:18:12 +0000

    Add package swh.indexer/4.2.2/swh_indexer-4.2.2.tar.gz

---
+++ packages/swh.indexer/swh_indexer-4.2.2.tar.gz/swh_indexer-4.2.2/swh/indexer/codemeta.py
@@ -0,0 +1,228 @@
+# Copyright (C) 2018-2022  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import collections
+import csv
+import itertools
+import json
+import os.path
+import re
+from typing import Any, Dict, List, Set, TextIO, Tuple
+
+from pyld import jsonld
+import rdflib
+
+import swh.indexer
+from swh.indexer.namespaces import ACTIVITYSTREAMS, CODEMETA, FORGEFED, SCHEMA, XSD
+
+_DATA_DIR = os.path.join(os.path.dirname(swh.indexer.__file__), "data")
+
+CROSSWALK_TABLE_PATH = os.path.join(_DATA_DIR, "codemeta", "crosswalk.csv")
+
+CODEMETA_V2_CONTEXT_PATH = os.path.join(_DATA_DIR, "codemeta", "codemeta-2.0.jsonld")
+CODEMETA_V3_CONTEXT_PATH = os.path.join(_DATA_DIR, "codemeta", "codemeta-3.0.jsonld")
+
+
+with open(CODEMETA_V2_CONTEXT_PATH) as fdv2, open(CODEMETA_V3_CONTEXT_PATH) as fdv3:
+    CODEMETA_V2_CONTEXT = json.load(fdv2)
+    CODEMETA_V3_CONTEXT = json.load(fdv3)
+
+with open(os.path.join(_DATA_DIR, "schema.org", "schemaorgcontext.jsonld")) as fd:
+    _SCHEMA_DOT_ORG_CONTEXT = json.load(fd)
+
+_EMPTY_PROCESSED_CONTEXT: Any = {"mappings": {}}
+_PROCESSED_CODEMETA_CONTEXT = jsonld.JsonLdProcessor().process_context(
+    _EMPTY_PROCESSED_CONTEXT, CODEMETA_V2_CONTEXT, None
+)
+
+CODEMETA_V2_CONTEXT_URL = "https://doi.org/10.5063/schema/codemeta-2.0"
+CODEMETA_V2_ALTERNATE_CONTEXT_URLS = {
+    "https://raw.githubusercontent.com/codemeta/codemeta/master/codemeta.jsonld",
+    "https://raw.githubusercontent.com/codemeta/codemeta/2.0/codemeta.jsonld",
+    "https://doi.org/doi:10.5063/schema/codemeta-2.0",
+    "http://purl.org/codemeta/2.0",
+}
+
+CODEMETA_V3_CONTEXT_URL = "https://w3id.org/codemeta/3.0"
+CODEMETA_V3_ALTERNATE_CONTEXT_URLS = {
+    "https://raw.githubusercontent.com/codemeta/codemeta/3.0/codemeta.jsonld"
+}
+
+PROPERTY_BLACKLIST = {
+    # CodeMeta properties that we cannot properly represent.
+    SCHEMA.softwareRequirements,
+    CODEMETA.softwareSuggestions,
+    # Duplicate of 'author'
+    SCHEMA.creator,
+}
+
+_codemeta_field_separator = re.compile(r"\s*[,/]\s*")
+
+
+def make_absolute_uri(local_name):
+    """Parses codemeta.jsonld, and returns the @id of terms it defines.
+
+    >>> make_absolute_uri("name")
+    'http://schema.org/name'
+    >>> make_absolute_uri("downloadUrl")
+    'http://schema.org/downloadUrl'
+    >>> make_absolute_uri("referencePublication")
+    'https://codemeta.github.io/terms/referencePublication'
+    """
+    uri = jsonld.JsonLdProcessor.get_context_value(
+        _PROCESSED_CODEMETA_CONTEXT, local_name, "@id"
+    )
+    assert uri.startswith(("@", CODEMETA, SCHEMA)), (local_name, uri)
+    return uri
+
+
+def read_crosstable(fd: TextIO) -> Tuple[Set[str], Dict[str, Dict[str, rdflib.URIRef]]]:
+    """
+    Given a file-like object to a `CodeMeta crosswalk table` (either the main
+    cross-table with all columns, or an auxiliary table with just the CodeMeta
+    column and one ecosystem-specific table); returns a list of all CodeMeta
+    terms, and a dictionary ``{ecosystem: {ecosystem_term: codemeta_term}}``
+
+    .. _CodeMeta crosswalk table: <https://codemeta.github.io/crosswalk/
+    """
+    reader = csv.reader(fd)
+    try:
+        header = next(reader)
+    except StopIteration:
+        raise ValueError("empty file")
+
+    data_sources = set(header) - {"Parent Type", "Property", "Type", "Description"}
+
+    codemeta_translation: Dict[str, Dict[str, rdflib.URIRef]] = {
+        data_source: {} for data_source in data_sources
+    }
+    terms = set()
+
+    for line in reader:  # For each canonical name
+        local_name = dict(zip(header, line))["Property"]
+        if not local_name:
+            continue
+        canonical_name = make_absolute_uri(local_name)
+        if rdflib.URIRef(canonical_name) in PROPERTY_BLACKLIST:
+            continue
+        terms.add(canonical_name)
+        for col, value in zip(header, line):  # For each cell in the row
+            if col in data_sources:
+                # If that's not the parentType/property/type/description
+                for local_name in _codemeta_field_separator.split(value):
+                    # For each of the data source's properties that maps
+                    # to this canonical name
+                    if local_name.strip():
+                        codemeta_translation[col][local_name.strip()] = rdflib.URIRef(
+                            canonical_name
+                        )
+
+    return (terms, codemeta_translation)
+
+
+with open(CROSSWALK_TABLE_PATH) as fd:
+    (CODEMETA_TERMS, CROSSWALK_TABLE) = read_crosstable(fd)
+
+
+def _document_loader(url, options=None):
+    """Document loader for pyld.
+
+    Reads the local codemeta.jsonld file instead of fetching it
+    from the Internet every single time."""
+    if (
+        url.lower() == CODEMETA_V2_CONTEXT_URL.lower()
+        or url.lower() in CODEMETA_V2_ALTERNATE_CONTEXT_URLS
+    ):
+        return {
+            "contextUrl": None,
+            "documentUrl": url,
+            "document": CODEMETA_V2_CONTEXT,
+        }
+    if (
+        url.lower() == CODEMETA_V3_CONTEXT_URL.lower()
+        or url.lower() in CODEMETA_V3_ALTERNATE_CONTEXT_URLS
+    ):
+        return {
+            "contextUrl": None,
+            "documentUrl": url,
+            "document": CODEMETA_V3_CONTEXT,
+        }
+    elif url == CODEMETA:
+        raise Exception(
+            "{} is CodeMeta's URI, use {} as context url".format(
+                CODEMETA, CODEMETA_V2_CONTEXT_URL
+            )
+        )
+    elif url.lower().rstrip("/") in ("http://schema.org", "https://schema.org"):
+        return {
+            "contextUrl": None,
+            "documentUrl": url,
+            "document": _SCHEMA_DOT_ORG_CONTEXT,
+        }
+    else:
+        raise Exception(f"Unknown context URL: {url}")
+
+
+def compact(doc, forgefed: bool):
+    """Same as `pyld.jsonld.compact`, but in the context of CodeMeta.
+
+    Args:
+        forgefed: Whether to add ForgeFed and ActivityStreams as compact URIs.
+          This is typically used for extrinsic metadata documents, which frequently
+          use properties from these namespaces.
+    """
+    contexts: List[Any] = [CODEMETA_V2_CONTEXT_URL]
+    if forgefed:
+        contexts.append(
+            {"as": str(ACTIVITYSTREAMS), "forge": str(FORGEFED), "xsd": str(XSD)}
+        )
+    return jsonld.compact(doc, contexts, options={"documentLoader": _document_loader})
+
+
+def expand(doc):
+    """Same as `pyld.jsonld.expand`, but in the context of CodeMeta."""
+    return jsonld.expand(doc, options={"documentLoader": _document_loader})
+
+
+def merge_documents(documents):
+    """Takes a list of metadata dicts, each generated from a different
+    metadata file, and merges them.
+
+    Removes duplicates, if any."""
+    documents = list(itertools.chain.from_iterable(map(expand, documents)))
+    merged_document = collections.defaultdict(list)
+    for document in documents:
+        for key, values in document.items():
+            if key == "@id":
+                # @id does not get expanded to a list
+                value = values
+
+                # Only one @id is allowed, move it to sameAs
+                if "@id" not in merged_document:
+                    merged_document["@id"] = value
+                elif value != merged_document["@id"]:
+                    if value not in merged_document[SCHEMA.sameAs]:
+                        merged_document[SCHEMA.sameAs].append(value)
+            else:
+                for value in values:
+                    if isinstance(value, dict) and set(value) == {"@list"}:
+                        # Value is of the form {'@list': [item1, item2]}
+                        # instead of the usual [item1, item2].
+                        # We need to merge the inner lists (and mostly
+                        # preserve order).
+                        merged_value = merged_document.setdefault(key, {"@list": []})
+                        for subvalue in value["@list"]:
+                            # merged_value must be of the form
+                            # {'@list': [item1, item2]}; as it is the same
+                            # type as value, which is an @list.
+                            if subvalue not in merged_value["@list"]:
+                                merged_value["@list"].append(subvalue)
+                    elif value not in merged_document[key]:
+                        merged_document[key].append(value)
+
+    # XXX: we should set forgefed=True when merging extrinsic_metadata documents.
+    # however, this function is only used to merge multiple files of the same
+    # directory (which is only for intrinsic-metadata), so it is not an issue for now
+    return compact(merged_document, forgefed=False)
---
+++ packages/swh.indexer/swh_indexer-4.2.2.tar.gz/swh_indexer-4.2.2/swh/indexer/tests/test_bibtex.py
@@ -0,0 +1,588 @@
+# Copyright (C) 2023  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import textwrap
+
+import pytest
+
+from swh.indexer.bibtex import cff_to_bibtex, codemeta_to_bibtex
+from swh.model.swhids import QualifiedSWHID
+
+
+def test_empty():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+        }
+    ) == textwrap.dedent(
+        """\
+            @software{REPLACEME
+            }
+            """
+    )
+
+
+def test_minimal():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_empty_author_list():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": [],
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+@pytest.mark.parametrize("key", ["version", "softwareVersion"])
+def test_version_minimal(key):
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+            key: "1.2.3",
+        }
+    ) == textwrap.dedent(
+        """\
+        @softwareversion{REPLACEME,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/",
+            version = "1.2.3"
+        }
+        """
+    )
+
+
+def test_id():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+            "identifier": "example-software",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{example-software,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+@pytest.mark.parametrize("key", ["@id", "id", "identifier"])
+def test_id_url(key):
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+            key: "http://example.org/example-software",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+@pytest.mark.parametrize("key", ["@id", "id", "identifier"])
+def test_id_doi(key):
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+            key: "https://doi.org/10.1000/182",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            doi = "https://doi.org/10.1000/182",
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_license():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+            "license": "https://spdx.org/licenses/Apache-2.0",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            license = "Apache-2.0",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_licenses():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+            "license": [
+                "https://spdx.org/licenses/Apache-2.0",
+                "https://spdx.org/licenses/GPL-3.0",
+            ],
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            license = "Apache-2.0 and GPL-3.0",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_organization_author():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {
+                "@type": "Organization",
+                "name": "Example University",
+            },
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Example University",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_affiliation():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {
+                "name": "Jane Doe",
+                "affiliation": {"@type": "Organization", "name": "Example University"},
+            },
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            organization = "Example University",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_author_id():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {
+                "name": "Jane Doe",
+                "id": "https://orcid.org/0000-0002-0000-000X",
+            },
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_author_invalid_id():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {
+                "name": "Jane Doe",
+                "id": "https://orcid.org/ 0000-0002-0000-000X",
+            },
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_invalid_date():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "TBD",
+            "license": "https://spdx.org/licenses/Apache-2.0",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            license = "Apache-2.0",
+            date = "TBD",
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_context_contains_schema_org():
+    assert codemeta_to_bibtex(
+        {
+            "@context": [
+                "https://doi.org/10.5063/schema/codemeta-2.0",
+                "http://schema.org",
+            ],
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+            "license": "https://spdx.org/licenses/Apache-2.0",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            license = "Apache-2.0",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+@pytest.mark.parametrize(
+    "author",
+    [
+        {
+            "type": "Role",
+            "schema:author": {
+                "name": "Jane Doe",
+                "id": "https://orcid.org/0000-0002-0000-000X",
+            },
+        },
+        [
+            {
+                "type": "Role",
+                "schema:author": {
+                    "name": "Jane Doe",
+                    "id": "https://orcid.org/0000-0002-0000-000X",
+                },
+            }
+        ],
+        {
+            "type": "Role",
+            "schema:author": {
+                "name": "Jane Doe",
+            },
+        },
+        [
+            {
+                "type": "Role",
+                "schema:author": {
+                    "name": "Jane Doe",
+                },
+            }
+        ],
+    ],
+)
+def test_author_role(author):
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://w3id.org/codemeta/3.0",
+            "author": author,
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
+
+
+def test_cff_empty():
+    assert cff_to_bibtex("") == textwrap.dedent(
+        """\
+        @software{REPLACEME
+        }
+        """
+    )
+
+
+def test_cff_invalid():
+    assert cff_to_bibtex("foo") == textwrap.dedent(
+        """\
+        @software{REPLACEME
+        }
+        """
+    )
+
+
+def test_cff_minimal():
+    assert (
+        cff_to_bibtex(
+            """
+cff-version: 1.2.0
+message: "If you use this software, please cite it as below."
+authors:
+  - family-names: Druskat
+    given-names: Stephan
+title: "My Research Software"
+date-released: 2021-08-11
+url: "http://example.org/"
+            """
+        )
+        == textwrap.dedent(
+            """\
+            @software{REPLACEME,
+                author = "Druskat, Stephan",
+                date = "2021-08-11",
+                year = "2021",
+                month = aug,
+                title = "My Research Software",
+                url = "http://example.org/"
+            }
+            """
+        )
+    )
+
+
+def test_cff_orchid_with_trailing_whitespace():
+    assert (
+        cff_to_bibtex(
+            """
+cff-version: 1.2.0
+message: "If you use this software, please cite it as below."
+authors:
+  - family-names: Druskat
+    given-names: Stephan
+    orcid: "https://orcid.org/0000-0001-0000-000X "
+title: "My Research Software"
+date-released: 2021-08-11
+url: "http://example.org/"
+            """
+        )
+        == textwrap.dedent(
+            """\
+            @software{REPLACEME,
+                author = "Druskat, Stephan",
+                date = "2021-08-11",
+                year = "2021",
+                month = aug,
+                title = "My Research Software",
+                url = "http://example.org/"
+            }
+            """
+        )
+    )
+
+
+def test_swhid_type_snp():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+        },
+        QualifiedSWHID.from_string(
+            "swh:1:snp:da39a3ee5e6b4b0d3255bfef95601890afd80709"
+        ),
+    ) == textwrap.dedent(
+        """\
+        @software{swh-snp-da39a3e,
+            swhid = "swh:1:snp:da39a3ee5e6b4b0d3255bfef95601890afd80709"
+        }
+        """
+    )
+
+
+def test_swhid_type_rev():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+        },
+        QualifiedSWHID.from_string(
+            "swh:1:rev:5b909292bcfe6099d726c0b5194165c72f93b767"
+        ),
+    ) == textwrap.dedent(
+        """\
+        @softwareversion{swh-rev-5b90929,
+            swhid = "swh:1:rev:5b909292bcfe6099d726c0b5194165c72f93b767"
+        }
+        """
+    )
+
+
+def test_swhid_type_cnt():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+        },
+        QualifiedSWHID.from_string(
+            "swh:1:cnt:5b909292bcfe6099d726c0b5194165c72f93b767;lines=5-10"
+        ),
+    ) == textwrap.dedent(
+        """\
+        @codefragment{swh-cnt-5b90929-L5-L10,
+            swhid = "swh:1:cnt:5b909292bcfe6099d726c0b5194165c72f93b767;lines=5-10"
+        }
+        """
+    )
+
+
+def test_codemeta_v3_context():
+    assert codemeta_to_bibtex(
+        {
+            "@context": "https://w3id.org/codemeta/3.0",
+            "author": {"name": "Jane Doe"},
+            "name": "Example Software",
+            "url": "http://example.org/",
+            "datePublished": "2023-10-10",
+        }
+    ) == textwrap.dedent(
+        """\
+        @software{REPLACEME,
+            author = "Doe, Jane",
+            date = "2023-10-10",
+            year = "2023",
+            month = oct,
+            title = "Example Software",
+            url = "http://example.org/"
+        }
+        """
+    )
