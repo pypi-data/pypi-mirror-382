5d594a7d501e50b8de738e81fd0366a6a22904ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 08:07:21 +0000

    Add package collective.externallinkfilter/2.0/collective_externallinkfilter-2.0.tar.gz

---
+++ packages/collective.externallinkfilter/collective_externallinkfilter-2.0.tar.gz/collective_externallinkfilter-2.0/CHANGES.rst
@@ -0,0 +1,27 @@
+Changelog
+=========
+
+
+2.0 (2025-10-08)
+----------------
+
+- Drop python2 and Plone 5 support. Mark as Plone 6 only
+  [erral]
+
+
+1.1 (2025-10-08)
+----------------
+
+- Set image loading to be lazy
+  [libargutxi]
+
+- Set image width and height if the value is empty
+  [libargutxi]
+
+
+
+1.0 (2023-02-23)
+----------------
+
+- Initial release.
+  [erral]
---
+++ packages/collective.externallinkfilter/collective_externallinkfilter-2.0.tar.gz/collective_externallinkfilter-2.0/setup.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""Installer for the collective.externallinkfilter package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
+
+
+setup(
+    name="collective.externallinkfilter",
+    version="2.0",
+    description=("An override for resolve_uid_and_caption filter for external links"),
+    long_description=long_description,
+    # Get more from https://pypi.org/classifiers/
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords="Python Plone CMS",
+    author="Mikel Larreategi",
+    author_email="mlarreategi@codesyntax.com",
+    url="https://github.com/collective/collective.externallinkfilter",
+    project_urls={
+        "PyPI": "https://pypi.python.org/pypi/collective.externallinkfilter",
+        "Source": ("https://github.com/collective/collective.externallinkfilter"),
+        "Tracker": "https://github.com/collective/collective.externallinkfilter/issues",
+        # 'Documentation': 'https://collective.externallinkfilter.readthedocs.io/en/latest/',
+    },
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["collective"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.9",
+    install_requires=[
+        "setuptools",
+        # -*- Extra requirements: -*-
+        "plone.outputfilters",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+            # Plone KGS does not use this version, because it would break
+            # Remove if your package shall be part of coredev.
+            # plone_coredev tests as of 2016-04-01.
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
