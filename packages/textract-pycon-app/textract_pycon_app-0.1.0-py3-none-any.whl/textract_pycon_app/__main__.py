df8f5b2692ca3bea843af0e76e034fa9e968ada8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 07:05:15 +0000

    Add package textract-pycon-app/0.1.0/textract_pycon_app-0.1.0-py3-none-any.whl

---
+++ packages/textract-pycon-app/textract_pycon_app-0.1.0-py3-none-any.whl/textract_pycon_app/__main__.py
@@ -0,0 +1,20 @@
+import sys
+
+import boto3  # type: ignore
+from textract_pycon import get_unique_words
+
+
+def main() -> None:
+    bucket_name = sys.argv[1]
+    document_name = sys.argv[2]
+
+    client = boto3.client("textract")
+    response = client.detect_document_text(
+        Document={"S3Object": {"Bucket": bucket_name, "Name": document_name}}
+    )
+
+    print(get_unique_words(response))
+
+
+if __name__ == "__main__":
+    main()
