01f2e35d05c67b8ea2b82ae048a50a7f197a3c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:17:56 +0000

    Add package ethos-u-vela/4.4.1/ethos_u_vela-4.4.1-cp312-cp312-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/ethos-u-vela/ethos_u_vela-4.4.1-cp312-cp312-manylinux_2_17_i686.manylinux2014_i686.whl/ethosu/vela/compiler_driver.py
@@ -0,0 +1,283 @@
+# SPDX-FileCopyrightText: Copyright 2020-2024 Arm Limited and/or its affiliates <open-source-office@arm.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the License); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Description:
+# Contains the main sequencing of the compiler.
+import time
+
+from . import extract_npu_subgraphs
+from . import graph_optimiser
+from . import high_level_command_stream_generator
+from . import high_level_command_to_npu_op
+from . import live_range
+from . import lut
+from . import mark_tensors
+from . import npu_performance
+from . import npu_serialisation
+from . import pass_packing
+from . import scheduler
+from . import tensor_allocation
+from .debug_database import DebugDatabase
+from .nn_graph import PassPlacement
+from .nn_graph import TensorAllocator
+from .operation import Op
+from .rewrite_graph import verify_graph_health
+from .rewrite_graph import visit_graph_post_order
+from .scheduler import OptimizationStrategy
+from .tensor import MemArea
+from .tensor import MemType
+from .tensor import Tensor
+from .utils import progress_print
+
+
+class CompilerOptions:
+    """Set of options to change compiler behaviour - verbosity, targets, turning off passes.
+
+    Note the difference between ArchitectureFeatures and CompilerOptions
+    - ArchitectureFeatures is for changing the Ethos-U and system architecture
+    - CompilerOptions is for changing the behaviour of the compiler"""
+
+    def __init__(
+        self,
+        verbose_graph=False,
+        verbose_quantization=False,
+        verbose_packing=False,
+        verbose_tensor_purpose=False,
+        verbose_tensor_format=False,
+        verbose_allocation=False,
+        verbose_high_level_command_stream=False,
+        verbose_register_command_stream=False,
+        verbose_operators=False,
+        verbose_weights=False,
+        verbose_cycle_estimate=False,
+        verbose_performance=False,
+        verbose_progress=False,
+        show_cpu_operations=False,
+        tensor_allocator=TensorAllocator.Greedy,
+        timing=False,
+        force_symmetric_int_weights=False,
+        output_dir="outputs",
+        cpu_tensor_alignment=Tensor.AllocationQuantum,
+        hillclimb_max_iterations=None,
+    ):
+
+        self.verbose_graph = verbose_graph
+        self.verbose_quantization = verbose_quantization
+        self.verbose_packing = verbose_packing
+        self.verbose_tensor_purpose = verbose_tensor_purpose
+        self.verbose_tensor_format = verbose_tensor_format
+        self.verbose_allocation = verbose_allocation
+        self.verbose_high_level_command_stream = verbose_high_level_command_stream
+        self.verbose_register_command_stream = verbose_register_command_stream
+        self.verbose_operators = verbose_operators
+        self.verbose_weights = verbose_weights
+        self.verbose_cycle_estimate = verbose_cycle_estimate
+        self.verbose_performance = verbose_performance
+        self.verbose_progress = verbose_progress
+        self.show_cpu_operations = show_cpu_operations
+        self.tensor_allocator = tensor_allocator
+        self.timing = timing
+        self.force_symmetric_int_weights = force_symmetric_int_weights
+        self.output_dir = output_dir
+        self.cpu_tensor_alignment = cpu_tensor_alignment
+        self.hillclimb_max_iterations = hillclimb_max_iterations
+
+    def __str__(self):
+        return type(self).__name__ + ": " + str(self.__dict__)
+
+    __repr__ = __str__
+
+
+def next_sram_factor(alloc_results):
+    # Bisects to find the max SRAM usage that successfully can be fitted with the tensor allocator.
+    # Returns tuple (factor, dry_test), with factor is None (stop) or 0 <= factor <= 1 (next SRAM factor to try),
+    # dry_test is True while still bisecting.
+    upper = 1.0
+    lower = 0.7
+    MAX_ITERATIONS = 8
+    if len(alloc_results) == 0:
+        # First iteration, try max SRAM, keep the result if it succeeds
+        return (upper, False)
+    elif len(alloc_results) == 1:
+        if alloc_results[0]:
+            # The allocator succeeded at first try; stop
+            return (None, False)
+        else:
+            # Start bisecting, try lowerbound SRAM
+            return (lower, True)
+    elif len(alloc_results) > MAX_ITERATIONS:
+        # Stop
+        return (None, False)
+    if not alloc_results[1]:
+        # Allocation at lower failed; search interval 0 - lower
+        upper = lower
+        lower = 0
+    best = lower
+    for success in alloc_results[2:]:
+        middle = (lower + upper) / 2
+        if success:
+            best = max(best, middle)
+            lower = middle
+        else:
+            upper = middle
+    if len(alloc_results) == MAX_ITERATIONS:
+        # Done bisecting; repeat the best match, but not as dry test
+        return (best, False)
+    # Next try; run only as dry test
+    return ((lower + upper) / 2, True)
+
+
+def _record_operator(op, arch):
+    if op.type not in (Op.Const, Op.Placeholder):
+        DebugDatabase.add_source(op)
+
+
+def _check_schedule(nng, arch, scheduler_options):
+    # check sram usage for optimisation strategy
+    sram_usage = nng.get_root_subgraph().memory_used.get(MemArea.Sram)
+    if sram_usage is not None and scheduler_options.optimization_strategy == OptimizationStrategy.Performance:
+        if sram_usage > scheduler_options.optimization_sram_limit:
+            print(
+                f"Warning: SRAM target for arena memory area exceeded."
+                f" Target = {scheduler_options.optimization_sram_limit} Bytes,"
+                f" Actual = {sram_usage} Bytes"
+            )
+
+
+def compiler_driver(nng, arch, options, scheduler_options, network_type, output_basename):
+    assert verify_graph_health(nng)
+    verbose_progress = scheduler_options.verbose_progress
+
+    # Pre-optimisation operator tracking
+    for sg in nng.subgraphs:
+        visit_graph_post_order(sg.output_tensors, arch, [], [_record_operator])
+
+    progress_print(verbose_progress, "Performing graph optimisation")
+    nng = graph_optimiser.optimise_graph(
+        nng, arch, network_type, options.verbose_graph, options.force_symmetric_int_weights
+    )
+    assert verify_graph_health(nng)
+
+    if options.verbose_quantization:
+        nng.print_graph_with_tensor_quantization()
+
+    progress_print(verbose_progress, "Defining tensor purpose")
+    nng = mark_tensors.mark_tensor_purpose(nng, arch, options.verbose_tensor_purpose)
+    assert verify_graph_health(nng)
+
+    progress_print(verbose_progress, "Performing pass packing")
+    pass_packing.pack_into_passes(nng, arch, options.verbose_packing)
+    assert verify_graph_health(nng)
+
+    progress_print(verbose_progress, "Extracting npu subgraphs")
+    extract_npu_subgraphs.extract_npu_subgraphs(nng, arch)
+
+    assert verify_graph_health(nng)
+    if options.timing:
+        start = time.time()
+
+    progress_print(verbose_progress, "Scheduling passes")
+    # Run the scheduler
+    scheduler.schedule_passes(nng, arch, options, scheduler_options)
+    _check_schedule(nng, arch, scheduler_options)
+
+    if options.timing:
+        stop = time.time()
+        print("Scheduling took %f s" % (stop - start))
+        start = time.time()
+
+    # LiveRanges for constant tensors for all Npu subgraphs
+    permanent_storage = arch.permanent_storage_mem_area
+    lr_graph_flash = live_range.LiveRangeGraph()
+
+    # Placeholders for scratch and flash tensors that are common for all Npu subgraphs
+    scratch_tens = None
+    scratch_fast_tens = None
+    flash_tens = None
+
+    # Create list of NPU subgraphs with same order as the list of all subgraphs
+    npu_subgraphs = [sg for sg in nng.subgraphs if sg.placement == PassPlacement.Npu]
+
+    progress_print(verbose_progress, "Calculating live ranges for constant NPU tensors")
+    # Calculate live ranges for all constant Npu tensors, in permanent storage
+    for sg in npu_subgraphs:
+        lr_graph_flash = live_range.create_linear_live_range_graph(
+            sg,
+            permanent_storage,
+            MemType.Permanent_NPU,
+            lr_graph=lr_graph_flash,
+        )
+
+    if npu_subgraphs:
+        progress_print(verbose_progress, "Allocating NPU constant tensors to the first NPU subgraph")
+        # Allocate all Npu constant tensors to the first Npu subgraph since it is
+        # processed first during serialization into tensors
+        first_npu_sg = npu_subgraphs[0]
+        tensor_allocation.allocate_tensors(
+            nng,
+            first_npu_sg,
+            arch,
+            permanent_storage,
+            set((MemType.Permanent_NPU,)),
+            tensor_allocator=TensorAllocator.LinearAlloc,
+            verbose_allocation=options.verbose_allocation,
+            lr_graph=lr_graph_flash,
+        )
+
+    root_sg = nng.get_root_subgraph()
+
+    progress_print(verbose_progress, "Generating command stream")
+    # Generate command streams and serialise Npu-ops into tensors
+    for sg in npu_subgraphs:
+        high_level_command_stream_generator.generate_high_level_command_stream_for_schedule(
+            nng, sg, arch, options.verbose_high_level_command_stream
+        )
+        lut.optimize_high_level_cmd_stream(sg, arch)
+        high_level_command_to_npu_op.generate_register_command_stream_for_sg(
+            nng, sg, arch, options.verbose_register_command_stream
+        )
+        scratch_tens, scratch_fast_tens, flash_tens = npu_serialisation.serialise_npu_subgraph_into_tensors(
+            sg, arch, scratch_tens, scratch_fast_tens, flash_tens
+        )
+
+    # Create list of CPU subgraphs with same order as the list of all subgraphs
+    cpu_subgraphs = [sg for sg in nng.subgraphs if sg.placement == PassPlacement.Cpu]
+    for sg in cpu_subgraphs:
+        npu_serialisation.rewrite_npu_call_ops(sg, arch)
+
+    # Set Scratch and Fast_scratch Tensor size
+    if scratch_tens is not None:
+        scratch_tens.set_all_shapes([root_sg.memory_used_per_type.get(MemType.Scratch, 0)])
+    if scratch_fast_tens is not None:
+        scratch_fast_tens.set_all_shapes([root_sg.memory_used_per_type.get(MemType.Scratch_fast, 0)])
+
+    progress_print(verbose_progress, "Allocating CPU constant tensors")
+    # Allocate all Cpu constant tensors, this is done last because the Npu-ops
+    # have to be serialized into flash and scratch tensors first
+    tensor_allocation.allocate_tensors(
+        nng,
+        root_sg,
+        arch,
+        permanent_storage,
+        set((MemType.Permanent_CPU,)),
+        tensor_allocator=TensorAllocator.LinearAlloc,
+        verbose_allocation=options.verbose_allocation,
+        cpu_tensor_alignment=options.cpu_tensor_alignment,
+    )
+    progress_print(verbose_progress, "Calculating new performance for the network")
+    npu_performance.calc_new_performance_for_network(
+        nng, arch, network_type, options.verbose_performance, output_basename
+    )
---
+++ packages/ethos-u-vela/ethos_u_vela-4.4.1-cp312-cp312-manylinux_2_17_i686.manylinux2014_i686.whl/ethosu/vela/tosa_reader.py
@@ -0,0 +1,369 @@
+# SPDX-FileCopyrightText: Copyright 2021-2024 Arm Limited and/or its affiliates <open-source-office@arm.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the License); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Description:
+# Functions used to read from a TOSA format file.
+import os.path
+import struct
+import sys
+
+import numpy as np
+
+from .nn_graph import Graph
+from .nn_graph import Subgraph
+from .operation import ExplicitScaling
+from .operation import Op
+from .operation import Operation
+from .reader_util import align_tensor_indices_to_nng
+from .reader_util import clone_and_reshape_tensor
+from .reader_util import decode_str
+from .reader_util import fixup_tensors
+from .shape4d import Shape4D
+from .tensor import QuantizationParameters
+from .tensor import shape_num_elements
+from .tensor import Tensor
+from .tflite_mapping import DataType
+from .tosa.Op import Op as TosaOp
+from .tosa.TosaGraph import TosaGraph as TG
+from .tosa_mapping import datatype_map
+from .tosa_mapping import datatype_map_numpy
+from .tosa_mapping import tosa_operator_map
+from .tosa_mapping import unsupported_tosa_operators
+
+
+class TosaSubgraph:
+    def __init__(self, graph, block):
+        self.graph = graph
+        self.name = decode_str(block.Name())
+
+        self.tensors = []
+        for idx in range(block.TensorsLength()):
+            self.tensors.append(self.parse_tensor(block.Tensors(idx)))
+
+        for idx in range(block.OperatorsLength()):
+            self.parse_operator(idx, block.Operators(idx))
+
+        # Get the subgraph inputs and outputs
+        self.inputs = self.get_sg_inputs_remove_duplicates(block)
+        self.outputs = self.get_sg_outputs_remove_duplicates(block)
+        fixup_tensors(self.inputs, self.tensors)
+
+    def get_sg_inputs_remove_duplicates(self, block):
+        inputs = []
+        for idx in range(block.InputsLength()):
+            tens_data = block.Inputs(idx)
+            self.add_not_duplicate(tens_data, inputs, "input")
+        return inputs
+
+    def get_sg_outputs_remove_duplicates(self, block):
+        outputs = []
+        for idx in range(block.OutputsLength()):
+            tens_data = block.Outputs(idx)
+            self.add_not_duplicate(tens_data, outputs, "output")
+        return outputs
+
+    def add_not_duplicate(self, tens_data, tensors, warning_str):
+        name = decode_str(tens_data)
+        tensor = self.get_tensor_by_name(name)
+        if tensor not in tensors:
+            tensors.append(tensor)
+        else:
+            print(f"Warning: Subgraph {warning_str} tensor ({tensor}) already seen. Removing the duplicate.")
+
+    def get_tensor_by_name(self, name):
+        for tens in self.tensors:
+            if tens.name == name:
+                return tens
+        return None
+
+    def parse_operator(self, op_index, op_data):
+        op_code = op_data.Op()
+        if op_code in unsupported_tosa_operators:
+            print("Unsupported Operator", op_code)
+            for opname in dir(TosaOp):
+                if op_code == getattr(TosaOp, opname):
+                    print(f"  {opname}")
+            return
+
+        op_type, attr_serializer, quant_serializer, indices = tosa_operator_map[op_code]
+        inputs = []
+        outputs = []
+        for idx in range(op_data.InputsLength()):
+            input = decode_str(op_data.Inputs(idx))
+            input_tens = self.get_tensor_by_name(input)
+            inputs.append(input_tens)
+            if input_tens is None:
+                print(f"could not find named input tensor {input}::{input_tens}")
+            assert input_tens is not None
+
+        for idx in range(op_data.OutputsLength()):
+            output = decode_str(op_data.Outputs(idx))
+            output_tens = self.get_tensor_by_name(output)
+            outputs.append(output_tens)
+            if output_tens is None:
+                print(f"could not find named output tensor {output}::{output_tens}")
+            assert output_tens is not None
+
+        name = "unknown_op_name"
+        if len(outputs):
+            name = outputs[0].name
+        inputs = align_tensor_indices_to_nng(op_type, indices, inputs)
+        op = Operation(op_type, name)
+        op.op_index = op_index
+        op.inputs = inputs
+        op.outputs = outputs
+
+        for out in op.outputs:
+            out.ops = [op]
+
+        # TODO Transpose_conv and conv3d
+        if op.type.is_depthwise_conv2d_op() or op.type.is_conv2d_op() or op.type == Op.FullyConnected:
+
+            def _remove_producing_identity_op(prod_op):
+                # find the producing op that is not an identity op and return it
+                while prod_op.type == Op.Identity:
+                    prod_op = prod_op.inputs[0].ops[0]  # get previous op
+                return prod_op
+
+            def _check_and_get_connection(prod_op, tens):
+                # check weight producing op can be connected to the weight tensor
+                assert len(prod_op.outputs) == 1
+                assert tens.shape == prod_op.outputs[0].shape
+                # only need to connect the current op connection as the tensor consuming connections haven't been
+                # initialised yet
+                return prod_op.outputs[0]
+
+            # remove identity ops directly connected to the weight input of conv like ops
+            weights_producer_op = _remove_producing_identity_op(inputs[1].ops[0])
+            inputs[1] = _check_and_get_connection(weights_producer_op, inputs[1])  # update connection
+
+            if weights_producer_op.type == Op.Transpose:
+                # remove transpose op such that the weight op will a const op
+                transpose_op = weights_producer_op
+                # remove identity ops directly connected to the input of the transpose op
+                transpose_producer_op = _remove_producing_identity_op(transpose_op.inputs[0].ops[0])
+                transpose_op.inputs[0] = _check_and_get_connection(
+                    transpose_producer_op, transpose_op.inputs[0]
+                )  # update connection
+
+                perms = transpose_op.attrs["perms"]
+                inputs[1] = clone_and_reshape_tensor(transpose_op.inputs[0], perms, False)
+
+            if weights_producer_op.type == Op.Reshape:
+                # remove reshape op such that the weight op will a const op
+                reshape_op = weights_producer_op
+                # remove identity ops directly connected to the input of the reshape op
+                reshape_producer_op = _remove_producing_identity_op(reshape_op.inputs[0].ops[0])
+                reshape_op.inputs[0] = _check_and_get_connection(
+                    reshape_producer_op, reshape_op.inputs[0]
+                )  # update connection
+
+                tens = reshape_op.inputs[0].clone("_reshape", False)
+                tens.values = np.reshape(tens.values, reshape_op.ofm.shape)
+                tens.shape = reshape_op.ofm.shape
+                tens._original_shape = tens.shape
+                tens.bandwidth_shape = tens.shape
+                tens.storage_shape = tens.shape
+
+                tmp_op = Operation(Op.Const, tens.name)
+                tmp_op.set_output_tensor(tens)
+                inputs[1] = tens
+
+            assert inputs[1].values is not None
+
+            if op.type == Op.FullyConnected:
+                inputs[1] = clone_and_reshape_tensor(inputs[1], (1, 0), False)
+            elif op.type.is_conv2d_op():
+                inputs[1] = clone_and_reshape_tensor(inputs[1], (1, 2, 3, 0), False)
+            elif op.type.is_depthwise_conv2d_op():
+                HWCM_to_HWOI = (0, 1, 3, 2)
+                inputs[1] = clone_and_reshape_tensor(inputs[1], HWCM_to_HWOI, False)
+            if op.type.needs_bias() and len(inputs) <= op_type.info.indices.biases[0]:
+                # No Bias tensor
+                inputs.append(None)
+            if inputs[-1] and inputs[-1].values is not None:
+                # Since bias tensor is used for both bias and scale,
+                # a clone with a unique equivalence_id is needed
+                inputs[-1] = clone_and_reshape_tensor(inputs[-1], (0,), True)
+
+            op.explicit_scaling = ExplicitScaling(False, [0], [1])  # no scaling
+
+        if attr_serializer is not None:
+            op.attrs = attr_serializer.deserialize(op_data)
+
+            if "pad" in op.attrs:
+                op.attrs["padding"] = op.attrs["pad"]  # attribute was renamed to padding
+                padding = op.attrs["padding"]  # [top, bottom, left, right]
+                op.attrs["explicit_padding"] = (
+                    padding[0],
+                    padding[2],
+                    padding[1],
+                    padding[3],
+                )  # [top, left, bottom, right]
+            if "stride" in op.attrs:
+                stride = op.attrs["stride"]
+                if len(stride) == 2:
+                    op.attrs["strides"] = (1, stride[0], stride[1], 1)
+                    del op.attrs["stride"]
+                else:
+                    # TODO CONV3D more to be done....
+                    print("Unsupported kernel dimensions: ", len(stride))
+                    assert False
+            if "dilation" in op.attrs:
+                dilation = op.attrs["dilation"]
+                if len(dilation) == 2:
+                    op.attrs["dilation"] = (1, dilation[0], dilation[1], 1)
+                elif len(dilation) == 3:
+                    # TODO CONV3D more to be done....
+                    op.attrs["dilation"] = (dilation[0], dilation[1], dilation[2], 1)
+            if "kernel" in op.attrs:
+                kernel = op.attrs["kernel"]
+                if len(kernel) == 2:
+                    op.attrs["ksize"] = (1, kernel[0], kernel[1], 1)
+                else:
+                    # TODO CONV3D more to be done....
+                    print("Unsupported kernel dimensions: ", len(kernel))
+                    assert False
+            if "shift" in op.attrs and op.type == Op.Mul:
+                shift = op.attrs["shift"]
+                if shift != 0:
+                    op.explicit_scaling = ExplicitScaling(False, [shift], [1])
+            if op.type.is_depthwise_conv2d_op():
+                assert op.weights.shape[-1] % op.ifm.shape[-1] == 0
+                depth_multiplier = op.weights.shape[-1] / op.ifm.shape[-1]
+                if depth_multiplier > 1:
+                    assert op.ifm.shape[-1] == 1 and op.ofm.shape[-1] == depth_multiplier, (
+                        "For depth multipliers > 1, IFM channels must be 1 and "
+                        "OFM channels must be equal to the depth multiplier"
+                    )
+                op.attrs["depth_multiplier"] = depth_multiplier
+            if op.type == Op.SplitSliceRead:
+                op.read_offsets[0] = Shape4D.from_list(list(op.attrs["start"]), 0)
+                op.read_shapes[0] = op.attrs["size"]
+
+            # TODO tensor zero points currently set here
+            # zero points part of Rescale operation, handled in tosa_graph_optimizer
+            if "input_zp" in op.attrs:
+                self.set_tensor_zp(op.ifm, op.attrs["input_zp"])
+            if "weight_zp" in op.attrs:
+                self.set_tensor_zp(op.weights, op.attrs["weight_zp"])
+            if "output_zp" in op.attrs:
+                self.set_tensor_zp(op.ofm, op.attrs["output_zp"])
+            if "a_zp" in op.attrs:
+                self.set_tensor_zp(op.ifm, op.attrs["a_zp"])
+            if "b_zp" in op.attrs:
+                self.set_tensor_zp(op.ifm2, op.attrs["b_zp"])
+
+    def parse_tensor(self, tens_data):
+        name = decode_str(tens_data.Name())
+        np_shape = tens_data.ShapeAsNumpy()
+        shape = list(np_shape) if isinstance(np_shape, np.ndarray) else []
+        tens_dtype = tens_data.Type()
+        dtype = datatype_map[tens_dtype]
+
+        tens = Tensor(shape, dtype, name)
+
+        # Initialize quantization parameters
+        tens.quantization = QuantizationParameters()
+
+        if dtype == DataType.uint8:
+            tens.quantization.quant_min = 0
+            tens.quantization.quant_max = (1 << dtype.bits) - 1
+        elif dtype in (DataType.int8, DataType.int16, DataType.int32, DataType.int48):
+            tens.quantization.quant_min = -(1 << (dtype.bits - 1))
+            tens.quantization.quant_max = (1 << (dtype.bits - 1)) - 1
+
+        tens.values = None
+
+        data_length = tens_data.DataLength()
+        if data_length != 0:
+            data_as_numpy = tens_data.DataAsNumpy()
+            if tens_dtype in datatype_map_numpy:
+                np_dtype = datatype_map_numpy[tens_dtype]
+
+                # TOSA pads the tensor data
+                shape_elements = shape_num_elements(shape)
+                values = np.array(data_as_numpy.view(np_dtype))
+                values = values[0:shape_elements]
+                tens.values = values.reshape(shape)
+            else:
+                # int48 is only expected as an accumulated data/output format, int4 not supported
+                print(f"Error: unsupported/unexpected Tensor type {dtype}, with data")
+                assert False
+
+        return tens
+
+    def set_tensor_zp(self, tens, zp):
+        if tens.quantization.zero_point is None:
+            tens.quantization.zero_point = zp
+        elif tens.quantization.zero_point != zp:
+            print("Error: Setting tensor zp not possible, tensor already has different zero point")
+            assert False
+
+
+class TosaGraph:
+    def __init__(self, filename, batch_size, feed_dict, output_node_names, initialisation_nodes):
+        self.op_times = {}
+        if batch_size is None:
+            batch_size = 1
+        self.batch_size = batch_size
+        self.name = os.path.splitext(os.path.basename(filename))[0]
+        self.initialisation_nodes = initialisation_nodes
+
+        with open(filename, "rb") as f:
+            buf = bytearray(f.read())
+
+        try:
+            parsing_step = "parsing root"
+            tosa_graph = TG.GetRootAsTosaGraph(buf, 0)
+
+            parsing_step = "parsing version"
+            self.check_version(tosa_graph)
+
+            parsing_step = "parsing single main region"
+            assert 1 == tosa_graph.RegionsLength()
+            assert b"main" == tosa_graph.Regions(0).Name()
+
+            parsing_step = "parsing blocks length"
+            self.subgraphs = []
+            for b_idx in range(tosa_graph.Regions(0).BlocksLength()):
+                parsing_step = f"parsing block {b_idx}"
+                self.subgraphs.append(TosaSubgraph(self, tosa_graph.Regions(0).Blocks(b_idx)))
+
+            self.nng = Graph(self.name, self.batch_size)
+            for tosa_sg in self.subgraphs:
+                sg = Subgraph(tosa_sg.name)
+                sg.original_inputs = tosa_sg.inputs  # Preserve the original input order
+                sg.output_tensors = tosa_sg.outputs
+                self.nng.subgraphs.append(sg)
+
+        except (struct.error, TypeError, RuntimeError) as e:
+            print(f'Error: Invalid .tosa file. Got "{e}" while {parsing_step}.')
+            sys.exit(1)
+
+    def check_version(self, tosa_graph):
+        version = tosa_graph.Version()
+        version_str = f"{version._Major()}.{version._Minor()}.{version._Patch()}"
+        if version_str not in ("0.80.0", "0.80.1"):
+            print(f"Unsupported TOSA version: {version_str}")
+            assert False
+
+
+def read_tosa(filename, batch_size, feed_dict, output_node_names, initialisation_nodes):
+    tosa_graph = TosaGraph(filename, batch_size, feed_dict, output_node_names, initialisation_nodes)
+    nng = tosa_graph.nng
+    nng.refresh_after_modification()
+    return nng
