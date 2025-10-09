108a3c1d968e1e9156e98570f70be09940ba90ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 04:05:44 +0000

    Add package super-calculadora-e-KNUCKLES/0.1.1/super_calculadora_e_knuckles-0.1.1.tar.gz

---
+++ packages/super-calculadora-e-KNUCKLES/super_calculadora_e_knuckles-0.1.1.tar.gz/super_calculadora_e_knuckles-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Lendo o README.md para o PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="super_calculadora_e_KNUCKLES",  # ⚡ Nome com hífen para o PyPI
+    version="0.1.1",
+    description="Biblioteca BASICA.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Rafael Deps ",
+    author_email="rafaeldeps15@gmail.com",
+    url="https://github.com/RafaelDeps/testes_Lib",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
