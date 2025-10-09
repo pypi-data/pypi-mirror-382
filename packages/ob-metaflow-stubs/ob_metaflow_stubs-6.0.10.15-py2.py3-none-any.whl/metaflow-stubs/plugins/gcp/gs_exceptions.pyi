7d6b5815d415e89fe517206d66c50f0c89e48197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:16 +0000

    Add package ob-metaflow-stubs/6.0.10.15/ob_metaflow_stubs-6.0.10.15-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.10.15-py2.py3-none-any.whl/metaflow-stubs/plugins/gcp/gs_exceptions.pyi
@@ -0,0 +1,18 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.10.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-09T09:15:42.232120                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ...exception import MetaflowException as MetaflowException
+
+class MetaflowGSPackageError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.10.15-py2.py3-none-any.whl/metaflow-stubs/system/__init__.pyi
@@ -0,0 +1,14 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.10.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-09T09:15:42.176936                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import system_monitor as system_monitor
+from .system_monitor import SystemMonitor as SystemMonitor
+from . import system_logger as system_logger
+from .system_logger import SystemLogger as SystemLogger
+
