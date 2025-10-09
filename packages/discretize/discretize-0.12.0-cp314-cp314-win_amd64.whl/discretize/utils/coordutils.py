7c3b4a7e2e1f53f589ac441f74924bebb312fd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:30:27 +0000

    Add package discretize/0.12.0/discretize-0.12.0-cp314-cp314-win_amd64.whl

---
+++ packages/discretize/discretize-0.12.0-cp314-cp314-win_amd64.whl/discretize/utils/coordutils.py
@@ -0,0 +1,6 @@
+from discretize.utils.coordinate_utils import *  # NOQA F401,F403
+
+raise ImportError(
+    "Importing from discretize.coordutils is deprecated behavoir. Please import "
+    "from discretize.utils. This message will be removed in version 1.0.0 of discretize.",
+)
