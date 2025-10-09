c421a3693e57d37b7d51e354a5e78672fba89229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 06:06:11 +0000

    Add package OpenTAKServer/1.5.14/opentakserver-1.5.14.tar.gz

---
+++ packages/OpenTAKServer/opentakserver-1.5.14.tar.gz/opentakserver-1.5.14/opentakserver/blueprints/marti_api/device_profile_marti_api.py
@@ -0,0 +1,160 @@
+import datetime
+import io
+import os
+import traceback
+import uuid
+import zipfile
+from urllib.parse import urlparse
+
+from xml.etree.ElementTree import Element, SubElement, tostring
+
+from flask import Blueprint, request, current_app as app, Response
+from werkzeug.wsgi import FileWrapper
+
+import opentakserver
+from opentakserver.extensions import db, logger
+from opentakserver.models.DeviceProfiles import DeviceProfiles
+from opentakserver.models.Packages import Packages
+from opentakserver.models.DataPackage import DataPackage
+
+device_profile_marti_api_blueprint = Blueprint('device_profile_marti_api_blueprint', __name__)
+
+
+def create_profile_zip(enrollment=True, syncSecago=-1):
+    # preference.pref
+    prefs = Element("preferences")
+    pref = SubElement(prefs, "preference", {"version": "1", "name": "com.atakmap.app_preferences"})
+
+    enable_update_server = SubElement(pref, "entry",
+                                      {"key": "appMgmtEnableUpdateServer", "class": "class java.lang.Boolean"})
+    enable_update_server.text = "true"
+
+    update_server_address = SubElement(pref, "entry", {"key": "atakUpdateServerUrl", "class": "class java.lang.String"})
+    update_server_address.text = f"https://{urlparse(request.url_root).hostname}:{app.config.get('OTS_MARTI_HTTPS_PORT')}/api/packages"
+
+    startup_sync = SubElement(pref, "entry", {"key": "repoStartupSync", "class": "class java.lang.Boolean"})
+    startup_sync.text = "true"
+
+    enable_profiles = SubElement(pref, "entry",
+                                 {"key": "deviceProfileEnableOnConnect", "class": "class java.lang.Boolean"})
+    enable_profiles.text = "true"
+
+    ca_location = SubElement(pref, "entry", {"key": "updateServerCaLocation", "class": "class java.lang.String"})
+    ca_location.text = "/storage/emulated/0/atak/cert/truststore-root.p12"
+
+    ca_password = SubElement(pref, "entry", {"key": "updateServerCaPassword", "class": "class java.lang.String"})
+    ca_password.text = app.config.get("OTS_CA_PASSWORD")
+
+    enable_channels_host = SubElement(pref, "entry",
+                                      {'key': f'prefs_enable_channels_host-{urlparse(request.url_root).hostname}',
+                                       'class': 'class java.lang.String'})
+    enable_channels_host.text = "true"
+
+    enable_channels = SubElement(pref, "entry", {'key': 'prefs_enable_channels', 'class': 'class java.lang.String'})
+    enable_channels.text = "true" if app.config.get("OTS_ENABLE_CHANNELS") else "false"
+
+    # MANIFEST file
+    manifest = Element("MissionPackageManifest", {"version": "2"})
+    config = SubElement(manifest, "Configuration")
+    SubElement(config, "Parameter", {"name": "uid", "value": str(uuid.uuid4())})
+    SubElement(config, "Parameter", {"name": "name", "value": "Device Profile"})
+    SubElement(config, "Parameter", {"name": "onReceiveDelete", "value": "true"})
+
+    contents = SubElement(manifest, "Contents")
+    SubElement(contents, "Content", {"ignore": "false", "zipEntry": "5c2bfcae3d98c9f4d262172df99ebac5/preference.pref"})
+    SubElement(contents, "Content",
+               {"ignore": "false", "zipEntry": "5c2bfcae3d98c9f4d262172df99ebac5/truststore-root.p12"})
+
+    zip_buffer = io.BytesIO()
+    zipf = zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False)
+
+    # Add the maps to the zip
+    if app.config.get("OTS_PROFILE_MAP_SOURCES") and enrollment:
+        maps_path = os.path.join(os.path.dirname(opentakserver.__file__), "maps")
+        for root, dirs, map_files in os.walk(maps_path):
+            for map in map_files:
+                zipf.writestr(f"maps/{map}", open(os.path.join(maps_path, map), 'r').read())
+                SubElement(contents, "Content", {"ignore": "false", "zipEntry": f"maps/{map}"})
+
+    plugins = None
+    if enrollment:
+        device_profiles = db.session.execute(
+            db.session.query(DeviceProfiles).filter_by(enrollment=True, active=True)).all()
+        plugins = db.session.execute(db.session.query(Packages).filter_by(install_on_enrollment=True)).all()
+        data_packages = db.session.execute(db.session.query(DataPackage).filter_by(install_on_enrollment=True)).all()
+    elif syncSecago > 0:
+        publish_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(seconds=syncSecago)
+
+        device_profiles = db.session.execute(db.session.query(DeviceProfiles).filter_by(connection=True, active=True)
+                                             .filter(DeviceProfiles.publish_time >= publish_time)).all()
+        data_packages = db.session.execute(db.session.query(DataPackage).filter_by(install_on_connection=True)
+                                           .filter(DataPackage.submission_time >= publish_time)).all()
+        plugins = db.session.execute(db.session.query(Packages).filter_by(install_on_connection=True)
+                                     .filter(Packages.publish_time >= publish_time)).all()
+    else:
+        device_profiles = db.session.execute(
+            db.session.query(DeviceProfiles).filter_by(connection=True, active=True)).all()
+        data_packages = db.session.execute(db.session.query(DataPackage).filter_by(install_on_connection=True)).all()
+
+    for profile in device_profiles:
+        p = SubElement(pref, "entry", {"key": profile[0].preference_key, "class": profile[0].value_class})
+        p.text = profile[0].preference_value
+
+    if plugins:
+        for plugin in plugins:
+            plugin = plugin[0]
+
+            SubElement(contents, "Content",
+                       {"ignore": "false", "zipEntry": f"5c2bfcae3d98c9f4d262172df99ebac5/{plugin.file_name}"})
+            zipf.write(os.path.join(app.config.get("OTS_DATA_FOLDER"), "packages", plugin.file_name),
+                       f"5c2bfcae3d98c9f4d262172df99ebac5/{plugin.file_name}")
+
+    if data_packages:
+        for data_package in data_packages:
+            data_package = data_package[0]
+            SubElement(contents, "Content",
+                       {"ignore": "false", "zipEntry": f"5c2bfcae3d98c9f4d262172df99ebac5/{data_package.filename}"})
+            zipf.write(os.path.join(app.config.get("UPLOAD_FOLDER"), f"{data_package.hash}.zip"),
+                       f"5c2bfcae3d98c9f4d262172df99ebac5/{data_package.filename}")
+
+    zipf.writestr("MANIFEST/manifest.xml", tostring(manifest))
+
+    zipf.writestr("5c2bfcae3d98c9f4d262172df99ebac5/preference.pref", tostring(prefs))
+
+    with open(os.path.join(app.config.get("OTS_CA_FOLDER"), 'truststore-root.p12'), 'rb') as truststore:
+        zipf.writestr("5c2bfcae3d98c9f4d262172df99ebac5/truststore-root.p12", truststore.read())
+
+    return zip_buffer
+
+
+# Authentication for /Marti endpoints handled by client cert validation
+# EUDs hit this endpoint after a successful certificate enrollment
+@device_profile_marti_api_blueprint.route('/Marti/api/tls/profile/enrollment')
+def enrollment_profile():
+    try:
+        profile_zip = create_profile_zip()
+        profile_zip.seek(0)
+        return Response(FileWrapper(profile_zip), mimetype="application/zip", direct_passthrough=True)
+    except BaseException as e:
+        logger.error(f"Failed to send enrollment package: {e}")
+        logger.debug(traceback.format_exc())
+        return '', 500
+
+
+# EUDs hit this endpoint when the app connects to the server if repoStartupSync is enabled
+@device_profile_marti_api_blueprint.route('/Marti/api/device/profile/connection')
+def connection_profile():
+    try:
+        syncSecago = -1
+        if 'syncSecago' in request.args:
+            try:
+                syncSecago = int(request.args['syncSecago'])
+            except ValueError:
+                pass
+        profile_zip = create_profile_zip(False, syncSecago)
+        profile_zip.seek(0)
+        return Response(FileWrapper(profile_zip), mimetype="application/zip", direct_passthrough=True)
+    except BaseException as e:
+        logger.error(f"Failed to send enrollment package: {e}")
+        logger.debug(traceback.format_exc())
+        return '', 500
---
+++ packages/OpenTAKServer/opentakserver-1.5.14.tar.gz/opentakserver-1.5.14/opentakserver/defaultconfig.py
@@ -0,0 +1,201 @@
+import secrets
+import string
+import random
+
+import pyotp
+from pathlib import Path
+import os
+
+import opentakserver
+
+
+class DefaultConfig:
+    SECRET_KEY = secrets.token_hex()
+    DEBUG = False
+
+    OTS_DATA_FOLDER = os.path.join(Path.home(), 'ots')
+    OTS_LISTENER_ADDRESS = "127.0.0.1"
+    OTS_LISTENER_PORT = 8081  # OTS will listen for HTTP requests on this port. Nginx will listen on ports 80, 443,
+    # 8080, 8443, and 8446 and proxy requests to OTS_LISTENER_PORT
+    OTS_MARTI_HTTP_PORT = 8080
+    OTS_MARTI_HTTPS_PORT = 8443
+    OTS_ENABLE_TCP_STREAMING_PORT = True
+    OTS_TCP_STREAMING_PORT = 8088
+    OTS_SSL_STREAMING_PORT = 8089
+    OTS_BACKUP_COUNT = 7
+    OTS_ENABLE_CHANNELS = True
+    OTS_RABBITMQ_SERVER_ADDRESS = "127.0.0.1"
+    OTS_MEDIAMTX_ENABLE = True
+    OTS_MEDIAMTX_API_ADDRESS = "http://localhost:9997"
+    OTS_MEDIAMTX_TOKEN = secrets.token_urlsafe(30 * 3 // 4)
+    OTS_SSL_VERIFICATION_MODE = 2  # Equivalent to ssl.CERT_REQUIRED. https://docs.python.org/3/library/ssl.html#ssl.SSLContext.verify_mode
+    OTS_SSL_CERT_HEADER = 'X-Ssl-Cert'
+    OTS_NODE_ID = ''.join(random.choices(string.ascii_lowercase + string.digits, k=32))
+    OTS_CA_NAME = 'OpenTAKServer-CA'
+    OTS_CA_FOLDER = os.path.join(OTS_DATA_FOLDER, 'ca')
+    OTS_CA_PASSWORD = 'atakatak'
+    OTS_CA_EXPIRATION_TIME = 3650  # In days, defaults to 10 years
+    OTS_CA_COUNTRY = 'WW'
+    OTS_CA_STATE = 'XX'
+    OTS_CA_CITY = 'YY'
+    OTS_CA_ORGANIZATION = 'ZZ'
+    OTS_CA_ORGANIZATIONAL_UNIT = 'OpenTAKServer'
+    OTS_CA_SUBJECT = '/C={}/ST={}/L={}/O={}/OU={}'.format(OTS_CA_COUNTRY, OTS_CA_STATE, OTS_CA_CITY,
+                                                          OTS_CA_ORGANIZATION, OTS_CA_ORGANIZATIONAL_UNIT)
+    # Messages queued in RabbitMQ will auto-delete after 1 day if not consumed https://www.rabbitmq.com/docs/ttl
+    # Set to '0' to disable auto-deletion
+    OTS_RABBITMQ_TTL = '86400000'
+    # How many CoT messages that cot_parser processes should prefetch. https://www.rabbitmq.com/docs/consumer-prefetch
+    OTS_RABBITMQ_PREFETCH = 2
+    OTS_COT_PARSER_PROCESSES = 1
+
+    # See https://docs.python.org/3/library/logging.handlers.html#logging.handlers.TimedRotatingFileHandler
+    OTS_LOG_ROTATE_WHEN = 'midnight'
+    OTS_LOG_ROTATE_INTERVAL = 0
+
+    # ADS-B Settings
+    OTS_AIRPLANES_LIVE_LAT = 40.744213
+    OTS_AIRPLANES_LIVE_LON = -73.986939
+    OTS_AIRPLANES_LIVE_RADIUS = 10
+
+    OTS_ENABLE_PLUGINS = True
+    OTS_PLUGIN_REPO = "https://repo.opentakserver.io/brian/prod/"
+    OTS_PLUGIN_PREFIXES = ["ots-", "ots_"]
+
+    # AIS Settings
+    OTS_AISHUB_USERNAME = None
+    OTS_AISHUB_SOUTH_LAT = None
+    OTS_AISHUB_WEST_LON = None
+    OTS_AISHUB_NORTH_LAT = None
+    OTS_AISHUB_EAST_LON = None
+    OTS_AISHUB_MMSI_LIST = ""
+    OTS_AISHUB_IMO_LIST = ""
+
+    OTS_PROFILE_MAP_SOURCES = True
+
+    OTS_ENABLE_MUMBLE_AUTHENTICATION = False
+
+    OTS_IP_WHITELIST = ["127.0.0.1"]
+
+    # Meshtastic settings
+    OTS_ENABLE_MESHTASTIC = False
+    OTS_MESHTASTIC_TOPIC = "opentakserver"
+    OTS_MESHTASTIC_PUBLISH_INTERVAL = 30
+    OTS_MESHTASTIC_DOWNLINK_CHANNELS = []
+    OTS_MESHTASTIC_NODEINFO_INTERVAL = 3
+
+    # Gmail settings
+    OTS_ENABLE_EMAIL = False
+    OTS_EMAIL_DOMAIN_WHITELIST = []
+    OTS_EMAIL_DOMAIN_BLACKLIST = []
+    OTS_EMAIL_TLD_WHITELIST = []
+    OTS_EMAIL_TLD_BLACKLIST = []
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_SSL = False
+    MAIL_USE_TLS = True
+    MAIL_DEBUG = False
+    MAIL_DEFAULT_SENDER = None
+    MAIL_MAX_EMAILS = None
+    MAIL_SUPPRESS_SEND = False
+    MAIL_ASCII_ATTACHMENTS = False
+    MAIL_USERNAME = None
+    MAIL_PASSWORD = None
+
+    OTS_DELETE_OLD_DATA_SECONDS = 0
+    OTS_DELETE_OLD_DATA_MINUTES = 0
+    OTS_DELETE_OLD_DATA_HOURS = 0
+    OTS_DELETE_OLD_DATA_DAYS = 0
+    OTS_DELETE_OLD_DATA_WEEKS = 1
+
+    # flask-sqlalchemy
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg://ots:POSTGRESQL_PASSWORD@127.0.0.1/ots"
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = False
+
+    ALLOWED_EXTENSIONS = ['zip', 'xml', 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'kml', 'kmz', 'p12']
+
+    UPLOAD_FOLDER = os.path.join(OTS_DATA_FOLDER, 'uploads')
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
+    # Flask-Security-Too
+    SECURITY_PASSWORD_SALT = str(secrets.SystemRandom().getrandbits(128))
+    REMEMBER_COOKIE_SAMESITE = "strict"
+    SESSION_COOKIE_SAMESITE = "strict"
+    SECURITY_USERNAME_ENABLE = True
+    SECURITY_USERNAME_REQUIRED = True
+    SECURITY_TRACKABLE = True
+    SECURITY_CSRF_COOKIE_NAME = "XSRF-TOKEN"
+    WTF_CSRF_TIME_LIMIT = None
+    SECURITY_CSRF_IGNORE_UNAUTH_ENDPOINTS = True
+    WTF_CSRF_CHECK_DEFAULT = False
+    SECURITY_RETURN_GENERIC_RESPONSES = True
+    SECURITY_URL_PREFIX = "/api"
+    SECURITY_CHANGEABLE = True
+    SECURITY_CHANGE_URL = "/password/change"
+    SECURITY_RESET_URL = "/password/reset"
+    SECURITY_PASSWORD_LENGTH_MIN = 8
+    SECURITY_REGISTERABLE = OTS_ENABLE_EMAIL
+    SECURITY_CONFIRMABLE = OTS_ENABLE_EMAIL
+    SECURITY_RECOVERABLE = OTS_ENABLE_EMAIL
+    SECURITY_TWO_FACTOR = True
+    SECURITY_TOTP_SECRETS = {1: pyotp.random_base32()}
+    SECURITY_TOTP_ISSUER = "OpenTAKServer"
+    SECURITY_TWO_FACTOR_ENABLED_METHODS = ["authenticator", "email"]
+    SECURITY_TWO_FACTOR_RESCUE_MAIL = MAIL_USERNAME
+    SECURITY_TWO_FACTOR_ALWAYS_VALIDATE = False
+    SECURITY_CSRF_PROTECT_MECHANISMS = ["session", "basic"]
+    SECURITY_LOGIN_WITHOUT_CONFIRMATION = True
+    SECURITY_POST_CONFIRM_VIEW = "/login"
+    SECURITY_REDIRECT_BEHAVIOR = 'spa'
+    SECURITY_RESET_VIEW = '/reset'
+    SECURITY_USERNAME_MIN_LENGTH = 1
+    SECURITY_MSG_USERNAME_DISALLOWED_CHARACTERS = ("Username can contain only letters, numbers, underscores, and periods", "error")
+
+    SCHEDULER_API_ENABLED = False
+    JOBS = [
+        {
+            "id": "get_airplanes_live_data",
+            "func": "opentakserver.blueprints.scheduled_jobs:get_airplanes_live_data",
+            "trigger": "interval",
+            "seconds": 0,
+            "minutes": 1,
+            "next_run_time": None
+        },
+        {
+            "id": "delete_video_recordings",
+            "func": "opentakserver.blueprints.scheduled_jobs:delete_video_recordings",
+            "trigger": "interval",
+            "seconds": 0,
+            "minutes": 1,
+            "next_run_time": None
+        },
+        {
+            "id": "purge_data",
+            "func": "opentakserver.blueprints.scheduled_jobs:purge_data",
+            "trigger": "cron",
+            "day": "*",
+            "hour": 0,
+            "minute": 0,
+            "next_run_time": None
+        },
+        {
+            "id": "ais",
+            "func": "opentakserver.blueprints.scheduled_jobs:get_aishub_data",
+            "trigger": "interval",
+            "seconds": 0,
+            "minutes": 1,
+            "next_run_time": None
+        },
+        {
+            "id": "delete_old_data",
+            "func": "opentakserver.blueprints.scheduled_jobs:delete_old_data",
+            "trigger": "interval",
+            "seconds": 0,
+            "minutes": 1,
+            "next_run_time": None
+        }
+    ]
