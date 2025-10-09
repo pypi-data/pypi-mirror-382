779a0276aee2f6e3310d675977ee50fc3ef24f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:17:41 +0000

    Add package scikit-plots/0.4.0.post5/scikit_plots-0.4.0.post5-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0.post5-cp311-cp311-musllinux_1_2_aarch64.whl/scikitplot/cexternals/_astropy/__init__.py
@@ -0,0 +1,25 @@
+"""
+Astropy is a package intended to contain core functionality and some
+common tools needed for performing astronomy and astrophysics research with
+Python. It also provides an index for other astronomy packages and tools for
+managing them.
+
+Documentation is available in the docstrings and
+online at https://www.astropy.org/.
+"""
+
+# cexternals/_astropy/__init__.py
+# https://docs.astropy.org/en/latest/index.html
+
+#######################################################################
+
+# Define the astropy version
+__version__ = "7.1.dev876+ge06963e7b"
+__author__ = "Astropy Developers"
+__author_email__ = "astropy.team@gmail.com"
+
+# Define the astropy git hash
+from ..._build_utils.gitversion import git_remote_version
+
+__git_hash__ = git_remote_version(url="https://github.com/scikit-plots/astropy")[0]
+del git_remote_version
