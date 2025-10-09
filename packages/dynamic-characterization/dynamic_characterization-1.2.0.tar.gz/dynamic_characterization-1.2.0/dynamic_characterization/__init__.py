6cef1851531365627a07a54cf3b8a3e4daea6578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 05:06:54 +0000

    Add package dynamic-characterization/1.2.0/dynamic_characterization-1.2.0.tar.gz

---
+++ packages/dynamic-characterization/dynamic_characterization-1.2.0.tar.gz/dynamic_characterization-1.2.0/dynamic_characterization/__init__.py
@@ -0,0 +1,20 @@
+"""
+Collection of dynamic characterization functions for life cycle inventories with temporal information.
+"""
+
+__all__ = (
+    "__version__",
+    "dynamic_characterization.characterize",
+    "dynamic_characterization.create_characterization_functions_from_method",
+    "original_temporalis_functions",
+    "ipcc_ar6",
+    # Add functions and variables you want exposed in `dynamic_characterization.` namespace here
+)
+
+__version__ = "1.2.0"
+
+from . import ipcc_ar6, original_temporalis_functions
+from .dynamic_characterization import (
+    characterize,
+    create_characterization_functions_from_method,
+)
---
+++ packages/dynamic-characterization/dynamic_characterization-1.2.0.tar.gz/dynamic_characterization-1.2.0/dynamic_characterization/ipcc_ar6/__init__.py
@@ -0,0 +1,26 @@
+"""
+Dynamic characterization functions from based on IPCC AR6.
+
+Relevant scientific publications:
+Schivley2015: Relevant scientific publication on the numerical calculation of CRF: https://doi.org/10.1021/acs.est.5b01118
+Forster2023: Numerical values from IPCC AR6 Chapter 7 (Table 7.15): https://doi.org/10.1017/9781009157896.009
+"""
+
+__all__ = (
+    "characterize_co2",
+    "characterize_co2_uptake",
+    "characterize_co",
+    "characterize_ch4",
+    "characterize_n2o",
+    "create_generic_characterization_function",
+    # Add functions and variables you want exposed in `dynamic_characterization.` namespace here
+)
+
+from .radiative_forcing import (
+    characterize_ch4,
+    characterize_co,
+    characterize_co2,
+    characterize_co2_uptake,
+    characterize_n2o,
+    create_generic_characterization_function,
+)
