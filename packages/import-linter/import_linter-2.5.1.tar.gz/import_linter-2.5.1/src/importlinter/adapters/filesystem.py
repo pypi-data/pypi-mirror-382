4f4113c9fad083e28ca5a0f6e93a30f431e3c49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:06:54 +0000

    Add package import-linter/2.5.1/import_linter-2.5.1.tar.gz

---
+++ packages/import-linter/import_linter-2.5.1.tar.gz/import_linter-2.5.1/src/importlinter/adapters/filesystem.py
@@ -0,0 +1,23 @@
+import os
+from typing import Optional
+
+from importlinter.application.ports import filesystem as ports
+
+
+class FileSystem(ports.FileSystem):
+    """
+    File system adapter that delegates to built in file system functions.
+    """
+
+    def join(self, *components: str) -> str:
+        return os.path.join(*components)
+
+    def read(self, file_name: str, encoding: Optional[str] = None) -> str:
+        with open(file_name, encoding=encoding) as file:
+            return file.read()
+
+    def exists(self, file_name: str) -> bool:
+        return os.path.isfile(file_name)
+
+    def getcwd(self) -> str:
+        return os.getcwd()
