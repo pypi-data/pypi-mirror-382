5c2d0407c3d25e38833dfb06de0409a2262179ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 07:05:08 +0000

    Add package crepes/0.9.0/crepes-0.9.0.tar.gz

---
+++ packages/crepes/crepes-0.9.0.tar.gz/crepes-0.9.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="crepes",
+    version="0.9.0",
+    author="Henrik Boström",
+    author_email="bostromh@kth.se",
+    description="Conformal classifiers, regressors, and predictive systems (crepes)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/henrikbostrom/crepes",
+    project_urls={
+        "Bug Tracker": "https://github.com//henrikbostrom/crepes/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=["numpy", "pandas", "scipy"],
+    python_requires=">=3.10",
+)
