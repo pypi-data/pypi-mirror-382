8d71487e49029635057d9d0726cf2b28c2ce77c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 16:09:01 +0000

    Add package ibridges/2.0.0/ibridges-2.0.0.tar.gz

---
+++ packages/ibridges/ibridges-2.0.0.tar.gz/ibridges-2.0.0/ibridges/cli/__main__.py
@@ -0,0 +1,36 @@
+"""Entry point for CLI interface."""
+
+import argparse
+import sys
+
+from ibridges.cli.other import CLI_BULTIN_COMMANDS
+from ibridges.cli.shell import get_all_shell_commands
+
+
+def create_parser():
+    """Create an argparse parser for the CLI.
+
+    Returns
+    -------
+        An argparse.ArgumentParser object with all the subcommands.
+
+    """
+    main_parser = argparse.ArgumentParser(prog="ibridges")
+    subparsers = main_parser.add_subparsers(dest="subcommand")
+
+    for command_class in get_all_shell_commands()+CLI_BULTIN_COMMANDS:
+        subpar = command_class.get_parser(subparsers.add_parser)
+        subpar.set_defaults(func=command_class.run_command)
+    return main_parser
+
+def main():
+    """Start main function of the CLI."""
+    parser = create_parser()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        return
+    args = parser.parse_args(sys.argv[1:])
+    args.func(args)
+
+if __name__ == "__main__":
+    main()
