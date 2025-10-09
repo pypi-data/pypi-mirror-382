56a9771530e33e344510cb997e5035117e743687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 14:07:01 +0000

    Add package BCBvcl4pyAPI/0.1.9/bcbvcl4pyapi-0.1.9.tar.gz

---
+++ packages/BCBvcl4pyAPI/bcbvcl4pyapi-0.1.9.tar.gz/bcbvcl4pyapi-0.1.9/src/BCBvcl4pyAPI/__init__.py
@@ -0,0 +1,12 @@
+from .BCBvcl4pyAPI         import TStringList, TList, DynamicArray
+from .HyperDynamicArrayAPI import HyperDynamicArray
+from .AnsiStringAPI        import AnsiString
+from .TMemoryStreamAPI.py  import TMemoryStream
+from .TIniFileAPI          import TIniFile
+
+__all__ = ["TStringList", "TList", "DynamicArray", 
+           "HyperDynamicArray", 
+           "AnsiString",
+           "TMemoryStream",
+           "TIniFile"
+          ]
