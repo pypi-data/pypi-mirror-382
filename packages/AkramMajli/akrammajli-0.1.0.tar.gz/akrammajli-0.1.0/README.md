e79a92d386e0630c10b8530c0d98b5fdc81fbf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 05:06:51 +0000

    Add package AkramMajli/0.1.0/akrammajli-0.1.0.tar.gz

---
+++ packages/AkramMajli/akrammajli-0.1.0.tar.gz/akrammajli-0.1.0/README.md
@@ -0,0 +1,11 @@
+# AkramMajli
+An educational Python package with multiple examples: greetings, math utilities and plotting functions.
+
+## Usage
+```python
+from AkramMajli import say_hello, add, plot_list
+
+print(say_hello("Majli"))
+print(add(5, 3))
+
+plot_list([1,3,2,5,7])
---
+++ packages/AkramMajli/akrammajli-0.1.0.tar.gz/akrammajli-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AkramMajli",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    url="https://example.com",  # رابط صالح لتجنب خطأ URL
+    author="Akram Majli",
+    author_email="akalm1900@gmail.com",
+    description="A Python library with examples, functions, and plots.",
+    long_description="Long description here",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
