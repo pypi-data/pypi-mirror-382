f8a7e6d58b20aa2bca32b19685456d71250f0c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 06:06:16 +0000

    Add package run-r/0.2.1/run_r-0.2.1-py3-none-any.whl

---
+++ packages/run-r/run_r-0.2.1-py3-none-any.whl/run_r/__init__.py
@@ -0,0 +1,11 @@
+"""
+run_r: Execute R scripts from Python and retrieve workspace variables.
+
+This package provides a simple interface to run R scripts and extract all variables
+from the R workspace after execution.
+"""
+
+from .run_r import run_r_script, RScriptRunner, find_rscript
+
+__version__ = "0.2.1"
+__all__ = ["run_r_script", "RScriptRunner", "find_rscript"]
