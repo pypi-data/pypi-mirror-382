9f84d54c0ffe9d297a7258f0aaa022b0319f7066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 06:05:57 +0000

    Add package sqlmesh/0.224.1.dev38/sqlmesh-0.224.1.dev38.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.224.1.dev38.tar.gz/sqlmesh-0.224.1.dev38/sqlmesh/migrations/v0077_fix_column_type_hash_calculation.py
@@ -0,0 +1,9 @@
+"""Use the model's dialect when calculating the hash for the column types."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
