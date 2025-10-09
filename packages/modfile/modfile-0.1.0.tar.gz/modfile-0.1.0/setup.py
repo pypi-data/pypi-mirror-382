63cdd56d6b7c8b1b03383f740b07024d9818bade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:29:59 +0000

    Add package modfile/0.1.0/modfile-0.1.0.tar.gz

---
+++ packages/modfile/modfile-0.1.0.tar.gz/modfile-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modfile',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'tqdm',
+        'yt-dlp'
+    ],
+    entry_points={
+        'console_scripts': [
+            'modfile = hee888.app:main'
+        ]
+    },
+    python_requires='>=3.8',
+    author='Khumi',
+    description='Fast multi-threaded video downloader',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
