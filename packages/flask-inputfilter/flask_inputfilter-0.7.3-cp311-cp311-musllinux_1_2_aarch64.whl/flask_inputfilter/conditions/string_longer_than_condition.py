1728d9388b4dd51848e6a1502d99ea8a355a19d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 04:06:13 +0000

    Add package flask-inputfilter/0.7.3/flask_inputfilter-0.7.3-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.7.3-cp311-cp311-musllinux_1_2_aarch64.whl/flask_inputfilter/conditions/string_longer_than_condition.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from flask_inputfilter.models import BaseCondition
+
+
+class StringLongerThanCondition(BaseCondition):
+    """
+    Checks if the length of the string in one field is longer than the string
+    in another field.
+
+    **Parameters:**
+
+    - **longer_field** (*str*): The field expected to have a longer string.
+    - **shorter_field** (*str*): The field expected to have a shorter string.
+
+    **Expected Behavior:**
+
+    Validates that the string in ``longer_field`` has a greater length
+    than the string in ``shorter_field``.
+
+    **Example Usage:**
+
+    .. code-block:: python
+
+        class StringLengthFilter(InputFilter):
+            description: str = field()
+            summary: str = field()
+
+            condition(
+                StringLongerThanCondition(
+                    longer_field='description',
+                    shorter_field='summary'
+                )
+            )
+    """
+
+    __slots__ = ("longer_field", "shorter_field")
+
+    def __init__(self, longer_field: str, shorter_field: str) -> None:
+        self.longer_field = longer_field
+        self.shorter_field = shorter_field
+
+    def check(self, value: dict[str, str]) -> bool:
+        return len(value.get(self.longer_field) or 0) > len(
+            value.get(self.shorter_field) or 0
+        )
