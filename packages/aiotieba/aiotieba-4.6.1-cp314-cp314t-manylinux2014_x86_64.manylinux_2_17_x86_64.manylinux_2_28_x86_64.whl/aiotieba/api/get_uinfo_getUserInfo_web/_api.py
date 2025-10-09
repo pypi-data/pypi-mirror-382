a74afc4dfa23af0f823662bdd1827079b41939bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:13 +0000

    Add package aiotieba/4.6.1/aiotieba-4.6.1-cp314-cp314t-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/aiotieba/aiotieba-4.6.1-cp314-cp314t-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/aiotieba/api/get_uinfo_getUserInfo_web/_api.py
@@ -0,0 +1,29 @@
+import yarl
+
+from ...const import WEB_BASE_HOST
+from ...core import HttpCore
+from ...exception import TiebaServerError
+from ...helper import parse_json
+from ._classdef import UserInfo_guinfo_web
+
+
+def parse_body(body: bytes) -> UserInfo_guinfo_web:
+    res_json = parse_json(body)
+    if code := res_json["errno"]:
+        raise TiebaServerError(code, res_json["errmsg"])
+
+    user_dict = res_json["chatUser"]
+    user = UserInfo_guinfo_web.from_tbdata(user_dict)
+
+    return user
+
+
+async def request(http_core: HttpCore, user_id: int) -> UserInfo_guinfo_web:
+    params = [("chatUid", user_id)]
+
+    request = http_core.pack_web_get_request(
+        yarl.URL.build(scheme="http", host=WEB_BASE_HOST, path="/im/pcmsg/query/getUserInfo"), params
+    )
+
+    body = await http_core.net_core.send_request(request, read_bufsize=2 * 1024)
+    return parse_body(body)
