96e6284c22b023f55dbb86da1fa54dc16727de6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:30:23 +0000

    Add package discretize/0.12.0/discretize-0.12.0-cp312-cp312-win_arm64.whl

---
+++ packages/discretize/discretize-0.12.0-cp312-cp312-win_arm64.whl/discretize/utils/curvutils.py
@@ -0,0 +1,6 @@
+from discretize.utils.curvilinear_utils import *  # NOQA F401,F403
+
+raise ImportError(
+    "Importing from discretize.curvutils is deprecated behavoir. Please import "
+    "from discretize.utils. This message will be removed in version 1.0.0 of discretize.",
+)
