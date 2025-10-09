9909e3bdc46a88a497cbf48f438e7c274b832d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 17:07:16 +0000

    Add package ob-metaflow-stubs/6.0.10.15/ob-metaflow-stubs-6.0.10.15.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.10.15.tar.gz/ob-metaflow-stubs-6.0.10.15/metaflow-stubs/plugins/airflow/__init__.pyi
@@ -0,0 +1,13 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.10.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-09T09:15:42.203403                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import exception as exception
+from . import airflow_utils as airflow_utils
+from . import sensors as sensors
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.10.15.tar.gz/ob-metaflow-stubs-6.0.10.15/metaflow-stubs/plugins/aws/batch/__init__.pyi
@@ -0,0 +1,13 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.10.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-09T09:15:42.233880                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import batch_client as batch_client
+from . import batch as batch
+from . import batch_decorator as batch_decorator
+
