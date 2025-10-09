85f8fd06673400123edb84a439f5b527a8fa8294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 07:05:39 +0000

    Add package appfl/2.0.0/appfl-2.0.0-py3-none-any.whl

---
+++ packages/appfl/appfl-2.0.0-py3-none-any.whl/appfl/algorithm/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module provides a base class to define the functions required to implement PPFL algorithms.
+Please use:
+    - `appfl.algorithm.aggregator` for server aggregation algorithms,
+    - `appfl.algorithm.trainer` for client local training algorithms,
+    - `appfl.algorithm.scheduler` for server scheduling algorithms
+"""
