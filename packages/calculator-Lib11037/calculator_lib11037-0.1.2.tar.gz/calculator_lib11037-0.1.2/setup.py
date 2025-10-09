4584789abf08c00f1bd2f17c870bc0fd1da1d647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 04:05:48 +0000

    Add package calculator-Lib11037/0.1.2/calculator_lib11037-0.1.2.tar.gz

---
+++ packages/calculator-Lib11037/calculator_lib11037-0.1.2.tar.gz/calculator_lib11037-0.1.2/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+# Lendo o README.md para o PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="calculator_Lib11037",  # ⚡ Nome com hífen para o PyPI
+    version="0.1.2",
+    description="Biblioteca de operações matemáticas básicas em Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Gustavo Firme Fiorot",
+    author_email="gffiorot@gmail.com",
+    url="https://github.com/gffiorot/TesteLib",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
