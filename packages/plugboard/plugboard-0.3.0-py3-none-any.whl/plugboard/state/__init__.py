3e40c1dd0cd792ed0e3d6f5481b6fa71e6c8c97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 16:10:04 +0000

    Add package plugboard/0.3.0/plugboard-0.3.0-py3-none-any.whl

---
+++ packages/plugboard/plugboard-0.3.0-py3-none-any.whl/plugboard/state/__init__.py
@@ -0,0 +1,14 @@
+"""State submodule providing functionality related to persisting process or component state."""
+
+from plugboard.state.dict_state_backend import DictStateBackend
+from plugboard.state.ray_state_backend import RayStateBackend
+from plugboard.state.sqlite_state_backend import SqliteStateBackend
+from plugboard.state.state_backend import StateBackend
+
+
+__all__ = [
+    "StateBackend",
+    "DictStateBackend",
+    "RayStateBackend",
+    "SqliteStateBackend",
+]
