27c7cdf9857c0a4cc331cf22c821527677fe3a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:18:51 +0000

    Add package utubes/0.0.61/utubes-0.0.61-py3-none-any.whl

---
+++ packages/utubes/utubes-0.0.61-py3-none-any.whl/Utubes/__init__.py
@@ -0,0 +1,20 @@
+appname = "utubes"
+version = "0.0.61"
+pythons = "~=3.10"
+
+DATA03 = 'ㅤ'
+DATA01 = None
+DATA02 = ['Natural Language :: English',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Localization',
+          'Topic :: Software Development :: User Interfaces',
+          'Topic :: Software Development :: Version Control']
+
+mention = ['Ytdlp', 'Utube', 'Youtube']
+install = ["yt-dlp", "requests", "instaloader", "beautifulsoup4"]
