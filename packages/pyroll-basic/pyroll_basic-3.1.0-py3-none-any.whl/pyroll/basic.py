f203fa682ec9396183bf811871c35e19d68634b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 05:06:51 +0000

    Add package pyroll-basic/3.1.0/pyroll_basic-3.1.0-py3-none-any.whl

---
+++ packages/pyroll-basic/pyroll_basic-3.1.0-py3-none-any.whl/pyroll/basic.py
@@ -0,0 +1,25 @@
+VERSION = "3.1.0"
+
+import pyroll.core as core
+from pyroll.core import (
+    BoxGroove, ConstrictedBoxGroove, SquareGroove, DiamondGroove, RoundGroove, FalseRoundGroove, GothicGroove,
+    UpsetBoxGroove, UpsetOvalGroove,
+    CircularOvalGroove, FlatOvalGroove, SwedishOvalGroove, ConstrictedSwedishOvalGroove, Oval3RadiiGroove,
+    Oval3RadiiFlankedGroove, SplineGroove, GenericElongationGroove, FlatGroove, Transport, RollPass, Unit, Roll,
+    Profile, Rotator, PassSequence, Hook, HookHost, HookFunction,
+    DeformationUnit, DiskElementUnit, ConstrictedUpsetBoxGroove, ConstrictedCircularOvalGroove, HexagonalGroove,
+    ThreeRollPass,
+)
+
+import pyroll.freiberg_flow_stress as freiberg_flow_stress
+from pyroll.freiberg_flow_stress import FreibergFlowStressCoefficients
+import pyroll.integral_thermal as integral_thermal
+import pyroll.lippmann_mahrenholz_force_torque as lippmann_mahrenholz_force_torque
+import pyroll.wusatowski_spreading as wusatowski_spreading
+import pyroll.lendl_equivalent_method as lendl_equivalent_method
+import pyroll.zouhar_contact as zouhar_contact
+import pyroll.gripping_analysis as gripping_analysis
+import pyroll.interface_friction as interface_friction
+import pyroll.elastic_mill_spring as elastic_mill_spring
+import pyroll.report as report
+import pyroll.export as export
