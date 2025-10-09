83d2bad19c91b5428f0037e276d6c1ee32c20f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 03:19:32 +0000

    Add package osz2/1.0.1/osz2-1.0.1-py3-none-any.whl

---
+++ packages/osz2/osz2-1.0.1-py3-none-any.whl/osz2/__init__.py
@@ -0,0 +1,13 @@
+
+__author__ = "Lekuru"
+__email__ = "contact@lekuru.xyz"
+__version__ = "1.0.1"
+__license__ = "MIT"
+
+from .package import Osz2Package
+from .types import MetadataType
+from .file import File
+
+from .simple_cryptor import SimpleCryptor
+from .xxtea_reader import XXTEAReader
+from .xxtea import XXTEA
