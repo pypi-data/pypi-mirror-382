6d967b1c4271336fa6922fc217db88d27357bce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 05:06:58 +0000

    Add package siphashc/2.6/siphashc-2.6.tar.gz

---
+++ packages/siphashc/siphashc-2.6.tar.gz/siphashc-2.6/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Setup for the siphashc module."""
+
+from setuptools import Extension, setup
+
+setup(
+    ext_modules=[
+        Extension(
+            name="siphashc",
+            sources=["siphashc.c", "siphash/siphash.c"],
+            language="c",
+        ),
+    ],
+)
