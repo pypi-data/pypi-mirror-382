07a05e8a335d458fcbd866b394f82330f266f909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-09 15:29:45 +0000

    Add package mangoautomation/1.1.2/mangoautomation-1.1.2.tar.gz

---
+++ packages/mangoautomation/mangoautomation-1.1.2.tar.gz/mangoautomation-1.1.2/mangoautomation/uidrive/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: # @Time   : 2023-07-15 11:57
+# @Author : 毛鹏
+import sys
+
+from ..uidrive._async_element import AsyncElement
+from ..uidrive._base_data import BaseData
+from ..uidrive._driver_object import DriverObject
+from ..uidrive._sync_element import SyncElement
+
+
+__all__ = [
+    'AsyncElement',
+    'BaseData',
+    'DriverObject',
+    'SyncElement',
+]
