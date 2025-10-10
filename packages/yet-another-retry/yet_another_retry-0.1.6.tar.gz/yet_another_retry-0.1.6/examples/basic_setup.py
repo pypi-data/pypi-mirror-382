55d4646998485f19e3bc247bc1f719802551f678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:45 +0000

    Add package yet-another-retry/0.1.6/yet_another_retry-0.1.6.tar.gz

---
+++ packages/yet-another-retry/yet_another_retry-0.1.6.tar.gz/yet_another_retry-0.1.6/examples/basic_setup.py
@@ -0,0 +1,9 @@
+from yet_another_retry import retry
+
+
+@retry()
+def my_function():
+    """This function will use the default retry and exception handlers and fail the default nr of times"""
+
+    print("Raising an error")
+    raise Exception("Raising an error")
