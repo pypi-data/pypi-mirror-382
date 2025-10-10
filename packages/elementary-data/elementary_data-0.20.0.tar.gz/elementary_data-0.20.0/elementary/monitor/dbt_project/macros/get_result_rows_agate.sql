6eddb18615cbc9f1b8bbbc9804485062e70e5f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:45 +0000

    Add package elementary-data/0.20.0/elementary_data-0.20.0.tar.gz

---
+++ packages/elementary-data/elementary_data-0.20.0.tar.gz/elementary_data-0.20.0/elementary/monitor/dbt_project/macros/get_result_rows_agate.sql
@@ -0,0 +1,17 @@
+{% macro get_result_rows_agate(days_back, valid_ids_query = none) %}
+  {% set query %}
+  select
+    elementary_test_results_id,
+    result_row
+  from {{ ref("elementary", "test_result_rows") }}
+  where {{ elementary.edr_datediff(elementary.edr_cast_as_timestamp('detected_at'), elementary.edr_current_timestamp(), 'day') }} < {{ days_back }}
+  {% if valid_ids_query %}
+    and elementary_test_results_id in ({{ valid_ids_query }})
+  {% endif %}
+  {% endset %}
+  {% set res = elementary.run_query(query) %}
+  {% if not res %}
+    {% do return({}) %}
+  {% endif %}
+  {% do return(res.group_by("elementary_test_results_id")) %}
+{% endmacro %}
