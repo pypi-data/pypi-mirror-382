390cbff9bebd9499bead8ac4c4739110683b5ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:05:51 +0000

    Add package volvocarsapi/0.4.3/volvocarsapi-0.4.3.tar.gz

---
+++ packages/volvocarsapi/volvocarsapi-0.4.3.tar.gz/volvocarsapi-0.4.3/README.md
@@ -0,0 +1,85 @@
+# Volvo Cars API
+
+This python library lets you use the [Volvo Cars API](https://developer.volvocars.com/apis/). Authorisation follows the OAuth 2.0 authorisation code flow with PKCE.
+
+
+[![GitHub release][releases-shield]][releases]
+[![Sponsor][sponsor-shield]](#sponsor)
+
+## Requirements
+
+You'll need a Volvo Developers account and create an [API application](https://developer.volvocars.com/account/). Make sure to fill in all required fields and write down the retrieved client id and secret.
+
+## Usage
+
+A script is worth a thousand words.
+
+```py
+# Retrieved at the end of the API application registration process
+client_id = "abc"
+client_secret = "very-secret-key"
+
+# Scopes that have been requested during the API application registration. You can also
+# use less scopes than requested.
+scopes = [
+    "openid",
+    "conve:battery_charge_level",
+    "conve:brake_status",
+    "conve:climatization_start_stop",
+    "conve:command_accessibility",
+    "conve:commands",
+    "conve:diagnostics_engine_status",
+    # ...
+]
+
+# Must be excactly as entered during the API application registration
+redirect_uri = "htttps://my-volvo-application.io/callback" 
+
+# The VCC API key retrieved after the API application registration process
+api_key = "abcdef-1234567890-abcdef"
+
+client_session = _get_client_session()
+
+# Authenticate
+auth = VolvoCarsAuth(
+    client_session,
+    client_id=client_id,
+    client_secret=client_secret,
+    scopes=scopes,
+    redirect_uri=redirect_uri)
+auth_uri = await auth.get_auth_uri() # optionally pass state
+
+# Redirect user to auth_uri and wait for the callback
+# ...
+
+# Extract code from the callback url
+code = _get_code_from_url()
+await auth.async_request_token(code)
+
+# The VIN must be linked to the account you sign in with
+vin = "YV123456789012345"
+
+# Create API client
+api = VolvoCarsApi(client_session, auth, api_key, vin)
+
+# Make API request
+engine_warnings = await api.async_get_engine_warnings()
+```
+
+<a name="sponsor"></a>
+
+## 🚗 Powered by dreams
+
+If you'd like to show your appreciation for this project, feel free to toss a coin to your dev. Donations help keep things running — and who knows, maybe one day they'll add up to a Volvo EX90 (hey, let me dream!). If you're feeling generous, you may donate one too — I'll even come pick it up! 😁
+
+[![ko-fi sponsor][kofi-sponsor-shield]][kofi-sponsor]
+[![github sponsor][github-sponsor-shield]][github-sponsor]
+
+
+[releases-shield]: https://img.shields.io/github/v/release/thomasddn/volvo-cars-api?style=flat-square
+[releases]: https://github.com/thomasddn/volvo-cars-api/releases
+[sponsor-shield]: https://img.shields.io/static/v1?label=sponsor&message=%E2%9D%A4&color=%23fe8e86&style=flat-square
+[kofi-sponsor-shield]: https://img.shields.io/badge/Support_me_on_Ko--fi-%E2%9D%A4-fe8e86?style=for-the-badge&logo=kofi&logoColor=ffffff
+[kofi-sponsor]: https://ko-fi.com/N4N7UZ6KN
+[github-sponsor-shield]: https://img.shields.io/badge/Support_me_on_GitHub-%E2%9D%A4-fe8e86?style=for-the-badge&logo=github&color=fe8e86
+[github-sponsor]: https://github.com/sponsors/thomasddn
---
+++ packages/volvocarsapi/volvocarsapi-0.4.3.tar.gz/volvocarsapi-0.4.3/tests/test_api.py
@@ -0,0 +1,148 @@
+"""API tests."""
+
+from datetime import UTC, datetime
+import logging
+from unittest.mock import AsyncMock
+
+from aiohttp import ClientResponseError, ClientSession, RequestInfo
+import pytest
+
+from volvocarsapi.api import VolvoCarsApi
+from volvocarsapi.auth import VolvoCarsAuth
+from volvocarsapi.models import VolvoApiException, VolvoAuthException
+
+from . import configure_mock, load_json_fixture
+
+
+async def test_get_odometer_successful(
+    mock_client_session: ClientSession, mock_token_manager: VolvoCarsAuth
+) -> None:
+    """Test successful odometer request."""
+    odometer_json = load_json_fixture("odometer")
+    _mock_response(mock_client_session, odometer_json)
+
+    api = VolvoCarsApi(mock_client_session, mock_token_manager, "secretapikey")
+
+    result = await api.async_get_odometer()
+    assert result["odometer"].value == 30000
+    assert result["odometer"].unit == "km"
+    assert result["odometer"].timestamp == datetime(
+        2024, 12, 30, 14, 18, 56, 0, tzinfo=UTC
+    )
+
+
+async def test_get_odometer_token_failure(
+    mock_client_session: ClientSession, mock_token_manager: VolvoCarsAuth
+) -> None:
+    """Test token failure for odometer request."""
+    configure_mock(
+        mock_token_manager.async_get_access_token,
+        side_effect=ClientResponseError(None, None, status=400),
+    )
+
+    api = VolvoCarsApi(mock_client_session, mock_token_manager, "secretapikey")
+
+    with pytest.raises(VolvoAuthException):
+        await api.async_get_odometer()
+
+
+async def test_get_odometer_failure(
+    mock_client_session: ClientSession, mock_token_manager: VolvoCarsAuth
+) -> None:
+    """Test general failure for odometer request."""
+    _mock_response_failure(mock_client_session, 500)
+
+    api = VolvoCarsApi(mock_client_session, mock_token_manager, "secretapikey")
+
+    with pytest.raises(VolvoApiException):
+        await api.async_get_odometer()
+
+
+async def test_get_odometer_auth_failure(
+    mock_client_session: ClientSession, mock_token_manager: VolvoCarsAuth
+) -> None:
+    """Test auth failure for odometer request."""
+    _mock_response_failure(mock_client_session, 401)
+
+    api = VolvoCarsApi(mock_client_session, mock_token_manager, "secretapikey")
+
+    with pytest.raises(VolvoAuthException):
+        await api.async_get_odometer()
+
+
+async def test_access_token_successful(
+    mock_client_session: ClientSession, mock_token_manager: VolvoCarsAuth
+) -> None:
+    """Test successful access token request."""
+    api = VolvoCarsApi(mock_client_session, mock_token_manager, "secretapikey")
+
+    access_token = await api.async_get_access_token()
+    assert access_token == "valid_token_123"
+
+
+@pytest.mark.parametrize(
+    "status",
+    [400, 401, 403],
+)
+async def test_access_token_raises_auth_exception(
+    mock_client_session: ClientSession, mock_token_manager: VolvoCarsAuth, status: int
+) -> None:
+    """Test if getting access token raises a `VolvoAuthException`."""
+    configure_mock(
+        mock_token_manager.async_get_access_token,
+        side_effect=ClientResponseError(None, None, status=status),
+    )
+
+    api = VolvoCarsApi(mock_client_session, mock_token_manager, "secretapikey")
+
+    with pytest.raises(VolvoAuthException):
+        await api.async_get_access_token()
+
+
+@pytest.mark.parametrize(
+    "status",
+    [404, 500],
+)
+async def test_access_token_raises_api_exception(
+    mock_client_session: ClientSession, mock_token_manager: VolvoCarsAuth, status: int
+) -> None:
+    """Test if getting access token raises a `VolvoApiException`."""
+    configure_mock(
+        mock_token_manager.async_get_access_token,
+        side_effect=ClientResponseError(None, None, status=status),
+    )
+
+    api = VolvoCarsApi(mock_client_session, mock_token_manager, "secretapikey")
+
+    with pytest.raises(VolvoApiException):
+        await api.async_get_access_token()
+
+
+async def test_redacted_url(
+    mock_client_session: ClientSession, mock_token_manager: VolvoCarsAuth, caplog: pytest.LogCaptureFixture
+) -> None:
+    """Test if request url is redacted in logs."""
+    odometer_json = load_json_fixture("odometer")
+    _mock_response(mock_client_session, odometer_json)
+
+    vin = "YV1ABCDEFG1234567"
+    api = VolvoCarsApi(mock_client_session, mock_token_manager, "secretapikey", vin)
+
+    with caplog.at_level(logging.DEBUG):
+        await api.async_get_odometer()
+        assert "Request [odometer]:" in caplog.text
+        assert vin not in caplog.text
+
+
+def _mock_response(mock_client_session: ClientSession, json: dict) -> None:
+    mock_response = AsyncMock()
+    mock_response.__aenter__.return_value.json = AsyncMock(return_value=json)
+    mock_client_session.request.return_value = mock_response
+
+
+def _mock_response_failure(mock_client_session: ClientSession, status: int) -> None:
+    mock_client_session.request.side_effect = ClientResponseError(
+        RequestInfo(url="http://127.0.0.1", method="GET", headers={}),
+        None,
+        status=status,
+    )
