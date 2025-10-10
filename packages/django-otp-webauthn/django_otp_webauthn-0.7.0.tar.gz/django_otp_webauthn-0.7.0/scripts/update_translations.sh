fcd1d1f00388bb03ff2a2e64ed1aa0564fc9ca86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:27 +0000

    Add package django-otp-webauthn/0.7.0/django_otp_webauthn-0.7.0.tar.gz

---
+++ packages/django-otp-webauthn/django_otp_webauthn-0.7.0.tar.gz/django_otp_webauthn-0.7.0/scripts/update_translations.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# This script is used to update and manage translations.
+# Usage:
+#
+# Update all languages:
+# ./update_translations.sh --all
+#
+# Create or update existing language:
+# ./update_translations.sh -l <language_code>
+
+set -e
+
+IGNORE_PATHS="--ignore venv --ignore client --ignore .tox --ignore dist --ignore node_modules --ignore docs"
+
+echo "Updating djangojs.po files..."
+# Update JavaScript translations
+# Note: static must be compiled before running this command: `cd client && yarn build`
+# Note: source locations are omitted because they refer to the compiled files, not the source files
+python manage.py makemessages $IGNORE_PATHS -d djangojs --no-location --no-obsolete $@
+
+echo "\nUpdating django.po files..."
+# Update Django translations
+python manage.py makemessages $IGNORE_PATHS --no-obsolete $@
