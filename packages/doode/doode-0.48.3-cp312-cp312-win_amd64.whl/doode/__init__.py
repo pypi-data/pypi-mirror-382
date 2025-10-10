da817545766bdb390af43ccffc1c71a4c3f5d195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:05:59 +0000

    Add package doode/0.48.3/doode-0.48.3-cp312-cp312-win_amd64.whl

---
+++ packages/doode/doode-0.48.3-cp312-cp312-win_amd64.whl/doode/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# file: dood/__init__.py
+# Author: Hadi Cahyadi <cumulus13@gmail.com>
+# Date: 2025-10-09 12:06:19.500811
+# Description: 
+# License: MIT
+
+from .doode import Dood
