535ca40ebcea51cf2a9721b94dbe56e42ffea290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 07:06:05 +0000

    Add package pmagpy/4.3.8/pmagpy-4.3.8.tar.gz

---
+++ packages/pmagpy/pmagpy-4.3.8.tar.gz/pmagpy-4.3.8/pmagpy/mapping/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# __init__.py: -*- Python -*-  DESCRIPTIVE TEXT.
+# 
+#  Copyright (c) 2014 Lori Jonestrask
+#  Author: Lori Jonestrask (mintblue87@gmail.com) .
+#!/usr/bin/env python
+# __init__.py: -*- Python -*-  DESCRIPTIVE TEXT.
+# 
+#  Copyright (c) 2014 Lori Jonestrask
+#  Author: Lori Jonestrask (mintblue87@gmail.com) .
+from . import map_magic
+
+__all__ = [map_magic]
