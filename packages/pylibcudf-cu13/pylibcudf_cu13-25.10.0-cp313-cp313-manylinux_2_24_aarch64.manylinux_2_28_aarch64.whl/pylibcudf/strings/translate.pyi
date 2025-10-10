94b58c4b3deb92ba3dd1e063727528de94934bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:06:23 +0000

    Add package pylibcudf-cu13/25.10.0/pylibcudf_cu13-25.10.0-cp313-cp313-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/pylibcudf-cu13/pylibcudf_cu13-25.10.0-cp313-cp313-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/pylibcudf/strings/translate.pyi
@@ -0,0 +1,25 @@
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
+from collections.abc import Mapping
+from enum import IntEnum
+
+from rmm.pylibrmm.stream import Stream
+
+from pylibcudf.column import Column
+from pylibcudf.scalar import Scalar
+
+class FilterType(IntEnum):
+    KEEP = ...
+    REMOVE = ...
+
+def translate(
+    input: Column,
+    chars_table: Mapping[int | str, int | str],
+    stream: Stream | None = None,
+) -> Column: ...
+def filter_characters(
+    input: Column,
+    characters_to_filter: Mapping[int | str, int | str],
+    keep_characters: FilterType,
+    replacement: Scalar,
+    stream: Stream | None = None,
+) -> Column: ...
