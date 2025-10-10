cd1edd9177a44a2bd3f833759a5407ccbe8f1044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:06:00 +0000

    Add package v440/2.0.0.dev197/v440-2.0.0.dev197.tar.gz

---
+++ packages/v440/v440-2.0.0.dev197.tar.gz/v440-2.0.0.dev197/src/v440/_utils/Cfg.py
@@ -0,0 +1,18 @@
+import enum
+import functools
+import tomllib
+from importlib import resources
+from typing import *
+
+__all__ = ["Cfg"]
+
+
+class Cfg(enum.Enum):
+    cfg = None
+
+    @functools.cached_property
+    def data(self: Self) -> dict:
+        "This cached property holds the cfg data."
+        text: str = resources.read_text("v440._utils", "cfg.toml")
+        ans: dict = tomllib.loads(text)
+        return ans
