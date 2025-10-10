56fffd8cac7aa8e970b31ee7e89206c922c57dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:36 +0000

    Add package dicom-wsi/1.0.5/dicom_wsi-1.0.5-py3-none-any.whl

---
+++ packages/dicom-wsi/dicom_wsi-1.0.5-py3-none-any.whl/dicom_wsi/parse_wsi.py
@@ -0,0 +1,33 @@
+import logging
+
+import pyvips
+
+from . import mapping as mp
+
+logger = logging.getLogger(__name__)
+
+
+def get_wsi(cfg):
+    """
+    Update the YAML defaults with image-specific attributes
+
+    :param cfg:
+    :return: cfg, wsi_object
+    """
+    wsi_fn = cfg.get('General').get('WSIFile')
+    pyvips.logger.setLevel(30)
+    wsi = pyvips.Image.new_from_file(wsi_fn, access='sequential')
+
+    if wsi_fn.endswith('svs'):
+        logger.debug('Assuming SVS file from file extension')
+        try:
+            cfg = mp.map_aperio_features(cfg, wsi)
+        except:
+            logger.warning('Aperio metadata not found. Trying something else')
+            cfg = mp.map_other_features(cfg, wsi)
+    else:
+        logger.debug('Assuming non SVS file from file extension')
+        cfg = mp.map_other_features(cfg, wsi)
+
+    # noinspection PyUnboundLocalVariable
+    return cfg, wsi
