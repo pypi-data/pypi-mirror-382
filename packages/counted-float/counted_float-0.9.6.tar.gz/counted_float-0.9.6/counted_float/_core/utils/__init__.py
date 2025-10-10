43c32ffba4fa88529d015d7ea19f80d69d51d841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:59 +0000

    Add package counted-float/0.9.6/counted_float-0.9.6.tar.gz

---
+++ packages/counted-float/counted_float-0.9.6.tar.gz/counted_float-0.9.6/counted_float/_core/utils/__init__.py
@@ -0,0 +1,6 @@
+from ._cpu_freq import get_cpu_frequency_mhz_current, get_cpu_frequency_mhz_max, get_cpu_frequency_mhz_min
+from ._formatting import format_latency, format_time_duration
+from ._geo_mean import geo_mean
+from ._latency import convert_nsecs_to_cycles
+from ._missing_data import impute_missing_data
+from ._timer import Timer
---
+++ packages/counted-float/counted_float-0.9.6.tar.gz/counted_float-0.9.6/counted_float/benchmarking/__init__.py
@@ -0,0 +1,6 @@
+from counted_float._core.benchmarking import FlopsBenchmarkResults, run_flops_benchmark
+
+__all__ = [
+    "FlopsBenchmarkResults",
+    "run_flops_benchmark",
+]
