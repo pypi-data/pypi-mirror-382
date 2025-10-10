9c3a5cadf95f6d6d5eb3ea5a15593b1e36f36683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 03:18:37 +0000

    Add package powerplatform-management/2.0.3203.1269/powerplatform_management-2.0.3203.1269-py3-none-any.whl

---
+++ packages/powerplatform-management/powerplatform_management-2.0.3203.1269-py3-none-any.whl/mspp_management/models/role_assignment_response_value.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+from collections.abc import Callable
+from dataclasses import dataclass, field
+from kiota_abstractions.serialization import AdditionalDataHolder, Parsable, ParseNode, SerializationWriter
+from typing import Any, Optional, TYPE_CHECKING, Union
+
+@dataclass
+class RoleAssignmentResponse_value(AdditionalDataHolder, Parsable):
+    # Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
+    additional_data: dict[str, Any] = field(default_factory=dict)
+
+    # Role assignment Id
+    id: Optional[str] = None
+    # The Id of the assigned principal
+    principal_id: Optional[str] = None
+    # The Id of the role definition
+    role_definition_id: Optional[str] = None
+    # The assignment scope
+    scope: Optional[str] = None
+    
+    @staticmethod
+    def create_from_discriminator_value(parse_node: ParseNode) -> RoleAssignmentResponse_value:
+        """
+        Creates a new instance of the appropriate class based on discriminator value
+        param parse_node: The parse node to use to read the discriminator value and create the object
+        Returns: RoleAssignmentResponse_value
+        """
+        if parse_node is None:
+            raise TypeError("parse_node cannot be null.")
+        return RoleAssignmentResponse_value()
+    
+    def get_field_deserializers(self,) -> dict[str, Callable[[ParseNode], None]]:
+        """
+        The deserialization information for the current model
+        Returns: dict[str, Callable[[ParseNode], None]]
+        """
+        fields: dict[str, Callable[[Any], None]] = {
+            "id": lambda n : setattr(self, 'id', n.get_str_value()),
+            "principalId": lambda n : setattr(self, 'principal_id', n.get_str_value()),
+            "roleDefinitionId": lambda n : setattr(self, 'role_definition_id', n.get_str_value()),
+            "scope": lambda n : setattr(self, 'scope', n.get_str_value()),
+        }
+        return fields
+    
+    def serialize(self,writer: SerializationWriter) -> None:
+        """
+        Serializes information the current object
+        param writer: Serialization writer to use to serialize this model
+        Returns: None
+        """
+        if writer is None:
+            raise TypeError("writer cannot be null.")
+        writer.write_str_value("id", self.id)
+        writer.write_str_value("principalId", self.principal_id)
+        writer.write_str_value("roleDefinitionId", self.role_definition_id)
+        writer.write_str_value("scope", self.scope)
+        writer.write_additional_data_value(self.additional_data)
+    
+
