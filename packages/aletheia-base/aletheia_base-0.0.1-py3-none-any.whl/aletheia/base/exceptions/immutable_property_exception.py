b4cb092fdd8da54e44d67cfccb26e2000d913fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:32 +0000

    Add package aletheia-base/0.0.1/aletheia_base-0.0.1-py3-none-any.whl

---
+++ packages/aletheia-base/aletheia_base-0.0.1-py3-none-any.whl/aletheia/base/exceptions/immutable_property_exception.py
@@ -0,0 +1,5 @@
+from .base import AletheiaException
+
+
+class ImmutablePropertyException(AletheiaException):
+    pass
---
+++ packages/aletheia-base/aletheia_base-0.0.1-py3-none-any.whl/aletheia/base/exceptions/property_not_found_exception.py
@@ -0,0 +1,5 @@
+from .base import AletheiaException
+
+
+class PropertyNotFoundException(AletheiaException):
+    pass
