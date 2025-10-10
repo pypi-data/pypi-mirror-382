e181a9b3adc9fae7f4e39431b7243050f94496b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:05:57 +0000

    Add package abstractcore/2.2.1/abstractcore-2.2.1-py3-none-any.whl

---
+++ packages/abstractcore/abstractcore-2.2.1-py3-none-any.whl/abstractllm/processing/__init__.py
@@ -0,0 +1,14 @@
+"""
+AbstractCore Processing Module
+
+Basic text processing capabilities built on top of AbstractCore,
+demonstrating how to leverage the core infrastructure for real-world tasks.
+"""
+
+from .basic_summarizer import BasicSummarizer, SummaryStyle, SummaryLength
+from .basic_extractor import BasicExtractor
+
+__all__ = [
+    'BasicSummarizer', 'SummaryStyle', 'SummaryLength',
+    'BasicExtractor'
+]
