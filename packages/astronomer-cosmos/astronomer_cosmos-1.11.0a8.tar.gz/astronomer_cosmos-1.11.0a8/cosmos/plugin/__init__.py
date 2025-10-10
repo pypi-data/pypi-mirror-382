3b7c7f0ed09e66a88ec165740b05dee955b4340f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:06:13 +0000

    Add package astronomer-cosmos/1.11.0a8/astronomer_cosmos-1.11.0a8.tar.gz

---
+++ packages/astronomer-cosmos/astronomer_cosmos-1.11.0a8.tar.gz/astronomer_cosmos-1.11.0a8/cosmos/plugin/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from airflow import __version__ as airflow_version
+from packaging import version
+
+from cosmos.constants import _AIRFLOW3_MAJOR_VERSION
+
+if TYPE_CHECKING:  # pragma: no cover
+    from .af3_plugin_impl import CosmosAF3Plugin as _CosmosAF3PluginType
+    from .plugin_impl import CosmosPlugin as _CosmosPluginType
+
+CosmosPlugin: _CosmosPluginType | _CosmosAF3PluginType | None = None
+
+# Airflow 2.x (FAB/Flask) plugin
+if version.parse(airflow_version).major < _AIRFLOW3_MAJOR_VERSION:
+    from .plugin_impl import CosmosPlugin as CosmosPlugin  # type: ignore[assignment]
+else:
+    # Airflow 3.x (FastAPI) plugin
+    from .af3_plugin_impl import CosmosAF3Plugin as CosmosPlugin  # type: ignore[assignment]
