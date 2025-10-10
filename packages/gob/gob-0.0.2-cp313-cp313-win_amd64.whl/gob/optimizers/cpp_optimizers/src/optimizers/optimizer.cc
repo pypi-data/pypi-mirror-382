76c8217d4517a04dbe6e826913c4b3bad2afa53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 06:06:21 +0000

    Add package gob/0.0.2/gob-0.0.2-cp313-cp313-win_amd64.whl

---
+++ packages/gob/gob-0.0.2-cp313-cp313-win_amd64.whl/gob/optimizers/cpp_optimizers/src/optimizers/optimizer.cc
@@ -0,0 +1,20 @@
+/*
+ * Created in 2024 by Gaëtan Serré
+ */
+
+#include "optimizers/optimizer.hh"
+
+result Optimizer::py_minimize(PyObject *f)
+{
+  auto f_cpp = [f](dyn_vector x) -> double
+  {
+    PyObject *my_list = (PyObject *)vector_to_nparray(x);
+    PyObject *args = PyTuple_New(1);
+    PyTuple_SetItem(args, 0, my_list);
+    PyObject *result = PyObject_CallObject(f, args);
+    return PyFloat_AsDouble(result);
+  };
+  result_eigen res = this->minimize(f_cpp);
+  vector<double> x(res.first.data(), res.first.data() + res.first.size());
+  return {x, res.second};
+}
