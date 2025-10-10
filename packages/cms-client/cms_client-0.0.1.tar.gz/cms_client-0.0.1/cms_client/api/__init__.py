0dfcdc31d78a930be2540737e990c63a832578ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:05:58 +0000

    Add package cms-client/0.0.1/cms_client-0.0.1.tar.gz

---
+++ packages/cms-client/cms_client-0.0.1.tar.gz/cms_client-0.0.1/cms_client/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from cms_client.api.annotations_api import AnnotationsApi
+from cms_client.api.authentication_api import AuthenticationApi
+from cms_client.api.evaluating_api import EvaluatingApi
+from cms_client.api.generate_text_based_on_the_input_prompt_api import GenerateTextBasedOnTheInputPromptApi
+from cms_client.api.metadata_api import MetadataApi
+from cms_client.api.open_ai_compatible_api import OpenAICompatibleApi
+from cms_client.api.redaction_api import RedactionApi
+from cms_client.api.rendering_api import RenderingApi
+from cms_client.api.training_api import TrainingApi
+
