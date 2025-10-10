cf9a7c859d3c1318e3322c10f74e7fdd15b779e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 06:07:49 +0000

    Add package arraykit/1.2.0/arraykit-1.2.0-cp310-cp310-win_amd64.whl

---
+++ packages/arraykit/arraykit-1.2.0-cp310-cp310-win_amd64.whl/arraykit/__init__.py
@@ -0,0 +1,40 @@
+# pylint: disable=W0611
+# pylint: disable=E0401
+# pylint: disable=C0414
+
+from ._arraykit import __version__
+from ._arraykit import TriMap as TriMap
+from ._arraykit import ArrayGO as ArrayGO
+from ._arraykit import BlockIndex as BlockIndex
+from ._arraykit import ErrorInitTypeBlocks as ErrorInitTypeBlocks
+
+from ._arraykit import immutable_filter as immutable_filter
+from ._arraykit import mloc as mloc
+from ._arraykit import name_filter as name_filter
+from ._arraykit import shape_filter as shape_filter
+from ._arraykit import column_2d_filter as column_2d_filter
+from ._arraykit import column_1d_filter as column_1d_filter
+from ._arraykit import row_1d_filter as row_1d_filter
+from ._arraykit import array_deepcopy as array_deepcopy
+from ._arraykit import resolve_dtype as resolve_dtype
+from ._arraykit import resolve_dtype_iter as resolve_dtype_iter
+from ._arraykit import isna_element as isna_element
+from ._arraykit import dtype_from_element as dtype_from_element
+from ._arraykit import delimited_to_arrays as delimited_to_arrays
+from ._arraykit import iterable_str_to_array_1d as iterable_str_to_array_1d
+from ._arraykit import split_after_count as split_after_count
+from ._arraykit import get_new_indexers_and_screen as get_new_indexers_and_screen
+from ._arraykit import count_iteration as count_iteration
+from ._arraykit import first_true_1d as first_true_1d
+from ._arraykit import first_true_2d as first_true_2d
+from ._arraykit import slice_to_ascending_slice as slice_to_ascending_slice
+from ._arraykit import array_to_tuple_array as array_to_tuple_array
+from ._arraykit import array_to_tuple_iter as array_to_tuple_iter
+from ._arraykit import nonzero_1d as nonzero_1d
+from ._arraykit import is_objectable_dt64 as is_objectable_dt64
+from ._arraykit import is_objectable as is_objectable
+from ._arraykit import astype_array as astype_array
+from ._arraykit import AutoMap as AutoMap
+from ._arraykit import FrozenAutoMap as FrozenAutoMap
+from ._arraykit import NonUniqueError as NonUniqueError
+
