6727446af23a94bc4649e11dd509dfe53c1f1a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 03:17:57 +0000

    Add package randomdice/0.2.1/randomdice-0.2.1.tar.gz

---
+++ packages/randomdice/randomdice-0.2.1.tar.gz/randomdice-0.2.1/randomdice/__init__.py
@@ -0,0 +1,33 @@
+'''
+randomdice™ 
+----------------------------------------
+A Python package for generating random dice rolls.
+And a few other things.
+like a coin flip.
+or a turntable.
+and card and target!
+Our module has many functions. 
+So if you encounter any problems, please give me more advice,
+ and I will hurry to correct them.
+============================
+Author: aiwonderland
+Date: 2025-10-01
+============================
+
+'''
+
+from .die import Die
+from .coin import Coin
+from .spinwheel import SpinWheel
+from .card import Card
+from .target import Target
+from .File.txt import TxtFileHandler
+from .File.json_utils import JsonFileHandler
+
+
+
+
+__version__ = "0.2.1"
+__author__ = "aiwonderland"
+__license__ = "MIT"
+__all__ = ["Die", "Coin", "SpinWheel", "Card", "Target", "TxtFileHandler", "JsonFileHandler"]
