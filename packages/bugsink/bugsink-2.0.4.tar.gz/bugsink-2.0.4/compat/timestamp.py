e8cf47bd98592ff7f139f5b6b01873387be90131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 08:06:17 +0000

    Add package bugsink/2.0.4/bugsink-2.0.4.tar.gz

---
+++ packages/bugsink/bugsink-2.0.4.tar.gz/bugsink-2.0.4/compat/timestamp.py
@@ -0,0 +1,32 @@
+import datetime
+
+from django.utils.dateparse import parse_datetime
+from bugsink.utils import assert_
+
+
+def parse_timestamp(value):
+    """
+    > Indicates when the event was created in the Sentry SDK. The format is either a string as defined in RFC 3339 or a
+    > numeric (integer or float) value representing the number of seconds that have elapsed since the Unix epoch
+
+    > Timezone is assumed to be UTC if missing.
+
+    > Sub-microsecond precision is not preserved with numeric values due to precision limitations with floats (at least
+    > in our systems). With that caveat in mind, just send whatever is easiest to produce.
+
+    > All timestamps in the event protocol are formatted this way.
+    """
+
+    if isinstance(value, int) or isinstance(value, float):
+        return datetime.datetime.fromtimestamp(value, tz=datetime.timezone.utc)
+
+    return parse_datetime(value)
+
+
+def format_timestamp(value):
+    """the reverse of parse_timestamp"""
+
+    assert_(isinstance(value, datetime.datetime))
+    assert_(value.tzinfo == datetime.timezone.utc)
+
+    return value.isoformat()
