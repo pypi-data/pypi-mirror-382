8b8b35fb62402965f6336f1a3cf81655e6b03fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 07:05:53 +0000

    Add package cirquit/1.0.1/cirquit-1.0.1.tar.gz

---
+++ packages/cirquit/cirquit-1.0.1.tar.gz/cirquit-1.0.1/cirquit/__init__.py
@@ -0,0 +1,10 @@
+"""
+Cirquit - Quantum circuits, simplified.
+A developer-first quantum computing platform.
+"""
+
+__version__ = "1.0.1"
+
+from .cirquit import QuantumCircuit
+
+__all__ = ['QuantumCircuit', '__version__']
