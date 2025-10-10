ab555a67047de49d7636812cd5a55d3df76d7c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 03:18:03 +0000

    Add package pygixml/0.1.0/pygixml-0.1.0-cp310-cp310-musllinux_1_1_x86_64.whl

---
+++ packages/pygixml/pygixml-0.1.0-cp310-cp310-musllinux_1_1_x86_64.whl/pygixml/__init__.py
@@ -0,0 +1,22 @@
+"""
+pygixml - Python wrapper for pugixml using Cython
+
+A fast and efficient XML parser and manipulator for Python.
+"""
+
+from .pygixml import (
+    XMLDocument,
+    XMLNode,
+    XMLAttribute,
+    parse_string,
+    parse_file
+)
+
+__version__ = "0.1.0"
+__all__ = [
+    "XMLDocument",
+    "XMLNode", 
+    "XMLAttribute",
+    "parse_string",
+    "parse_file"
+]
