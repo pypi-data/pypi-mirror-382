a0d5ef0c6ce23fc115ee62f619b25bc89923a55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 03:17:57 +0000

    Add package meta-edc/1.2.6/meta_edc-1.2.6-py3-none-any.whl

---
+++ packages/meta-edc/meta_edc-1.2.6-py3-none-any.whl/meta_dashboard/urls.py
@@ -0,0 +1,40 @@
+from edc_protocol.research_protocol_config import ResearchProtocolConfig
+
+from .patterns import screening_identifier
+from .views import (
+    AeListboardView,
+    DeathReportListboardView,
+    ScreeningListboardView,
+    SubjectDashboardView,
+    SubjectListboardView,
+)
+
+app_name = "meta_dashboard"
+
+urlpatterns = SubjectListboardView.urls(
+    namespace=app_name,
+    label="subject_listboard",
+    identifier_pattern=ResearchProtocolConfig().subject_identifier_pattern,
+)
+urlpatterns += ScreeningListboardView.urls(
+    namespace=app_name,
+    label="screening_listboard",
+    identifier_label="screening_identifier",
+    identifier_pattern=screening_identifier,
+)
+urlpatterns += SubjectDashboardView.urls(
+    namespace=app_name,
+    label="subject_dashboard",
+    identifier_pattern=ResearchProtocolConfig().subject_identifier_pattern,
+)
+
+urlpatterns += AeListboardView.urls(
+    namespace=app_name,
+    label="ae_listboard",
+    identifier_pattern=ResearchProtocolConfig().subject_identifier_pattern,
+)
+urlpatterns += DeathReportListboardView.urls(
+    namespace=app_name,
+    label="death_report_listboard",
+    identifier_pattern=ResearchProtocolConfig().subject_identifier_pattern,
+)
