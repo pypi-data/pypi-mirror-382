c08eead8d9a9c1c22fd902483f76b9d3120dc900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 06:07:07 +0000

    Add package gnomelooks/0.3.0/gnomelooks-0.3.0-py3-none-any.whl

---
+++ packages/gnomelooks/gnomelooks-0.3.0-py3-none-any.whl/getlooks/core.py
@@ -0,0 +1,285 @@
+#! /usr/bin/python3
+
+import re
+import os
+from pathlib import Path
+import json
+import base64
+from datetime import datetime
+from urllib.parse import unquote
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+
+# pipi
+import requests
+from bs4 import BeautifulSoup
+
+# local
+from getlooks.utils import show_table, rprint, logger
+from getlooks.looks_path import DESK_THEME_PATH, STATE_PATH
+
+
+path_for = {
+    "GTK3/4 Themes": "themes",
+    "Full Icon Themes": "icons",
+    "Cursors": "cursors",
+}
+
+
+@dataclass
+class FileInfo:
+    """
+    Content related file info details
+    """
+
+    id: str
+    name: str
+    type: str
+    is_active: bool
+    url: str
+    size: str
+    downloads: int
+    changed_at: str
+    extracted_files: Optional[List[str]] = field(default_factory=list[str])
+
+    def __post_init__(self):
+        if isinstance(self.url, str):
+            self.url = unquote(self.url)
+
+    def date(self) -> datetime:
+        return datetime.strptime(self.changed_at, "%Y-%m-%d %H:%M:%S")
+
+
+@dataclass
+class ProductInfo:
+    """
+    Content related theme page details
+    """
+
+    type: str
+    project_id: str
+    author: str
+    description: str
+    title: str
+    likes: str
+    changed_at: str
+    system_tags: List[str]
+    files: List[FileInfo]
+    version: Optional[str] = ""
+    url: Optional[str] = field(default_factory=str)
+    install_path: Optional[str] = field(default_factory=str)
+
+    def date(self) -> datetime:
+        return datetime.strptime(f"{self.changed_at}", "%Y-%m-%d %H:%M:%S")
+
+    def __is_type(self, expect: str):
+        type = path_for.get(self.type)
+        if type != None and type == expect:
+            return True
+        else:
+            return False
+
+    def is_icon(self):
+        return self.__is_type("icons")
+
+    def is_theme(self):
+        return self.__is_type("themes")
+
+    def is_cursor(self):
+        return self.__is_type("cursors")
+
+    # def __post_init__(self):
+    #     files = []
+    #     for file in self.files:
+    #         if not isinstance(FileInfo, file):
+    #             _f = FileInfo(**file)
+    #             files.append(_f)
+    #         else:
+    #             files.append(file)
+    #     self.files = files
+
+
+class DeskEnv:
+    """
+    Check desktop env:
+    - gnome
+    - kde
+    - xfce4
+    and set theme path based on above env
+    """
+
+    USER = os.environ.get("USER")
+    SUDO_USER = os.environ.get("SUDO_USER")
+    HOME = (SUDO_USER and f"/home/{SUDO_USER}") or os.environ.get("HOME")
+    DESK_ENV = ""
+    state_file = Path(f"{HOME}/{STATE_PATH}-{USER}/deskenv")
+    valid_env = {
+        "1": "gnome",
+        "2": "kde",
+        "3": "xfce",
+    }
+
+    def __init__(self):
+        if os.path.isfile(self.state_file):
+            with open(self.state_file, "r") as s:
+                state = s.read()
+                self.DESK_ENV = state.strip()
+
+    def env_prompt(self):
+        if not os.path.isdir(self.state_file.parent):
+            os.makedirs(self.state_file.parent)
+
+        logger.error("Can't figureout desktop environment")
+        rprint(
+            "[yellow]Which one is your desktop environment ?\n"
+            "\nChoose you desktop environment id,[reset] \nExample: 1 for gnome\n"
+        )
+        show_table([self.valid_env], title="Select desktop environment")
+
+        print("\nEnter id for your desktop environment to continue: ", end="")
+        ans = int(input())
+        if ans <= len(self.valid_env):
+            self.DESK_ENV = self.valid_env[str(ans)]
+            with open(self.state_file, "w") as denv:
+                denv.write(f"{self.DESK_ENV}")
+        else:
+            exit(1)
+
+    def check_env(self):
+        """Updates self.DESK_ENV according to your desk env"""
+
+        if len(self.DESK_ENV) != 0:
+            return
+
+        # read /proc/sched_debug
+        XDG_DESK = os.environ.get("XDG_CURRENT_DESKTOP") or os.environ.get(
+            "ORIGINAL_XDG_CURRENT_DESKTOP"
+        )
+        if XDG_DESK:
+            # print(XDG_DESK)
+            ps = f"{XDG_DESK}".lower()
+
+            if re.search("gnome", ps):
+                self.DESK_ENV = self.valid_env["1"]
+            elif re.search("plasma|kde", ps):
+                self.DESK_ENV = self.valid_env["2"]
+            elif re.search("xfce", ps):
+                self.DESK_ENV = self.valid_env["3"]
+            else:
+                self.env_prompt()
+        else:
+            self.env_prompt()
+
+    def theme_path(self, theme_type):
+        """
+        set the path for themes
+        `theme_type` -> `themes` or `icons` or `cursor`
+        """
+
+        USER = self.USER
+        _d = DESK_THEME_PATH[self.DESK_ENV]
+
+        # arg = (themes or icons or cursor)
+        if theme_type in _d.keys():
+            if USER == "root":
+                path = _d[theme_type]["global"]
+            else:
+                path = _d[theme_type]["local"]
+
+        elif theme_type == "kde_global" and self.DESK_ENV == "kde":
+            if USER == "root":
+                path = _d["global_theme"]["global"]
+            else:
+                path = _d["global_theme"]["local"]
+        else:
+            raise ValueError(f"got unexpected theme_type -> {theme_type} ")
+
+        return path.replace("~", self.HOME)
+
+
+def scrapGnomeLooks(url):
+    """"""
+
+    try:
+        gnome_looks_page = requests.get(url, timeout=30)
+    except requests.exceptions.ConnectTimeout:
+        raise Exception(f"Connection timeout {url}")
+
+    if gnome_looks_page.status_code == 404:
+        logger.error("Page responded 404 (File not found)")
+        exit(1)
+
+    soup = BeautifulSoup(gnome_looks_page.text, "html.parser")
+
+    js_scripts = soup.find_all("script", type="text/javascript")
+
+    # where a variable contain json data of theme-files
+
+    # product details
+    pattern = r"productViewDataEncoded = '(ey.+)';"
+    _p = None
+    # Search all <script> tags
+    for script in js_scripts:
+        text = script.string or ""
+        match = re.search(pattern, text)
+        if match:
+            _p = base64.b64decode(match[1]).decode("utf-8")
+            break
+
+    if _p == None:
+        raise Exception("No data found")
+
+    # todict
+    p_info = json.loads(_p)
+    f_info = json.loads(requests.get(url + "/loadFiles").text)
+
+    # dataclasses
+    f_list: List[FileInfo] = []
+    count = 0
+
+    system_tags = [t["tag_name"] for t in p_info["systemTags"]]
+
+    for _f in f_info["files"]:
+        __t = {
+            "id": f"{count}",
+            # "g_id": _f["id"],
+            "is_active": bool(int(_f["active"])),
+            "name": _f["name"],
+            "type": _f["type"],
+            "url": _f["url"],
+            "changed_at": _f["updated_timestamp"],
+            "downloads": _f["downloaded_count_uk"],
+            "size": f"{int(_f['size']) / 1000000}"[:4] + " Mb",
+        }
+        if __t["is_active"]:
+            f_list.append(FileInfo(**__t))
+            count += 1
+
+    __p_info = {
+        "type": p_info["product"]["cat_title"],
+        "project_id": p_info["product"]["project_id"],
+        "author": p_info["product"]["username"],
+        "description": p_info["product"]["description"],
+        "title": p_info["product"]["title"],
+        "likes": p_info["tabCnt"]["cntLikes"],
+        "version": p_info["product"]["version"],
+        "changed_at": p_info["product"]["changed_at"],
+        "system_tags": system_tags,
+        "files": f_list,
+        "url": url,
+    }
+
+    # theme file
+
+    # collect active files and ignore archived ones.
+    return ProductInfo(**__p_info)
+
+
+def product_state_cache(data: Dict[str, dict]) -> Dict[str, ProductInfo]:
+    products: Dict[str, ProductInfo] = dict()
+    for p in data:
+        product = ProductInfo(**data[p])
+        product.files = [FileInfo(**f) for f in data[p]["files"]]
+        products[product.project_id] = product
+
+    return products
---
+++ packages/gnomelooks/gnomelooks-0.3.0-py3-none-any.whl/getlooks/utils.py
@@ -0,0 +1,293 @@
+#! /usr/bin/python3
+
+import os
+import json
+import random
+import shutil
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import List, Dict
+import logging
+
+# pipi
+import requests
+from colorama import Fore
+from rich.console import Console
+from rich.text import Text
+from rich.table import Table
+from rich.logging import RichHandler
+from rich import print as rprint
+
+# locals
+console = Console()
+
+# -- code --
+this_file_path = Path(os.path.abspath(__file__)).resolve()
+
+
+def _logger(debug_flag: str = ""):
+    # message
+    flag = os.environ.get(debug_flag)
+    logger = logging.getLogger(__name__)
+
+    if flag == None:
+        logger.setLevel(logging.INFO)
+    elif flag == "debug":
+        logger.setLevel(logging.DEBUG)
+    elif flag == "warning":
+        logger.setLevel(logging.WARNING)
+    elif flag == "info":
+        logger.setLevel(logging.INFO)
+
+    handler = RichHandler(log_time_format="")
+    logger.addHandler(handler)
+    return logger
+
+
+logger = _logger(debug_flag="TEST_THEME_INSTALLER")
+
+_colors = {
+    "green": "#8CC265",
+    "light_green": "#D0FF5E bold",
+    "blue": "#4AA5F0",
+    "cyan": "#76F6FF",
+    "yellow": "#F0A45D bold",
+    "red": "#E8678A",
+    "purple": "#8782E9 bold",
+}
+
+
+class JsonState:
+    cache: Dict[str, Dict] = dict()
+    cache_path: str = ""
+
+    def add(self, key: str, data: dict, dublicate: bool = False):
+        if not dublicate:
+            if key not in self.cache.keys():
+                self.cache[key] = data
+        else:
+            self.cache[key] = data
+
+    def put(self, key: str, data: dict) -> bool:
+        # , where:dict={}
+
+        if len(self.cache) == 0:
+            self.add(key, data)
+            return True
+
+        if key != None:
+            keys = self.cache[key].keys()
+
+        if not keys == data.keys():
+            print("old keys: ", keys)
+            print("new keys: ", data.keys())
+            raise ValueError("previos key datakeys not match")
+
+        self.cache[key] = data
+        return True
+
+    def load(self, cache_path: str):
+        if not cache_path.split(".")[-1] == "json":
+            raise ValueError("expected json file")
+
+        elif not os.path.exists(cache_path):
+            self.cache_path = cache_path
+            return
+
+        if not os.path.getsize(cache_path) == 0:
+            with open(cache_path, "r") as c:
+                data = json.loads(c.read())
+                self.cache = data
+
+        self.cache_path = cache_path
+
+    def reload(self):
+        self.load(self.cache_path)
+
+    def save(self):
+        parent_dir = os.path.dirname(self.cache_path)
+
+        if not os.path.exists(parent_dir):
+            os.makedirs(parent_dir)
+
+        with open(self.cache_path, "w") as c:
+            data = json.dumps(self.cache, indent=4)
+            c.write(data)
+            print(f"Updated state: {self.cache_path}")
+            return True
+
+
+class Message:
+    """Print message of type and color"""
+
+    colors = [
+        Fore.WHITE,
+        # blue
+        Fore.BLUE,
+        Fore.LIGHTBLUE_EX,
+        # green
+        Fore.GREEN,
+        Fore.LIGHTGREEN_EX,
+        # cyan
+        Fore.CYAN,
+        Fore.LIGHTCYAN_EX,
+        # red
+        # Fore.RED,
+        Fore.LIGHTRED_EX,
+        # magenta
+        Fore.MAGENTA,
+        Fore.LIGHTMAGENTA_EX,
+        # yellow
+        Fore.YELLOW,
+        Fore.LIGHTYELLOW_EX,
+    ]
+
+    def __init__(self):
+        pass
+
+    def sucesses(self, message: str):
+        print(Fore.LIGHTGREEN_EX + message + Fore.RESET)
+
+    def random_color(self, message: str, **kwargs):
+        r_color = random.choice(self.colors)
+        print(r_color + message + Fore.RESET, **kwargs)
+        return r_color
+
+
+message = Message()
+
+
+def ls(
+    path: str,
+    only_dir: bool = False,
+    include_path: bool = False,
+    ignore_items: list = [],
+) -> list:
+    """List files and directories of given path"""
+
+    if not os.path.exists(path):
+        raise Exception(f"Path: {path} not found")
+    if only_dir:
+        if include_path:
+            p_ls = [
+                path + f"/{file}"
+                for file in os.listdir(path)
+                if os.path.isdir(path + f"/{file}")
+            ]
+        else:
+            p_ls = [
+                file for file in os.listdir(path) if os.path.isdir(path + f"/{file}")
+            ]
+    else:
+        if include_path:
+            p_ls = [path + f"/{file}" for file in os.listdir(path)]
+        else:
+            p_ls = os.listdir(path)
+
+    for item in ignore_items:
+        if item in p_ls:
+            p_ls.remove(item)
+
+    p_ls.sort()
+    return p_ls
+
+
+def extract(path: str, at: str):
+    """Extract tar file"""
+    try:
+        shutil.unpack_archive(path, at)
+        logger.debug(f"""Extracted {path}, at {at}""")
+        return True
+    except:
+        logger.error(f"can't extract: {path}")
+        raise Exception("Invalid file to extract")
+
+
+def download(url: str, at: str):
+    """"""
+
+    file = requests.get(url, stream=True)
+
+    if not os.path.exists(at):
+        os.makedirs(at)
+
+    file_name: str = url.split("/")[-1]
+    if file.status_code == 200:
+        with open(f"{at}/{file_name}", "wb") as fw:
+            fw.write(file.content)
+        logger.debug(f"""Downloaded: {file_name} at {at}""")
+        return f"{at}/{file_name}"
+    else:
+        logger.error(f"request status_code: {file.status_code}")
+        exit(1)
+
+
+def dn_n_extract(url: str, at: str) -> list:
+    """Download `url` and extract `at` path."""
+
+    temp = TemporaryDirectory(prefix="looks_ex_", suffix="")
+    temp_dn = TemporaryDirectory(prefix="looks_dn_", suffix="")
+
+    # Download
+    logger.info("Downloading... \n")
+    dp = download(url, at=temp_dn.name)
+
+    # extract at tmp, and list folders
+    extract(dp, temp.name)
+    extracted_items: list = ls(temp.name, only_dir=True)
+
+    for f in extracted_items:
+        at_f = os.path.join(at, f)
+        if os.path.exists(at_f):
+            shutil.rmtree(at_f)
+        shutil.move(os.path.join(temp.name, f), at_f)
+        rprint(f"[green]Moved to:[reset] {at_f}")
+
+    temp_dn.cleanup()
+    temp.cleanup()
+    return extracted_items
+
+
+def dict_list_tbl(items=list[dict], ignore_keys: list = []):
+    keys = []
+    data = []
+
+    for item in items:
+        _tmp: tuple = ()
+        for key in [i for i in item.keys() if i not in ignore_keys]:
+            if key not in keys:
+                keys.append(key)
+            _tmp += (str(item[key]),)
+        data.append(_tmp)
+
+    return keys, data
+
+
+def show_table(
+    data: List[Dict], ignore_keys: List = [], title: str = "", border_style=""
+):
+    """rich table"""
+
+    text = Text(title, style=_colors["light_green"])
+
+    print()
+
+    table = Table(title=text, style=_colors["purple"], border_style=border_style)
+    columns, rows = dict_list_tbl(data, ignore_keys)
+
+    colors = {
+        0: _colors["yellow"],
+        1: _colors["red"],
+        2: _colors["green"],
+        3: _colors["cyan"],
+        4: _colors["blue"],
+    }
+
+    for count, col in enumerate(columns):
+        color = colors[count % len(colors)]
+        table.add_column(col, justify="left", style=color, no_wrap=True)
+    for row in rows:
+        table.add_row(*row)
+
+    console = Console()
+    console.print(table)
