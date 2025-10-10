0baf16aa7ea40fb7669e9b7119daf31b431b508c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:06:06 +0000

    Add package markdown_javadoc_references/0.4.5/markdown_javadoc_references-0.4.5.tar.gz

---
+++ packages/markdown_javadoc_references/markdown_javadoc_references-0.4.5.tar.gz/markdown_javadoc_references-0.4.5/src/markdown_javadoc_references/processor/autolink.py
@@ -0,0 +1,62 @@
+import re
+from collections.abc import Callable
+from typing import cast
+
+from ..reference import raw_pattern as ref_pattern
+from markdown.inlinepatterns import InlineProcessor
+
+from ..entities import Klass, Field, Method, Entity, Type
+from ..resolver import Resolver
+from ..util import get_logger
+
+logger = get_logger(__name__)
+
+def _default_formatter(ref: Entity) -> str:
+    match ref:
+        case Klass():
+            return f"@{ref.name}" if ref.type == Type.ANN_INTERFACE else ref.name
+        case Field():
+            return f'{ref.klass.name}#{ref.name}'
+        case Method():
+            return f'{ref.klass.name}#{ref.name}({ref.parameter_names_joined()})'
+        case _:
+            raise ValueError("Should not occur")
+
+def _compile_formatter(code: str) -> Callable[[Entity], str]:
+    namespace = {
+        "Klass": Klass,
+        "Method": Method,
+        "Field": Field,
+        "Entity": Entity,
+        "Type": Type
+    }
+
+    indented = '\n'.join("  " + line for line in code.splitlines())
+
+    wrapper = f"def autolink_format(ref: Entity) -> str:  \n{indented}\n"
+
+    exec(wrapper, namespace)
+    return cast(Callable[[Entity], str], namespace["autolink_format"])
+
+auto_link_pattern: str = rf'<(?!init>)({ref_pattern})>'
+
+class AutoLinkJavaDocProcessor(InlineProcessor):
+    def __init__(self, md, resolver: Resolver, autolink_format: str):
+        super().__init__(auto_link_pattern, md)
+        self.resolver = resolver
+        self.formatter = _compile_formatter(autolink_format) if autolink_format != '' else _default_formatter
+
+    def handleMatch(self, m: re.Match, data: str):
+        logger.debug(f"Handle auto link match: {m.group(0)}")
+
+        ref, el = self.resolver.resolve(m.group('whole_ref'), m.group(1))
+        if ref is not None:
+            try:
+                formatted = self.formatter(ref)
+            except Exception as e:
+                logger.error(f"Error while evaluating autolink ({el.get('href')}): {e}")
+                formatted = el.get('href')
+
+            el.text = formatted
+
+        return el, m.start(0), m.end(0)
