c484d9eda96a25274b95ec71dbfa288498358285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:40 +0000

    Add package h3sed/3.2/h3sed-3.2-py2.py3-none-any.whl

---
+++ packages/h3sed/h3sed-3.2-py2.py3-none-any.whl/h3sed/version/__init__.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Interface to game version differences.
+
+------------------------------------------------------------------------------
+This file is part of h3sed - Heroes3 Savegame Editor.
+Released under the MIT License.
+
+@created   22.03.2020
+@modified  23.09.2025
+------------------------------------------------------------------------------
+"""
+import collections
+
+from . import ab
+from . import hc
+from . import hota
+from . import roe
+from . import sod
+
+
+## Modules for game versions in order of release
+VERSIONS = collections.OrderedDict([
+    ("roe",     roe),     # Restoration of Erathia
+    ("ab",      ab),      # Armageddon's Blade
+    ("sod",     sod),     # Shadow of Death
+    ("hc",      hc),      # Heroes Chronicles
+    ("hota",    hota),    # Horn of The Abyss
+])
+
+ADAPT_CACHE = {} # {(name, value, version): value}
+
+
+def adapt(name, value, version=None):
+    """
+    Returns value adapted either for specified game version, or run through all versions.
+
+    @param   name     value name like "hero.regex"
+    @param   value    the value to adapt
+    @param   version  specific game version to adapt for, if any, like "sod" for Shadow of Death
+    """
+    cachekey = (name, value, version)
+    try:
+        if cachekey in ADAPT_CACHE: return ADAPT_CACHE[cachekey]
+    except Exception: pass # TypeError if value in cachekey is not hashable
+
+    if version:
+        if hasattr(VERSIONS[version], "adapt"):
+            value = VERSIONS[version].adapt(name, value)
+    else:
+        for module in VERSIONS.values():
+            if hasattr(module, "adapt"):
+                value = module.adapt(name, value)
+    try: ADAPT_CACHE[cachekey] = value
+    except Exception: pass # TypeError if value in cachekey is not hashable
+    return value
+
+
+def detect(savefile):
+    """Returns savefile game version, like "sod" for Shadow of Death. Raises if unknown."""
+    for version, module in VERSIONS.items():
+        if module.detect(savefile):
+            return version
+    raise ValueError("Not recognized as Heroes3 savefile of any supported game version.")
+
+
+def init():
+    """Initializes version data."""
+    for module in VERSIONS.values():
+        if hasattr(module, "init"): module.init()
+
+
+def title(version):
+    """Returns version title, like "Shadow of Death" for "sod"."""
+    return VERSIONS[version].TITLE
---
+++ packages/h3sed/h3sed-3.2-py2.py3-none-any.whl/h3sed/version/hc.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Subplugin for HOMM3 version "Heroes Chronicles".
+
+------------------------------------------------------------------------------
+This file is part of h3sed - Heroes3 Savegame Editor.
+Released under the MIT License.
+
+@created   13.09.2024
+@modified  05.10.2025
+------------------------------------------------------------------------------
+"""
+import re
+
+from .. import hero
+from .. import metadata
+from .. hero import make_artifact_cast
+
+NAME  = "hc"
+TITLE = "Heroes Chronicles"
+
+SAVEFILE_MAGIC = b"HCHRONSVG"
+
+
+class DataClass(hero.DataClass):
+
+    version = property(lambda self: NAME, doc="Game version in use")
+
+
+class Equipment(DataClass, hero.Equipment):
+    __slots__ = {k: make_artifact_cast(k, version=NAME) for k in hero.Equipment.__slots__}
+
+
+class ArmyStack(DataClass, hero.ArmyStack):   pass
+
+class Army(DataClass, hero.Army):             pass
+
+class Attributes(DataClass, hero.Attributes): pass
+
+class Inventory(DataClass, hero.Inventory):   pass
+
+class Skill(DataClass, hero.Skill):           pass
+
+class Skills(DataClass, hero.Skills):         pass
+
+class Spells(DataClass, hero.Spells):         pass
+
+
+
+def adapt(name, value):
+    """
+    Adapts certain categories:
+
+    - "savefile_magic_regex":       adds support for Chronicles savefiles
+    - "savefile_header_regex":      adds support for Chronicles savefiles
+    - "hero.PropertyName" classes:  returning version-specific data class
+    """
+    result = value
+    if "hero.ArmyStack" == name:
+        result = ArmyStack
+    elif "hero.Army" == name:
+        result = Army
+    elif "hero.Attributes" == name:
+        result = Attributes
+    elif "hero.Equipment" == name:
+        result = Equipment
+    elif "hero.Inventory" == name:
+        result = Inventory
+    elif "hero.Skill" == name:
+        result = Skill
+    elif "hero.Skills" == name:
+        result = Skills
+    elif "hero.Spells" == name:
+        result = Spells
+    elif "savefile_magic_regex" == name:
+        if hasattr(value, "pattern") and SAVEFILE_MAGIC not in value.pattern:
+            result = re.compile(value.pattern + b"|^%s" % SAVEFILE_MAGIC)
+    elif "savefile_header_regex" == name:
+        if hasattr(value, "pattern") and SAVEFILE_MAGIC not in value.pattern:
+            DEFAULT_MAGIC = metadata.Savefile.RGX_MAGIC.pattern.replace(b"^", b"")
+            if DEFAULT_MAGIC in value.pattern:
+                repl = b"(%s|%s)" % (DEFAULT_MAGIC, SAVEFILE_MAGIC)
+                pattern = value.pattern.replace(DEFAULT_MAGIC, repl, 1)
+                result = re.compile(pattern, value.flags)
+    return result
+
+
+def detect(savefile):
+    """Returns whether savefile bytes match Heroes Chronicles."""
+    return savefile.raw.startswith(SAVEFILE_MAGIC)
