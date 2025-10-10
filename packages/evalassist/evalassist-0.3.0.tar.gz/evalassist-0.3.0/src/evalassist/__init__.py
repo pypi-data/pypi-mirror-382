bbbbb332f52fb1bd10a9c689f16a045ee35506f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 06:07:27 +0000

    Add package evalassist/0.3.0/evalassist-0.3.0.tar.gz

---
+++ packages/evalassist/evalassist-0.3.0.tar.gz/evalassist-0.3.0/src/evalassist/__init__.py
@@ -0,0 +1,33 @@
+import logging
+
+import litellm
+
+litellm.drop_params = True
+
+# print(
+#     r"""
+# .-----------------------------------------------------------------------------.
+# |███████╗██╗   ██╗ █████╗ ██╗      █████╗ ███████╗███████╗██╗███████╗████████╗|
+# |██╔════╝██║   ██║██╔══██╗██║     ██╔══██╗██╔════╝██╔════╝██║██╔════╝╚══██╔══╝|
+# |█████╗  ██║   ██║███████║██║     ███████║███████╗███████╗██║███████╗   ██║   |
+# |██╔══╝  ╚██╗ ██╔╝██╔══██║██║     ██╔══██║╚════██║╚════██║██║╚════██║   ██║   |
+# |███████╗ ╚████╔╝ ██║  ██║███████╗██║  ██║███████║███████║██║███████║   ██║   |
+# |╚══════╝  ╚═══╝  ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚══════╝   ╚═╝   |
+# '-----------------------------------------------------------------------------'
+# """.encode('utf-8')
+# )
+
+root_pkg_logger = logging.getLogger(__name__)
+root_pkg_logger.propagate = False
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s"
+)
+handler.setFormatter(formatter)
+
+root_pkg_logger.addHandler(handler)
+
+root_pkg_logger.setLevel(logging.DEBUG)
