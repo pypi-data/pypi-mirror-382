5b3e35a564ee8c5efae60d24c62e6e0d99a86531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 06:06:13 +0000

    Add package profanity-hinglish/0.1.4/profanity_hinglish-0.1.4.tar.gz

---
+++ packages/profanity-hinglish/profanity_hinglish-0.1.4.tar.gz/profanity_hinglish-0.1.4/tests/test.py
@@ -0,0 +1,7 @@
+from profanity_hinglish import contains_hinglish_profanity
+
+print(contains_hinglish_profanity("This is clean"))  
+# False
+
+print(contains_hinglish_profanity("आंड मत कहा "))  
+# True
