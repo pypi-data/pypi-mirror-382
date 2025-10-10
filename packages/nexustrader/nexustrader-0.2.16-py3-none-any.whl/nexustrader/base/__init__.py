efdd49d781936b95f4521546f155ec019d4be41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:44 +0000

    Add package nexustrader/0.2.16/nexustrader-0.2.16-py3-none-any.whl

---
+++ packages/nexustrader/nexustrader-0.2.16-py3-none-any.whl/nexustrader/base/__init__.py
@@ -0,0 +1,24 @@
+from nexustrader.base.exchange import ExchangeManager
+from nexustrader.base.ws_client import WSClient
+from nexustrader.base.api_client import ApiClient
+from nexustrader.base.oms import OrderManagementSystem
+from nexustrader.base.ems import ExecutionManagementSystem
+from nexustrader.base.connector import (
+    PublicConnector,
+    PrivateConnector,
+    MockLinearConnector,
+)
+from nexustrader.base.retry import RetryManager
+
+
+__all__ = [
+    "ExchangeManager",
+    "WSClient",
+    "ApiClient",
+    "OrderManagementSystem",
+    "ExecutionManagementSystem",
+    "PublicConnector",
+    "PrivateConnector",
+    "MockLinearConnector",
+    "RetryManager",
+]
---
+++ packages/nexustrader/nexustrader-0.2.16-py3-none-any.whl/nexustrader/exchange/binance/error.py
@@ -0,0 +1,14 @@
+class BinanceError(Exception):
+    """
+    The base class for all Binance specific errors.
+    """
+
+    def __init__(self, code: int, message: str):
+        super().__init__(message)
+        self.code = code
+        self.message = message
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(code={self.code}, message='{self.message}')"
+
+    __str__ = __repr__
