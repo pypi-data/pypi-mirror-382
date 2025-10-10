0ff7a0a5bdc77e13713afb14e58510dbb16b4643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 07:05:55 +0000

    Add package exonware-xwnode/0.0.1.17/exonware_xwnode-0.0.1.17.tar.gz

---
+++ packages/exonware-xwnode/exonware_xwnode-0.0.1.17.tar.gz/exonware_xwnode-0.0.1.17/src/xwnode_wrapper.py
@@ -0,0 +1,6 @@
+# xwnode/src/xwnode_wrapper.py
+"""Redirect package: Please install exonware-xwnode instead."""
+raise ImportError(
+    "This is a redirect package. "
+    "Please install: pip install exonware-xwnode"
+)
