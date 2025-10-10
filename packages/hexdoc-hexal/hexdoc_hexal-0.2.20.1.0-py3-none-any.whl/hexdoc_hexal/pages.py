fc2893d810711cb155177c29eb1742cab337f6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 06:07:11 +0000

    Add package hexdoc-hexal/0.2.20.1.0/hexdoc_hexal-0.2.20.1.0-py3-none-any.whl

---
+++ packages/hexdoc-hexal/hexdoc_hexal-0.2.20.1.0-py3-none-any.whl/hexdoc_hexal/pages.py
@@ -0,0 +1,13 @@
+from hexdoc.patchouli.page import Page
+
+
+class EverbookEntryPage(Page, type="hexcasting:everbook_entry"):
+    pass
+
+
+class EverbookEntryPage_0_2_x(
+    Page,
+    type="hexal:everbook_entry",
+    template_type="hexcasting:everbook_entry",
+):
+    pass
