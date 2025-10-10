9b1e2f7c39fcbd883db2b6a217a19a3c7c0a964a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:45 +0000

    Add package bifrost/18.0.2/bifrost-18.0.2.tar.gz

---
+++ packages/bifrost/bifrost-18.0.2.tar.gz/bifrost-18.0.2/scripts/env-setup.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -euo pipefail
+
+. $(dirname $0)/install-deps.sh
+# NOTE(pas-ha) the above exports some useful variables like
+# $PYTHON , $PIP and $VENV depending on venv install or not
+
+DEFAULT_PIP_ANSIBLE='>=8,<9'
+
+ANSIBLE_COLLECTIONS_REQ=${ANSIBLE_COLLECTIONS_REQ:-$(dirname $0)/../ansible-collections-requirements.yml}
+ANSIBLE_COLLECTION_SOURCE_PATH=
+if [[ -d "${WORKSPACE:-}/openstack/ansible-collections-openstack" ]]; then
+    ANSIBLE_COLLECTION_SOURCE_PATH="${WORKSPACE}/openstack/ansible-collections-openstack"
+fi
+ANSIBLE_PIP_VERSION=${ANSIBLE_PIP_VERSION:-${DEFAULT_PIP_ANSIBLE}}
+ANSIBLE_SOURCE_PATH=${ANSIBLE_SOURCE_PATH:-ansible${ANSIBLE_PIP_VERSION}}
+
+BIFROST_COLLECTIONS_PATHS=${ANSIBLE_COLLECTIONS_PATHS:-}
+PLAYBOOKS_LIBRARY_PATH=$(dirname $0)/../playbooks/library
+
+echo "Installing/upgrading Ansible"
+ANSIBLE=${VENV}/bin/ansible
+if [ -f "$ANSIBLE" ]; then
+  ${PIP} uninstall -y ansible
+  ${PIP} uninstall -y ansible-base
+  ${PIP} uninstall -y ansible-core
+fi
+${PIP} install "${ANSIBLE_SOURCE_PATH}"
+
+ANSIBLE_GALAXY="${SUDO} ${VENV}/bin/ansible-galaxy"
+if [[ -z $BIFROST_COLLECTIONS_PATHS ]]; then
+    echo  "Setting ANSIBLE_COLLECTIONS_PATHS to virtualenv"
+    export ANSIBLE_COLLECTIONS_PATHS=${VENV}/collections
+    BIFROST_COLLECTIONS_PATHS=$ANSIBLE_COLLECTIONS_PATHS
+fi
+if [[ -n "$ANSIBLE_COLLECTION_SOURCE_PATH" ]]; then
+    ${SUDO} mkdir -p "$BIFROST_COLLECTIONS_PATHS/ansible_collections/openstack"
+    ${SUDO} ln -s "$ANSIBLE_COLLECTION_SOURCE_PATH" "$BIFROST_COLLECTIONS_PATHS/ansible_collections/openstack/cloud"
+fi
+
+# Install Collections
+if [[ -n "$ANSIBLE_COLLECTION_SOURCE_PATH" ]]; then
+    echo "Using openstack ansible collection from $ANSIBLE_COLLECTION_SOURCE_PATH"
+else
+    echo "Installing ansible collections on $BIFROST_COLLECTIONS_PATHS"
+    ${ANSIBLE_GALAXY} collection install -r ${ANSIBLE_COLLECTIONS_REQ} -p ${BIFROST_COLLECTIONS_PATHS}
+fi
+
+# Symlink Collections to the playbook directory. This removes the need of setting
+# ANSIBLE_COLLECTIONS_PATHS environment variable
+if [ ! -e "$(dirname $0)/../playbooks/collections" ]; then
+    echo "Creating a symbolic link to ansible collections in bifrost playbook directory"
+    ln -s ${ANSIBLE_COLLECTIONS_PATHS} "$(dirname $0)/../playbooks/collections"
+fi
+
+if [[ "${BIFROST_HIDE_PROMPT:-false}" != true ]]; then
+    echo
+    echo "To use bifrost, do"
+    echo "source ${VENV}/bin/activate"
+    echo "Then run playbooks as normal."
+    echo
+fi
