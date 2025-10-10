4c1e7d17d12ed6d07e87804844277d5727f82950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:06:02 +0000

    Add package guillotina-audit/2.0.10/guillotina_audit-2.0.10-py3-none-any.whl

---
+++ packages/guillotina-audit/guillotina_audit-2.0.10-py3-none-any.whl/guillotina_audit/__init__.py
@@ -0,0 +1,32 @@
+from guillotina import configure
+
+
+app_settings = {
+    "load_utilities": {
+        "audit": {
+            "provides": "guillotina_audit.interfaces.IAuditUtility",
+            "factory": "guillotina_audit.utility.AuditUtility",
+            "settings": {
+                "index_name": "audit",
+                "save_payload": False,
+                "index_permission_changes": False,
+            },
+        }
+    },
+    "audit_parser": "audit",
+    "commands": {
+        "audit-update-mappings": "guillotina_audit.commands.mappings.UpdateMappingsCommand",
+        "audit-update-settings": "guillotina_audit.commands.settings.UpdateSettingsCommand",
+        "audit-migrate": "guillotina_audit.commands.migrator.MigratorCommand",
+    },
+}
+
+
+def includeme(root, settings):
+    configure.scan("guillotina_audit.install")
+    configure.scan("guillotina_audit.utility")
+    configure.scan("guillotina_audit.subscriber")
+    configure.scan("guillotina_audit.api")
+    configure.scan("guillotina_audit.permissions")
+    configure.scan("guillotina_audit.models")
+    configure.scan("guillotina_audit.parser")
---
+++ packages/guillotina-audit/guillotina_audit-2.0.10-py3-none-any.whl/guillotina_audit/tests/fixtures.py
@@ -0,0 +1,60 @@
+from guillotina import testing
+from guillotina.component import query_utility
+from guillotina.tests.fixtures import _update_from_pytest_markers
+from guillotina_audit.interfaces import IAuditUtility
+
+import json
+import os
+import pytest
+
+
+ELASTICSEARCH = os.environ.get("ELASTICSEARCH", "True")
+
+annotations = {"elasticsearch": {"host": "localhost:9200"}}
+
+
+def base_settings_configurator(settings):
+    if "applications" not in settings:
+        settings["applications"] = []
+    settings["applications"].append("guillotina")
+    settings["applications"].append("guillotina_audit")
+    settings["applications"].append("guillotina_audit.tests.test_package")
+    settings["audit"] = {
+        "connection_settings": {
+            "hosts": [f"http://{annotations['elasticsearch']['host']}"]
+        }
+    }
+    settings["load_utilities"] = {
+        "audit": {
+            "provides": "guillotina_audit.interfaces.IAuditUtility",
+            "factory": "guillotina_audit.utility.AuditUtility",
+            "settings": {"index_name": "audit", "save_payload": True},
+        }
+    }
+
+
+testing.configure_with(base_settings_configurator)
+
+
+@pytest.fixture(scope="function")
+def elasticsearch_fixture(es):
+    settings = testing.get_settings()
+    host, port = es
+    settings = _update_from_pytest_markers(settings, None)
+    testing.configure_with(base_settings_configurator)
+    annotations["elasticsearch"]["host"] = f"{host}:{port}"
+    testing.configure_with(base_settings_configurator)
+    yield host, port
+
+
+@pytest.fixture(scope="function")
+async def guillotina_es(elasticsearch_fixture, guillotina):
+    audit_utility = query_utility(IAuditUtility)
+    await audit_utility.create_index()
+    response, status = await guillotina(
+        "POST", "/db/", data=json.dumps({"@type": "Container", "id": "guillotina"})
+    )
+    assert status == 200
+    yield guillotina
+    index = await audit_utility.get_current_index()
+    await audit_utility.async_es.indices.delete(index=index)
