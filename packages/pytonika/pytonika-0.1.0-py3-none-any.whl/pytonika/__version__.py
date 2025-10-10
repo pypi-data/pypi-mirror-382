84f9797be9eb3937722f32fcf12690cec62de90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:05:52 +0000

    Add package pytonika/0.1.0/pytonika-0.1.0-py3-none-any.whl

---
+++ packages/pytonika/pytonika-0.1.0-py3-none-any.whl/pytonika/__version__.py
@@ -0,0 +1,3 @@
+__title__ = "pytonika"
+__description__ = ""
+__version__ = "0.1.0"
---
+++ packages/pytonika/pytonika-0.1.0-py3-none-any.whl/pytonika/devices/routers/rut956.py
@@ -0,0 +1,15 @@
+from .router import Router
+
+
+class RUT956(Router):
+    def __init__(self, base_url: str) -> None:
+        super().__init__(base_url)
+
+        self._endpoints.extend([])
+
+    def __getattr__(self, attr: str):
+        for endpoint in self._endpoints:
+            if hasattr(endpoint, attr):
+                return getattr(endpoint, attr)
+
+        return super().__getattr__(attr)
