c83c9ddd3d6fe980e83e020061c7b213b495dd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 05:06:10 +0000

    Add package v440/2.0.0.dev194/v440-2.0.0.dev194-py3-none-any.whl

---
+++ packages/v440/v440-2.0.0.dev194-py3-none-any.whl/v440/_utils/Pattern.py
@@ -0,0 +1,31 @@
+import enum
+import functools
+import re
+from typing import *
+
+
+class Pattern(enum.StrEnum):
+
+    BASE = r"(?:[0-9]+!)?[0-9]+(?:\.[0-9]+)+"
+    PRE = r"[-_\.]?(?:alpha|a|beta|b|preview|pre|c|rc)(?:[-_\.]?[0-9]+)?"
+    POST = r"(?:-(?:[0-9]+))|(?:(?:[-_\.]?(?:post|rev|r))(?:[-_\.]?(?:[0-9]+))?)"
+    DEV = r"[-_\.]?dev(?:[-_\.]?[0-9]+)?"
+    QUAL = r"(?P<pre>%s)?(?P<post>%s)?(?P<dev>%s)?" % (PRE, POST, DEV)
+    PUBLIC = r"(v?([0-9]+!)?[0-9]+(\.[0-9]+)*)?"
+
+    @staticmethod
+    def compile(value: Any, /) -> re.Pattern:
+        "This staticmethod compiles the given value into a pattern."
+        return re.compile(value, re.VERBOSE)
+
+    @functools.cached_property
+    def bound(self: Self) -> re.Pattern:
+        return self.compile(r"^" + self.value + r"$")
+
+    @functools.cached_property
+    def leftbound(self: Self) -> re.Pattern:
+        return self.compile(r"^" + self.value)
+
+    @functools.cached_property
+    def unbound(self: Self) -> re.Pattern:
+        return self.compile(self.value)
