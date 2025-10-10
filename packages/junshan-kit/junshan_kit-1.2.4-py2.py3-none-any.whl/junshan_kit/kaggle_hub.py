20b8a6595a31d0463c0fa21cc253f22bb576b9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 04:06:23 +0000

    Add package junshan-kit/1.2.4/junshan_kit-1.2.4-py2.py3-none-any.whl

---
+++ packages/junshan-kit/junshan_kit-1.2.4-py2.py3-none-any.whl/junshan_kit/kaggle_hub.py
@@ -0,0 +1,38 @@
+import kagglehub
+import os
+import shutil
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+def list_user_datasets():
+    api = KaggleApi()
+    api.authenticate()
+
+    # Get the list of datasets for a specific user
+    datasets = api.dataset_list(user='junshan888')
+
+    print('*' * 60)
+    if datasets is not None:
+        for ds in datasets:
+            if ds is not None:
+                # Print the dataset title
+                print(ds.title)
+    print('*' * 60)
+    
+# example:  list_user_datasets()
+
+#---------------------------------------------------------------
+def read_data(data_name = 'evpd-test', copy_path = None):
+    path = kagglehub.dataset_download(f'junshan888/{data_name}')
+    # print("Downloaded to:", path)
+    if copy_path is not None:
+        # Create target directory if it doesn't exist
+        os.makedirs(copy_path, exist_ok=True)
+        # Copy dataset to target directory
+        shutil.copytree(path, copy_path, dirs_exist_ok=True)
+
+        print(f"✅ Dataset has been copied to: {copy_path}/{data_name}")
+
+# example: 
+read_data(copy_path='./exp_data')
+
+
