2977a5d4d0452ba29da4e5674ddf454740165f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 06:05:58 +0000

    Add package mchnpkg/0.1.4/mchnpkg-0.1.4.tar.gz

---
+++ packages/mchnpkg/mchnpkg-0.1.4.tar.gz/mchnpkg-0.1.4/src/mchnpkg/distributions/__init__.py
@@ -0,0 +1,6 @@
+from .cvdistributions import (
+    uniform, uniform_samples,
+    exponentialdist, exponential_samples,
+)
+__all__ = ["uniform", "uniform_samples", "exponentialdist", "exponential_samples"]
+
