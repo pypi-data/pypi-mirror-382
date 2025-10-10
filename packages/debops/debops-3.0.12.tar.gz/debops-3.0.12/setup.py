6e6d681003ec7752ed8ba0e2804e5b4de2e4bb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 07:06:20 +0000

    Add package debops/3.0.12/debops-3.0.12.tar.gz

---
+++ packages/debops/debops-3.0.12.tar.gz/debops-3.0.12/setup.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2014-2019 Hartmut Goebel <h.goebel@crazy-compilers.com>
+# Copyright (C) 2014-2020 Maciej Delmanowski <drybjed@gmail.com>
+# Copyright (C) 2016      Robin Schneider <ypid@riseup.net>
+# Copyright (C) 2014-2020 DebOps <https://debops.org/>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Installation in development mode:
+#   pipx install --editable .
+
+from setuptools import setup, find_packages
+import subprocess
+import glob
+import os
+import re
+
+
+def find_files(directory, strip):
+    """
+    Using glob patterns in ``package_data`` that matches a directory can
+    result in setuptools trying to install that directory as a file and
+    the installation to fail.
+
+    This function walks over the contents of *directory* and returns a list
+    of only filenames found. The filenames will be stripped of the *strip*
+    directory part to allow for location relative to the package.
+    """
+
+    result = []
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if not filename.endswith('.pyc'):
+                filename = os.path.join(root, filename)
+                result.append(os.path.relpath(filename, strip))
+    return result
+
+
+README = open('README.md').read()
+
+try:
+    unicode
+except NameError:
+    # Required for Python 3.x
+    class unicode(object):
+        def __new__(cls, s):
+            if isinstance(s, str):
+                return s
+            return s and s.decode('utf-8') or None
+
+# Retrieve the project version from 'git describe' command and store it in the
+# __version__.py and VERSION files, needed for correct installation of the
+# Python package
+try:
+    with open(os.devnull, 'w') as devnull:
+        GIT_RELEASE = subprocess.check_output(
+                      ['git', 'describe'], stderr=devnull
+                      ).strip().lstrip(b'v').decode('utf-8').split('-')
+        if len(GIT_RELEASE) > 1:
+            RELEASE = (GIT_RELEASE[0] + '.dev' + GIT_RELEASE[1]
+                       + '+' + GIT_RELEASE[2])
+        else:
+            RELEASE = GIT_RELEASE[0]
+except subprocess.CalledProcessError:
+    try:
+        RELEASE = open('VERSION').read().strip()
+    except Exception:
+        try:
+            with open('CHANGELOG.rst', 'r') as changelog:
+                for count, line in enumerate(changelog):
+                    if re.search('^`debops v', line):
+                        RELEASE = line.split()[1].rstrip('`_').lstrip('v')
+                        break
+        except Exception:
+            RELEASE = '0.0.0'
+
+with open('VERSION', 'w') as version_file:
+    version_file.write('{}\n'.format(RELEASE))
+with open('src/debops/__version__.py', 'w') as version_file:
+    version_file.write('__version__ = "{}"\n'
+                       .format(RELEASE))
+
+MANPAGES_1 = []
+MANPAGES_5 = []
+if os.path.exists('docs/_build/man'):
+    for manpage in os.listdir('docs/_build/man'):
+        if os.path.isfile(os.path.join('docs/_build/man', manpage)):
+            if manpage.endswith('.1'):
+                MANPAGES_1.append(os.path.join('docs/_build/man', manpage))
+            elif manpage.endswith('.5'):
+                MANPAGES_5.append(os.path.join('docs/_build/man', manpage))
+else:
+    print('Warning: manual pages not built')
+
+setup(
+    install_requires=['distro', 'future', 'jinja2', 'packaging', 'pyyaml',
+                      'pyxdg', 'toml', 'python-dotenv', 'gitpython'],
+    extras_require={
+        'ansible': ['ansible', 'netaddr', 'passlib',
+                    'python-ldap', 'dnspython', 'pyopenssl']
+    },
+    package_dir={'': 'src'},
+    packages=find_packages('src', exclude=('tests', 'docs')),
+    data_files=[
+        ('share/man/man1', MANPAGES_1),
+        ('share/man/man5', MANPAGES_5),
+    ],
+    package_data={
+        'debops':
+            find_files('src/debops/_data',
+                       'src/debops')
+    },
+
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'debops = debops.__main__:main'
+        ]
+    },
+
+    # metadata for upload to PyPI
+    name='debops',
+    version=unicode(RELEASE),
+    description='Your Debian-based data center in a box',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='DebOps Developers',
+    author_email='debops-devel@lists.debops.org',
+    url='https://debops.org/',
+    license="GPL-3.0-or-later",
+    license_files=glob.glob("LICENSES/*.txt"),
+    keywords="ansible debian sysadmin",
+    python_requires='<4',
+    download_url='https://github.com/debops/debops'
+                 '/archive/v' + unicode(RELEASE) + '.tar.gz',
+    classifiers=[
+                'Development Status :: 5 - Production/Stable',
+                'Environment :: Console',
+                'Intended Audience :: Information Technology',
+                'Intended Audience :: System Administrators',
+                'License :: OSI Approved :: GNU General Public License v3 '
+                'or later (GPLv3+)',
+                'Natural Language :: English',
+                'Operating System :: POSIX',
+                'Programming Language :: Other Scripting Engines',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 3.8',
+                'Programming Language :: Python :: 3.9',
+                'Programming Language :: Python :: 3.10',
+                'Programming Language :: Python :: 3.11',
+                'Programming Language :: Python :: 3.12',
+                'Programming Language :: Python :: 3.13',
+                'Topic :: System :: Installation/Setup',
+                'Topic :: System :: Systems Administration',
+                'Topic :: Utilities'
+    ]
+)
---
+++ packages/debops/debops-3.0.12.tar.gz/debops-3.0.12/src/debops/cli.py
@@ -0,0 +1,212 @@
+# Copyright (C) 2020-2023 Maciej Delmanowski <drybjed@gmail.com>
+# Copyright (C) 2020-2023 DebOps <https://debops.org/>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from .exceptions import NoDefaultViewException
+from .config import Configuration
+from .subcommands import Subcommands
+from .projectdir import ProjectDir
+from .ansiblerunner import AnsibleRunner
+from .ansibleplaybookrunner import AnsiblePlaybookRunner
+from .envrunner import EnvRunner
+import logging
+import getpass
+import sys
+import os
+
+logger = logging.getLogger(__name__)
+
+
+class Interpreter(object):
+
+    def __init__(self, args=None):
+        self.args = args
+        self.config = Configuration()
+        self.parsed_args = Subcommands(self.args)
+        logger.debug('Command line interpreter initialized')
+        logger.debug('Control user=' + getpass.getuser())
+
+        if self.parsed_args.section == 'project':
+            if self.parsed_args.command == 'init':
+                self.do_project_init(self.parsed_args.args)
+            elif self.parsed_args.command == 'refresh':
+                self.do_project_refresh(self.parsed_args.args)
+            elif self.parsed_args.command == 'lock':
+                self.do_project_lock(self.parsed_args.args)
+            elif self.parsed_args.command == 'unlock':
+                self.do_project_unlock(self.parsed_args.args)
+            elif self.parsed_args.command == 'mkview':
+                self.do_project_mkview(self.parsed_args.args)
+            elif self.parsed_args.command == 'commit':
+                self.do_project_commit(self.parsed_args.args)
+
+        elif self.parsed_args.section == 'exec':
+            self.do_exec(self.parsed_args.args)
+
+        elif self.parsed_args.section in ['run', 'check']:
+            self.do_run(self.parsed_args.args)
+
+        elif self.parsed_args.section == 'env':
+            self.do_env(self.parsed_args.args)
+
+        elif self.parsed_args.section == 'config':
+            if self.parsed_args.command == 'list':
+                self.do_config_list(self.parsed_args.args)
+            elif self.parsed_args.command == 'get':
+                self.do_config_get(self.parsed_args.args)
+
+    def do_project_init(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config,
+                                 create=True, **vars(args))
+            project.create()
+        except (IsADirectoryError, NotADirectoryError,
+                PermissionError, ValueError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+    def do_project_refresh(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config)
+            project.refresh()
+        except (IsADirectoryError, NotADirectoryError,
+                PermissionError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+    def do_project_lock(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config,
+                                 view=args.view)
+            try:
+                project.lock()
+            except (NoDefaultViewException) as errmsg:
+                print('Error:', errmsg)
+                sys.exit(1)
+        except (IsADirectoryError, NotADirectoryError,
+                PermissionError, ChildProcessError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+    def do_project_unlock(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config,
+                                 view=args.view)
+            try:
+                project.unlock()
+            except (NoDefaultViewException) as errmsg:
+                print('Error:', errmsg)
+                sys.exit(1)
+        except (IsADirectoryError, NotADirectoryError,
+                PermissionError, ChildProcessError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+    def do_project_mkview(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config,
+                                 **vars(args))
+            project.mkview(view=args.new_view)
+        except (IsADirectoryError, NotADirectoryError, ValueError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+    def do_project_commit(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config)
+            project.commit(interactive=True)
+        except (IsADirectoryError, NotADirectoryError,
+                PermissionError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+    def do_exec(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config,
+                                 view=args.view)
+        except (IsADirectoryError, NotADirectoryError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+        try:
+            runner = AnsibleRunner(project, **vars(args))
+        except (NoDefaultViewException, ValueError,
+                FileNotFoundError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+        if args.eval:
+            runner.eval()
+            sys.exit(0)
+        else:
+            sys.exit(runner.execute())
+
+    def do_run(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config,
+                                 view=args.view)
+        except (IsADirectoryError, NotADirectoryError, ChildProcessError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+        try:
+            runner = AnsiblePlaybookRunner(project, **vars(args))
+        except (NoDefaultViewException, ValueError,
+                FileNotFoundError, ChildProcessError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+        if args.eval:
+            runner.eval()
+            sys.exit(0)
+        else:
+            try:
+                sys.exit(runner.execute())
+            except ChildProcessError as errmsg:
+                print('Error:', errmsg)
+                sys.exit(1)
+
+    def do_env(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config,
+                                 view=args.view)
+        except (IsADirectoryError, NotADirectoryError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+        try:
+            runner = EnvRunner(project, **vars(args))
+        except (NoDefaultViewException, ValueError,
+                FileNotFoundError) as errmsg:
+            print('Error:', errmsg)
+            sys.exit(1)
+
+        if args.command_args:
+            sys.exit(runner.execute())
+        else:
+            sys.exit(runner.show_env(scope=args.scope))
+
+    def do_config_list(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config)
+        except (IsADirectoryError, NotADirectoryError) as errmsg:
+            # This is not a project directory, so no project-dependent
+            # configuration is included
+            pass
+
+        self.config.config_list()
+
+    def do_config_get(self, args):
+        try:
+            project = ProjectDir(path=args.project_dir, config=self.config)
+        except (IsADirectoryError, NotADirectoryError) as errmsg:
+            # This is not a project directory, so no project-dependent
+            # configuration is included
+            pass
+
+        if args.key:
+            for option_name in args.key:
+                self.config.config_get(option_name, format=args.format,
+                                       keys=args.keys)
+        else:
+            self.config.config_get('.', format=args.format, keys=args.keys)
